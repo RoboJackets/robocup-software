63956911430263b7c11142d1e4ed314925ac09cd
circuitben <circuitben@7ccbd594-6f30-0410-bcae-e071bb1c4cdc>
2008-09-29 21:38:56 +0000

    Separate C++ and Python generators, because they work differently anyway. Lots of improvements.
    git-svn-id: https://svn.robojackets.org/robocup/software/trunk@609 7ccbd594-6f30-0410-bcae-e071bb1c4cdc

--- SerDes/Serialization.hpp
+++ SerDes/Serialization.hpp
@@ -3,9 +3,24 @@
 
 #include <stdint.h>
 #include <vector>
+#include <string>
 
 namespace Serialization
 {
+    // The & operator is used for single member serialization.
+    //
+    // Arrays are serialized with one of four templated functions.
+    //
+    // Variable and fixed-size arrays are handled differently because
+    // variable-size arrays need to serialize their size first.
+    //
+    // Arrays of enums are handled differently from arrays of other types
+    // because enumerated types are always int in C++, but the XML may specify
+    // another integer type.  In this case one of the array*Cast functions
+    // is used, which will convert each element to the desired type.
+    // The array*Cast functions are not used for all arrays because that would
+    // require a lot of redundant typenames in the generated code.
+    
     class WriteBuffer
     {
     public:
@@ -19,6 +34,15 @@ namespace Serialization
         void operator&(int64_t value);
         void operator&(float value);
         void operator&(double value);
+        
+        void operator&(const std::string &value)
+        {
+            *this & value.size();
+            for (unsigned int i = 0; i  < value.size(); ++i)
+            {
+                *this & (int8_t)value[i];
+            }
+        }
         
         template<typename S, typename T>
         void arrayVariable(std::vector<T> &x)
@@ -40,6 +64,27 @@ namespace Serialization
                 *this & x[i];
             }
         }
+        
+        template<typename S, typename C, typename T>
+        void arrayVariableCast(std::vector<T> &x)
+        {
+            *this & (S)x.size();
+            // Always use unsigned int here so at least the loop
+            // works correctly if sizetype is too small.
+            for (unsigned int i = 0; i < x.size(); ++i)
+            {
+                *this & (C)x[i];
+            }
+        }
+        
+        template<typename C, typename T, unsigned int N>
+        void arrayFixedCast(T (&x)[N])
+        {
+            for (unsigned int i = 0; i < N; ++i)
+            {
+                *this & (C)x[i];
+            }
+        }
     };
     
     class ReadBuffer
@@ -55,6 +100,17 @@ namespace Serialization
         void operator&(int64_t value);
         void operator&(float value);
         void operator&(double value);
+        
+        void operator&(std::string &value)
+        {
+            unsigned int size;
+            *this & size;
+            value.resize(size);
+            for (unsigned int i = 0; i  < value.size(); ++i)
+            {
+                *this & (int8_t)value[i];
+            }
+        }
         
         template<typename S, typename T>
         void arrayVariable(std::vector<T> &x)
@@ -74,6 +130,27 @@ namespace Serialization
             for (unsigned int i = 0; i < N; ++i)
             {
                 *this & x[i];
+            }
+        }
+    
+        template<typename S, typename C, typename T>
+        void arrayVariableCast(std::vector<T> &x)
+        {
+            S size;
+            *this & size;
+            x.resize(size);
+            for (S i = 0; i < size; ++i)
+            {
+                *this & (C)x[i];
+            }
+        }
+        
+        template<typename T, typename C, unsigned int N>
+        void arrayFixedCast(T (&x)[N])
+        {
+            for (unsigned int i = 0; i < N; ++i)
+            {
+                *this & (C)x[i];
             }
         }
     };
--- SerDes/t.cpp
+++ SerDes/t.cpp
@@ -7,6 +7,11 @@ void Serialization::WriteBuffer::operator&(uint8_t x)
     printf("uint8_t %d\n", x);
 }
 
+void Serialization::WriteBuffer::operator&(int8_t x)
+{
+    printf("int8_t %d\n", x);
+}
+
 void Serialization::WriteBuffer::operator&(int32_t x)
 {
     printf("int32_t %d\n", x);
@@ -35,6 +40,8 @@ int main()
     f.intArray.push_back(5);
     f.intArray.push_back(7);
     f.intArray.push_back(6);
+    f.e_array_var.push_back(LogFrame::B);
+    f.someText = "abc";
     buf & f;
     
     return 0;
