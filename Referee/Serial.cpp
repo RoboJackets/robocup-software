0b020c2230a39d2b70fc1dd3aee668f2c4e90dcb
mitch7500 <mitch7500@7ccbd594-6f30-0410-bcae-e071bb1c4cdc>
2008-11-04 01:21:49 +0000

    modified the Serial.hpp and Serial.cpp files
    git-svn-id: https://svn.robojackets.org/robocup/software/trunk@656 7ccbd594-6f30-0410-bcae-e071bb1c4cdc

--- Referee/Serial.cpp
+++ Referee/Serial.cpp
@@ -1,18 +1,10 @@
-#include <cstdlib>
-#include <cstdio>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <fcntl.h>
-#include <termios.h>
-#include <string.h>
 #include "Serial.hpp"
 
 /** controls */
 #define DEBUG
 #define RADIO_DEVICE "/dev/ttyS0"
 #define BAUD_RATES 20
-const speed_t baud_rates[BAUD_RATES][2] = {
+const speed_t BaudRates[BAUD_RATES][2] = {
   {      B0,      0 },
   {     B50,     50 },
   {     B75,     75 },
@@ -36,124 +28,117 @@ const speed_t baud_rates[BAUD_RATES][2] = {
 };
 
 
-/**
- * baud_rate_to_flag -
- *
- * convert from raw baud rate to teh 
- * appropriate baud flag */
-speed_t baud_rate_to_flag(unsigned int speed)
-{
-  int i;
-
-  for (i = 0; i < BAUD_RATES; i++){
-    if (baud_rates[i][1] == speed) 
-    	return(baud_rates[i][0]);
-  }
-
-  return(0);
-}
-
-
-/**
- * baud_flag_to_rate -
- *
- * This function converts from the speed s to
- * the baud flag */
-int baud_flag_to_rate(speed_t s)
-{
-  int i;
-
-  for(i = 0; i < BAUD_RATES; i++){
-    if (baud_rates[i][0] == s) 
-    	return(baud_rates[i][1]);
-  }
-
-  return(0);
-}
-
-
-/**************************** SERIAL CLASS ***********************************/
+/// SERIAL CLASS ///
 
 Serial::Serial(){
-	fd=0;
+	_fileDescriptor=0;
 }
 
-
 Serial::~Serial(){
-	   if (fd)
+	   if (_fileDescriptor)
 	    {
-	    	close(fd);
+		   Serial::close();
 	    }
 }
 
 
-/** Open - function opens the serial port device and configures it
- *        operate at the specified baud rate and parameters
- * return value: TRUE on success, FALSE on failure */        
-bool Serial::Open(char *device,int baud)
+/** This function opens the serial port device and configures it
+ *  to operate at the specified baud rate and parameters
+ *  return value: TRUE on success, FALSE on failure 
+ */        
+bool Serial::open(char *device,int baud)
 {
-	struct termios tio;
-	speed_t bf;
-	int baud_in,baud_out;
+	struct termios terminalIO;
+	speed_t baudSpeed;
+	int baudIn;
+	int baudOut;
 
 	/** open device */
-	fd = ::open(device,O_RDWR|O_SYNC);
-	if (fd < 0) 
+	_fileDescriptor = ::open(device,O_RDWR|O_SYNC);
+	if (_fileDescriptor < 0) 
 		return(false);
 
-	/** set control attributes -
-	 * binary I/O, no flow control */
-	tcgetattr(fd,&tio);
-	cfmakeraw(&tio);
-	tio.c_cflag &= ~(CRTSCTS|IXON|IXOFF);
+	/** set control attributes - binary I/O, no flow control */
+	tcgetattr(_fileDescriptor,&terminalIO);
+	cfmakeraw(&terminalIO);
+	terminalIO.c_cflag &= ~(CRTSCTS|IXON|IXOFF);
 
 	/** set serial port speed */
-	bf = baud_rate_to_flag(baud);
-	cfsetispeed(&tio,bf);
-	cfsetospeed(&tio,bf);
-	tcsetattr(fd,TCSANOW,&tio);
+	baudSpeed = baudRateToFlag(baud);
+	cfsetispeed(&terminalIO,baudSpeed);
+	cfsetospeed(&terminalIO,baudSpeed);
+	tcsetattr(_fileDescriptor,TCSANOW,&terminalIO);
 
 	/** test what actually got set */
-	memset(&tio,0,sizeof(tio));
-	tcgetattr(fd,&tio);
-	baud_in  = baud_flag_to_rate(cfgetispeed(&tio));
-	baud_out = baud_flag_to_rate(cfgetospeed(&tio));
+	memset(&terminalIO,0,sizeof(terminalIO));
+	tcgetattr(_fileDescriptor,&terminalIO);
+	baudIn  = baudFlagToRate(cfgetispeed(&terminalIO));
+	baudOut = baudFlagToRate(cfgetospeed(&terminalIO));
 
-	return((baud_in==baud) && (baud_out==baud));	
+	return((baudIn==baud) && (baudOut==baud));	
 }
 
 
 /** Close - function closes the serial port module */
-void Serial::Close(void)
+void Serial::close(void)
 {
-	if(fd){
-		::close(fd);
-		fd = 0;
+	if(_fileDescriptor){
+		::close(_fileDescriptor);
+		_fileDescriptor = 0;
 	}
 }
 
 
-/** WriteByte - function writes a single byte from the serial port */
-int Serial::WriteByte(char b)
+/** writeByte - function writes a single byte from the serial port */
+int Serial::writeByte(char b)
 {
 #ifdef DEBUG
 	printf("writing %c\n", b);
 #endif
 
-	return (::write(fd, &b, 1));
+	return (::write(_fileDescriptor, &b, 1));
 }
 
 
-/** ReadByte - function reads a single byte from the serial port */
-int Serial::ReadByte(void)
+/** readByte - function reads a single byte from the serial port */
+int Serial::readByte(void)
 {
 	char data;
-	int ret_val;
+	int returnValue;
 	
-	if ((ret_val = ::read(fd, &data, 1)) < 0)
+	if ((returnValue = ::read(_fileDescriptor, &data, 1)) < 0)
 	{
-		return (ret_val);
+		return (returnValue);
 	}
 	
 	return (data);
 }
+
+
+ /** This function convert from raw baud rate to teh appropriate baud flag */
+speed_t Serial::baudRateToFlag(unsigned int speed)
+{
+  int i;
+
+  for (i = 0; i < BAUD_RATES; i++){
+    if (BaudRates[i][1] == speed) 
+    	return(BaudRates[i][0]);
+  }
+
+  return(0);
+}
+
+
+ /** This function converts from the speed s to the baud flag */
+int Serial::baudFlagToRate(speed_t speed)
+{
+  int i;
+
+  for(i = 0; i < BAUD_RATES; i++){
+    if (BaudRates[i][0] == speed) 
+    	return(BaudRates[i][1]);
+  }
+
+  return(0);
+}
+
--- Referee/Serial.hpp
+++ Referee/Serial.hpp
@@ -1,38 +1,47 @@
 #ifndef SERIAL_HPP
 #define SERIAL_HPP
 
+#include <cstdlib>
+#include <cstdio>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <string.h>
 #include <unistd.h>
 
 class Serial
 {
-  /** file descriptor used to open serial port device */
-  int fd;
-
-public:
-  Serial();
-  ~Serial(); 
-
-
-  /** prototypes */
-  bool Open(char *device,int baud);
-  void Close(void);
-
+		/// methods ///
+	public:
+				
+		Serial();
+		~Serial(); 
   
-  /** This function reads bytes from the serial port */
-  int Read(void *buf,int size){
-	  return(::read(fd,buf,size));
-	  }
+		/** function opens the serial port device and configures it */
+		bool open(char *device,int baud);
   
-  /** This function writes bytes to the serial port */
-  int Write(void *buf,int size){
-	  return(::write(fd,buf,size));
-	  }
+		/** function closes the serial port module */
+		void close(void);
+		
+		/**  * This function converts from the speed s to the baud flag */
+		int baudFlagToRate(speed_t s);
+			
+		/** convert from raw baud rate to teh appropriate baud flag */
+		speed_t baudRateToFlag(unsigned int speed);
   
-  /** This function writes a single byte to the serial port */  
-  int WriteByte(char b);
+		/** This function writes a single byte to the serial port */  
+		int writeByte(char b);
   
-  /** This function reads a single byte from the serial port */
-  int ReadByte(void);
+		/** This function reads a single byte from the serial port */
+		int readByte(void);
+		
+  
+		/// members ///
+	private:
+		/** file descriptor used to open serial port device */
+		int _fileDescriptor;
   	
 };
 
