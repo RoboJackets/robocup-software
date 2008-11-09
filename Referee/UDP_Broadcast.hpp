4674603c46b0a827d515f77f9caef796cd36c91d
mitch7500 <mitch7500@7ccbd594-6f30-0410-bcae-e071bb1c4cdc>
2008-11-09 04:22:36 +0000

    modified the UDP_Broadcast and CMakeLists.txt files
    git-svn-id: https://svn.robojackets.org/robocup/software/trunk@660 7ccbd594-6f30-0410-bcae-e071bb1c4cdc

--- Referee/UDP_Broadcast.hpp
+++ Referee/UDP_Broadcast.hpp
@@ -1,6 +1,40 @@
 #ifndef UDP_BROADCAST_HPP
 #define UDP_BROADCAST_HPP
 
+#include <stdint.h>
+#include <string>
+#include <gtkmm.h>
+
+class UDP_Broadcast
+{
+	public:
+	
+		UDP_Broadcast() throw (IOError);
+		~UDP_Broadcast();
+    
+		/** IOError Class */
+		class IOError: public Glib::IOChannelError
+		{
+    		public:
+    			IOError(const std::string& msg): Glib::IOChannelError(
+    					Glib::IOChannelError::IO_ERROR, msg)
+    			{
+    			}
+    			IOError(const std::string& msg, int err): Glib::IOChannelError(
+    					Glib::IOChannelError::IO_ERROR, msg + ": " + strerror(err)) 
+    			{
+    			}
+    			virtual ~IOError() throw() {};
+		};
+    
 
 
+		/** Prototypes */
+		void set_destination(const std::string& host, const uint16_t port) throw (IOError);
+		void send(const std::string& buffer) throw (IOError);
+		void send(const void* buffer, const size_t buflen) throw (IOError); 
+    
+	protected:
+		int sock;
+    
 #endif /*UDP_BROADCAST_HPP_*/
