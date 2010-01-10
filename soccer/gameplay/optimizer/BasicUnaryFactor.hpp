14d504b16a62c66669c8d1eec390248ba3e483a4
alexgc <alexgc@7ccbd594-6f30-0410-bcae-e071bb1c4cdc>
2010-01-10 20:20:02 +0000

    Disabled the PassOptimizer due to a compilation error with gtsam
    git-svn-id: https://svn.robojackets.org/robocup/software/trunk@948 7ccbd594-6f30-0410-bcae-e071bb1c4cdc

---
+++ soccer/gameplay/optimizer/BasicUnaryFactor.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <gtsam/NonlinearFactor.h>
+#include "OptimizerConfig.hpp"
+
+namespace Gameplay {
+	namespace Optimization {
+		/**
+		 * a Gaussian nonlinear factor that takes 1 parameter
+		 * Note: cannot be serialized as contains function pointers
+		 * Specialized derived classes could do this
+		 */
+		class BasicUnaryFactor : public gtsam::NonlinearFactor<OptimizerConfig> {
+		private:
+
+			std::string key_;
+
+		public:
+
+			Vector (*h_)(const Vector&);
+			Matrix (*H_)(const Vector&);
+
+			/** Constructor */
+			BasicUnaryFactor(const Vector& z,		  // measurement
+					const double sigma,	  			  // variance
+					Vector (*h)(const Vector&),       // measurement function
+					const std::string& key1,          // key of the variable
+					Matrix (*H)(const Vector&));      // derivative of the measurement function
+
+			void print(const std::string& s = "") const;
+
+			/** Check if two factors are equal */
+			bool equals(const BasicUnaryFactor& f, double tol=1e-9) const;
+
+			/** error function */
+			inline Vector error_vector(const OptimizerConfig& c) const {
+				return z_ - h_(c[key_]);
+			}
+
+			/** linearize a non-linearFactor1 to get a linearFactor1 */
+			boost::shared_ptr<gtsam::GaussianFactor> linearize(const OptimizerConfig& c) const;
+		};
+	}
+}
--- soccer/gameplay/optimizer/PassOptimizer.cpp
+++ soccer/gameplay/optimizer/PassOptimizer.cpp
@@ -32,99 +32,99 @@ Gameplay::Optimization::PassOptimizer::~PassOptimizer() {}
 PassConfig Gameplay::Optimization::PassOptimizer::optimizePlan(
 		const PassConfig& init, bool verbose) const
 {
-	// build up the initial configuration and graph by reading through PassConfig
-	shared_config config(new OptimizerConfig);
-	OptimizerGraph graph;
-
-	// store the robots for lookup
-	vector<Robot*> robots;
-
-	// loop through the pass states to add them to the graph
-	int robot_num = 0;
-	PassState prevState;
-	BOOST_FOREACH(PassState s, init.passStateVector) {
-		if (s.stateType == PassState::INITIAL) {
-			// set up ball intercept for the first robot
-			graph.addBallIntercept(robot_num, gameplay_->state()->ball);
-
-		} else if (s.stateType == PassState::INTERMEDIATE) {
-			// pull out the robot and store
-			Robot* r = s.robot;
-			robots.push_back(r);
-
-			// add the robot to the config - initial and final poses
-			config->initRobot(robot_num, r->pos(), s.robotPos);
-			//config->initTime(robot_num, s); //TODO: add timing information
-
-			// add appropriate factors to the graph for basic movement
-			graph.addRobotInitConstraint(robot_num, r->pos()); /// fix initial point
-			graph.addRobotMotion(robot_num, r);				   /// add basic path shortening
-			//graph.addRobotFieldBound(robot_num);	           /// keep the robot on the field
-
-//			// add some priors to the robot poses to avoid scatter
-//			double prior_weight = 0.1;
-//			graph.addRobotPrior(robot_num, prior_weight);
-
-			// if this robot is receiving a pass, create the pass factors
-			if (prevState.stateType == PassState::INTERMEDIATE) {
-				graph.addPass(robot_num, robot_num-1); // all-in-one pass creation from two points
-			}
-
-			// goto next robot
-			++robot_num;
-		} else if (s.stateType == PassState::GOAL) {
-			// create the shot factor
-			graph.addShot(robot_num-1);
-		}
-		// save the state for connections
-		prevState = s;
-	}
-
-	// get an ordering
-	Ordering ordering = graph.getOrdering();
-
-	// print out the parts
-	if (verbose) {
-		cout << "Printing initial graph and config..." << endl;
-		config->print("Initial");
-		graph.print("Initial");
-		ordering.print("Guessed Ordering for the graph");
-	}
-
-	// create an SQP optimizer to optimize the graph
-	Optimizer optimizer(graph, ordering, config);
-
-	if (verbose) {
-		optimizer.print("Initial Optimizer State");
-	}
-
-	// iterate-solve using fixed iteration numbers for safety
-	//TODO: do more than one iteration here, it'll be necessary
-	//Optimizer next = optimizer.iterate(Optimizer::SILENT);
-	double thresh = 1e-2;
-	int maxIt = 1;
-	Optimizer next = optimizer.iterateSolve(thresh, thresh, thresh, maxIt, Optimizer::SILENT);
-	shared_const_config newConfig = next.config();
-
-	if (verbose) {
-		newConfig->print("Optimized SQP Config");
-	}
+//	// build up the initial configuration and graph by reading through PassConfig
+//	shared_config config(new OptimizerConfig);
+//	OptimizerGraph graph;
+//
+//	// store the robots for lookup
+//	vector<Robot*> robots;
+//
+//	// loop through the pass states to add them to the graph
+//	int robot_num = 0;
+//	PassState prevState;
+//	BOOST_FOREACH(PassState s, init.passStateVector) {
+//		if (s.stateType == PassState::INITIAL) {
+//			// set up ball intercept for the first robot
+//			graph.addBallIntercept(robot_num, gameplay_->state()->ball);
+//
+//		} else if (s.stateType == PassState::INTERMEDIATE) {
+//			// pull out the robot and store
+//			Robot* r = s.robot;
+//			robots.push_back(r);
+//
+//			// add the robot to the config - initial and final poses
+//			config->initRobot(robot_num, r->pos(), s.robotPos);
+//			//config->initTime(robot_num, s); //TODO: add timing information
+//
+//			// add appropriate factors to the graph for basic movement
+//			graph.addRobotInitConstraint(robot_num, r->pos()); /// fix initial point
+//			graph.addRobotMotion(robot_num, r);				   /// add basic path shortening
+//			//graph.addRobotFieldBound(robot_num);	           /// keep the robot on the field
+//
+////			// add some priors to the robot poses to avoid scatter
+////			double prior_weight = 0.1;
+////			graph.addRobotPrior(robot_num, prior_weight);
+//
+//			// if this robot is receiving a pass, create the pass factors
+//			if (prevState.stateType == PassState::INTERMEDIATE) {
+//				graph.addPass(robot_num, robot_num-1); // all-in-one pass creation from two points
+//			}
+//
+//			// goto next robot
+//			++robot_num;
+//		} else if (s.stateType == PassState::GOAL) {
+//			// create the shot factor
+//			graph.addShot(robot_num-1);
+//		}
+//		// save the state for connections
+//		prevState = s;
+//	}
+//
+//	// get an ordering
+//	Ordering ordering = graph.getOrdering();
+//
+//	// print out the parts
+//	if (verbose) {
+//		cout << "Printing initial graph and config..." << endl;
+//		config->print("Initial");
+//		graph.print("Initial");
+//		ordering.print("Guessed Ordering for the graph");
+//	}
+//
+//	// create an SQP optimizer to optimize the graph
+//	Optimizer optimizer(graph, ordering, config);
+//
+//	if (verbose) {
+//		optimizer.print("Initial Optimizer State");
+//	}
+//
+//	// iterate-solve using fixed iteration numbers for safety
+//	//TODO: do more than one iteration here, it'll be necessary
+//	//Optimizer next = optimizer.iterate(Optimizer::SILENT);
+//	double thresh = 1e-2;
+//	int maxIt = 1;
+//	Optimizer next = optimizer.iterateSolve(thresh, thresh, thresh, maxIt, Optimizer::SILENT);
+//	shared_const_config newConfig = next.config();
+//
+//	if (verbose) {
+//		newConfig->print("Optimized SQP Config");
+//	}
 
 	// reconstruct the PassConfig from the updated configuration
 	PassConfig optConfig;
-	robot_num = 0;
-	BOOST_FOREACH(PassState s, init.passStateVector) {
-		PassState newState;
-		if (s.stateType == PassState::INITIAL) {
-			newState = PassState(s.ballPos, s.stateType);
-		} else if (s.stateType == PassState::INTERMEDIATE) {
-			newState = PassState(newConfig->getFinal(robot_num), s.robot, newConfig->getFinal(robot_num));
-			++robot_num;
-		} else if (s.stateType == PassState::GOAL) {
-			newState = PassState(s.ballPos, s.stateType);
-		}
-		optConfig.addPassState(newState);
-	}
+//	robot_num = 0;
+//	BOOST_FOREACH(PassState s, init.passStateVector) {
+//		PassState newState;
+//		if (s.stateType == PassState::INITIAL) {
+//			newState = PassState(s.ballPos, s.stateType);
+//		} else if (s.stateType == PassState::INTERMEDIATE) {
+//			newState = PassState(newConfig->getFinal(robot_num), s.robot, newConfig->getFinal(robot_num));
+//			++robot_num;
+//		} else if (s.stateType == PassState::GOAL) {
+//			newState = PassState(s.ballPos, s.stateType);
+//		}
+//		optConfig.addPassState(newState);
+//	}
 
 	return optConfig;
 }
