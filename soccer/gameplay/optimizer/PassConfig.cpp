1cdd3d11cbf601eec1eb47ddd873a56390e4fc3c
alexgc <alexgc@7ccbd594-6f30-0410-bcae-e071bb1c4cdc>
2010-02-21 18:34:54 +0000

    New render code for PassConfigs
    git-svn-id: https://svn.robojackets.org/robocup/software/trunk@1059 7ccbd594-6f30-0410-bcae-e071bb1c4cdc

--- soccer/gameplay/optimizer/PassConfig.cpp
+++ soccer/gameplay/optimizer/PassConfig.cpp
@@ -78,36 +78,61 @@ void PassConfig::drawConfig(SystemState* sysState, int r, int g, int b) const {
 	float ball_radius = 0.05;
 	float pos_radius = 0.1;
 
-	// draw the path of the ball and the robot trajectories
-	int stateNum = 0;
-	PassState prevState;
-	BOOST_FOREACH(PassState state, passStateVector) {
-//		switch (state.stateType) {
-//		case PassState::INTERMEDIATE:
-//			// if in first state, draw the robot and ball positions
-//			// if in the last state, draw the ball
-//			break;
-//		case PassState::KICKPASS:
-//			break;
-//		case PassState::RECEIVEPASS:
-//			break;
-//		case PassState::KICKGOAL:
-//			break;
+	// Hardcoded printing, assuming
+	// Intermediate (init), KickPass, ReceivePass, KickGoal, Intermediate (ball in goal)
+	if (passStateVector.size() != 5)
+		throw invalid_argument("Incorrect size of passConfig - hardcoded for 5");
+
+	// pull out the states
+	const PassState start    = passStateVector[0],
+					kickPass = passStateVector[1],
+					recPass  = passStateVector[2],
+					kickGoal = passStateVector[3],
+					end      = passStateVector[4];
+
+	// render initial positions
+	sysState->debugCircles.push_back(drawCircle(start.robot1Pos, pos_radius, r,g,b));
+	sysState->debugCircles.push_back(drawCircle(start.robot2Pos, pos_radius, r,g,b));
+
+	// render kickPass states
+	sysState->debugCircles.push_back(drawCircle(kickPass.robot1Pos, pos_radius, r,g,b));
+	sysState->debugLines.push_back(drawLine(start.robot1Pos, kickPass.robot1Pos, r,g,b));
+	sysState->debugCircles.push_back(drawCircle(kickPass.ballPos, ball_radius, r,g,b));
+
+	// render recPass state
+	sysState->debugCircles.push_back(drawCircle(recPass.robot2Pos, pos_radius, r,g,b));
+	sysState->debugLines.push_back(drawLine(kickPass.ballPos, recPass.ballPos, r,g,b));
+	sysState->debugLines.push_back(drawLine(kickPass.robot2Pos, recPass.robot2Pos, r,g,b));
+	sysState->debugCircles.push_back(drawCircle(recPass.ballPos, ball_radius, r,g,b));
+
+	// render kickGoal state
+	sysState->debugCircles.push_back(drawCircle(kickGoal.robot2Pos, pos_radius, r,g,b));
+	sysState->debugCircles.push_back(drawCircle(kickGoal.ballPos, ball_radius, r,g,b));
+	sysState->debugLines.push_back(drawLine(kickGoal.ballPos, end.ballPos, r,g,b));
+	sysState->debugLines.push_back(drawLine(recPass.robot2Pos, kickGoal.robot2Pos, r,g,b));
+
+	// render final ball position
+	sysState->debugCircles.push_back(drawCircle(end.ballPos, ball_radius, r,g,b));
+
+	// Code to do arbitrary paths while printing excess information
+//	// draw the path of the ball and the robot trajectories
+//	int stateNum = 0;
+//	PassState prevState;
+//	BOOST_FOREACH(PassState state, passStateVector) {
+//
+//		// draw the ball
+//		sysState->debugCircles.push_back(drawCircle(state.ballPos, ball_radius, r,g,b));
+//
+//		// draw robots
+//		sysState->debugCircles.push_back(drawCircle(state.robot1Pos, pos_radius, r,g,b));
+//		sysState->debugCircles.push_back(drawCircle(state.robot2Pos, pos_radius, r,g,b));
+//
+//		if(stateNum > 0){
+//			sysState->debugLines.push_back(drawLine(prevState.ballPos, state.ballPos, r,g,b));
+//			sysState->debugLines.push_back(drawLine(prevState.robot1Pos, state.robot1Pos, r,g,b));
+//			sysState->debugLines.push_back(drawLine(prevState.robot2Pos, state.robot2Pos, r,g,b));
 //		}
-
-		// draw the ball
-		sysState->debugCircles.push_back(drawCircle(state.ballPos, ball_radius, r,g,b));
-
-		// draw robots
-		sysState->debugCircles.push_back(drawCircle(state.robot1Pos, pos_radius, r,g,b));
-		sysState->debugCircles.push_back(drawCircle(state.robot2Pos, pos_radius, r,g,b));
-
-		if(stateNum > 0){
-			sysState->debugLines.push_back(drawLine(prevState.ballPos, state.ballPos, r,g,b));
-			sysState->debugLines.push_back(drawLine(prevState.robot1Pos, state.robot1Pos, r,g,b));
-			sysState->debugLines.push_back(drawLine(prevState.robot2Pos, state.robot2Pos, r,g,b));
-		}
-		prevState = state;
-		stateNum++;
-	}
+//		prevState = state;
+//		stateNum++;
+//	}
 }
