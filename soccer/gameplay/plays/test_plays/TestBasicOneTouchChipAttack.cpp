e7de7ced660755c0ef4e0a710fd9e19c6c6b0250
Alex Cunningham <alexgc@gatech.edu>
2010-06-17 14:15:07 -0400

    Added one touch chip play

---
+++ soccer/gameplay/plays/test_plays/TestBasicOneTouchChipAttack.cpp
@@ -0,0 +1,42 @@
+#include "TestBasicOneTouchChipAttack.hpp"
+
+#include <boost/foreach.hpp>
+
+using namespace std;
+
+Gameplay::Plays::TestBasicOneTouchChipAttack::TestBasicOneTouchChipAttack(GameplayModule *gameplay):
+	Play(gameplay, 1), _kicker(gameplay)
+{
+}
+
+bool Gameplay::Plays::TestBasicOneTouchChipAttack::applicable()
+{
+	bool refApplicable =_gameplay->state()->gameState.playing();
+	return refApplicable;
+}
+
+bool Gameplay::Plays::TestBasicOneTouchChipAttack::assign(set<Robot *> &available)
+{
+	// remove non-visible robots
+	// this prevents bug with 2-robot tests where play would do nothing
+	BOOST_FOREACH(Robot *r, available){
+		if(!r->visible()){
+			available.erase(r);
+		}
+	}
+
+	_kicker.assign(available);
+	return _robots.size() >= _minRobots && robot()->hasChipper();
+}
+
+bool Gameplay::Plays::TestBasicOneTouchChipAttack::run()
+{
+	// check if the robot is in done state
+	if (_kicker.getState() == Gameplay::Behaviors::OneTouchKick::Done)
+		_kicker.restart();
+
+	// run the kick play
+	_kicker.kickType(Behaviors::OneTouchKick::CHIP);
+	_kicker.run();
+	return true;
+}
---
+++ soccer/gameplay/plays/test_plays/TestBasicOneTouchChipAttack.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "../../Play.hpp"
+
+#include "../../behaviors/OneTouchKick.hpp"
+
+namespace Gameplay
+{
+namespace Plays
+{
+/**
+ * This play just drives all robots to kick the ball continuously
+ * Useful for testing basic operation of the system, and should
+ * probably only be used with 1-2 robots on the field, as it
+ * will get chaotic
+ *
+ * This variant uses chip kicking and one touch kicking
+ */
+class TestBasicOneTouchChipAttack: public Play
+{
+public:
+	TestBasicOneTouchChipAttack(GameplayModule *gameplay);
+
+	/**
+	 * Applicable any time that the game is running
+	 */
+	virtual bool applicable();
+
+	/**
+	 * Takes the first robot as a kicker, ignores the rest
+	 */
+	virtual bool assign(std::set<Robot *> &available);
+
+	/** default run */
+	virtual bool run();
+
+protected:
+	Behaviors::OneTouchKick _kicker;
+};
+} // \Gameplay
+} // \Plays
