dade5173d5c5f50e8283d3d3f098cfb3bf45c39b
robojackets <robojackets@macbook.(none)>
2010-06-20 04:31:25 -0400

    Progress towards a defense

---
+++ soccer/gameplay/behaviors/GoalDefender.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../Behavior.hpp"
+#include "../Window.hpp"
+
+namespace Gameplay
+{
+	namespace Behaviors
+	{
+		class GoalDefender : public Behavior
+		{
+			public:
+				GoalDefender(GameplayModule *gameplay);
+
+				virtual bool run();
+				virtual bool done();
+
+				virtual float score(Robot* robot);
+
+			protected:
+				//Window evaluator for checking passes/shots
+				Gameplay::WindowEvaluator* _winEval;
+		};
+	}
+}
