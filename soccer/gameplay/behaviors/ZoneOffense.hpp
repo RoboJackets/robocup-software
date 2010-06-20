85647aa6b96c5be944db2ec55a65d86b1309341c
Alex Cunningham <alexgc@gatech.edu>
2010-06-20 10:11:33 -0400

    Added an implementation for a play with ZoneOffense

--- soccer/gameplay/behaviors/ZoneOffense.hpp
+++ soccer/gameplay/behaviors/ZoneOffense.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "../Behavior.hpp"
+#include "Kick.hpp"
+#include "Mark.hpp"
 
 namespace Gameplay
 {
@@ -11,34 +13,43 @@ namespace Gameplay
 			public:
 				ZoneOffense(GameplayModule *gameplay);
 				
+				virtual ~ZoneOffense();
+
 				/** note: can use either two or three robots */
 				virtual bool assign(std::set<Robot *> &available);
 
 				virtual bool run();
 
-				// loose quadrants
-				typedef enum {
-					LEFT_FORWARD,
-					RIGHT_FORWARD,
-					LEFT_HOME,
-					RIGHT_HOME
-				} Quadrant;
-
-				// splitting field into nine sections
+				/** zones - track which robot is handling the ball */
 				typedef enum {
 					LEFT,
-					CENTER,
-					RIGHT
-				} LRZone;
-				typedef enum {
-					HOMEFIELD,
+					RIGHT,
 					MIDFIELD,
-					OPPFIELD
-				} SOZone;
+					NONE
+				} Zone;
+
+				void activeZone(Zone zone) { _activeZone = zone; }
+				Zone activeZone() const { return _activeZone; }
 
 			protected:
 
 				Robot *_leftAttack, *_rightAttack, *_midfielder;
+
+				// sub-behaviors to simplify system
+				Kick * _kicker;
+				Mark *_markLeft, *_markRight, *_markMidfield;
+
+				Zone _activeZone; /// zone which is actively trying to get the ball
+
+				// Real zones - smaller, "NoMid" versions don't include midfielders
+				Geometry2d::Rect _midfieldZone,
+							     _leftZone, _rightZone,
+							     _leftNoMidZone, _rightNoMidZone;
+
+				// enlarged zones - used for hysteresis
+				Geometry2d::Rect _midfieldBigZone,
+								 _leftBigZone, _rightBigZone,
+								 _leftBigNoMidZone, _rightBigNoMidZone;
 		};
 	}
 }
