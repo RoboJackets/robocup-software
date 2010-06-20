98431ef3a96d9f7feb80e04321bc27955c613cf6
Alex Cunningham <alexgc@gatech.edu>
2010-06-20 06:35:59 -0400

    Skeleton play for using zone offense

--- soccer/gameplay/behaviors/ZoneOffense.cpp
+++ soccer/gameplay/behaviors/ZoneOffense.cpp
@@ -1,5 +1,9 @@
 #include "ZoneOffense.hpp"
 
+#include <iostream>
+#include <vector>
+#include <boost/foreach.hpp>
+
 using namespace std;
 
 Gameplay::Behaviors::ZoneOffense::ZoneOffense(GameplayModule *gameplay)
@@ -12,8 +16,47 @@ Gameplay::Behaviors::ZoneOffense::ZoneOffense(GameplayModule *gameplay)
 
 bool Gameplay::Behaviors::ZoneOffense::assign(std::set<Robot *> &available) {
 	takeAll(available);
-//	return _robots.size() >= _minRobots;
-	return true;
+
+	if (_robots.size() == 2) {
+		Robot *a = *_robots.begin(), *b = *(_robots.begin()++);
+		if (a->pos().x < b->pos().x) {
+			_leftAttack = a;
+			_rightAttack = b;
+		} else {
+			_leftAttack = b;
+			_rightAttack = a;
+		}
+		return true;
+	} else if (_robots.size() == 3) {
+
+		// find the midfielder
+		float backY = 100;
+		vector<Robot*> forwards;
+		BOOST_FOREACH(Robot * r, _robots) {
+			if (!r) {
+				_midfielder = r;
+				backY = _midfielder->pos().y;
+			} else if (r->pos().y < backY) {
+				backY = r->pos().y;
+				forwards.push_back(_midfielder);
+				_midfielder = r;
+			}
+		}
+		cout << "ZoneOffense: 3 robot version - found " << forwards.size() << " forwards" << endl;
+
+		// pick left and right
+		Robot *a = forwards[0], *b = forwards[1];
+		if (a->pos().x < b->pos().x) {
+			_leftAttack = a;
+			_rightAttack = b;
+		} else {
+			_leftAttack = b;
+			_rightAttack = a;
+		}
+
+		return true;
+	}
+	return false;
 }
 
 bool Gameplay::Behaviors::ZoneOffense::run() {
