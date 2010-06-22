15229be11c07a200a077698e9bc1d6a3f52beb89
robojackets <robojackets@macbook.(none)>
2010-06-21 21:37:57 -0400

    Changed prototype for Line, Added TestKicks play (partially works), improved fullback behavior

---
+++ soccer/gameplay/plays/test_plays/TestKicks.hpp
@@ -0,0 +1,40 @@
+/**
+ *  Example play: This is a template for a play.
+ *  To use, implement the functions and add the necessary member variables
+ *  and do a test replacement for ExamplePlay with whatever name you want.
+ */
+
+#pragma once
+
+#include "../Play.hpp"
+
+namespace Gameplay
+{
+	namespace Plays
+	{
+		class TestKicks: public Play
+		{
+			public:
+				typedef enum {
+					Deg0StationaryKick,
+					Deg45StationaryKick,
+					NumTests // make sure this is last
+				} Test;
+
+				TestKicks(GameplayModule *gameplay);
+
+				/** returns true if the play is currently applicable given gamestate */
+				virtual bool applicable();
+
+				/** Assigns robots to the play given a set of robots */
+				virtual bool assign(std::set<Robot *> &available);
+
+				/** Called every frame */
+				virtual bool run();
+
+			protected:
+				// Insert sub behaviors here as member variables
+				Test _test;
+		};
+	}
+}
