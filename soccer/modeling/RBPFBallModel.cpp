ca6ea5cc3447bfd9925e3b45c51c3622dcefea19
Alex Cunningham <alexgc@gatech.edu>
2011-01-22 13:45:15 -0500

    changed the number of particles for ball tracking from 20 to 10 for performance

--- soccer/modeling/RBPFBallModel.cpp
+++ soccer/modeling/RBPFBallModel.cpp
@@ -24,7 +24,7 @@ Modeling::RBPFBallModel::RBPFBallModel(RobotModel::RobotMap *robotMap, Configura
 	// Construct initial state covariance P (n x n)
 	Matrix P(6,6); P.clear(); P(0,0)=P(1,1)= P(2,2)=P(3,3)=P(4,4)=P(5,5)=0.01;
 	// Create Rbpf
-	int numParticles = 20; // Number of particles in filter
+	int numParticles = 10; // Number of particles in filter
 	raoBlackwellizedParticleFilter = new Rbpf(X,P,numParticles);
 	// create model graph
 	raoBlackwellizedParticleFilter->addModel(new RbpfModelRolling(_robotMap, config));
