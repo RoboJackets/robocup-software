e236c4cb6ca514a3ed344ccea3755396789187a0
Ben Johnson <circuitben@gmail.com>
2011-06-28 21:17:38 -0400

    Don't show kicker I2C failure on 2008 base

--- firmware/robot_2011/cpu/main.c
+++ firmware/robot_2011/cpu/main.c
@@ -75,8 +75,9 @@ void update_leds()
 	if (failures & Fail_Kicker_Charge)
 	{
 		flash(LED_LR, 2);
-	} else if (failures & Fail_Kicker_I2C)
+	} else if (!base2008 && failures & Fail_Kicker_I2C)
 	{
+		// 2008 bases have no kicker ADC
 		flash(LED_LR, 3);
 	} else if (!(kicker_status & Kicker_Charging) || (kicker_status & Kicker_Override))
 	{
