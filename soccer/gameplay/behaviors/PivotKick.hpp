d29f74a4aa2dd29618eac3da3b08fb2c7e6a9715
Alex Cunningham <alexgc@gatech.edu>
2011-06-30 18:59:58 -0400

    Added software-controllable parameters for PivotKick execution

--- soccer/gameplay/behaviors/PivotKick.hpp
+++ soccer/gameplay/behaviors/PivotKick.hpp
@@ -29,8 +29,32 @@ namespace Gameplay
 
 				void restart();
 				
+				// Default target is goal segment, but does not take into account
+				// obstacles.  Plays should use window evaluator to kick
 				Geometry2d::Segment target;
 				
+				// If off, will stay in State_Aim until turned on - use for synchronizing
+				// with other robots.  Defaults to true
+				bool enable_kick;
+
+				// If on, the robot will get desperate after aiming for too long and
+				// kick the ball downfield. Defaults to true
+				bool enable_desparate_kick;
+
+				// The speed to drive the dribbler at during aiming
+				// If high, adds lift to kick
+				// Default: full speed
+				short dribble_speed;
+
+				// If false, uses straight kicker, if true, uses chipper
+				// Default: false
+				bool use_chipper;
+
+				// Allows for different kicker settings, such as for
+				// passing with lower power.
+				// Default: 255 - full power
+				uint8_t kick_power;
+
 			private:
 
 				enum
