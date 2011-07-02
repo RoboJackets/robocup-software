b1bfd14d8f7958abc2b80336624b1a5effffb7b8
Alex Cunningham <alexgc@gatech.edu>
2011-07-01 22:35:02 -0400

    Added Yank with accompanying demo - needs testing on real robots

---
+++ soccer/gameplay/plays/demo_plays/DemoYank.cpp
@@ -0,0 +1,36 @@
+#include "DemoYank.hpp"
+
+#include <boost/foreach.hpp>
+
+using namespace std;
+
+REGISTER_PLAY_CATEGORY(Gameplay::Plays::DemoYank, "Demos")
+
+Gameplay::Plays::DemoYank::DemoYank(GameplayModule *gameplay):
+Play(gameplay),
+_yank(gameplay)
+{
+	_dribblerSpeed = config()->createInt("DemoYank/Dribber Speed", 127);
+}
+
+bool Gameplay::Plays::DemoYank::run()
+{
+	Geometry2d::Point ballPos = ball().pos;
+
+	set<OurRobot *> available = _gameplay->playRobots();
+	assignNearest(_yank.robot, available, ballPos);
+
+	// if we have kicked, we want to reset
+	if (_yank.done() &&  ball().valid &&
+			(!ballPos.nearPoint(Geometry2d::Point(0, Field_Length), Field_ArcRadius)))
+	{
+		_yank.restart();
+	}
+
+	// set flags from parameters
+	_yank.dribble_speed = *_dribblerSpeed;
+
+	_yank.run();
+
+	return true;
+}
