06a7cfefb3d135fa2c733164c9e6262ebb84a702
Alex Cunningham <alexgc@gatech.edu>
2011-07-02 21:20:31 -0400

    Added an assignment function with robot status requirements, placeholders for status checks in Robot

--- soccer/gameplay/Play.cpp
+++ soccer/gameplay/Play.cpp
@@ -78,3 +78,47 @@ bool assignNearest(OurRobot *&role, std::set<OurRobot*>& robots, Geometry2d::Poi
 	
 	return role != 0;
 }
+
+bool assignNearest(OurRobot *&role, std::set<OurRobot *> &robots, Geometry2d::Point pt,
+		bool hasChipper, bool hasKicker, bool hasEncoders, bool needVisible)
+{
+	if (role && (!needVisible || role->visible))
+	{
+		// Already has a usable robot
+		robots.erase(role);
+		return true;
+	}
+
+	OurRobot *bestRobot = 0;
+	float bestDistSq = -1;
+	BOOST_FOREACH(OurRobot *robot, robots)
+	{
+		// manage requirements
+		if ((!hasChipper || robot->hasChipper()) &&
+				(!hasKicker  || robot->hasKicker()) &&
+				(!hasEncoders || robot->hasEncoders()))
+		{
+			continue; // robot fails requirements
+		}
+
+		float dsq = (robot->pos - pt).magsq();
+		if (bestDistSq < 0 || dsq < bestDistSq)
+		{
+			bestDistSq = dsq;
+			bestRobot = robot;
+		}
+	}
+
+	if (bestRobot)
+	{
+		role = bestRobot;
+		robots.erase(role);
+
+		// set default flags - only gets reset at assignment, rather than each frame
+		role->avoidOpponents(true);
+		role->avoidAllTeammates(true);
+		role->avoidBall(Ball_Radius);
+	}
+
+	return role != 0;
+}
