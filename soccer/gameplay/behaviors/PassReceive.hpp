6cb5eb7aaed55cc88a70305b77140daa6db2fe77
robojackets <robojackets@robojackets>
2011-10-04 19:58:21 -0400

    Added functionality to PassReceive and updated PassPlay to use Pass Receive

--- soccer/gameplay/behaviors/PassReceive.hpp
+++ soccer/gameplay/behaviors/PassReceive.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../Behavior.hpp"
+#include "Kick.hpp"
 
 namespace Gameplay
 {
@@ -13,6 +14,34 @@ namespace Gameplay
 				PassReceive(GameplayModule *gameplay);
 				
 				virtual bool run();
+				void reset();
+				bool done();
+
+				bool passUseChip;
+				bool passUseLine;
+
+				uint8_t passPower;
+				uint64_t backupTime;
+
+			protected:
+				Behaviors::Kick _passer;
+
+				static ConfigInt *_backup_time;
+				static ConfigDouble *_backup_speed;
+
+
+
+			private:
+				uint64_t readyTime;
+				enum
+				{
+					State_Setup,
+					State_Ready,
+					State_Execute,
+					State_Kicked,
+					State_Done
+				} _state;
+
 
 		};
 	}
--- soccer/gameplay/plays/demo_plays/PassPlay.hpp
+++ soccer/gameplay/plays/demo_plays/PassPlay.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <gameplay/Play.hpp>
-#include <gameplay/behaviors/Kick.hpp>
+#include <gameplay/behaviors/PassReceive.hpp>
 namespace Gameplay
 {
 	namespace Plays
@@ -15,16 +15,12 @@ namespace Gameplay
 				/** Called every frame */
 				virtual bool run();
 			protected:
-				Behaviors::Kick _passer1;
-				Behaviors::Kick _passer2;
+				Behaviors::PassReceive _pass;
 				bool _passer1HasBall;
-				uint64_t readyTime;
 
 				static ConfigBool *_use_chipper;
 				static ConfigBool *_use_line;
 				static ConfigInt *_kick_power;
-				static ConfigInt *_backup_time;
-				static ConfigDouble *_backup_speed;
 		};
 	}
 }
