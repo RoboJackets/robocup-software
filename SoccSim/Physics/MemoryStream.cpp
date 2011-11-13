75c9e085c29f9220c9ccbce8fe744f93f6b4d3bf
Alex Cunningham <alexgc@gatech.edu>
2011-11-13 14:44:01 -0500

    Notes from debugging - doesn't appear to create a physxSDK properly

--- SoccSim/Physics/MemoryStream.cpp
+++ SoccSim/Physics/MemoryStream.cpp
@@ -1,85 +1,113 @@
 #include "MemoryStream.hpp"
+#include <stdio.h>
 
-MemoryStream::MemoryStream()
+MemoryStream::MemoryStream() : _readLoc(0)
 {
-    _readLoc = 0;
 }
 
 // Loading API
 NxU8 MemoryStream::readByte() const
 {
-    NxU8 b = read<NxU8>();
-    return b;
+	//    NxU8 b = read<NxU8>(); // FIXME: segfault here
+	//    return b;
+
+	//	T res = 0;
+	//	assert((_readLoc + sizeof(T)) < _data.size());
+	//	memcpy(&res, &_data[_readLoc], sizeof(T));
+	//	_readLoc += sizeof(T);
+
+	printf("In MemoryStream::readByte(): start\n");
+	NxU8 res = 0;
+	assert(sizeof(NxU8) == 1);
+	assert((_readLoc + 1) < _data.size());
+	memcpy(&res, &_data[_readLoc], 1);
+	_readLoc += 1;
+
+	printf("In MemoryStream::readByte(): end\n");
+	return res;
 }
 
 NxU16 MemoryStream::readWord() const
 {
-    NxU16 w = read<NxU16>();
-    return w;
+	//    NxU16 w = read<NxU16>();
+	//    return w;
+
+	NxU16 res = 0;
+	assert(sizeof(NxU16) == 2);
+	assert((_readLoc + sizeof(NxU16)) < _data.size());
+	memcpy(&res, &_data[_readLoc], sizeof(NxU16));
+	_readLoc += sizeof(NxU16);
+	return res;
 }
 
 NxU32 MemoryStream::readDword() const
 {
-    NxU32 d = read<NxU32>();
-    return d;
+	NxU32 d = read<NxU32>();
+	return d;
 }
 
-float MemoryStream::readFloat() const
+NxF32 MemoryStream::readFloat() const
 {
-    float f = read<float>();
-    return f;
+//	float f = read<float>();
+//	return f;
+
+	float res = 0;
+	assert((_readLoc + 4) < _data.size());
+	memcpy(&res, &_data[_readLoc], sizeof(4));
+	_readLoc += 4;
+	return res;
 }
 
-double MemoryStream::readDouble() const
+NxF64 MemoryStream::readDouble() const
 {
-    double f = read<double>();
-    return f;
+	NxF64 f = read<NxF64>();
+	return f;
 }
 
 void MemoryStream::readBuffer(void* buffer, NxU32 size) const
 {
-    uint8_t* bytes = (uint8_t*)buffer;
+	uint8_t* bytes = (uint8_t*)buffer;
 
-    memcpy (bytes, &_data[_readLoc], size);
-    _readLoc += size;
+	memcpy (bytes, &_data[_readLoc], size);
+	_readLoc += size;
 }
 
 // Saving API
 NxStream& MemoryStream::storeByte(NxU8 b)
 {
-    storeBuffer(&b, sizeof(b));
-    return *this;
+	storeBuffer(&b, sizeof(b));
+	return *this;
 }
 
 NxStream& MemoryStream::storeWord(NxU16 w)
 {
-    storeBuffer(&w, sizeof(w));
-    return *this;
+	storeBuffer(&w, sizeof(w));
+	return *this;
 }
 
 NxStream& MemoryStream::storeDword(NxU32 d)
 {
-    storeBuffer(&d, sizeof(d));
-    return *this;
+	storeBuffer(&d, sizeof(d));
+	return *this;
 }
 
 NxStream& MemoryStream::storeFloat(NxReal f)
 {
-    storeBuffer(&f, sizeof(f));
-    return *this;
+	storeBuffer(&f, sizeof(f));
+	return *this;
 }
 
 NxStream& MemoryStream::storeDouble(NxF64 f)
 {
-    storeBuffer(&f, sizeof(f));
-    return *this;
+	storeBuffer(&f, sizeof(f));
+	return *this;
 }
 
 NxStream& MemoryStream::storeBuffer(const void* buffer, NxU32 size)
 {
-    const unsigned int length = _data.size();
-    _data.resize(length + size);
-    memcpy(&_data[length], buffer, size);
+	const unsigned int length = _data.size();
+	_data.resize(length + size);
+	memcpy(&_data[length], buffer, size);
 
-    return *this;
+	return *this;
 }
--- SoccSim/Physics/MemoryStream.hpp
+++ SoccSim/Physics/MemoryStream.hpp
@@ -8,36 +8,36 @@
 
 class MemoryStream : public NxStream
 {
-    public:
-        MemoryStream();
+public:
+	MemoryStream();
 
-        virtual NxU8 readByte() const;
-        virtual NxU16 readWord() const;
-        virtual NxU32 readDword() const;
-        virtual float readFloat() const;
-        virtual double readDouble() const;
-        virtual void readBuffer(void* buffer, NxU32 size) const;
+	virtual NxU8 readByte() const; // FIXME: fails here
+	virtual NxU16 readWord() const;
+	virtual NxU32 readDword() const;
+	virtual NxF32 readFloat() const; // FIXME: also possible fail here
+	virtual NxF64 readDouble() const;
+	virtual void readBuffer(void* buffer, NxU32 size) const;
 
-        virtual NxStream& storeByte(NxU8 b);
-        virtual NxStream& storeWord(NxU16 w);
-        virtual NxStream& storeDword(NxU32 d);
-        virtual NxStream& storeFloat(NxReal f);
-        virtual NxStream& storeDouble(NxF64 f);
-        virtual NxStream& storeBuffer(const void* buffer, NxU32 size);
-        
-    private:
-        template <typename T>
-        T read() const
-        {
-            T res = 0;
-			assert((_readLoc + sizeof(T)) < _data.size());
-			memcpy(&res, &_data[_readLoc], sizeof(T));
-			_readLoc += sizeof(T);
-            
-            return res;
-        }
-        
-    private:
-        mutable std::vector<uint8_t> _data;
-        mutable unsigned int _readLoc;
+	virtual NxStream& storeByte(NxU8 b);
+	virtual NxStream& storeWord(NxU16 w);
+	virtual NxStream& storeDword(NxU32 d);
+	virtual NxStream& storeFloat(NxReal f); // FIXME: possible fail here
+	virtual NxStream& storeDouble(NxF64 f);
+	virtual NxStream& storeBuffer(const void* buffer, NxU32 size);
+
+private:
+	template <typename T>
+	T read() const
+	{
+		T res = 0;
+		assert((_readLoc + sizeof(T)) < _data.size());
+		memcpy(&res, &_data[_readLoc], sizeof(T));
+		_readLoc += sizeof(T);
+
+		return res;
+	}
+
+private:
+	mutable std::vector<uint8_t> _data;
+	mutable NxU32 _readLoc;
 };
