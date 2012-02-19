18ea38507b3dee306ee147ff40a44ae881af3518
eric <ehuang3@gatech.edu>
2012-02-19 16:04:36 -0500

    added option to block opp robot's shot window

--- soccer/gameplay/behaviors/positions/Fullback.hpp
+++ soccer/gameplay/behaviors/positions/Fullback.hpp
@@ -35,20 +35,30 @@ namespace Gameplay
 				Fullback(GameplayModule *gameplay, Side side = Center);
 
 				virtual bool run();
-				
+
+				/** set the robot to block the ball */
+				void blockBall(bool b){ _blockBall = b; }
+
+				/** set the robot to block an opp robot */
+				void blockRobot(OpponentRobot * robot) { _blockRobot = robot; _blockBall = false; }
+				OpponentRobot * blockRobot() const { return _blockRobot;}
+
 				void side(Side s) { _side = s; }
 				Side side() const { return _side; }
 
 				std::set<Fullback *> otherFullbacks;
 				
 				OurRobot *robot;
-				
+				OpponentRobot *_blockRobot;
+
 			protected:
 				//Window evaluator for checking passes/shots
 				Gameplay::WindowEvaluator _winEval;
 
 				Side _side;
 				State _state;
+				/** defaults to true */
+				bool _blockBall;
 		};
 	}
 }
