6c92bbf3f8b6b2b0c9ba066419e4e3939b6351b2
eric <ehuang3@gatech.edu>
2012-05-26 21:07:20 -0400

    Subclassed SimRobot -> Robot

--- simulator/physics/SimRobot.cpp
+++ simulator/physics/SimRobot.cpp
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <math.h>
 
-#define CUBE_HALF_EXTENTS 1
 
 using namespace std;
 
@@ -11,10 +10,6 @@ using namespace std;
 // static physics parameters
 static const float maxEngineForce = 3000.f; //this should be engine/velocity dependent
 static const float maxBreakingForce = 100.f;
-static const float steeringIncrement = 0.04f;
-static const float steeringClamp = 0.3f;
-static const float wheelRadius = 0.5f/2.f;
-static const float wheelWidth = 0.4f/2.f;
 static const float wheelFriction = 1000; //BT_LARGE_FLOAT;
 static const float suspensionStiffness = 1000.f;
 static const float suspensionDamping = 2.3f;
@@ -22,10 +17,44 @@ static const float suspensionCompression = 4.4f;
 static const float rollInfluence = 0.0f;
 static const btScalar suspensionRestLength = 0.5f;//0.6f
 
-static const btScalar maxVelocity = 10; //m/s? unused
 
+void SimRobot::position(float x, float y){
+	if(_carChassis)
+		return;
+	btTransform trans;
+	_carChassis->getMotionState()->getWorldTransform(trans);
+	trans.setOrigin(btVector3(y*mtodm,trans.getOrigin().y(),x*mtodm));//map field coord -> engine WS; x->z, y->x;
+	_carChassis->setCenterOfMassTransform(trans);
+	if (_vehicle) {
+		_vehicle->resetSuspension();
+		for (int i = 0; i < _vehicle->getNumWheels(); i++) {
+			//synchronize the wheels with the (interpolated) chassis worldtransform
+			_vehicle->updateWheelTransform(i, true);
+		}
+	}
+}
 
-void SimRobot::initPhysics(bool blue, const btVector3& pos) {
+void SimRobot::velocity(float x, float y, float w){ }
+
+Geometry2d::Point SimRobot::getPosition() const {
+	if(!_carChassis)
+		return _startPos;
+	btTransform trans;
+	_carChassis->getMotionState()->getWorldTransform(trans);
+	btVector3 pos = trans.getOrigin();
+	return Geometry2d::Point(pos.z(),pos.x())/mtodm;
+}
+
+float SimRobot::getAngle() const{
+	if(!_carChassis)
+		return 0;
+	btTransform trans;
+	_carChassis->getMotionState()->getWorldTransform(trans);
+	return trans.getRotation().getAngle();
+}
+
+void SimRobot::initPhysics(const bool& blue) {
+	printf("init physics\n");
 	// Assumes that Y is up
 	int rightIndex = 0;
 	int upIndex = 1;
@@ -35,7 +64,7 @@ void SimRobot::initPhysics(bool blue, const btVector3& pos) {
 
 	// Create omni-bot chassis
 	btConvexHullShape* convexShape = new btConvexHullShape();
-	int numPoints = 100;
+	int numPoints = 50;
 	float PI = 3.14159265;
 	float mrad = asin((Sim_Robot_MouthWidth/2.f)/Sim_Robot_Radius); //angle from mouth center to corner
 	float rad_incr = 2*(PI-mrad)/(float)numPoints;
@@ -49,72 +78,71 @@ void SimRobot::initPhysics(bool blue, const btVector3& pos) {
 		angle += rad_incr;
 	}
 	convexShape->initializePolyhedralFeatures();
+	_simEngine->addCollisionShape((btCollisionShape*)convexShape);
 
-	btCollisionShape* chassisShape = convexShape;
-	_simEngine->addCollisionShape(chassisShape);
-
-	// Color chassis
-	btVector3* color = new btVector3(1.f, 1.0f, 0.5f); //yellow
+	// Chassis color
+	btVector3* color = new btVector3(1.f,1.f,0.5f);
 	if(blue)
 		*color = btVector3(0.f,0.f,1.f);
-	chassisShape->setUserPointer(color);
+	convexShape->setUserPointer(color);
 
-	// Create raycast vehicle model
+	// Init spawn location of robot in engine WS
 	btTransform vehicleTr;
 	vehicleTr.setIdentity();
+	if(blue)
+		vehicleTr.setRotation(btQuaternion(btVector3(0,1,0),PI));
 	float connectionHeight = -0.1f;//Wheel connection height
-	vehicleTr.setOrigin(btVector3(0, Sim_Robot_Height/2.f-connectionHeight, 0)+pos);//spawn location in WS
+	btVector3 pos = btVector3(_startPos.y,0,_startPos.x)*mtodm;//map field coord -> engine WS; x->z, y->x; m -> dm
+	vehicleTr.setOrigin(btVector3(0, Sim_Robot_Height/2.f-connectionHeight, 0)+pos);//origin is at chassis center of mass loc by default
 
-	_carChassis = _simEngine->localCreateRigidBody(800, vehicleTr, chassisShape);
-	//_carChassis->setDamping(0.2,0.2);
-
-	_wheelShape = new btCylinderShapeX(
-			btVector3(Sim_Wheel_Width/2.f, Sim_Wheel_Radius, Sim_Wheel_Radius));
+	// Create chassis rigid body
+	_carChassis = _simEngine->localCreateRigidBody(800, vehicleTr, convexShape);
 
 	resetScene(); // force initial physics state - everything stationary
 
-	/// create vehicle
-	{
+	// Create wheel for rendering
+	_wheelShape = new btCylinderShapeX(
+			btVector3(Sim_Wheel_Width/2.f, Sim_Wheel_Radius, Sim_Wheel_Radius));
 
+	// Create raycast vehicle
+	{
 		_vehicleRayCaster = new btDefaultVehicleRaycaster(_simEngine->dynamicsWorld());
 		_vehicle = new btRaycastVehicle(_tuning, _carChassis, _vehicleRayCaster);
 
-		///never deactivate the vehicle
+		// Never deactivate the vehicle
 		_carChassis->setActivationState(DISABLE_DEACTIVATION);
 
 		_simEngine->addVehicle(_vehicle);
 
 		bool isFrontWheel = false;
 
-		//choose coordinate system
+		// Choose coordinate system
 		_vehicle->setCoordinateSystem(rightIndex, upIndex, forwardIndex);
 
-
-		///Omni-wheels
+		// Position omni-wheels // Chassis orientation corrected for automatically in addWheel()
+		btVector3 connectPoint = btVector3(0,connectionHeight,Sim_Robot_Radius-Sim_Wheel_Width/2.f);
+		btVector3 forward = btVector3(0,0,1);
 		//FL
-		btVector3 connectionPointCS0 = btVector3(0,connectionHeight,Sim_Robot_Radius-Sim_Wheel_Width/2.f).rotate(btVector3(0,1,0),3.141/4.f*1);
-		wheelAxleCS = btVector3(0,0,1).rotate(btVector3(0,1,0), 3.141/4.f*1);
+		btVector3 connectionPointCS0 = connectPoint.rotate(btVector3(0,1,0),PI/4.f*1);
+		wheelAxleCS = forward.rotate(btVector3(0,1,0), PI/4.f*1);
 		_vehicle->addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS,
-				suspensionRestLength, wheelRadius, _tuning, isFrontWheel);
-
+				suspensionRestLength, Sim_Wheel_Radius, _tuning, isFrontWheel);
 		//FR
-		connectionPointCS0 = btVector3(0,connectionHeight,Sim_Robot_Radius-Sim_Wheel_Width/2.f).rotate(btVector3(0,1,0),3.141/4.f*-1);
-		wheelAxleCS = btVector3(0,0,1).rotate(btVector3(0,1,0), 3.141/4.f*-1);
+		connectionPointCS0 = connectPoint.rotate(btVector3(0,1,0),PI/4.f*-1);
+		wheelAxleCS = forward.rotate(btVector3(0,1,0), PI/4.f*-1);
 		_vehicle->addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS,
-				suspensionRestLength, wheelRadius, _tuning, isFrontWheel);
-
+				suspensionRestLength, Sim_Wheel_Radius, _tuning, isFrontWheel);
 		//BR
-		connectionPointCS0 = btVector3(0,connectionHeight,Sim_Robot_Radius-Sim_Wheel_Width/2.f).rotate(btVector3(0,1,0),3.141/4.f*5);
-				wheelAxleCS = btVector3(0,0,1).rotate(btVector3(0,1,0), 3.141/4.f*5);
+		connectionPointCS0 = connectPoint.rotate(btVector3(0,1,0),PI/4.f*5);
+				wheelAxleCS = forward.rotate(btVector3(0,1,0), PI/4.f*5);
 		isFrontWheel = false;
 		_vehicle->addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS ,
-				suspensionRestLength, wheelRadius, _tuning, isFrontWheel);
-
+				suspensionRestLength, Sim_Wheel_Radius, _tuning, isFrontWheel);
 		//BL
-		connectionPointCS0 = btVector3(0,connectionHeight,Sim_Robot_Radius-Sim_Wheel_Width/2.f).rotate(btVector3(0,1,0),3.141/4.f*3);
-				wheelAxleCS = btVector3(0,0,1).rotate(btVector3(0,1,0), 3.141/4.f*3);
+		connectionPointCS0 = connectPoint.rotate(btVector3(0,1,0),PI/4.f*3);
+				wheelAxleCS = forward.rotate(btVector3(0,1,0), 3.141/4.f*3);
 		_vehicle->addWheel(connectionPointCS0, wheelDirectionCS0, wheelAxleCS,
-				suspensionRestLength, wheelRadius, _tuning, isFrontWheel);
+				suspensionRestLength, Sim_Wheel_Radius, _tuning, isFrontWheel);
 
 		for (int i = 0; i < _vehicle->getNumWheels(); i++) {
 			btWheelInfo& wheel = _vehicle->getWheelInfo(i);
@@ -125,6 +153,8 @@ void SimRobot::initPhysics(bool blue, const btVector3& pos) {
 			wheel.m_rollInfluence = rollInfluence;
 		}
 	}
+
+	resetScene(); // force initial physics state - everything stationary
 }
 
 void SimRobot::steerLeft() {
@@ -182,7 +212,6 @@ void SimRobot::drawWheels(GL_ShapeDrawer* shapeDrawer, const btVector3& worldBou
 
 void SimRobot::resetScene() {
 	engineForce(0);
-	_vehicleSteering = 0.f;
 	_carChassis->setCenterOfMassTransform(((btDefaultMotionState* ) _carChassis->getMotionState())->m_startWorldTrans);
 	_carChassis->setLinearVelocity(btVector3(0, 0, 0));
 	_carChassis->setAngularVelocity(btVector3(0, 0, 0));
--- simulator/physics/SimRobot.hpp
+++ simulator/physics/SimRobot.hpp
@@ -1,19 +1,14 @@
 #pragma once
 
+#include "Robot.hpp"
+
 #include <physics/SimEngine.hpp>
 #include <physics/PhysicsConstants.hpp>
 #include "GL_ShapeDrawer.h"
 
-class SimRobot {
-
-	//Wheel indices
-	static const int FL = 0;
-	static const int FR = 1;
-	static const int BR = 2;
-	static const int BL = 3;
-
+class SimRobot : public Robot
+{
 protected:
-
 	// physics components
 	btRigidBody* _carChassis;
 	btRaycastVehicle::btVehicleTuning _tuning;
@@ -22,33 +17,37 @@ protected:
 	btCollisionShape* _wheelShape;
 
 	// control inputs
-	float* _engineForce;
+	float _engineForce[4];
 	float _breakingForce;
-	float _vehicleSteering;
 
 	// links to the engine
 	SimEngine *_simEngine;
 
 public:
 
-	SimRobot(SimEngine* engine)
-	: _carChassis(0), _vehicle(0), _wheelShape(0),
-	  _simEngine(engine)
+	SimRobot(Environment* env, unsigned int id, Robot::RobotRevision rev, const Geometry2d::Point& pos, SimEngine* engine) :
+		Robot(env,id,rev,pos), _carChassis(0), _vehicle(0), _wheelShape(0), _simEngine(engine)
 	{
-		_engineForce = new float[4];
-		engineForce(0.f);//
+		engineForce(0);
 		_breakingForce = 0.f;
-		_vehicleSteering = 0.f;
 	}
 
 	~SimRobot() {
 		delete _vehicleRayCaster;
 		delete _vehicle;
 		delete _wheelShape;
-		delete _engineForce;
 	}
 
-	void initPhysics(bool blue, const btVector3& pos);
+public:
+	//Entity interface
+	virtual void position(float x, float y); // world coords
+	virtual void velocity(float x, float y, float w); // body coords
+	virtual Geometry2d::Point getPosition() const;
+	virtual float getAngle() const;
+
+public:
+
+	void initPhysics(const bool& blue);
 
 	void drawWheels(GL_ShapeDrawer* shapeDrawer, const btVector3& worldBoundsMin, const btVector3& worldBoundsMax);
 
@@ -58,20 +57,14 @@ public:
 
 	void getWorldTransform(btTransform& chassisWorldTrans) const;
 
-	//utility
-	void position(float x, float y);
-
 	// access
 	void engineForce    (int wheelIndex, float val) { _engineForce[wheelIndex] = val; }
 	void engineForce 	(float val) { for(int i=0; i<4; i++){ _engineForce[i] = val; } }
 	void breakingForce  (float val) { _breakingForce   = val; }
-	void vehicleSteering(float val) { _vehicleSteering = val; }
 
-	float* engineForce   () const { return _engineForce;     }
-	float breakingForce  () const { return _breakingForce;   }
-	float vehicleSteering() const { return _vehicleSteering; }
+	const float* engineForce   () const { return _engineForce; }
+	float breakingForce  () const { return _breakingForce; }
 	btRigidBody* carChassis() const { return _carChassis; }
-	btRaycastVehicle* vehicle() const { return _vehicle; }
 
 	// bang-bang steering and throttle
 	void steerLeft();
