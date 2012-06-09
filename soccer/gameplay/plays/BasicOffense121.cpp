7776cb032c1629da854e3a794c069a986248785e
eric <ehuang3@gatech.edu>
2012-06-09 02:10:59 -0400

    Added null check for BasicOffense121

--- soccer/gameplay/plays/BasicOffense121.cpp
+++ soccer/gameplay/plays/BasicOffense121.cpp
@@ -150,8 +150,10 @@ bool Gameplay::Plays::BasicOffense121::run()
 			shot_obs.vertices.push_back(Geometry2d::Point(Field_GoalWidth / 2, Field_Length));
 			shot_obs.vertices.push_back(Geometry2d::Point(-Field_GoalWidth / 2, Field_Length));
 			shot_obs.vertices.push_back(ballProj);
-			_support1.robot->localObstacles(ObstaclePtr(new PolygonObstacle(shot_obs)));
-			_support2.robot->localObstacles(ObstaclePtr(new PolygonObstacle(shot_obs)));
+			if(_support1.robot)
+				_support1.robot->localObstacles(ObstaclePtr(new PolygonObstacle(shot_obs)));
+			if(_support1.robot)
+				_support2.robot->localObstacles(ObstaclePtr(new PolygonObstacle(shot_obs)));
 		}
 	}
 
