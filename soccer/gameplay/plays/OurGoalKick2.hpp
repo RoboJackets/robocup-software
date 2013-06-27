9d550d03dda0f0f041ede0259d1fe044c68da2f5
Matthew Barulic <matthew.barulic@gmail.com>
2013-06-27 13:48:30 -0400

    Changes Kickoff behavior to shoot at the goal, not random. Adds OurGoalKick2. Makes OurGoalKick2 the default Goal kick.

---
+++ soccer/gameplay/plays/OurGoalKick2.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "../Play.hpp"
+
+#include <gameplay/behaviors/Kick.hpp>
+#include <gameplay/behaviors/positions/Fullback.hpp>
+#include <gameplay/behaviors/Move.hpp>
+#include <gameplay/PreventDoubleTouch.hpp>
+
+namespace Gameplay
+{
+	namespace Plays
+	{
+
+		class OurGoalKick2: public Play
+		{
+			public:
+				static void createConfiguration(Configuration *cfg);
+
+				OurGoalKick2(GameplayModule *gameplay);
+				
+				// same as free kick, but looks for direct and near our goal line
+				// 1 for good, INF otherwise
+				static float score(GameplayModule *gameplay);
+				virtual bool run();
+			
+			protected:
+				Behaviors::Kick _kicker;
+				Behaviors::Move _center1;
+				Behaviors::Move _center2;
+				Behaviors::Fullback _fullback1, _fullback2;
+				PreventDoubleTouch _pdt;
+
+				bool hasChipper;
+
+				static ConfigDouble *_downFieldRange;
+				static ConfigDouble *_minChipRange;
+				static ConfigDouble *_maxChipRange;
+				static ConfigInt *_chipper_power;
+				static ConfigInt *_kicker_power;
+
+				static ConfigDouble *_field_length_mult;
+				static ConfigDouble *_field_width_mult;
+				static ConfigDouble *_field_length_off;
+				static ConfigDouble *_field_width_off;
+		};
+	}
+}
