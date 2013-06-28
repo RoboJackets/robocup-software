662244508f1a50e5dd3ba7e6c22609aa2ff0eb66
Justin Buchanan <justbuchanan@gmail.com>
2013-06-28 02:02:19 -0400

    fixed OurGoalKic_Pass to keep running until the receiver gets the ball

--- soccer/gameplay/plays/OurCornerKick_pass.cpp
+++ soccer/gameplay/plays/OurCornerKick_pass.cpp
@@ -91,20 +91,6 @@ bool Gameplay::Plays::OurCornerKick_Pass::run()
 	if ( available.size() < requiredBotCount ) return false;
 
 
-
-	// choose a target for the kick
-	// if straight shot on goal is available, take it
-	// float goal_x = (ball().pos.x < 0) ? Field_GoalWidth / 2.0 : -Field_GoalWidth / 2.0;
-	// Segment target(Point(goal_x, Field_Length), Point(goal_x, Field_Length - *_targetSegmentWidth));
-
-
-	// if ( !_passDone ) {
-	// 	if ( _receiver1.done() || _receiver2.done() ) {
-	// 		_passDone = true;
-	// 	}
-	// }
-
-
 	//	if the pass isn't done yet, setup for the pass
 	if ( !_passCtxt.done() ) {
 		//	Geometry2d::Point FindReceivingPoint(SystemState* state, Robot* robot, Geometry2d::Point ballPos, Geometry2d::Segment receivingLine);
@@ -121,99 +107,12 @@ bool Gameplay::Plays::OurCornerKick_Pass::run()
 		}
 
 
-		
-
-		// Point passerTarget;
-
-
-		//	choose which receiver to pass to
-		// bool firstIsBetter;
-		// if ( target1Score == -1 ) {
-		// 	firstIsBetter = false;
-		// } else if ( target2Score == -1 ) {
-		// 	firstIsBetter = true;
-		// } else if ( _passingToFirstReceiver && (target2Score - target1Score) > *_receiverChoiceHysterisis ) {
-		// 	firstIsBetter = false;
-		// } else if ( !_passingToFirstReceiver && (target1Score - target2Score) > *_receiverChoiceHysterisis ) {
-		// 	firstIsBetter = true;
-		// } else {
-		// 	firstIsBetter = _passingToFirstReceiver;
-		// }
-		// _passingToFirstReceiver = firstIsBetter;
-
-
-
-		//	setup passer && receivers appropriately for the chosen point
-		// if ( firstIsBetter ) {
-		// 	passerTarget = passTarget1;
-
-		// 	_passer.partner = &_receiver1;
-		// 	_receiver1.partner = &_passer;
-
-		// 	_receiver2.partner = NULL;
-		// 	if ( _receiver2.robot ) _receiver2.robot->addText("Dummy");
-		// } else {
-		// 	passerTarget = passTarget2;
-
-		// 	_passer.partner = &_receiver2;
-		// 	_receiver2.partner = &_passer;
-
-		// 	_receiver1.partner = NULL;
-		// }
-
-
-		// _passer.actionTarget = passerTarget;
-
-		// _receiver1.actionTarget = passTarget1;
-		// _receiver2.actionTarget = passTarget2;
-
-
 		assignNearest(_passer.robot, available, ball().pos);
 		assignNearest(_receiver1.robot, available, _receiver1.actionTarget);
 		assignNearest(_receiver2.robot, available, _receiver2.actionTarget);
 
-
-		// _passer.robot->addText(QString("Using receiver %1").arg(firstIsBetter ? "L" : "R"));
-		// _passer.robot->addText(QString("pt1(%1, %2").arg(passTarget1.x).arg(passTarget1.y));
-		// _passer.robot->addText(QString("pt2(%1, %2)").arg(passTarget2.x).arg(passTarget2.y));
-
-
 		_passCtxt.run();
-
-
-		// const GameState &gs = _gameplay->state()->gameState;
-		// if ( gs.canKick() ) {
-		// 	_passer.robot->disableAvoidBall();
-		// }
-
-
-
-		// _pdt.backoff.robots.clear();
-		// _pdt.backoff.robots.insert(_passer.robot);
-		
-		//	run passing behaviors
-		// _pdt.run();	//	runs passer
-		// _receiver1.run();
-		// _receiver2.run();
-
-		// uint8_t dspeed = 60;
-		// if ( _receiver1.robot ) {
-		// 	_receiver1.robot->dribble(dspeed);
-		// }
-		// if ( _receiver2.robot ) {
-		// 	_receiver2.robot->dribble(dspeed);
-		// }
 	}
-
-
-	// //	unkick both receivers
-	// if ( _receiver1.robot ) {
-	// 	_receiver1.robot->unkick();
-	// }
-	// if ( _receiver2.robot ) {
-	// 	_receiver2.robot->unkick();
-	// }
-
 
 
 
