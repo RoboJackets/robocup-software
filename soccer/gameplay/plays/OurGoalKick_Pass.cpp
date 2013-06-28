ca0048801bb62b7e28487dcae3be2b2b79f6da7c
Justin Buchanan <justbuchanan@gmail.com>
2013-06-28 01:16:05 -0400

    added OurGoalKick_Pass play based on new passing context

---
+++ soccer/gameplay/plays/OurGoalKick_Pass.cpp
@@ -0,0 +1,105 @@
+#include "OurGoalKick_Pass.hpp"
+#include <framework/RobotConfig.hpp>
+#include <boost/foreach.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace Geometry2d;
+
+REGISTER_PLAY_CATEGORY(Gameplay::Plays::OurGoalKick_Pass, "Restarts")
+
+namespace Gameplay
+{
+	namespace Plays
+	{
+		REGISTER_CONFIGURABLE(OurGoalKick_Pass)
+	}
+}
+
+ConfigDouble *Gameplay::Plays::OurGoalKick_Pass::_downFieldRange;
+ConfigInt *Gameplay::Plays::OurGoalKick_Pass::_kicker_power;
+ConfigDouble *Gameplay::Plays::OurGoalKick_Pass::_field_length_mult;
+ConfigDouble *Gameplay::Plays::OurGoalKick_Pass::_field_width_mult;
+ConfigDouble *Gameplay::Plays::OurGoalKick_Pass::_field_length_off;
+ConfigDouble *Gameplay::Plays::OurGoalKick_Pass::_field_width_off;
+
+void Gameplay::Plays::OurGoalKick_Pass::createConfiguration(Configuration *cfg)
+{
+	_kicker_power = new ConfigInt(cfg, "OurGoalKick_Pass/Kicker Power", 255);
+	_downFieldRange = new ConfigDouble(cfg, "OurGoalKick_Pass/Downfield Range", Field_Length / 2.);
+	_field_length_mult = new ConfigDouble(cfg, "OurGoalKick_Pass/Centers Length Multiplier", 0.6);
+	_field_length_off = new ConfigDouble(cfg, "OurGoalKick_Pass/Centers Length Offset", 0.0);
+	_field_width_mult = new ConfigDouble(cfg, "OurGoalKick_Pass/Centers Width Multiplier", 0.15);
+	_field_width_off = new ConfigDouble(cfg, "OurGoalKick_Pass/Centers Width Offset", 0.0);
+}
+
+Gameplay::Plays::OurGoalKick_Pass::OurGoalKick_Pass(GameplayModule *gameplay):
+	Play(gameplay),
+	_passer(gameplay),
+	_receiver1(gameplay),
+	_receiver2(gameplay),
+	_fullback1(gameplay, Behaviors::Fullback::Left),
+	_fullback2(gameplay, Behaviors::Fullback::Right),
+	_passCtxt(_gameplay, &_passer)
+{
+	_fullback2.otherFullbacks.insert(&_fullback1);
+	_fullback1.otherFullbacks.insert(&_fullback2);
+
+	_passCtxt.addReceiver(&_receiver1);
+	_passCtxt.addReceiver(&_receiver2);
+}
+
+float Gameplay::Plays::OurGoalKick_Pass::score ( Gameplay::GameplayModule* gameplay )
+{
+	const GameState &gs = gameplay->state()->gameState;
+	Point ballPos = gameplay->state()->ball.pos;
+	return (gs.setupRestart() && gs.ourDirect() && ballPos.y < 1.5) ? 1 : INFINITY;
+}
+
+bool Gameplay::Plays::OurGoalKick_Pass::run()
+{
+	set<OurRobot *> available = _gameplay->playRobots();
+
+
+
+	if ( !_passCtxt.done() ) {
+
+		//	FIXME: assign rcv points to the receivers
+
+
+		Point rcv1Target = Point(
+				-Field_Width * _field_width_mult->value() + _field_width_off->value(),
+				Field_Length * _field_length_mult->value() + _field_length_off->value()
+				);
+
+
+		Point rcv2Target = Point(
+				Field_Width * _field_width_mult->value() + _field_width_off->value(),
+				Field_Length * _field_length_mult->value() + _field_length_off->value()
+				);
+
+
+		_passCtxt.setReceivePointForReceiver(&_receiver1, rcv1Target);
+		_passCtxt.setReceivePointForReceiver(&_receiver2, rcv2Target);
+
+
+
+
+		_passCtxt.updateReceiverScoresBasedOnChannelWidth();
+
+		assignNearest(_passer.robot, available, ball().pos);
+		assignNearest(_receiver1.robot, available, _receiver1.actionTarget);
+		assignNearest(_receiver2.robot, available, _receiver2.actionTarget);
+
+		_passCtxt.run();
+	}
+
+	assignNearest(_fullback1.robot, available, Geometry2d::Point(-Field_GoalHeight/2.0, 0.0));
+	assignNearest(_fullback2.robot, available, Geometry2d::Point( Field_GoalHeight/2.0, 0.0));
+
+
+	_fullback1.run();
+	_fullback2.run();
+	
+	return !_passCtxt.done();
+}
---
+++ soccer/gameplay/plays/OurGoalKick_Pass.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "../Play.hpp"
+
+#include <gameplay/behaviors/Kick.hpp>
+#include <gameplay/behaviors/positions/Fullback.hpp>
+#include <gameplay/behaviors/Move.hpp>
+#include <gameplay/PreventDoubleTouch.hpp>
+#include <gameplay/tactics/PassingContext.hpp>
+#include <gameplay/tactics/DumbReceive.hpp>
+#include <gameplay/tactics/StablePass.hpp>
+
+namespace Gameplay
+{
+	namespace Plays
+	{
+
+		class OurGoalKick_Pass: public Play
+		{
+			public:
+				static void createConfiguration(Configuration *cfg);
+
+				OurGoalKick_Pass(GameplayModule *gameplay);
+				
+				// same as free kick, but looks for direct and near our goal line
+				// 1 for good, INF otherwise
+				static float score(GameplayModule *gameplay);
+				virtual bool run();
+			
+			protected:
+				StablePass _passer;
+				DumbReceive _receiver1, _receiver2;
+				Behaviors::Fullback _fullback1, _fullback2;
+
+				PassingContext _passCtxt;
+
+				static ConfigDouble *_downFieldRange;
+				// static ConfigDouble *_minChipRange;
+				// static ConfigDouble *_maxChipRange;
+				// static ConfigInt *_chipper_power;
+				static ConfigInt *_kicker_power;
+
+				static ConfigDouble *_field_length_mult;
+				static ConfigDouble *_field_width_mult;
+				static ConfigDouble *_field_length_off;
+				static ConfigDouble *_field_width_off;
+		};
+	}
+}
