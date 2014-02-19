8ca3acdedc889cc9bc6b6acf41fa4857dce4d246
Justin Buchanan <justbuchanan@gmail.com>
2014-02-18 20:30:43 -0500

    added play to test some motion control ideas
    find it in test_plays/MotionControlPlay.{hpp, cpp}

---
+++ soccer/gameplay/plays/test_plays/MotionControlPlay.cpp
@@ -0,0 +1,29 @@
+
+#include "MotionControlPlay.hpp"
+
+using namespace std;
+
+REGISTER_PLAY_CATEGORY(Gameplay::Plays::MotionControlPlay, "Test")
+
+Gameplay::Plays::MotionControlPlay::MotionControlPlay(GameplayModule *gameplay):
+	Play(gameplay) {}
+
+float Gameplay::Plays::MotionControlPlay::score(GameplayModule *gameplay) {
+	return 0;
+}
+
+bool Gameplay::Plays::MotionControlPlay::run()
+{
+	//	TODO: stuff
+
+
+
+
+
+
+
+
+
+
+	return true;
+}
