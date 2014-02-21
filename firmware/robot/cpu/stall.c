cb06b98638cbea6e5533ee3bb38d7bd6b178943a
Justin Buchanan <justbuchanan@gmail.com>
2014-02-21 15:41:35 -0500

    moved and doxygenized hall-effect information

--- firmware/robot/cpu/stall.c
+++ firmware/robot/cpu/stall.c
@@ -8,19 +8,6 @@
 #include "fpga.h"
 #include "status.h"
 
-// Stall detection uses a rough estimate of the temperature of the motor windings.
-// For each motor there is a stall counter.  The counter increases with motor command
-// (representing applied voltage), decreases with speed (representing back-EMF, airflow, and
-// energy delivered to the mechanical load), and decreases at a constant rate
-// (representing loss of heat to the environment).
-//
-// When the stall counter for a motor reaches a threshold, that motor is considered to have
-// stalled until the stall flag is manually reset (or the board is reset).
-//
-// There is a possible wiring failure mode that is very important to detect:
-// If a single hall effect sensor phase is disconnected, the apparent hall state may be valid but
-// incorrect, resulting in the motor being "electrically stalled".  This will lead to the
-// motor windings burning out if speed control tries to drive the motor at high power.
 
 // Coefficient for speed
 static const int Stall_kSpeed = 80;
