fabb65451176610e9df219d35376012f051c45cc
Justin Buchanan <justbuchanan@gmail.com>
2014-02-23 13:33:29 -0500

    removed watchdog

--- watchdog/cpp/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CONFIG_H_
-#define _CONFIG_H_
-
-// Loads a configuration file.
-// The caller must hold the process list mutex when calling this.
-void config_load(const char *filename);
-
-#endif // _CONFIG_H_
