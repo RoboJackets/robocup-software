97624eb0e098a92ed08186046119c48476f1c06c
Justin Buchanan <justbuchanan@gmail.com>
2014-02-24 14:15:31 -0500

    refactored motioncontrol play
    it can now be used with arbitrary paths

--- soccer/gameplay/plays/test_plays/MotionControlPlay.cpp
+++ soccer/gameplay/plays/test_plays/MotionControlPlay.cpp
@@ -26,6 +26,57 @@ REGISTER_PLAY_CATEGORY(Gameplay::Plays::MotionControlPlay, "Test")
 Gameplay::Plays::MotionControlPlay::MotionControlPlay(GameplayModule *gameplay):
 	Play(gameplay), _pidController(1, 0, 0) {
 		testStarted = false;
+
+		path = [](float timeIntoLap, Point &targetPos, Point &targetVel) {
+			//	path geometry
+			float fudgeFactor = .15;
+			float maxX = Field_Width / 2.0 - Robot_Radius - fudgeFactor;
+			Point ptA(-maxX, 0.5);
+			Point ptB(maxX, 0.5);
+
+			float totalDist = (ptA - ptB).mag();
+
+			//	using trapezoidal velocity profile (m/s)
+			float maxSpeed = 1.0;
+			float maxAcceleration = 1.0;
+
+			//	when we're speeding up and slowing down - the sides of the trapezoid
+			float rampTime = maxSpeed / maxAcceleration;
+			float rampDist = 0.5 * maxAcceleration * powf(rampTime, 2.0);	//	Sf = 1/2*a*t^2
+
+			//	when we're going at max speed
+			float distAtMaxSpeed = (totalDist - 2.0 * rampDist);
+			float timeAtMaxSpeed = distAtMaxSpeed / maxSpeed;
+
+			float targetX;
+			float targetSpeed;
+			if (timeIntoLap < rampTime) {	//	we're speeding up
+				targetX = 0.5 * maxAcceleration * timeIntoLap * timeIntoLap;
+				targetSpeed = maxAcceleration * timeIntoLap;
+
+				// robot->addText(QString("%1").arg(timeIntoLap));
+			} else if (timeIntoLap < (rampTime + timeAtMaxSpeed)) {	//	at plateau, going max speed
+				targetX = rampDist + maxSpeed * (timeIntoLap - rampTime);
+				targetSpeed = maxSpeed;
+				// robot->addText("Plateau");
+			} else if (timeIntoLap < timeAtMaxSpeed + rampTime*2) {	//	we're slowing down
+				float deccelTime = timeIntoLap - (rampTime + timeAtMaxSpeed);
+				targetX = rampDist + distAtMaxSpeed + 
+							maxSpeed * deccelTime - 0.5 * maxAcceleration * deccelTime * deccelTime;
+				targetSpeed = maxSpeed - deccelTime * maxAcceleration;
+
+				// robot->addText("Ramp down");
+			} else {
+				//	restart for another lap
+				return false;
+			}
+			// robot->addText(QString("%1").arg(timeIntoLap));
+			//	what the robot SHOULD be doing right now at time t = @timeIntoLap
+			targetPos = Point(ptA.x + targetX, ptA.y);
+			targetVel = Point(targetSpeed, 0);
+
+			return true;
+		};
 }
 
 float Gameplay::Plays::MotionControlPlay::score(GameplayModule *gameplay) {
@@ -34,22 +85,20 @@ float Gameplay::Plays::MotionControlPlay::score(GameplayModule *gameplay) {
 
 bool Gameplay::Plays::MotionControlPlay::run()
 {
-	//	path geometry
-	float fudgeFactor = .15;
-	float maxX = Field_Width / 2.0 - Robot_Radius - fudgeFactor;
-	Point ptA(-maxX, 0.5);
-	Point ptB(maxX, 0.5);
+	//	get the start info
+	Point startPt; Point startVel;
+	path(0, startPt, startVel);
 
-	//	get a robot (closest to ptA);
+	//	get a robot (closest to startPt);
 	OurRobot *robot = nullptr;
 	set<OurRobot *> available = _gameplay->playRobots();
-	assignNearest(robot, available, ptA);
+	assignNearest(robot, available, startPt);
 	if (!robot) return true;
 
-	//	move the bot toward ptA if we're not test
+	//	move the bot toward startPt if we're not test
 	if (!testStarted) {
-		robot->move(ptA);
-		if ( (robot->pos - ptA).mag() < 0.05 ) {
+		robot->move(startPt);
+		if ( (robot->pos - startPt).mag() < 0.05 ) {
 			robot->stop();
 			testStarted = true;
 
@@ -61,54 +110,18 @@ bool Gameplay::Plays::MotionControlPlay::run()
 	}
 
 
-	float totalDist = (ptA - ptB).mag();
-
-	//	using trapezoidal velocity profile (m/s)
-	float maxSpeed = 1.0;
-	float maxAcceleration = 1.0;
-
-	//	when we're speeding up and slowing down - the sides of the trapezoid
-	float rampTime = maxSpeed / maxAcceleration;
-	float rampDist = 0.5 * maxAcceleration * powf(rampTime, 2.0);	//	Sf = 1/2*a*t^2
-
-	//	when we're going at max speed
-	float distAtMaxSpeed = (totalDist - 2.0 * rampDist);
-	float timeAtMaxSpeed = distAtMaxSpeed / maxSpeed;
-
 	//	how long we've been on this lap
 	float timeIntoLap = (float)((timestamp() - lapStartTime) / 1000000.0f);
 
-	float targetX;
-	float targetSpeed;
-	if (timeIntoLap < rampTime) {	//	we're speeding up
-		targetX = 0.5 * maxAcceleration * timeIntoLap * timeIntoLap;
-		targetSpeed = maxAcceleration * timeIntoLap;
 
-		robot->addText(QString("%1").arg(timeIntoLap));
-	} else if (timeIntoLap < (rampTime + timeAtMaxSpeed)) {	//	at plateau, going max speed
-		targetX = rampDist + maxSpeed * (timeIntoLap - rampTime);
-		targetSpeed = maxSpeed;
-		robot->addText("Plateau");
-	} else if (timeIntoLap < timeAtMaxSpeed + rampTime*2) {	//	we're slowing down
-		float deccelTime = timeIntoLap - (rampTime + timeAtMaxSpeed);
-		targetX = rampDist + distAtMaxSpeed + 
-					maxSpeed * deccelTime - 0.5 * maxAcceleration * deccelTime * deccelTime;
-		targetSpeed = maxSpeed - deccelTime * maxAcceleration;
-
-		robot->addText("Ramp down");
-	} else {
-		//	restart for another lap
+	Point targetPos, targetVel;
+	if (!path(timeIntoLap, targetPos, targetVel)) {
 		testStarted = false;
 	}
-	robot->addText(QString("%1").arg(timeIntoLap));
-	//	what the robot SHOULD be doing right now at time t = @timeIntoLap
-	Point targetPos(ptA.x + targetX, ptA.y);
-	Point targetVel(targetSpeed, 0);
-
 
 	//	draw
 	state()->drawCircle(targetPos, .04, Qt::blue);
-	state()->drawLine(ptA, ptB, Qt::blue);
+	// state()->drawLine(ptA, ptB, Qt::blue);
 
 	//	errorz
 	Point posError = targetPos - robot->pos;
@@ -120,7 +133,7 @@ bool Gameplay::Plays::MotionControlPlay::run()
 	_pidController.kd = *_pid_d;
 
 	//	controller
-	float correctedVelocity = targetSpeed + _pidController.run(velError.mag());
+	float correctedVelocity = targetVel.x + _pidController.run(velError.mag());
 	Point vel(correctedVelocity, 0);
 	//	set the robot's velocity
 	robot->worldVelocity(vel);
