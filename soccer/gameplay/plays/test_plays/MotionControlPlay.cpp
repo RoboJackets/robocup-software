d66737f5f8048c4e0f6f8b0b3ed1c356ce4f1b39
Justin Buchanan <justbuchanan@gmail.com>
2014-02-24 21:59:11 -0500

    cleanup motion control play

--- soccer/gameplay/plays/test_plays/MotionControlPlay.cpp
+++ soccer/gameplay/plays/test_plays/MotionControlPlay.cpp
@@ -149,19 +149,14 @@ bool Gameplay::Plays::MotionControlPlay::run()
 	//	how long we've been on this lap
 	float timeIntoLap = (float)((timestamp() - lapStartTime) / 1000000.0f);
 
-
+	//	query the path for where we should be
 	Point targetPos, targetVel;
 	if (!path(timeIntoLap, targetPos, targetVel)) {
 		testStarted = false;
 	}
 
-	//	draw
+	//	draw target pt
 	state()->drawCircle(targetPos, .04, Qt::blue);
-	// state()->drawLine(ptA, ptB, Qt::blue);
-
-	//	errorz
-	Point posError = targetPos - robot->pos;
-	// Point velError = targetVel - robot->vel;
 
 	//	pid config
 	_pidControllerX.kp = *_pid_p;
@@ -173,11 +168,12 @@ bool Gameplay::Plays::MotionControlPlay::run()
 	_pidControllerY.kd = *_pid_d;
 
 	//	controller
+	Point posError = targetPos - robot->pos;
 	Point correctedVelocity(
 		targetVel.x + _pidControllerX.run(posError.x),
 		targetVel.y + _pidControllerY.run(posError.y)
 		);
-		 // = targetVel + _pidController.run(velError.mag())*velError / velError.mag();
+
 	robot->worldVelocity(correctedVelocity);
 
 	return true;
