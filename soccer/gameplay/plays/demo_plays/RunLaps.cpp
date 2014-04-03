b058229161f4fb864e2b86c1867329f0d8614dfc
Justin Buchanan <justbuchanan@gmail.com>
2014-04-03 19:40:05 -0400

    finished RunLaps play

--- soccer/gameplay/plays/demo_plays/RunLaps.cpp
+++ soccer/gameplay/plays/demo_plays/RunLaps.cpp
@@ -2,12 +2,16 @@
 #include "RunLaps.hpp"
 
 using namespace std;
+using namespace Geometry2d;
 
-REGISTER_PLAY_CATEGORY(Gameplay::Plays::RunLaps, "Demo")
+
+REGISTER_PLAY_CATEGORY(Gameplay::Plays::RunLaps, "Demos")
 
 Gameplay::Plays::RunLaps::RunLaps(GameplayModule *gameplay):
 	Play(gameplay)
 {
+	_targetingLeft = true;
+	_stabilizeTimeout.setIntervalInSeconds(3);
 }
 
 float Gameplay::Plays::RunLaps::score(GameplayModule *gameplay)
@@ -17,28 +21,33 @@ float Gameplay::Plays::RunLaps::score(GameplayModule *gameplay)
 
 bool Gameplay::Plays::RunLaps::run()
 {
-	set<OurRobot *> available = _gameplay->playRobots();
+	set<OurRobot *> available = gameplay()->playRobots();
 
 	//	target point
 	float fudgeFactor = .15;
 	float maxX = Field_Width / 2.0 - Robot_Radius - fudgeFactor;
 	float x = _targetingLeft ? -maxX : maxX;
-	float y = 0.5;
+	float y = 0.8;
 	Point target(x, y);
 
 
-	OurRobot *robot;
-	assignNearest(robot, available, startPt);
+	OurRobot *robot = nullptr;
+	assignNearest(robot, available, target);
 
 	if (!robot) return false;
 
+	robot->move(target);
 
-	
+	//	swap directions if we made it there
+	if (!_arrived && (robot->pos - target).mag() < 0.02) {
+		_arrived = true;
+		_stabilizeTimeout.reset();
+	}
 
-
-
-
-
+	if (_arrived && _stabilizeTimeout.isTimedOut()) {
+		_targetingLeft = !_targetingLeft;
+		_arrived = false;
+	}
 
 	return true;
 }
