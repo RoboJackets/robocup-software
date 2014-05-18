e30738e302de90679a45da87dc08e754ae51b23d
Justin Buchanan <justbuchanan@gmail.com>
2014-05-18 15:26:17 -0400

    removed junk stuff from boost python wrapper

--- soccer/python/robocup.cpp
+++ soccer/python/robocup.cpp
@@ -3,39 +3,6 @@
 using namespace boost::python;
 
 
-class Point {
-public:
-	Point(float x = 0, float y = 0) {
-		_x = x;
-		_y = y;
-	}
-
-	float x() const {
-		return _x;
-	}
-	void set_x(float x) {
-		_x = x;
-	}
-
-	float y() const {
-		return _y;
-	}
-	void set_y(float y) {
-		_y = y;
-	}
-
-private:
-	float _x, _y;
-};
-
-
-class Robot {
-public:
-	Point pos, vel;
-	float angle, angle_vel;
-};
-
-
 BOOST_PYTHON_MODULE(robocup)
 {
 	class_<Point>("Point", init<float, float>())
@@ -50,5 +17,3 @@ BOOST_PYTHON_MODULE(robocup)
 		.def_readwrite("angle_vel", &Robot::angle_vel)
 	;
 }
-
-
