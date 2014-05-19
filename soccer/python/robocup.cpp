05cdcd0d53c38aab0db314691ecb129a9fcadea5
Justin Buchanan <justbuchanan@gmail.com>
2014-05-18 21:50:07 -0400

    started wrapping classes for python module

--- soccer/python/robocup.cpp
+++ soccer/python/robocup.cpp
@@ -1,19 +1,25 @@
-
 #include <boost/python.hpp>
 using namespace boost::python;
+
+#include <Geometry2d/Point.hpp>
+#include <Robot.hpp>
+
 
 
 BOOST_PYTHON_MODULE(robocup)
 {
-	class_<Point>("Point", init<float, float>())
-		.add_property("x", &Point::x, &Point::set_x)
-		.add_property("y", &Point::y, &Point::set_y)
+	class_<Geometry2d::Point>("Point", init<float, float>())
+		.def_readwrite("x", &Geometry2d::Point::x)
+		.def_readwrite("y", &Geometry2d::Point::y)
 	;
 
-	class_<Robot>("Robot", init<>())
+
+	class_<Robot>("Robot", init<int, bool>())
+		.def("shell_id", &Robot::shell)
+		.def("is_ours", &Robot::self)
 		.def_readwrite("pos", &Robot::pos)
 		.def_readwrite("vel", &Robot::vel)
 		.def_readwrite("angle", &Robot::angle)
-		.def_readwrite("angle_vel", &Robot::angle_vel)
+		.def_readwrite("angle_vel", &Robot::angleVel)
 	;
 }
