a698f9df095d07b818c0d833a8d540f85c636d01
Justin Buchanan <justbuchanan@gmail.com>
2014-05-20 21:54:35 -0400

    forked skill.py off of behavior.py

---
+++ soccer/gameplay2/skill.py
@@ -0,0 +1,13 @@
+from behavior import Behavior
+
+
+# a skill is a behavior that pertains to a SINGLE robot
+class Skill(Behavior):
+
+    @property
+    def robot(self):
+        return self._robot
+    @robot.setter
+    def robot(self, value):
+        self._robot = value
+    
