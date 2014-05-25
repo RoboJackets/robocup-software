394c140a76f0192d718a5b1512bfc8b5b45bd936
Justin Buchanan <justbuchanan@gmail.com>
2014-05-25 17:58:17 -0400

    improved state transitions for PivotKick

--- soccer/gameplay2/pivot_kick.py
+++ soccer/gameplay2/pivot_kick.py
@@ -16,10 +16,13 @@ class PivotKick(Behavior):
 		self.add_state(PivotKick.State.aiming, Behavior.State.running)
 		self.add_state(PivotKick.State.kicking, Behavior.State.running)
 
-		self.add_transition(Behavior.State.start, PivotKick.State.capturing, lambda: True, 'dummy')
-		self.add_transition(PivotKick.State.capturing, PivotKick.State.aiming, lambda: True, 'dummy')
-		self.add_transition(PivotKick.State.aiming, PivotKick.State.kicking, lambda: True, 'dummy')
-		self.add_transition(PivotKick.State.kicking, Behavior.State.completed, lambda: True, 'dummy')
+		self.add_transition(Behavior.State.start, PivotKick.State.capturing, lambda: True, 'immediate')
+		self.add_transition(PivotKick.State.capturing, PivotKick.State.aiming, lambda: True, 'has ball')
+		self.add_transition(PivotKick.State.aiming, PivotKick.State.kicking, lambda: True, 'aim error < threshold')
+		self.add_transition(PivotKick.State.kicking, Behavior.State.completed, lambda: True, 'kick complete')
+
+		self.add_transition(PivotKick.State.aiming, PivotKick.State.capturing, lambda: False, 'fumble')
+		self.add_transition(PivotKick.State.kicking, PivotKick.State.capturing, lambda: False, 'fumble')
 
 
 	def execute_capturing(self):
