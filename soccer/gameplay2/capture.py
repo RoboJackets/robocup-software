6b7de9242354564c68757752164c4d41e9df7a96
Justin Buchanan <justbuchanan@gmail.com>
2014-05-25 22:18:31 -0400

    started capture behavior

---
+++ soccer/gameplay2/capture.py
@@ -0,0 +1,28 @@
+from skill import *
+from enum import Enum
+
+
+class Capture(Skill):
+
+	class State(Enum):
+		course_approach = 1
+		fine_approach = 2
+
+
+	def __init__(self):
+		super().__init__(continuous=False)
+
+		self.add_state(Capture.State.course_approach, Behavior.State.running)
+		self.add_state(Capture.State.fine_approach, Behavior.State.running)
+
+		self.add_transition(Behavior.State.start, Capture.State.course_approach, lambda: True, 'immediately')
+		self.add_transition(Capture.State.course_approach, Capture.State.fine_approach,
+			lambda:
+				True,
+			'dist to ball < threshold'
+			)
+		self.add_transition(Capture.State.fine_approach, Behavior.State.completed,
+			lambda:
+				True,
+			'has ball'
+			)
--- soccer/gameplay2/test.py
+++ soccer/gameplay2/test.py
@@ -3,21 +3,20 @@ from fsm import *
 from pivot_kick import *
 from timed_behavior import *
 from behavior_sequence import *
+from capture import *
 from time import *
 
 
-
-
-for b in [PivotKick(), BehaviorSequence([PivotKick()]), TimedBehavior(PivotKick(), 2)]:
+for b in [PivotKick(), BehaviorSequence([PivotKick()]), TimedBehavior(PivotKick(), 2), Capture()]:
 	b.write_diagram_png('diagrams/' + b.__class__.__name__ + '.png')
 
 
-seq2 = BehaviorSequence([PivotKick(), PivotKick(), PivotKick()])
-bhvr = BehaviorSequence([PivotKick(), TimedBehavior(PivotKick(), 0.8), PivotKick(), seq2])
-bhvr.robot = 1
-while not bhvr.is_done_running():
-    print(str(bhvr) + "\n")
-    bhvr.run()
-    sleep(0.5) # make this value bigger or smaller to control whether the TimedBehavior above will succeed
+# seq2 = BehaviorSequence([PivotKick(), PivotKick(), PivotKick()])
+# bhvr = BehaviorSequence([PivotKick(), TimedBehavior(PivotKick(), 0.8), PivotKick(), seq2])
+# bhvr.robot = 1
+# while not bhvr.is_done_running():
+#     print(str(bhvr) + "\n")
+#     bhvr.run()
+#     sleep(0.5) # make this value bigger or smaller to control whether the TimedBehavior above will succeed
 
-print(str(bhvr) + "\n")
+# print(str(bhvr) + "\n")
