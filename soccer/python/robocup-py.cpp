bc05ac512945eb735134c29252e6c565e7131f44
Justin Buchanan <justbuchanan@gmail.com>
2014-05-26 20:08:07 -0400

    worked on boost::python wrapping of c++ classes

--- soccer/python/robocup-py.cpp
+++ soccer/python/robocup-py.cpp
@@ -4,9 +4,14 @@ using namespace boost::python;
 
 #include <Geometry2d/Point.hpp>
 #include <Robot.hpp>
+#include <SystemState.hpp>
+#include <protobuf/LogFrame.pb.h>
 
 
-
+/**
+ * The code in this block wraps up c++ classes and makes them
+ * accessible to python in the 'robocup' module.
+ */
 BOOST_PYTHON_MODULE(robocup)
 {
 	class_<Geometry2d::Point>("Point", init<float, float>())
@@ -14,6 +19,13 @@ BOOST_PYTHON_MODULE(robocup)
 		.def_readwrite("y", &Geometry2d::Point::y)
 	;
 
+	//	TODO: add the rest of GameState stuff here
+	//		I'm holding off for now because GameState needs some attention on the C++
+	//		side of things before we spread its shortcomings into the python world too...
+	class_<GameState>("GameState")
+		.def_readonly("our_score", &GameState::ourScore)
+		.def_readonly("their_score", &GameState::theirScore)
+	;
 
 	class_<Robot>("Robot", init<int, bool>())
 		.def("shell_id", &Robot::shell)
@@ -22,5 +34,29 @@ BOOST_PYTHON_MODULE(robocup)
 		.def_readwrite("vel", &Robot::vel)
 		.def_readwrite("angle", &Robot::angle)
 		.def_readwrite("angle_vel", &Robot::angleVel)
+	;
+
+	class_<OurRobot, bases<Robot> >("OurRobot", init<int, SystemState*>());
+
+	class_<OpponentRobot, bases<Robot> >("OpponentRobot", init<int>());
+
+	class_<Ball>("Ball", init<>())
+		.def_readonly("pos", &Ball::pos)
+		.def_readonly("vel", &Ball::vel)
+	;
+
+	class_<SystemState>("SystemState")
+		.def_readonly("our_robots", &SystemState::self)
+		.def_readonly("their_robots", &SystemState::opp)
+		.def_readonly("ball", &SystemState::ball)
+		.def_readonly("game_state", &SystemState::gameState)
+		.def_readonly("timestamp", &SystemState::timestamp)
+
+		//	debug drawing methods
+		.def("draw_circle", &SystemState::drawCircle)
+		.def("draw_path", &SystemState::drawPath)
+		.def("draw_text", &SystemState::drawText)
+		.def("draw_obstacle", &SystemState::drawObstacle)
+		.def("draw_obstacles", &SystemState::drawObstacles)
 	;
 }
