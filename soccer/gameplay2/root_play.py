d7a8156227a6558ba19c472f79e0a8f608b07697
Justin Buchanan <justbuchanan@gmail.com>
2014-05-26 22:57:26 -0400

    got root_play working!
    the root_play is loaded up at GameplayModule instantiation
    and its run() method is called each time the GameplayModule
    runs

---
+++ soccer/gameplay2/root_play.py
@@ -0,0 +1,23 @@
+from play import *
+from behavior import *
+
+
+# the RootPlay is basically the python-side of the c++ GameplayModule
+# it coordinates the selection of the 'actual' play and handles the goalie behavior
+class RootPlay(Play):
+
+    def __init__(self):
+        super().__init__(continuous=True)
+        self._play = None
+
+        self.add_transition(Behavior.State.start, Behavior.State.running, lambda: True, 'immediately')
+
+
+    @property
+    def play(self):
+        return self._play
+
+
+    @property
+    def goalie_behavior(self):
+        return self._goalie_behavior
