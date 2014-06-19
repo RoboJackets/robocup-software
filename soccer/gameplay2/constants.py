5465c6701d899681771d5375379825dba69e1d86
Justin Buchanan <justbuchanan@gmail.com>
2014-06-19 16:29:15 -0400

    added constants file
    this mirrors the Constants.hpp file that C++ uses
    
    we need to make sure to keep these in sync

---
+++ soccer/gameplay2/constants.py
@@ -0,0 +1,36 @@
+
+
+class Robot:
+    Radius = 0.09
+
+
+class Ball:
+    Radius = 0.0215
+    
+
+class Field:
+    Length = 6.05
+    Width = 4.05
+
+    # diameter of the center circle 
+    CenterRadius = 0.5
+
+    LineWidth = 0.01
+
+    GoalWidth = 0.700
+    GoalDepth = 0.180
+    GoalHeight = 0.160
+
+    # Distance of the penalty marker from the goal line 
+    PenaltyDist = 0.750
+    PenaltyDiam = 0.010
+
+    # flat area for defence markings 
+    GoalFlat = 0.35
+
+    # Radius of the goal arcs 
+    ArcRadius = 0.8
+
+    Border = 0.25
+    FloorLength = Length + 2.0 * Border;
+    FloorWidth = Width + 2.0 * Border;
