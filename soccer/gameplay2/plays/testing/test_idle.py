12baa4d28e92f92944643495f9c4f14ae1f7d724
Justin Buchanan <justbuchanan@gmail.com>
2014-06-25 20:51:55 -0400

    fixed up matt's circle_up tactic

--- soccer/gameplay2/plays/testing/test_idle.py
+++ soccer/gameplay2/plays/testing/test_idle.py
@@ -6,14 +6,12 @@ import main
 
 class TestIdle(play.Play):
 
-	def __init__(self):
-		super().__init__(continuous=True)
-		self.add_transition(behavior.Behavior.State.start, behavior.Behavior.State.running, lambda: True, "immediately")
+    def __init__(self):
+        super().__init__(continuous=True)
+        self.add_transition(behavior.Behavior.State.start, behavior.Behavior.State.running, lambda: True, "immediately")
 
-	def on_enter_running(self):
-		print("enter")
-		self.add_subbehavior(tactics.circle_up.CircleUp(), name='CircleUp', required=True)
+    def on_enter_running(self):
+            self.add_subbehavior(tactics.circle_up.CircleUp(), name='CircleUp', required=True)
 
-	def on_exit_running(self):
-		print("exit")
-		self.remove_subbehavior('CircleUp')
+    def on_exit_running(self):
+        self.remove_subbehavior('CircleUp')
