75a0b559170064041aef67eabb6a01c01c2e57b1
Justin Buchanan <justbuchanan@gmail.com>
2014-06-29 16:01:58 -0400

    wrote Stopped play

--- soccer/gameplay2/plays/stopped.py
+++ soccer/gameplay2/plays/stopped.py
@@ -1,5 +1,7 @@
 import play
 import behavior
+import tactics
+
 
 # when we get the Stopped command from the referee,
 # we run this play.  See the rules to see what we're allowed to do while the game is stopped
@@ -10,3 +12,23 @@ class Stopped(play.Play):
             behavior.Behavior.State.running,
             lambda: True,
             'immediately')
+
+
+    def score(self):
+        return 0 if main.game_state().is_stopped() else float("inf")
+
+
+    def on_enter_running(self):
+        left = tactics.positions.fullback.Fullback()
+        self.add_subbehavior(left, 'left_fullback', required=False, priority=50)
+
+        right = tactics.positions.fullback.Fullback()
+        self.add_subbehavior(right, 'right_fullback', required=False, priority=49)
+
+        idle = tactics.circle_near_ball.CircleNearBall()
+        self.add_subbehavior('circle_up', idle, required=False, priority=1)
+
+
+    def on_exit_running(self):
+        for name in ['circle_up', 'right_fullback', 'left_fullback']:
+            self.remove_subbehavior(name)
