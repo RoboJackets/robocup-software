d5add921aa91328a34263fb08114dc881a44880d
Justin Buchanan <justbuchanan@gmail.com>
2014-06-29 14:30:13 -0400

    wrote stress test play

---
+++ soccer/gameplay2/plays/stress_test.py
@@ -0,0 +1,40 @@
+import play
+import skills
+import robocup
+import behavior
+import constants
+
+
+# Makes a robot continually run laps
+# note: only really works with one robot on the field at a time
+class StressTest(play.Play):
+
+    def __init__(self):
+        super().__init__(continuous=True)
+
+        self.points = [robocup.Point(constants.Field.Width * 1.0/3.0, constants.Field.Length / 6.0),
+            robocup.Point(-constants.Field.Width * 1.0/3.0, constants.Field.Length * 2.0/6.0),
+            robocup.Point(constants.Field.Width * 1.0/3.0, constants.Field.Length * 2.0/6.0),
+            robocup.Point(-constants.Field.Width * 1.0/3.0, constants.Field.Length / 6.0)
+        ]
+        self.index = 0
+
+        self.add_transition(behavior.Behavior.State.start,
+            behavior.Behavior.State.running,
+            lambda: True,
+            'immediately')
+
+
+    def on_enter_running(self):
+        m = skills.move.Move()
+        self.add_subbehavior(m, 'move', required=True)
+    def on_exit_running(self):
+        self.remove_subbehavior('move')
+
+
+    def execute_running(self):
+        m = self.subbehavior_with_name('move')
+        if m.state == behavior.Behavior.State.completed:
+            # increment index
+            self.index = (self.index + 1) % len(self.points)
+        m.pos = self.points[self.index]
