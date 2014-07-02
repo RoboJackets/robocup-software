08c6dd6614b49885593bc92b79ba4431a945ee7d
Justin Buchanan <justbuchanan@gmail.com>
2014-07-02 02:33:25 -0400

    more work on Aim

--- soccer/gameplay2/skills/aim.py
+++ soccer/gameplay2/skills/aim.py
@@ -27,5 +27,38 @@ class Aim(single_robot_behavior.SingleRobotBehavior):
             lambda: True,
             'immediately')
 
+        for state in Aim.State:
+            self.add_transition(state,
+                behavior.Behavior.State.failed,
+                lambda: not self.robot.has_ball(),
+                'fumble')
 
-    # TODO: port the functionality from PivotKick
+        self.add_transition(Aim.State.aiming,
+            Aim.State.aimed,
+            lambda: self.is_aimed(),
+            'error < threshold and not rotating too fast')
+
+        self.add_transition(Aim.State.aimed,
+            Aim.State.aiming,
+            lambda: not self.is_aimed(),
+            'error > threshold or rotating too fast')
+
+
+    # returns True if we're aimed at our target within our error thresholds and we're not rotating too fast
+    def is_aimed(self):
+        raise NotImplementedError()
+
+
+    # we're aiming at a particular point on our target segment, what is this target?
+    def aim_target_point(self):
+        raise NotImplementedError()
+
+
+    def execute_aiming(self):
+        # TODO: port the functionality from PivotKick
+        raise NotImplementedError()
+
+
+    def execute_aimed(self):
+        self.robot.face(self.aim_target_point())
+
