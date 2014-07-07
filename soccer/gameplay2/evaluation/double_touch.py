82485a3e62979769c1f972f2c3b6988d8cdd7799
Justin Buchanan <justbuchanan@gmail.com>
2014-07-07 01:47:49 -0400

    a little more double-touch work

--- soccer/gameplay2/evaluation/double_touch.py
+++ soccer/gameplay2/evaluation/double_touch.py
@@ -2,7 +2,13 @@ import evaluation.ball
 
 
 # This module implements our observance of the double-touch rule
-# At each iteration, you must call `update()` to do tracking of the rul
+#
+# From the official rules (as of 2013):
+#   "If, after the ball enters play other than due to a forced restart, the kicker touches the ball a second
+#    time (without holding the ball) before it has touched another robot:
+#      an indirect free kick is awarded to the opposing team, the kick to be taken from the place
+#      where the infringement occurred (see Law 13)"
+#
 
 
 _forbidden_ball_toucher = None
@@ -10,10 +16,16 @@ _forbidden_ball_toucher = None
 
 def reset():
     global _forbidden_ball_toucher
+    # TODO: reset other tracking state
     _forbidden_ball_toucher = None
 
 
+# At each iteration, you must call `update()` to do tracking of the rule
 def update():
+    # for bot in main.our_robots():
+    #     if bot.just_kicked():
+    #         _forbidden_ball_toucher = bot.shell_id()
+
     global _forbidden_ball_toucher
     raise NotImplementedError()
 
