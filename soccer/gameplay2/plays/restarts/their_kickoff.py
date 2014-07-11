e6c98837c5479745a96c83889f222730204d48cf
Justin Buchanan <justbuchanan@gmail.com>
2014-07-10 22:18:25 -0400

    ported TheirKickoff to python

---
+++ soccer/gameplay2/plays/restarts/their_kickoff.py
@@ -0,0 +1,31 @@
+import play
+import behavior
+import robocup
+import tactics.positions.fullback
+import tactics.circle_near_ball
+
+
+class TheirKickoff(play.Play):
+
+    def __init__(self):
+        super().__init__(continuous=True)
+
+        self.add_trantition(behavior.Behavior.State.start,
+            behavior.Behavior.State.running,
+            lambda: True,
+            'immediately')
+
+
+        fullback1 = tactics.positions.fullback.Fullback(tactics.positions.fullback.Fullback.Side.left)
+        self.add_subbehavior(fullback1, 'fullback1', priority=2)
+
+        fullback2 = tactics.positions.fullback.Fullback(tactics.positions.fullback.Fullback.Side.right)
+        self.add_subbehavior(fullback2, 'fullback2', priority=1)
+
+        circle_up = tactics.circle_near_ball.CircleNearBall()
+        self.add_subbehavior(circle_up, 'circle_up')
+
+
+    def score(self):
+        gs = main.game_state()
+        return 0 if gs.is_setup_state() and gs.is_their_kickoff()
