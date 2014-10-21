19fe6896233f44f563394676dfc679efb1ee15be
Justin Buchanan <justbuchanan@gmail.com>
2014-10-21 15:38:58 -0400

    Revert "removed simulator.  #105"
    This reverts commit dc4b8d1c62ec70f6ed0074a94191d5ad67ca467f.
    
    Conflicts:
    	SConstruct

---
+++ simulator/physics/Entity.cpp
@@ -0,0 +1,22 @@
+#include <btBulletCollisionCommon.h>
+#include <btBulletDynamicsCommon.h>
+
+#include "Entity.hpp"
+#include "Environment.hpp"
+
+Entity::Entity(Environment* env) :
+    _env(env)
+{
+    
+}
+
+Entity::~Entity()
+{
+}
+
+Geometry2d::Point Entity::getPosition() const
+{
+	Geometry2d::Point result;
+	// placeholder
+	return result;
+}
