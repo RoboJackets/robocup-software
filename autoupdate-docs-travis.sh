92893e0f1503e9ff24cee5202a941398f722bd9d
Justin Buchanan <justbuchanan@gmail.com>
2014-10-24 09:49:27 -0400

    build doxygen from source on travis-ci.  closes #129

--- autoupdate-docs-travis.sh
+++ autoupdate-docs-travis.sh
@@ -5,9 +5,18 @@ set -e
 # This script is called by .travis.yml to update our doxygen documentation, which is stored on the gh-pages branch of this repo
 # It's based on this blog post: http://philipuren.com/serendipity/index.php?/archives/21-Using-Travis-to-automatically-publish-documentation.html
 
+
+
 if [ "$(git rev-parse master)" = "$TRAVIS_COMMIT" ]; then
-    sudo apt-get install doxygen
+    # build doxygen from source so we can have the latest and greatest
+    sudo apt-get install flex bison 
+    git clone https://github.com/doxygen/doxygen.git
+    cd doxygen
+    ./configure
+    make
+    sudo make install
     doxygen --version
+
     git config --global user.name "$GIT_USERNAME"
     git config --global user.email $GIT_EMAIL
     git clone -b gh-pages git://github.com/robojackets/robocup-software api_docs
