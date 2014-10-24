4b38e5d72b814c8eb8bbf82df0a6a6063161c198
Justin Buchanan <justbuchanan@gmail.com>
2014-10-23 22:45:44 -0400

    documentation site auto-updates when pushing to master branch. #127

---
+++ autoupdate-docs-travis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# This script is called by .travis.yml to update our doxygen documentation, which is stored on the gh-pages branch of this repo
+# It's based on this blog post: http://philipuren.com/serendipity/index.php?/archives/21-Using-Travis-to-automatically-publish-documentation.html
+
+git config --global user.name "$GIT_USERNAME"
+git config --global user.email $GIT_EMAIL
+git clone -b gh-pages git://github.com/robojackets/robocup-software api_docs
+doxygen
+cp doxygen.css api_docs/
+cd api_docs
+git add --all
+git commit -m 'auto-updated api docs'
+git push https://$GH_TOKEN@github.com/robojackets/robocup-software gh-pages
