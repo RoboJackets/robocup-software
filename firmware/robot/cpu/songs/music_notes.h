91d18d72bfa3d1992431bef4bb322407adf36d57
jgkamat <jaygkamat@gmail.com>
2014-11-08 16:29:16 -0500

    Fixed garble in "still alive" and add it to documentation.

--- firmware/robot/cpu/songs/music_notes.h
+++ firmware/robot/cpu/songs/music_notes.h
@@ -2,26 +2,35 @@
 #define MUSIC_NOTES_H
 
 // constants for music notes and their frequencies
-const int A4 = 440
-const int As4 = 466
-const int Bf4 = 466
-const int B4 = 494
-const int C5 = 523
-const int Cs5 = 554
-const int Df5 = 554
-const int D5 = 587
-const int Ds5 = 622
-const int Ef5 = 622
-const int E5 = 659
-const int F5 = 698
-const int Fs5 = 740
-const int Gf5 = 740
-const int G5 = 784
-const int Gs5 = 830
-const int Af5 = 830
-const int A5 = 880
-const int As5 = 932
-const int Bf5 = 932
-const int B5 = 988
+
+#define C4 227
+#define D4 294
+#define E4 329
+#define F4 349
+#define G4 392
+#define A4 440
+#define As4 466
+#define Bf4 466
+#define B4 494
+#define C5 523
+#define Cs5 554
+#define Df5 554
+#define D5 587
+#define Ds5 622
+#define Ef5 622
+#define E5 659
+#define F5 698
+#define Fs5 740
+#define Gf5 740
+#define G5 784
+#define Gs5 830
+#define Af5 830
+#define A5 880
+#define As5 932
+#define Bf5 932
+#define B5 988
+#define C6 1047
+
+#define CLEAR 2000
 
 #endif
