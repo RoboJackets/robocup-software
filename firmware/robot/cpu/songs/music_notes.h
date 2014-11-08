4fabbfabdc5a4c8e212fd3eadac4ebec29775332
KingTing25 <joshting25@gmail.com>
2014-11-07 19:43:01 -0500

    Adds music note constants

---
+++ firmware/robot/cpu/songs/music_notes.h
@@ -0,0 +1,27 @@
+#ifndef MUSIC_NOTES_H
+#define MUSIC_NOTES_H
+
+// constants for music notes and their frequencies
+const int A4 = 440
+const int As4 = 466
+const int Bf4 = 466
+const int B4 = 494
+const int C5 = 523
+const int Cs5 = 554
+const int Df5 = 554
+const int D5 = 587
+const int Ds5 = 622
+const int Ef5 = 622
+const int E5 = 659
+const int F5 = 698
+const int Fs5 = 740
+const int Gf5 = 740
+const int G5 = 784
+const int Gs5 = 830
+const int Af5 = 830
+const int A5 = 880
+const int As5 = 932
+const int Bf5 = 932
+const int B5 = 988
+
+#endif
