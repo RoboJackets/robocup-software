3d4ff29ae046069880f6d3a748b295d3e5d1b115
jgkamat <jaygkamat@gmail.com>
2014-12-02 20:45:46 -0500

    Added multiple mbed support and conform to style. Fixes issue #156

--- util/robot2015-prog.sh
+++ util/robot2015-prog.sh
@@ -2,14 +2,19 @@
 
 # This script takes the argument as a filename and copies it to all mbeds attached!
 
+# this causes the script to fail if any command below fails
+set -e
+
+# checks for file existence
 if [ ! -e "$1" ]; then
-	echo "File $1 not found..."
-	exit 1
+    echo "File $1 not found..."
+    exit 1
 fi
 
+# checks to see if the device files used are present
 if [ ! -e "/dev/disk/by-id" ]; then
-	echo "Your linux distro is not supported =("
-	exit 2
+    echo "Your linux distro is not supported =("
+    exit 2
 fi
 
 MBED_DEVICES="$(ls /dev/disk/by-id/ | grep mbed)"
@@ -17,19 +22,24 @@ MBED_DEVICES="$(ls /dev/disk/by-id/ | grep mbed)"
 # This prepends the directory structure to all found mbed devices
 MBED_DEVICES_PATH="$(ls /dev/disk/by-id/ | grep mbed | sed 's\.*\/dev/disk/by-id/&\g')"
 
+# errors out if no mbed devices were found
 if [ -z $MBED_DEVICES ]; then
-	echo "No mbed device detected!"
-	exit 3
+    echo "No mbed device detected!"
+    exit 3
 fi
 
-echo -e Attempting to mount $MBED_DEVICES
 echo -e Devices path is $MBED_DEVICES_PATH
 
+# use newlines as delimiters in the forloop, rather than all whitespace
+IFS=$'\n'
 
-sudo mkdir -p /mnt/script/MBED
-sudo mount $MBED_DEVICES_PATH /mnt/script/MBED
-sudo cp $1 /mnt/script/MBED/
-sudo umount /mnt/script/MBED/
-sudo rmdir /mnt/script/MBED
+# loop through all mbed devices, mount them, copy over the bin file, and unmount them
+for i in $MBED_DEVICES_PATH; do
+    echo Installing on $i
+    sudo mkdir -p /mnt/script/MBED
+    sudo mount $i /mnt/script/MBED
+    sudo cp $1 /mnt/script/MBED/
+    sudo umount /mnt/script/MBED/
+    sudo rmdir /mnt/script/MBED
+done
 
-# echo  $(ls /dev/disk/by-id/ | grep mbed)
