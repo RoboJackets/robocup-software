d7a89dc258fd4776f6e1bcc960226dc5b4afc33d
Matthew Barulic <matthew.barulic@gmail.com>
2014-12-15 09:23:34 -0500

    Makes processor update its world->team transform when the MainWindow changes the field size. Issue #163

--- soccer/MainWindow.cpp
+++ soccer/MainWindow.cpp
@@ -944,6 +944,7 @@ void MainWindow::on_actionVisionPrimary_Half_triggered()
 {
 	_processor->changeVisionChannel(SharedVisionPortSinglePrimary);
 	Field_Dimensions::Current_Dimensions = Field_Dimensions::Single_Field_Dimensions;
+	_processor->recalculateWorldToTeamTransform();
 	_ui.actionVisionPrimary_Half->setChecked(true);
 	_ui.actionVisionSecondary_Half->setChecked(false);
 	_ui.actionVisionFull_Field->setChecked(false);
@@ -953,6 +954,7 @@ void MainWindow::on_actionVisionSecondary_Half_triggered()
 {
 	_processor->changeVisionChannel(SharedVisionPortSingleSecondary);
 	Field_Dimensions::Current_Dimensions = Field_Dimensions::Single_Field_Dimensions;
+	_processor->recalculateWorldToTeamTransform();
 	_ui.actionVisionPrimary_Half->setChecked(false);
 	_ui.actionVisionSecondary_Half->setChecked(true);
 	_ui.actionVisionFull_Field->setChecked(false);
@@ -962,6 +964,7 @@ void MainWindow::on_actionVisionFull_Field_triggered()
 {
 	_processor->changeVisionChannel(SharedVisionPortDoubleOld);
 	Field_Dimensions::Current_Dimensions = Field_Dimensions::Double_Field_Dimensions;
+	_processor->recalculateWorldToTeamTransform();
 	_ui.actionVisionPrimary_Half->setChecked(false);
 	_ui.actionVisionSecondary_Half->setChecked(false);
 	_ui.actionVisionFull_Field->setChecked(true);
