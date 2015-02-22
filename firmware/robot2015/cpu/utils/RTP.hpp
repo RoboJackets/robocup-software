a95e069f765d68c73376d60173117fbe8ad85c28
Will Stuckey <guyfleeman@gmail.com>
2015-02-22 15:01:46 -0500

    fixed #213

--- firmware/robot2015/cpu/utils/RTP.hpp
+++ firmware/robot2015/cpu/utils/RTP.hpp
@@ -7,15 +7,21 @@
 
 typedef struct _RTP_t {
     uint8_t total_size;
-    union { // [128 Bytes]
+    union 
+    { // [128 Bytes]
         uint8_t raw[RTP_MAX_DATA_SIZE + 6]; // [128 Bytes] 1.1
-        struct {    // [128 Bytes] 1.2
-            union { // [4 Bytes]
+        struct 
+	{    // [128 Bytes] 1.2
+            union 
+	    { // [4 Bytes]
                 uint8_t full_header[4]; // [4 Bytes] 2.1
-                struct {    // 2.2
-                    union { // [3 Bytes]
+                struct 
+		{    // 2.2
+                    union 
+		    { // [3 Bytes]
                         uint8_t header[3];  // 3.1
-                        struct {    // 3.2
+                        struct 
+			{    // 3.2
                             uint8_t payload_size;
                             uint8_t address;    // THIS MAY CHANGE
                             uint8_t sfs : 1, ack : 1, subclass : 2, port : 4;
