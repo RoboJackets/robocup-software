75b49b5baee6595787695d64ccefbaa76d131e96
Jeremy Feltracco <jpfeltracco@gatech.edu>
2015-03-01 13:08:32 -0500

    Added documentation to expander initialization.

--- firmware/robot2015/cpu/IOExpander.cpp
+++ firmware/robot2015/cpu/IOExpander.cpp
@@ -1,2 +1,7 @@
 #include "IOExpander.h"
+/*
+ * Creates a new IO Exapnder object on I2C pins 9 and 10.
+ * 0x40 refers to the MCP base address. To have more, 
+ * use 0x40, 0x42, 0x44 etc.
+ */
 MCP23017 *expander = new MCP23017(p9, p10, 0x40);
