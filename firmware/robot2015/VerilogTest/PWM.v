ecf463ed0e170c5b826eeb41413262c834457d73
Doho <ChrisDoho@gmail.com>
2015-03-10 22:37:30 -0400

    Verilog files to drive one phase of BLDC

---
+++ firmware/robot2015/VerilogTest/PWM.v
@@ -0,0 +1,18 @@
+`include "phase_driver.vh"
+
+module PWM(clock, duty_cycle, pwm);
+
+	input clock;
+	input [`DUTY_CYCLE_WIDTH - 1:0] duty_cycle;
+	output pwm;
+	reg pwm = 0;
+
+reg [`COUNTER_WIDTH - 1:0] counter = 0;
+
+always @ (posedge clock)
+begin
+	counter = counter + 1;
+	if (counter >= `MAX_COUNTER) counter = 0;
+	pwm = (counter < duty_cycle*`DUTY_CYCLE_STEP_RES) ? 1 : 0; 
+end
+endmodule
---
+++ firmware/robot2015/VerilogTest/pwm_tb.v
@@ -0,0 +1,48 @@
+//`timescale 1ns/100ps //timescale / operation precision /**Borked
+
+`include "phase_driver.vh"
+
+module pwm_tb;
+
+/*Output values to 'test.vcd' file */
+initial
+ begin
+    $dumpfile("test.vcd");
+    $dumpvars(0,pwm_tb);
+ end
+
+/*inputs to DUT*/
+ reg [`DUTY_CYCLE_WIDTH - 1:0] duty_cycle;
+ reg clock;
+ reg high_z = 0;
+
+/*outputs from DUT*/
+ wire pwmH, pwmL, low_mod;
+
+
+
+initial begin
+	
+	duty_cycle = 8'h10; //dutycycle
+	clock = 0;
+
+end
+
+
+
+
+
+ always begin
+ 	 #1 clock = !clock;
+ end
+ always begin
+ 	 #50000 high_z = 1;
+ 	 #100000 $finish;
+ end
+/* instantiate PWM high and low */
+
+PWM_LOW pwm_low_driver (clock, duty_cycle, low_mod);
+
+Phase_Driver phasedriver(clock, duty_cycle, high_z, pmwH, pwmL);
+
+ endmodule
