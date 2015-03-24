a2ecb206abf7d7483c4e6cd20409186afd246980
Justin Buchanan <justbuchanan@gmail.com>
2015-03-24 15:54:24 -0400

    added simple visualizer for robot trajectory

--- modeling/justin/robocup.m
+++ modeling/justin/robocup.m
@@ -13,6 +13,44 @@
 
 
 
+ % Mass of bot (Kg)
+M_bot = 1;
+
+% Moment of inertia of bot about the z (vertical) axis throug the center of mass TODO: units
+I_bot = 1;
+
+% Gear ratio of motor to wheel.  w_motor * g = w_wheel
+g = 2.0 / 7.0;
+
+% Radius of omni-wheels (meters)
+r = 0.03;
+
+% Distance from the center of wheel to the center of the robot (meters)
+L = 0.1;
+
+% Resistance of motor phase (ohms)
+R = 1;
+
+% Back-emf constant of motor (TODO: units?)
+K_e = 1;
+
+% Torque constant of motor (TODO: units?)
+K_t = 1;
+
+% Viscous friction coefficient of wheel assembly.  Tau_friction =w_motor * K_f TODO: units?
+K_f = 1;
+
+% Moment of inertia of wheel assembly.  Tau_accel = I_asm*w_dot_motor
+I_asm = 1;
+
+% Max voltage applied to the motor phases (Volts)
+V = 1;
+
+% Angle of each wheel axis relative to the +x axis (radians)
+wheel_angles = (pi/4 + (pi/2)*0:3)';
+
+
+
 %% Hard Limits (for safety)
 
 limits.max_accel = 3;           % m/s^2
