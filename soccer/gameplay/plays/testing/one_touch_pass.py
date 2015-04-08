1df7362da3edf698928082f6379eaf2c83a4e87a
Jay Kamat <github@jgkamat.33mail.com>
2015-04-07 20:21:12 -0400

    Switched onetouchpass to use the state machine

--- soccer/gameplay/plays/testing/one_touch_pass.py
+++ soccer/gameplay/plays/testing/one_touch_pass.py
@@ -7,6 +7,7 @@ import tactics.behavior_sequence
 import robocup
 import constants
 import main
+from enum import Enum
 
 
 ## Continually runs a coordinated pass to opposite sides of the field
@@ -15,45 +16,51 @@ class OneTouchPass(play.Play):
     ReceiveXCoord = 1
     ReceiveYCoord = constants.Field.Length * 5.0 / 6.0
 
+    class State(Enum):
+        shooting = 1   # Shooting into the goal
+
     def __init__(self):
         super().__init__(continuous=False)
 
+        for state in OneTouchPass.State:
+            self.add_state(state, behavior.Behavior.State.running)
+
         self.add_transition(behavior.Behavior.State.start,
-            behavior.Behavior.State.running,
-            lambda: True,
-            'immediately')
+                behavior.Behavior.State.running,
+                lambda: True,
+                'immediately')
 
+        self.add_transition(behavior.Behavior.State.running,
+                OneTouchPass.State.shooting,
+                lambda: self.subbehavior_with_name('pass').state == behavior.Behavior.State.completed,
+                'preparing to shoot')
 
-        pass_bhvr = tactics.coordinated_pass.CoordinatedPass()
-        self.add_subbehavior(pass_bhvr, 'pass')
-
-
-
+        self.add_transition(OneTouchPass.State.shooting,
+                behavior.Behavior.State.completed,
+                lambda: self.subbehavior_with_name('kick').state == behavior.Behavior.State.completed,
+                'Shooter shot')
 
     def reset_receive_point(self):
         pass_bhvr = self.subbehavior_with_name('pass')
         pass_bhvr.receive_point = robocup.Point(OneTouchPass.ReceiveXCoord, OneTouchPass.ReceiveYCoord)
 
-
-    def execute_running(self):
-        pass_bhvr = self.subbehavior_with_name('pass')
-
-
+    def on_enter_running(self):
+        pass_bhvr = tactics.coordinated_pass.CoordinatedPass()
+        self.add_subbehavior(pass_bhvr, 'pass')
         if pass_bhvr.receive_point == None:
             self.reset_receive_point()
 
-        if self.has_subbehavior_with_name('kick'):
-            kick = self.subbehavior_with_name('kick')
-            # TODO, if this isnt needed, merge with lower if
 
-        elif pass_bhvr.is_done_running():
-            kick = skills.pivot_kick.PivotKick()
-            kick.target = constants.Field.TheirGoalSegment
-            kick.aim_params['desperate_timeout'] = 3
-            self.add_subbehavior(kick, 'kick', required=True)
+    def on_enter_shooting(self):
+        kick = skills.pivot_kick.PivotKick()
+        self.add_subbehavior(kick, 'kick', required=True)
+        kick = self.subbehavior_with_name('kick')
+        kick.target = constants.Field.TheirGoalSegment
+        kick.aim_params['desperate_timeout'] = 3
 
     def on_exit_running(self):
-        self.remove_all_subbehaviors()
-        # TODO This restarts play if another robot is asigned the ball. Make it exit either way.
+        self.remove_subbehavior('pass')
 
+    def on_exit_shooting(self):
+        self.remove_subbehavior('kick')
 
