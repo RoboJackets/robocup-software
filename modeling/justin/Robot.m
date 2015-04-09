534f895b7ff6d8db45dcd3cb0431a30a738b0b72
Justin Buchanan <justbuchanan@gmail.com>
2015-04-08 23:30:44 -0400

    Model works!

--- modeling/justin/Robot.m
+++ modeling/justin/Robot.m
@@ -14,93 +14,16 @@ classdef Robot < matlab.System & matlab.system.mixin.CustomIcon & matlab.system.
     end
     
     
-    properties
-        % Mass of bot (Kg)
-        M_bot = 1;
-
-        % Moment of inertia of bot about the z (vertical) axis throug the center of mass TODO: units
-        I_bot = 1;
-
-        % Gear ratio of motor to wheel.  w_motor * g = w_wheel
-        g = 2.0 / 7.0;
-
-        % Radius of omni-wheels (meters)
-        r = 0.03;
-
-        % Distance from the center of wheel to the center of the robot (meters)
-        L = 0.1;
-
-        % Resistance of motor phase (ohms)
-        R = 1;
-
-        % Back-emf constant of motor (TODO: units?)
-        K_e = 1;
-
-        % Torque constant of motor (TODO: units?)
-        K_t = 1;
-
-        % Viscous friction coefficient of wheel assembly.  Tau_friction =w_motor * K_f TODO: units?
-        K_f = 1;
-
-        % Moment of inertia of wheel assembly.  Tau_accel = I_asm*w_dot_motor
-        I_asm = 1;
-
-        % Max voltage applied to the motor phases (Volts)
-        V = 1;
-
-        % Angle of each wheel axis relative to the +x axis (radians)
-        wheel_angles = (pi/4 + (pi/2)*(0:3))';
-    end
-    
-    
-
-    properties (Dependent = true)
-        J % inertial matrix
-        G % robot geometry matrix.  maps wheel velocities/forces to robot velocities/forces
-        
-        % matrices for final nonlinear state equation
-        % x_dot = A_1*x + A_2*dphidt*x + B*u
-        A_1
-        A_2
-        B
-    end
-    
-    methods
-        function J = get.J(obj)
-            J = [obj.M_bot 0 0;
-                0 obj.M_bot 0;
-                0 0 obj.I_bot];
-        end
-        
-        function G = get.G(obj)
-            G = [-sin(obj.wheel_angles(1)), -sin(obj.wheel_angles(2)), -sin(obj.wheel_angles(3)), -sin(obj.wheel_angles(4));
-                  cos(obj.wheel_angles(1)),  cos(obj.wheel_angles(2)),  cos(obj.wheel_angles(3)),  cos(obj.wheel_angles(4));
-                                   1/obj.L,                   1/obj.L,                   1/obj.L,                   1/obj.L];
-        end
-        
-        function A_1 = get.A_1(obj)
-            A_1 = - (obj.K_e*obj.K_t/obj.R + obj.K_f) / (obj.M_bot*obj.g^2*obj.r^2 + obj.I_asm) * eye(3);
-        end
-        
-        function A_2 = get.A_2(obj)
-            A_2 = - (obj.M_bot*obj.g^2*obj.r^2)/(obj.M_bot*obj.g^2*obj.r^2 + obj.I_asm) * [0, 1, 0; -1, 0, 0; 0, 0, 0];
-        end
-        
-        function B = get.B(obj)
-            B = obj.g*obj.r*obj.K_t*obj.G / obj.R / (obj.M_bot*obj.g^2*obj.r^2 + obj.I_asm);
-        end
-    end
-    
-    
-    
-    
     methods (Access = protected)
 
         % indicate that the output doesn't depend on the input directly,
         % only on the current state
-        function [flag1, flag2] = isInputDirectFeedthroughImpl(~, ~, ~)
+        function [flag1, flag2, flag3, flag4, flag5] = isInputDirectFeedthroughImpl(~, ~, ~, ~, ~, ~)
             flag1 = false;
             flag2 = false;
+            flag3 = false;
+            flag4 = false;
+            flag5 = false;
         end
         
         
@@ -112,7 +35,7 @@ classdef Robot < matlab.System & matlab.system.mixin.CustomIcon & matlab.system.
         % Specify the number of inputs to the step() method.
         % inputs: u, dt
         function num = getNumInputsImpl(~)
-            num = 2;
+            num = 5;
         end
         
         % outputs: X_g, X_b_dot
@@ -139,15 +62,15 @@ classdef Robot < matlab.System & matlab.system.mixin.CustomIcon & matlab.system.
         end
         
         
-        function [X_g, X_b_dot] = outputImpl(obj, ~, ~)
-           X_g = obj.X_g;
-           X_b_dot = obj.X_b_dot;
-        end
-        
-        
         function [flag1, flag2] = isOutputComplexImpl(obj)
             flag1 = false;
             flag2 = false;
+        end
+        
+        
+        function [X_g, X_b_dot] = outputImpl(obj, ~, ~, ~, ~, ~)
+           X_g = obj.X_g
+           X_b_dot = obj.X_b_dot;
         end
         
         
@@ -158,7 +81,7 @@ classdef Robot < matlab.System & matlab.system.mixin.CustomIcon & matlab.system.
         end
         
         
-        function validateInputsImpl(~, u, dt)
+        function validateInputsImpl(~, u, dt, A_1, A_2, B)
             sz = size(u);
             if ~(sz(1) == 4 && sz(2) == 1)
                error('Wrong dimensions for input, should be [4, 1]') 
@@ -171,26 +94,18 @@ classdef Robot < matlab.System & matlab.system.mixin.CustomIcon & matlab.system.
             end
         end
         
-        function updateImpl(obj, u, dt)
+        function updateImpl(obj, u, dt, A_1, A_2, B)
             tspan = [0, dt];
             options = odeset;
             
-%             [t, xa] = ode45(@f, tspan, x_b_dot_0, odeset, u)
+            [t, xa] = ode45(@calculate_bot_accel, tspan, [obj.X_g; obj.X_b_dot], options, obj, u, A_1, A_2, B);
             
-            [t, xa] = ode45(@calculate_bot_accel, tspan, [obj.X_g; obj.X_b_dot], options, obj, u);
-            
-            xa;
-            
-            dt;
-            
-% @(t,y)evalfcn(t,y,arg1,arg2,...)
             result = xa(end, :);
             obj.X_g = result(1:3)';
             obj.X_b_dot = result(4:6)';
             
-            
-            obj.X_b_dot
-            obj.X_g;
+%             obj.X_b_dot;
+%             obj.X_g;
         end
     end
     
@@ -198,9 +113,12 @@ classdef Robot < matlab.System & matlab.system.mixin.CustomIcon & matlab.system.
     
     % input and output names
     methods (Access = protected)
-        function [in1name, in2name] = getInputNamesImpl(~)
+        function [in1name, in2name, in3name, in4name, in5name] = getInputNamesImpl(~)
             in1name = 'Motor Voltages';
             in2name = 'dt';
+            in3name = 'A_1';
+            in4name = 'A_2';
+            in5name = 'B';
         end
         
         function [out1name, out2name] = getOutputNamesImpl(~)
@@ -213,7 +131,6 @@ classdef Robot < matlab.System & matlab.system.mixin.CustomIcon & matlab.system.
     % Custom name for the block in SimuLink
     methods (Access = protected)
         function icon = getIconImpl(~)
-%             image(imread('robot_render.jpg'))
             icon = sprintf('RoboCup Robot\nDynamics');
         end
     end
@@ -224,8 +141,7 @@ end
 
 
 % calculate X_b_dot_dot, the acceleration in the body frame
-function accel = calculate_bot_accel(t, y, obj, u)
-
+function accel = calculate_bot_accel(t, y, obj, u, A_1, A_2, B)
     X_g = y(1:3);
     X_b_dot = y(4:6);
 
@@ -236,6 +152,6 @@ function accel = calculate_bot_accel(t, y, obj, u)
 
     dPhiDt = [0 0 1]*X_b_dot;
 
-    X_b_dot_dot = obj.A_1*X_b_dot + obj.A_2*X_b_dot*dPhiDt + obj.B*u;
+    X_b_dot_dot = A_1*X_b_dot + A_2*X_b_dot*dPhiDt + B*u;
     accel = [gbR*X_b_dot; X_b_dot_dot];
 end
---
+++ modeling/justin/RobotLqt.m
@@ -0,0 +1,154 @@
+
+% Linear Quadratic Tracking (LQT) controller for the robot
+classdef RobotLqt < matlab.System & matlab.system.mixin.Propagates
+    
+    properties (DiscreteState)
+        u;
+    end
+    
+    
+    properties
+        % Translational velocity error weight
+        trans_vel_weight = 1;
+        
+        % Rotational velocity error weight
+        rot_vel_weight = 1;
+        
+        % Control voltage weight
+        u_weight = 1;
+    end
+    
+    
+    properties (Dependent = true)
+        Q;
+        R;
+    end
+    
+    
+    methods
+        function Q = get.Q(obj)
+            Q = [obj.trans_vel_weight,                    0,                  0, 0, 0, 0;
+                                    0, obj.trans_vel_weight,                  0, 0, 0, 0;
+                                    0                     0, obj.rot_vel_weight, 0, 0, 0;
+                                    0,                    0,                  0, 1, 0, 0;
+                                    0,                    0,                  0, 0, 1, 0;
+                                    0,                    0,                  0, 0, 0, 1];
+        end
+       
+        function R = get.R(obj)
+            R = eye(4) * obj.u_weight;
+        end
+    end
+    
+    
+    methods (Access = protected)
+
+        % input: command body velocity
+        function num = getNumInputsImpl(~)
+            num = 5;
+        end
+
+        % output: voltages for motors
+        function num = getNumOutputsImpl(~)
+            num = 1;
+        end
+
+        function sz = getOutputSizeImpl(~)
+            sz = [4 1];
+        end
+
+        function t1 = getOutputDataTypeImpl(~)
+            t1 = 'double';
+        end
+
+        function [in1name, in2name, in3name, in4name, in5name] = getInputNamesImpl(~)
+            in1name = 'Cmd Velocity';
+            in2name = 'Curr. Velocity';
+            in3name = 'A_1';
+            in4name = 'A_2';
+            in5name = 'B';
+        end
+
+        function name = getOutputNamesImpl(~)
+            name = 'u';
+        end
+        
+        
+        function fixedout = isOutputFixedSizeImpl(~)
+            fixedout = true;
+        end
+        
+        
+        function flag1 = isOutputComplexImpl(~)
+            flag1 = false;
+        end
+
+        
+        % TODO: implement parameter validator
+        
+
+        function u = stepImpl(obj, cmdVel, currVel, A_1, A_2, B)
+            % TODO: do LQT
+
+            
+            % x_b_dot_dot = A_1*x_b_dot + A_2*dPhiDt*x_b_dot + B*u
+            % y = x_b_dot = eye(3)*x_b_dot
+            
+            
+            % linearize by evaluating at current angular velocity
+            A = A_1 + A_2*currVel(3);
+            
+            % y = Cx + Du
+            C = eye(3);
+            D = zeros(3, 4);    % no feed-forward
+            
+            % our system is non-linear, but we linearize it to apply linear
+            % quadratic tracking
+%             sys = ss(A, B, C, D);
+            % TODO: there's an additional weighting parameter N, what does
+            % it do and do we need it?  It's omitted now, which sets it to
+            % all zeros.
+%             [K, S, e] = lqi(sys, obj.Q, obj.R);
+            
+
+
+            % Nonlinear LQT based on this paper:
+            % http://www.cs.berkeley.edu/~pabbeel/cs287-fa12/slides/LQR.pdf
+
+
+            % X_dot = A_1*X + A_2*X*X[3] + B*u
+            
+            % partial wrt X: A_1 + A_2
+            
+            prevX = currVel;
+            prevXdot = [0 0 0]';
+            
+            
+            % linearize model by taking derivataive
+            A_t = A_1 + A_2*prevXdot*prevX(3) + A_2*prevX*prevXdot(3);
+            B_t = B;
+            
+            
+            
+            % Here's a good explanation of Lagrange multipliers:
+            % http://www.slimy.com/~steuard/teaching/tutorials/Lagrange.html
+            
+            % Good (but long) intro to controls and LQR/LQT:
+            % https://math.berkeley.edu/~evans/control.course.pdf
+            
+            
+            u = prevU - K*(currVel - cmdVel);
+            
+            
+%             [K, S, E] = lqr(A, B, obj.Q, obj.R);
+            
+%             prevU = obj.u;
+            
+%             u = prevU + K*(currVel
+            
+
+%             u = -K*cmdVel;
+        end
+    end
+    
+end
