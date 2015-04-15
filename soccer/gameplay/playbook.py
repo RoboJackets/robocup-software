14929daec6070eb787bf0a5332e0a25479c5bc2f
Roi Atalla <roi@roiatalla.com>
2015-04-14 20:35:04 -0400

    Removed save feature for playbooks. Revert in future when UI stuff is done to be able to test this. #260

--- soccer/gameplay/playbook.py
+++ soccer/gameplay/playbook.py
@@ -9,11 +9,8 @@ def load_from_file(playbook_file):
 
     with open(playbook_file, 'r') as f:
         for play in f:
-            plays.append(play.strip().split('/'))
+        	play = play.strip()
+        	if not play:
+            	plays.append(play.split('/'))
 
     return plays
-
-def save_to_file(playbook_file, list_of_plays):
-    with open(playbook_file, 'w') as f:
-        for play in list_of_plays:
-            f.write('/'.join(play) + '\n')
