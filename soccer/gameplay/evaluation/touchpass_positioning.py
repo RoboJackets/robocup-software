981dcf08fa503c6be7e4e2ea8288165fa2dd1854
Jay Kamat <github@jgkamat.33mail.com>
2015-06-28 10:57:28 -0700

    Add docs and add a debug shot line

--- soccer/gameplay/evaluation/touchpass_positioning.py
+++ soccer/gameplay/evaluation/touchpass_positioning.py
@@ -3,12 +3,11 @@ import constants
 import main
 import evaluation.passing
 
-# The Touchpass Positioner finds the best location within a rectangle
-
+## The Touchpass Positioner finds the best location within a rectangle
+#
 # Example usage:
 # tpos = TouchpassPositioner()
 # best_pt = tpos.evaluate(ball.pos, robot2.pos)
-#
 class TouchpassPositioner:
 
     def __init__(self):
@@ -23,23 +22,23 @@ class TouchpassPositioner:
     def debug(self, value):
         self._debug = value
 
-    # Returns a robocup.Rect object that is the default location to be evaluated
-    # Takes a current ball position/initial kick position (robocup.Point)
+    ## Returns a robocup.Rect object that is the default location to be evaluated
     #
-    # Mainly for internal use
+    # Takes a current ball position/initial kick position (robocup.Point)
     def generate_default_rectangle(self, kick_point):
         offset_from_edge = 0.3
 
         if kick_point.x > 0:
             # Ball is on right side of field
-            toReturn = robocup.Rect(robocup.Point(0, min(constants.Field.Length - offset_from_edge, main.ball().pos.y - 0.5)),
+            toReturn = robocup.Rect(robocup.Point(0, min(constants.Field.Length - offset_from_edge, main.ball().pos.y - 0.3)),
                     robocup.Point(-constants.Field.Width / 2 + offset_from_edge, min(constants.Field.Length * 3 / 4, main.ball().pos.y - 2)))
         else:
             # Ball is on left side of field
-            toReturn = robocup.Rect(robocup.Point(0, min(constants.Field.Length - offset_from_edge, main.ball().pos.y - 0.5)),
+            toReturn = robocup.Rect(robocup.Point(0, min(constants.Field.Length - offset_from_edge, main.ball().pos.y - 0.3)),
                     robocup.Point(constants.Field.Width / 2 - offset_from_edge, min(constants.Field.Length * 3 / 4, main.ball().pos.y - 2)))
         return toReturn
 
+    ## Returns a list of robocup.Point object that represent candidate points. Takes in a robocup.Rect
     def get_points_from_rect(self, rect, threshold=0.50):
         outlist = []
         currentx = rect.min_x()
@@ -57,6 +56,9 @@ class TouchpassPositioner:
             currentx = rect.min_x()
         return outlist
 
+    ## Finds the best receive point for a bounce-pass.
+    #
+    # Takes in an initial kick point and an optional evaluation zone.
     def eval_best_receive_point(self, kick_point, evaluation_zone=None):
         # Autogenerate kick point
         if evaluation_zone == None:
