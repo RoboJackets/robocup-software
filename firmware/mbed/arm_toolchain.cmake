0ad518ed4b8a141e62aba5bb927d7e698f90f452
Justin Buchanan <justbuchanan@gmail.com>
2015-06-30 00:17:04 -0400

    cleanup

--- firmware/mbed/arm_toolchain.cmake
+++ firmware/mbed/arm_toolchain.cmake
@@ -3,7 +3,7 @@
 # under BSD License, see https://uwe-arzt.de/bsd-license/
 # ------------------------------------------------------------------------------
 include(CMakeForceCompiler)
- 
+
 #-------------------------------------------------------------------------------
 set(CMAKE_SYSTEM_NAME Generic)
 # set(CMAKE_SYSTEM_VERSION 1)
@@ -15,7 +15,6 @@ CMAKE_FORCE_CXX_COMPILER(arm-none-eabi-g++ GNU)
 
 #-------------------------------------------------------------------------------
 set(TOOLCHAIN TOOLCHAIN_GCC_ARM)
-
 
 #-------------------------------------------------------------------------------
 # define presets
