6dd9efb0c86334b22c979e90e5287dce7db4ba95
Matthew Barulic <matthew.barulic@gmail.com>
2015-07-08 19:53:40 -0400

    Initial port of WindowEvaluator. Issue #112

---
+++ soccer/WindowEvaluator.cpp
@@ -0,0 +1,181 @@
+//
+// Created by matt on 7/8/15.
+//
+
+#include "WindowEvaluator.h"
+#include "Constants.hpp"
+#include <algorithm>
+
+using namespace std;
+using namespace Geometry2d;
+
+Window::Window() {
+}
+
+Window::Window(double t0, double t1)
+  : t0(t0),
+    t1(t1)
+{
+}
+
+WindowEvaluator::WindowEvaluator(SystemState *systemState)
+  : system(systemState)
+{
+}
+
+WindowingResult WindowEvaluator::eval_pt_to_pt(Point origin, Point target) {
+  auto dir_vec = (target - origin).perpCCW().normalized();
+  auto segment = Segment{target + dir_vec * Robot_Radius, target - dir_vec * Robot_Radius};
+
+  return eval_pt_to_seg(origin, segment);
+}
+
+WindowingResult WindowEvaluator::eval_pt_to_opp_goal(Point origin) {
+  Segment their_goal{Point{Field_Dimensions::Current_Dimensions.Length(),-Field_Dimensions::Current_Dimensions.GoalWidth()/2},
+                     Point{Field_Dimensions::Current_Dimensions.Length(), Field_Dimensions::Current_Dimensions.GoalWidth()/2}};
+
+  return eval_pt_to_seg(origin, their_goal);
+}
+
+WindowingResult WindowEvaluator::eval_pt_to_our_goal(Point origin) {
+  Segment our_goal{Point{0,-Field_Dimensions::Current_Dimensions.GoalWidth()/2},
+                     Point{0, Field_Dimensions::Current_Dimensions.GoalWidth()/2}};
+
+  return eval_pt_to_seg(origin, our_goal);
+}
+
+void WindowEvaluator::obstacle_range(vector<Window>& windows, double& t0, double& t1) {
+  // Ignore degenerate obstacles
+  if(t0 == t1)
+    return;
+
+  if(t0 > t1)
+    swap(t0, t1);
+
+  auto iter = windows.begin();
+  while(iter != windows.end()) {
+    auto& w = *iter;
+    if(t0 <= w.t0 && t1 >= w.t1) {
+      // this window is fully covered by the obstacle, so remove it
+      iter = windows.erase(iter);
+    }
+    else if(t0 > w.t0 && t1 < w.t1) {
+      // the window fully contains the obstacle, so we split the window
+      Window w2{t1, w.t1};
+      w.t1 = t0;
+      iter = windows.insert(iter+1, w2);
+    }
+    else if(t0 > w.t0 && t0 < w.t1) {
+      // the obstacle covers the end of the window
+      w.t1 = t0;
+      iter++;
+    }
+    else if(t1 > w.t0 && t1 < w.t1) {
+      // the obstacle covers the beginning of the window
+      w.t0 = t1;
+      iter++;
+    }
+    else {
+      iter++;
+    }
+  }
+}
+
+void WindowEvaluator::obstacle_robot(vector<Window> windows, Point origin, Segment target, Point bot_pos) {
+  auto n = (bot_pos - origin).normalized();
+  auto t = n.perpCCW();
+  auto r = Robot_Radius + Ball_Radius;
+
+  Segment seg{bot_pos - n * Robot_Radius + t * r,
+              bot_pos - n*Robot_Radius - t * r};
+
+  if(debug) {
+    // main.system_state().draw_line(seg, constants.Colors.Red, "debug")
+  }
+
+  auto end = target.delta().magsq();
+  array<double, 2> extent = {0, end};
+
+  for(int i = 0; i < 2; i++) {
+    Line edge{origin, seg.pt[i]};
+    auto d = edge.delta().magsq();
+
+    Point intersect;
+    if(edge.intersects(target, &intersect) && (intersect - origin).dot(edge.delta()) > d) {
+      auto f = (intersect - target.pt[0]).dot(target.delta());
+      if(f < 0)
+        extent[i] = 0;
+      else if(f > end)
+        extent[i] = end;
+      else
+        extent[i] = f;
+    }
+    else {
+      return;
+    }
+    obstacle_range(windows, extent[0], extent[1]);
+  }
+}
+
+WindowingResult WindowEvaluator::eval_pt_to_seg(Point origin, Segment target) {
+  auto end = target.delta().magsq();
+
+  // if target is a zero-length segment, there are no windows
+  if(end == 0)
+    return make_pair(vector<Window>{}, boost::none);
+
+  if(debug) {
+    // main.system_state().draw_line(target, constants.Colors.Blue, "Debug")
+  }
+
+  vector<Window> windows = {Window{0, end}};
+
+  // apply the obstacles
+
+  vector<Robot*> bots;
+
+  vector<Robot*> all_bots;
+  all_bots.insert(all_bots.end(), system->self.begin(), system->self.end());
+  all_bots.insert(all_bots.end(), system->opp.begin(), system->opp.end());
+  copy_if(all_bots.begin(), all_bots.end(), bots.begin(), [=](Robot* bot){
+    return find(excluded_robots.begin(), excluded_robots.end(), bot) != excluded_robots.end() && bot->visible;
+  });
+
+  vector<Point> bot_locations;
+  for_each(bots.begin(), bots.end(), [&bot_locations](Robot* bot){
+    bot_locations.push_back(bot->pos);
+  });
+
+  bot_locations.insert(bot_locations.end(), hypothetical_robot_locations.begin(), hypothetical_robot_locations.end());
+
+  for(auto& pos : bot_locations) {
+    auto d = (pos - origin).mag();
+    // whether or not we can ship over this bot
+    auto chip_overable =  chip_enabled
+                          && (d < max_chip_range - Robot_Radius)
+                          && (d > min_chip_range + Robot_Radius);
+    if(!chip_overable) {
+      obstacle_robot(windows, origin, target, pos);
+    }
+  }
+
+  auto p0 = target.pt[0];
+  auto delta = target.delta() / end;
+
+  for(auto& w : windows) {
+    w.segment = Segment{p0 + delta * w.t0, p0 + delta * w.t1};
+    w.a0 = (w.segment.pt[0] - origin).angle() * RadiansToDegrees;
+    w.a1 = (w.segment.pt[1] - origin).angle() * RadiansToDegrees;
+  }
+
+  boost::optional<Window> best{!windows.empty(), *max_element(windows.begin(),windows.end(),[](Window& a, Window& b) -> bool{
+    return a.segment.delta().magsq() < b.segment.delta().magsq();
+  })};
+
+  if(debug && best) {
+    system->drawLine(best->segment, QColor{"Green"}, "Debug");
+    system->drawLine(Line{origin, best->segment.center()}, QColor{"Green"}, "Debug");
+  }
+
+  return make_pair(windows, best);
+}
