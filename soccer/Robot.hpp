c168b5a6a2b0b67a077f25a681ffbb650ae8f8a3
Albert Shaw <ashaw596@gmail.com>
2015-07-23 13:52:06 +0800

    Fix some comment messages

--- soccer/Robot.hpp
+++ soccer/Robot.hpp
@@ -219,7 +219,8 @@ public:
 
 
 	/**
-	 * @brief Move to a given point using the default RRT planner
+	 * @brief Move to a given point bypassing the RRT Path planner. This will plan a direct path ignoring 
+	 *			all obstacles and the starting velocity	
 	 * @param endSpeed - the speed we should be going when we reach the end of the path
 	 */
 	void moveDirect(const Geometry2d::Point &goal, float endSpeed = 0);
