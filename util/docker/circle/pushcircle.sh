c7454d985ae9e68b36f6c03c347bc8817f8fa7b9
Jay Kamat <github@jgkamat.33mail.com>
2015-07-27 12:05:12 -0700

    Run deployment after triggering success

--- util/docker/circle/pushcircle.sh
+++ util/docker/circle/pushcircle.sh
@@ -10,4 +10,10 @@ SHA_SUM_SETUP="$(${ROBOCUP_ROOT}/util/docker/getsetupsha.sh)"
 SHA_SUM="$(git rev-parse HEAD)"
 IMAGE_NAME="jgkamat/robocup-ci"
 
-docker build -t ${IMAGE_NAME}:${SHA_SUM}  -f ${ROBOCUP_ROOT}/util/docker/circle/Dockerfile ${ROBOCUP_ROOT}
+docker push ${IMAGE_NAME}:${SHA_SUM}
+
+if [ "$(git rev-parse --abbrev-ref HEAD)" != "HEAD" ]; then
+    docker tag "${IMAGE_NAME}:${SHA_SUM}" "${IMAGE_NAME}:$(git rev-parse --abbrev-ref HEAD)"
+    docker push "${IMAGE_NAME}:$(git rev-parse --abbrev-ref HEAD)"
+fi
+
