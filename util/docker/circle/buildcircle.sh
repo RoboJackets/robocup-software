b5d8af0e5dd34b490469712c08461f271ece8986
Jay Kamat <github@jgkamat.33mail.com>
2015-07-26 18:07:06 -0700

    Refine setup scripts and add circle yml

--- util/docker/circle/buildcircle.sh
+++ util/docker/circle/buildcircle.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 # Builds our circle image which runs tests
+set -e
 
-
-DIR=$(dirname "${BASH_SOURCE}")
+DIR=$(cd $(dirname $0) ; pwd -P)
 ROBOCUP_ROOT="${DIR}/../../../"
 
 # Get sha sum
 SHA_SUM_SETUP="$(${ROBOCUP_ROOT}/util/docker/getsetupsha.sh)"
 SHA_SUM="$(git rev-parse HEAD)"
-IMAGE_NAME="jgkamat/robocup-software"
+IMAGE_NAME="jgkamat/robocup-ci"
 
-sudo docker build -t ${IMAGE_NAME}:${SHA_SUM}  -f util/docker/circle/Dockerfile ${ROBOCUP_ROOT}
+docker build -t ${IMAGE_NAME}:${SHA_SUM}  -f ${ROBOCUP_ROOT}/util/docker/circle/Dockerfile ${ROBOCUP_ROOT}
 docker push ${IMAGE_NAME}:${SHA_SUM}
+
+if [ "$(git rev-parse --abbrev-ref HEAD)" != "HEAD" ]; then
+    docker tag "${IMAGE_NAME}:${SHA_SUM} ${IMAGE_NAME}:$(git rev-parse --abbrev-ref HEAD)"
+    docker push "${IMAGE_NAME}:$(git rev-parse --abbrev-ref HEAD)"
+fi
+
