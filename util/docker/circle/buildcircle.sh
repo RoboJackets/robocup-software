0f594efe77959b4726c7f75f1e5689725b4d314b
Jay Kamat <github@jgkamat.33mail.com>
2015-08-02 12:52:05 -0700

    Use robojackets instead of jgkamat

--- util/docker/circle/buildcircle.sh
+++ util/docker/circle/buildcircle.sh
@@ -8,6 +8,6 @@ ROBOCUP_ROOT="${DIR}/../../../"
 # Get sha sum
 SHA_SUM_SETUP="$(${ROBOCUP_ROOT}/util/docker/getsetupsha.sh)"
 SHA_SUM="$(git rev-parse HEAD)"
-IMAGE_NAME="jgkamat/robocup-ci"
+IMAGE_NAME="robojackets/robocup-ci"
 
 docker build -t ${IMAGE_NAME}:${SHA_SUM}  -f ${ROBOCUP_ROOT}/util/docker/circle/Dockerfile ${ROBOCUP_ROOT}
