9d443efc2124ddf6fcec56c6466e9ee6f37fcec4
Jay Kamat <github@jgkamat.33mail.com>
2015-08-09 16:36:14 -0700

    Fixed token authentication when running tests

--- util/docker/circle/buildcircle.sh
+++ util/docker/circle/buildcircle.sh
@@ -2,6 +2,11 @@
 # Builds our circle image which runs tests
 set -e
 
+if [ "$GH_TOKEN" = "" ]; then
+    echo "Github token not set!"
+    exit 1
+fi
+
 DIR=$(cd $(dirname $0) ; pwd -P)
 ROBOCUP_ROOT="${DIR}/../../../"
 
@@ -10,4 +15,7 @@ SHA_SUM_SETUP="$(${ROBOCUP_ROOT}/util/docker/getsetupsha.sh)"
 SHA_SUM="$(git rev-parse HEAD)"
 IMAGE_NAME="robojackets/robocup-ci"
 
-docker build -t ${IMAGE_NAME}:${SHA_SUM}  -f ${ROBOCUP_ROOT}/util/docker/circle/Dockerfile ${ROBOCUP_ROOT}
+docker build -t tmp_ci_image -f ${ROBOCUP_ROOT}/util/docker/circle/Dockerfile ${ROBOCUP_ROOT}
+BUILT_IMAGE="tmp_ci_image"
+DOCKER_IMAGE="$(docker run $BUILT_IMAGE ./util/docker/maketest.sh ${GH_TOKEN})"
+docker commit $DOCKER_IMAGE ${IMAGE_NAME}:${SHA_SUM}
