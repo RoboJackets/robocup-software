81639bbc212872a0c4bb09fd65a9101c593acf5e
Jon <jjones646@gmail.com>
2015-08-10 03:10:54 -0400

    moving the utils directory over to the common2015 build section since everything in it is generic

--- firmware/robot2015/src-ctrl/utils/numparser/numparser.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string>
-
-bool isInt(std::string& s);
--- firmware/robot2015/src-ctrl/utils/rtos-mgmt/helperFuncs.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-#include <string>
-
-
-void setISRPriorities(void);
-void imAlive(void const*);
-std::string decode_marcstate(uint8_t);
