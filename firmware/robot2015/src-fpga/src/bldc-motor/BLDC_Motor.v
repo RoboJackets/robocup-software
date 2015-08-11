affbc289812737c0e0974bf9acb0a77292ba9e26
Jon <jjones646@gmail.com>
2015-08-11 02:27:13 -0400

    Adding the long lost files! probably gotta change a few things since these weren't the set of files that went through synthesis, but they're really close.

---
+++ firmware/robot2015/src-fpga/src/bldc-motor/BLDC_Motor.v
@@ -0,0 +1,132 @@
+/*
+*  BLDC_Motor.v
+*  
+*  A BLDC controller that includes hall sensor error detection and incremental
+*  duty cycles during startup for reducing motor startup current.
+*  
+*/
+
+
+`include "BLDC_Driver.v"
+`include "BLDC_Hall_Counter.v"
+`include "BLDC_Encoder_Counter.v"
+
+
+/*
+============================ 
+DRV8303 REGISTER INFORMATION
+============================
+
+SPI Format:
+============================
+- 16-bit length
+- 1 read/write bit [15]
+- 4 address bits [14..11]
+- 11 data bits [10..0]
+============================
+
+Address 0x00: R (auto reset after read)
+============================
+bit [10]:   FAULT
+bit [9]:    GVDD_UV
+bit [8]:    PVDD_UV
+bit [7]:    OTSD
+bit [6]:    OTW
+bit [5]:    AH_OC
+bit [4]:    AL_OC
+bit [3]:    BH_OC
+bit [2]:    BL_OC
+bit [1]:    CH_OC
+bit [0]:    CL_OC
+============================
+
+Address 0x01: R (auto reset after read)
+============================
+bit  [7]:      GVDD_OV
+bits [6..4]:   RESERVED
+bits [3..0]:   DEV_ID
+============================
+
+Address 0x02: R/W
+============================
+bits [10..6]: OC_ADJ_SET (write with 0x0D everytime)
+bits [5..4]:  OC_MODE
+bit  [3]:     PWM_MODE
+bit  [2]:     GATE_RESET
+bits [1..0]:  GATE_CURRENT
+============================
+
+Address 0x03: R/W
+============================
+bits [10..7]:   RESERVED
+bit  [6]:       OC_TOFF
+bit  [5]:       DC_CAL_CH2 (write 0x01 here, then read DC offset value and write back to 0x00)
+bit  [4]:       DC_CAL CH1 (write 0x01 here, then read DC offset value and write back to 0x00)
+bits [3..2]:    GAIN (set to 0x01 everytime)
+bits [1..0]:    OCTW_SET
+============================
+*/
+
+
+// BLDC_Motor module
+module BLDC_Motor ( clk, en, reset_counts, duty_cycle, enc, hall, phaseH, phaseL, enc_count, hall_count, hall_fault );
+
+// Module parameters - passed parameters will overwrite the values here
+parameter MIN_DUTY_CYCLE =          ( 0 );
+parameter MAX_DUTY_CYCLE =          ( 'h3FF );
+parameter MAX_DUTY_CYCLE_COUNTER =  ( MAX_DUTY_CYCLE );
+parameter ENCODER_COUNT_WIDTH =     ( 15 );
+parameter HALL_COUNT_WIDTH =        ( 7 );
+
+// Local parameters - can not be altered outside this module
+localparam DUTY_CYCLE_WIDTH =       ( $clog2( MAX_DUTY_CYCLE ) );
+
+// Module inputs/outputs
+input clk, en, reset_counts;
+input [DUTY_CYCLE_WIDTH-1:0] duty_cycle;
+input [1:0] enc;
+input [2:0] hall;
+output [2:0] phaseH, phaseL;
+output [ENCODER_COUNT_WIDTH-1:0] enc_count;
+output [HALL_COUNT_WIDTH-1:0] hall_count;
+output hall_fault;
+// ===============================================
+
+
+// Instantiation of all the modules required for complete functioning with all sensors
+// ===============================================
+BLDC_Encoder_Counter #(         // Instantiation of the encoder for counting the ticks
+    .COUNT_WIDTH                ( ENCODER_COUNT_WIDTH )
+    ) encoder_counter (
+    .clk                        ( clk ) ,
+    .reset                      ( reset_counts ) ,
+    .enc                        ( enc ) ,
+    .count                      ( enc_count )
+);
+
+BLDC_Hall_Counter #(            // Instantiation of the hall effect sensor's counter
+    .COUNTER_WIDTH              ( HALL_COUNT_WIDTH )
+    ) hall_counter (
+    .clk                        ( clk ) ,
+    .reset                      ( reset_counts ) ,
+    .hall                       ( hall ) ,
+    .count                      ( hall_count )
+);
+
+BLDC_Driver #(                  // Instantiation of the motor driving module
+    .PHASE_DRIVER_MAX_COUNTER   ( MAX_DUTY_CYCLE_COUNTER ) ,
+    .MAX_DUTY_CYCLE             ( MAX_DUTY_CYCLE ) ,
+    .MIN_DUTY_CYCLE             ( MIN_DUTY_CYCLE ) ,
+    .DUTY_CYCLE_STEP_RES        ( 1 ) ,
+    .DEAD_TIME                  ( 2 )
+    ) bldc_motor (
+    .clk                        ( clk ) ,
+    .en                         ( en ) ,
+    .hall                       ( hall ) ,
+    .duty_cycle                 ( duty_cycle ) ,
+    .phaseH                     ( phaseH ) ,
+    .phaseL                     ( phaseL ) ,
+    .fault                      ( hall_fault )
+);
+
+endmodule
---
+++ firmware/robot2015/src-fpga/src/bldc-motor/Phase_Driver.v
@@ -0,0 +1,97 @@
+//`include "phase_driver.vh"
+
+/**
+This module is responsible for driving one phase of the BLDC. It generates both
+the low and high side PWM (Pulse Width Modulation) signals necessary for the
+switching regulator.
+
+The switching regulator, in this case, is two mosfets. One mosfet connects the
+phase to the supply voltage, the other connects the phase to ground. The 'high
+side PWM' drives the high side mosfet and the low side PWM signal drives the
+low side mosfet. High side means the mosfet connects the load (the phase coil)
+to the supply voltage, low side means the mosfet connects the load to ground. A
+logical high on the PWM signal will turn on its respective mosfet. An 'on'
+mosfet can be simplified as a conductor (closed circuit) while an 'off' mosfet
+can be simplifed as an infinite resistance (an open circuit).
+
+//image of single phase high and low mosfets (half H bridge)
+
+The amount of time a PWM signal stays logically high relative to the pwm period
+is called the duty cycle (high time / period). In this case, the duty cycle for
+the high side PWM represents the percentage of time per duty cycle that the
+phase is connected to the supply voltage, and the duty cycle for the low side
+PWM represents the percentage of time per duty cycle that the coil is connected
+to ground.
+
+Any voltage in the range of ground to the supply can be acheived can by
+rapidly changing whether the phase coil is connected to the supply voltage or
+ground. If the PWM frequency is fast enough, the voltage supplied to the coil is
+equal to the average voltage supplied to the phase over the period of the highside
+duty cycle (HS duty cycle * supply voltage). To accomplish this in code, a
+counter register is set that increments with every clock tick. This value is
+then constantly compared to duty cycle. If the counter is less than the duty cycle
+the high side PWM is driven high and the low side PWM is driven low. If the 
+counter is greater than the duty cycle, the low side PWM is driven high and the
+high side PWM is driven low. 
+
+//pretty matlab picture
+
+Unfortunately, it is not as simple as inverting the high side PWM signal to
+create the required low side PWM signal. This has the potential to cause 'shoot 
+through' where both the high and low side fets are on simultaniously and the
+supply voltage is shorted to ground. This is caused by fet turn off delay time, 
+which is the amount of time between the PWM signal going low and the fet turning 
+off. This value can be found on the mosfet datasheet. In this PWM module, 
+the turn off delay is corrected by dead time, where both the high side and low
+side fets are turned off to ensure no shoot through occurs. 
+
+//pretty picture of dead time
+
+The only instances where dead time is not neccessary is when the PWM signal is
+either unity/zero or the phase is set to high impedance. In these cases the fets
+are do not alternate going on and off so there is no danger of shoot through.
+
+--Doho*/
+
+
+module Phase_Driver ( clk, duty_cycle, high_z, pwm_high, pwm_low );
+
+parameter DEAD_TIME;            // dead time in units of clock ticks
+parameter COUNTER_WIDTH;        // bits available to counter
+parameter MAX_COUNTER;          // PWM period = MAX_COUNTER * clockPeriod
+parameter DUTY_CYCLE_WIDTH;     // bits available to duty_cycle
+parameter MAX_DUTY_CYCLE;       // Value represeting a duty cycle of 100%
+parameter DUTY_CYCLE_STEP_RES;  // ceil( MAX_COUNTER / MAX_DUTY_CYCLE ) 
+
+input   clk;
+input   [DUTY_CYCLE_WIDTH-1:0] duty_cycle;
+input   high_z;
+output  pwm_high, pwm_low;
+// ===============================================  
+
+// wire pwm_high, pwm_low;
+wire h, l;
+//wire deadtime;
+
+reg [COUNTER_WIDTH-1:0] counter = 0;
+
+//assign deadtime  = (duty_cycle == MAX_DUTY_CYCLE || duty_cycle == 0) ? 0 : DEAD_TIME;
+
+assign h = ( ( counter + DEAD_TIME ) < ( duty_cycle * DUTY_CYCLE_STEP_RES ) ) ? 1 : 0;
+assign l = ( ( counter >= ( duty_cycle * DUTY_CYCLE_STEP_RES ) ) && ( ( counter + DEAD_TIME ) < MAX_COUNTER ) ) ? 1 : 0;
+
+
+assign  pwm_high = 	(high_z == 1) ? 0 : h;
+
+assign	pwm_low = 	(high_z == 1) ? 0 :
+					(duty_cycle == 0) ? 1 :
+                    l;
+
+
+always @(posedge clk) begin : PHASE_DRIVER
+	counter = counter + 1;
+	if (counter >= MAX_COUNTER) counter = 0;
+end
+
+
+endmodule
