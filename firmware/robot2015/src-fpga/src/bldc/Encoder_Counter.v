9f21a1ed563b54d82a397c935329804b90fabec8
Jon <jjones646@gmail.com>
2015-08-11 01:51:31 -0400

    adding Doho's verilog files into the firmware

---
+++ firmware/robot2015/src-fpga/src/bldc/Encoder_Counter.v
@@ -0,0 +1,33 @@
+module Encoder_Counter (
+    input clk,
+    input [1:0] enc,
+    output reg [15:0] count = 0
+);
+
+reg [1:0] enc_sync = 0;
+reg [1:0] last_enc = 0;
+
+wire count_up =
+    (last_enc == 2'b00 && enc_sync == 2'b01) ||
+    (last_enc == 2'b01 && enc_sync == 2'b11) ||
+    (last_enc == 2'b11 && enc_sync == 2'b10) ||
+    (last_enc == 2'b10 && enc_sync == 2'b00);
+
+wire count_down =
+    (last_enc == 2'b10 && enc_sync == 2'b11) ||
+    (last_enc == 2'b11 && enc_sync == 2'b01) ||
+    (last_enc == 2'b01 && enc_sync == 2'b00) ||
+    (last_enc == 2'b00 && enc_sync == 2'b10);
+
+always @(posedge clk) begin
+    if (count_up) begin
+        count <= count + 1;
+    end else if (count_down) begin
+        count <= count - 1;
+    end
+
+    enc_sync <= enc;
+    last_enc <= enc_sync;
+end
+
+endmodule
--- firmware/robot2015/src-fpga/src/robocup.v
+++ firmware/robot2015/src-fpga/src/robocup.v
@@ -18,93 +18,53 @@
 // simulation as on the real hardware.
 // Use includes rather than a project file to make integration into a simulation easier.
 
-`include "motor.v"
-`include "kicker.v"
-`include "encoder.v"
-`include "kicker_i2c.v"
-`include "hall_counter.v"
-
+`include "BLDC_Motor.v"
 `include "git_version.vh"
 
-// Conventions:
-//  Hall effect sensor vectors are [2:0] which maps to {a, b, c}.
-//  Half-bridge drive vectors are [1:0] which maps to {high, low}.
-//  Motor output vectors are [5:0] which maps to
-//      {high_a, low_a, high_b, low_b, high_c, low_c}
-//       MxA_H   MxA_L  MxB_H   MxB_L  MxC_H   MxC_L
 
 module robocup (
 	// Clock
 	input sysclk,
-	
-	// Hall effect sensors on motors
-	input m1hall_a, m1hall_b, m1hall_c,
-	input m2hall_a, m2hall_b, m2hall_c,
-	input m3hall_a, m3hall_b, m3hall_c,
-	input m4hall_a, m4hall_b, m4hall_c,
-	input m5hall_a, m5hall_b, m5hall_c,
-	
-	// Half-bridge outputs
-	output m1a_h, m1a_l, m1b_h, m1b_l, m1c_h, m1c_l,
-	output m2a_h, m2a_l, m2b_h, m2b_l, m2c_h, m2c_l,
-	output m3a_h, m3a_l, m3b_h, m3b_l, m3c_h, m3c_l,
-	output m4a_h, m4a_l, m4b_h, m4b_l, m4c_h, m4c_l,
-	output m5a_h, m5a_l, m5b_h, m5b_l, m5c_h, m5c_l,
-	
-	// Encoders
-	input m1enc_a, m1enc_b,
-	input m2enc_a, m2enc_b,
-	input m3enc_a, m3enc_b,
-	input m4enc_a, m4enc_b,
 
-	// Discharge button
-	input discharge,
-	
-	// Kicker
-	input kdone,
-	output kcharge, kkick, kchip,
-	inout ksda, kscl,
-	
-	// Microcontroller interface
-	input flash_ncs,
-	input fpga_ncs,
-	input mosi, sck,
-	inout miso
+	// Hall effect sensors
+	input [4:0] hall_a, hall_b, hall_c,
+
+	// Encoders
+	input [3:0] enc_a, enc_b,
+
+	// 3-half-bridge
+	output [4:0] phase_aH, phase_aL, phase_bH, phase_bL, phase_cH, phase_cL;
+
+	// Phase driver chip select pins
+	output [4:0] drv_ncs,
+
+	// ADC chip select pins
+	output [1:0] adc_ncs,
+
+	input spi_slave_sck, spi_slave_mosi, spi_slave_ncs
+	inout spi_slave_miso,
+
+	input spi_master_miso
+	output spi_master_sck, spi_master_mosi
+
+	parameter DATA_WIDTH=4;
+		parameter CLK_DIVIDER_WIDTH=4;
 );
 
+
 // This is sent as the first byte of every SPI transfer
-localparam LOGIC_VERSION = 8'h04;
+localparam LOGIC_VERSION = 8'h05;
 
 // Status that can be read over SPI
 wire [5:1] motor_fault;
 reg [15:0] encoder_capture_1 = 0;
-reg [15:0] encoder_capture_2 = 0;
-reg [15:0] encoder_capture_3 = 0;
-reg [15:0] encoder_capture_4 = 0;
-
 reg [7:0] hall_count_capture_1 = 0;
-reg [7:0] hall_count_capture_2 = 0;
-reg [7:0] hall_count_capture_3 = 0;
-reg [7:0] hall_count_capture_4 = 0;
-reg [7:0] hall_count_capture_5 = 0;
-
-// Motor commands
 reg [5:1] motor_dir = 0;
 reg [8:0] motor_speed_1 = 0;
-reg [8:0] motor_speed_2 = 0;
-reg [8:0] motor_speed_3 = 0;
-reg [8:0] motor_speed_4 = 0;
-reg [8:0] motor_speed_5 = 0;
-
 reg [1:0] drive_mode_1 = 0;
-reg [1:0] drive_mode_2 = 0;
-reg [1:0] drive_mode_3 = 0;
-reg [1:0] drive_mode_4 = 0;
-reg [1:0] drive_mode_5 = 0;
 
-wire [7:0] kicker_status;
-wire [7:0] kicker_voltage;
-wire kicker_voltage_ok;
+
+
 
 // SPI interface
 
@@ -114,8 +74,7 @@ reg [7:0] spi_dr = 0;
 // Synchronized and delayed interface signals
 reg sck_s = 0, sck_d = 0;
 reg mosi_s = 0;
-reg ncs_s = 0;
-reg ncs_d = 0;
+reg ncs_s = 0, ncs_d = 0;
 
 // Value of MOSI at the last SCK rising edge
 reg mosi_sampled = 0;
@@ -136,8 +95,8 @@ wire spi_end_strobe = (ncs_d == 0 && ncs_s == 1);
 // Received data
 reg [7:0] spi_rx[0:14];
 
-wire sck_rising = (sck_d == 0 && sck_s == 1);
-wire sck_falling = (sck_d == 1 && sck_s == 0);
+wire 	sck_rising 	=	( sck_d == 0 && sck_s == 1 ), 
+		sck_falling	= 	( sck_d == 1 && sck_s == 0 );
 
 // High for one cycle when spi_dr needs to be loaded with the next byte to transmit.
 // This does not apply to byte 0: that byte must be loaded whenever ncs_s == 1.
@@ -234,26 +193,9 @@ end
 // Drive MISO only if we are selected
 assign miso = fpga_ncs ? 1'bz : spi_dr[7];
 
-reg kick_strobe = 0;
-reg [7:0] kick_strength = 0;
-reg kick_select = 0;
-reg charge = 0;
-
 // Watchdog timer for motor updates.
 // When this overflows, all motor outputs are reset.
 reg [21:0] watchdog = 0;
-
-// Capture all encoder counts together at the beginning of a transfer
-wire [15:0] encoder_1;
-wire [15:0] encoder_2;
-wire [15:0] encoder_3;
-wire [15:0] encoder_4;
-
-wire [7:0] hall_count_1;
-wire [7:0] hall_count_2;
-wire [7:0] hall_count_3;
-wire [7:0] hall_count_4;
-wire [7:0] hall_count_5;
 
 always @(posedge sysclk) begin
 	if (spi_start_strobe) begin
@@ -326,92 +268,57 @@ always @(posedge sysclk) begin
 			// but it does work if the counter saturates.  Both cases work on version 13.1
 			watchdog <= watchdog + 1;
 		end
-		
-		kick_strobe <= 0;
 	end
 end
 
-// PWM clock divider and counter
-reg [8:0] pwm_phase = 0;
-reg pwm_direction = 0;
-always @(posedge sysclk) begin
-	if (pwm_direction == 0) begin
-		if (pwm_phase == 9'h1fd) begin
-			pwm_direction <= 1;
-		end
-		pwm_phase <= pwm_phase + 1;
-	end else begin
-		if (pwm_phase == 9'h001) begin
-			pwm_direction <= 0;
-		end
-		pwm_phase <= pwm_phase - 1;
-	end
-end
 
-wire [2:0] hall_1 = {m1hall_a, m1hall_b, m1hall_c};
-wire [2:0] hall_2 = {m2hall_a, m2hall_b, m2hall_c};
-wire [2:0] hall_3 = {m3hall_a, m3hall_b, m3hall_c};
-wire [2:0] hall_4 = {m4hall_a, m4hall_b, m4hall_c};
-wire [2:0] hall_5 = {m5hall_a, m5hall_b, m5hall_c};
+// This is the interface used for communications back to the mbed
+spi_slave #(
+	.DATA_WIDTH(DATA_WIDTH)
+	)
+	spi_slave_interface
+	(
+	.CPOL(1'b0),
+	.CPHA(1'b0),
+	.datai(slave_data0),
+	.datao(slave_data0),
+	.sclk(sclk[0]),
+	.csb(csb[0]),
+	.din(din[0]),
+	.dout(dout[0])
+);
 
-// Synchronize the hall inputs to the clock
-reg [2:0] hall_sync_1, hall_sync_2, hall_sync_3, hall_sync_4, hall_sync_5;
-always @(posedge sysclk) begin
-	hall_sync_1 <= hall_1;
-	hall_sync_2 <= hall_2;
-	hall_sync_3 <= hall_3;
-	hall_sync_4 <= hall_4;
-	hall_sync_5 <= hall_5;
-end
 
-hall_counter hall_counter_1(sysclk, hall_sync_1, hall_count_1);
-hall_counter hall_counter_2(sysclk, hall_sync_2, hall_count_2);
-hall_counter hall_counter_3(sysclk, hall_sync_3, hall_count_3);
-hall_counter hall_counter_4(sysclk, hall_sync_4, hall_count_4);
-hall_counter hall_counter_5(sysclk, hall_sync_5, hall_count_5);
+// This handles all of the SPI communications to/from the motor board
+Motor_Board_Comm motor_board_comm_module();
 
-// Motor drivers
-motor md_1(sysclk, pwm_phase, motor_dir[1], motor_speed_1, drive_mode_1, hall_sync_1, {m1a_h, m1a_l, m1b_h, m1b_l, m1c_h, m1c_l}, motor_fault[1]);
-motor md_2(sysclk, pwm_phase, motor_dir[2], motor_speed_2, drive_mode_2, hall_sync_2, {m2a_h, m2a_l, m2b_h, m2b_l, m2c_h, m2c_l}, motor_fault[2]);
-motor md_3(sysclk, pwm_phase, motor_dir[3], motor_speed_3, drive_mode_3, hall_sync_3, {m3a_h, m3a_l, m3b_h, m3b_l, m3c_h, m3c_l}, motor_fault[3]);
-motor md_4(sysclk, pwm_phase, motor_dir[4], motor_speed_4, drive_mode_4, hall_sync_4, {m4a_h, m4a_l, m4b_h, m4b_l, m4c_h, m4c_l}, motor_fault[4]);
-motor md_5(sysclk, pwm_phase, motor_dir[5], motor_speed_5, drive_mode_5, hall_sync_5, {m5a_h, m5a_l, m5b_h, m5b_l, m5c_h, m5c_l}, motor_fault[5]);
 
-// Encoders
-encoder counter1(sysclk, {m1enc_a, m1enc_b}, encoder_1);
-encoder counter2(sysclk, {m2enc_a, m2enc_b}, encoder_2);
-encoder counter3(sysclk, {m3enc_a, m3enc_b}, encoder_3);
-encoder counter4(sysclk, {m4enc_a, m4enc_b}, encoder_4);
+// These are all of the instances for the drive motors and the dribbler
+genvar i;
+generate for (i = 0; i < 5; i = i + 1)
+	begin: Motor_Param_Inst
 
-// Button synchronization for firing kicker manually
-reg charge_enable = 1;
-reg button_sync = 0;
-reg done_sync = 0;
-always @(posedge sysclk) begin
-	button_sync <= ~discharge;
-	done_sync <= ~kdone;
-	
-	if (button_sync) begin
-		charge_enable <= 0;
-	end
-end
+		wire [1:0] enc[i] = { enc_a[i], enc_b[i] };
+		wire [2:0] hall[i] = { hall_a[i], hall_b[i], hall_c[i] };
+		wire [5:0] phaseOut[i] = { phase_aH[i], phase_aL[i], phase_bH[i], phase_bL[i], phase_cH[i], phase_cL[i] };
 
-// Kicker
-wire lockout;
-assign kicker_status = {1'b0, kicker_voltage_ok, kick_select, ~charge_enable, charge, kcharge, lockout, done_sync};
+    end
+endgenerate
+generate for (i = 0; i < 5; i = i + 1)
+	begin: Motor_Inst
 
-wire kick_pulse;
-kicker kicker(sysclk, button_sync, kick_strobe, kick_strength, charge & charge_enable, kcharge, kick_pulse, lockout);
+		BLDC_Motor motor_i (
+			.clk(sysclk),
+			.duty_cycle(duty_cycle_sync[i]),
+			.hall(hall_sync[i]),
+			.enc(enc_sync[i]),
+			.phaseH(phaseH_setup[i]),
+			.phaseL(phaseL_setup[i]),
+			.hall_count(hall_count_setup[i]),
+			.enc_count(enc_count_setup[i])
+		);
 
-// Send the kick pulse to either the kicker or the chipper
-assign kkick = kick_pulse && (kick_select == 0);
-assign kchip = kick_pulse && (kick_select == 1);
-
-// Kicker voltage monitor
-wire scl_out, sda_out;
-kicker_i2c kicker_i2c(sysclk, scl_out, sda_out, ksda, kicker_voltage_ok, kicker_voltage);
-
-assign kscl = scl_out ? 1'bz : 1'b0;
-assign ksda = sda_out ? 1'bz : 1'b0;
+    end
+endgenerate
 
 endmodule
