ea689a7ab5eb617aa6cbf77b63f112e027622b35
Jay Kamat <github@jgkamat.33mail.com>
2015-08-18 02:09:31 -0400

    Fix commiting of ci images and print out test info

--- util/docker/circle/buildcircle.sh
+++ util/docker/circle/buildcircle.sh
@@ -13,9 +13,15 @@ ROBOCUP_ROOT="${DIR}/../../../"
 # Get sha sum
 SHA_SUM_SETUP="$(${ROBOCUP_ROOT}/util/docker/getsetupsha.sh)"
 SHA_SUM="$(git rev-parse HEAD)"
+INPUT_IMAGE_NAME="robojackets/robocup-baseimage"
 IMAGE_NAME="robojackets/robocup-ci"
+
+# Lets trick the dockerfile into using the image we want.
+docker tag -f ${INPUT_IMAGE_NAME}:${SHA_SUM_SETUP} ${INPUT_IMAGE_NAME}:latest_build
+docker push ${INPUT_IMAGE_NAME}:latest_build
 
 docker build -t tmp_ci_image -f ${ROBOCUP_ROOT}/util/docker/circle/Dockerfile ${ROBOCUP_ROOT}
 BUILT_IMAGE="tmp_ci_image"
-DOCKER_IMAGE="$(docker run $BUILT_IMAGE ./util/docker/maketest.sh ${GH_TOKEN})"
-docker commit $DOCKER_IMAGE ${IMAGE_NAME}:${SHA_SUM}
+docker run ${BUILT_IMAGE} ./util/docker/maketest.sh ${GH_TOKEN}
+# Commit the latest image
+docker commit "$(docker ps -aq | head -n1)" ${IMAGE_NAME}:${SHA_SUM}
