62ba416604365fdbcf792cd55362d4fa4508d3d5
Justin Buchanan <justbuchanan@gmail.com>
2015-08-19 18:01:41 -0400

    fixed cmake dependency issue with gtest

--- cmake/SetupGTest.cmake
+++ cmake/SetupGTest.cmake
@@ -3,9 +3,6 @@ include(ExternalProject)
 
 ExternalProject_Add(googletest
     GIT_REPOSITORY ${PROJECT_SOURCE_DIR}/third_party/googletest
-    CMAKE_ARGS -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG:PATH=DebugLibs
-               -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE:PATH=ReleaseLibs
-    PREFIX "${CMAKE_CURRENT_BINARY_DIR}"
     # disable install step
     INSTALL_COMMAND ""
 )
@@ -15,10 +12,12 @@ set_target_properties(googletest PROPERTIES EXCLUDE_FROM_ALL TRUE)
 ExternalProject_Get_Property(googletest source_dir)
 include_directories(${source_dir}/include)
 
-# specify link libraries
+# specify link directory where the built gtest libs are placed
 ExternalProject_Get_Property(googletest binary_dir)
-set(GTEST_BOTH_LIBRARIES
-    ${binary_dir}/libgtest.a
-    ${binary_dir}/libgtest_main.a
+link_directories(${binary_dir})
+
+set(GTEST_LIBRARIES
+    libgtest.a
+    libgtest_main.a
     pthread
 )
