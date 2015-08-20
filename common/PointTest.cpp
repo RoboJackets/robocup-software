fc378378cadd1080add8d0403ceec3c9a62321e6
Justin Buchanan <justbuchanan@gmail.com>
2015-08-20 12:32:34 -0400

    minor test cleanup

--- common/PointTest.cpp
+++ common/PointTest.cpp
@@ -1,12 +1,11 @@
 #include <gtest/gtest.h>
-// #include <../../common/Point.hpp>
 #include <Geometry2d/Point.hpp>
 
 using namespace std;
 using namespace Geometry2d;
 
 
-TEST(testPoint, clampBig) {
+TEST(Point, clampBig) {
     Point p0(10.0, 10.0);
     p0.clamp(10);
     EXPECT_FLOAT_EQ(10, p0.mag());
@@ -14,7 +13,7 @@ TEST(testPoint, clampBig) {
 
 //  if you clamp a vector to a value that's bigger
 //  than it's magnitued, it shouldn't change
-TEST(testPoint, clampSmall) {
+TEST(Point, clampSmall) {
     Point p0(10, 0);
     p0.clamp(15);
     EXPECT_FLOAT_EQ(10, p0.mag());
