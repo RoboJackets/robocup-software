bca149e45b2b4023b547b712c24ae99381699125
Justin Buchanan <justbuchanan@gmail.com>
2015-08-23 23:29:45 -0400

    run checkstyle with circleci

--- util/docker/maketest.sh
+++ util/docker/maketest.sh
@@ -72,7 +72,7 @@ ci_task 'make' 'compile' 'A check to see if the code compiles'
 ci_task 'make test-soccer' 'test-soccer' 'A check to see if soccer tests pass'
 ci_task 'make robot2015' 'firmware' 'A check to see if firmware works'
 ci_task 'make test-firmware' 'test-firmware' 'A check to see if firmware tests pass'
-ci_task 'true' 'style' 'A check to see if style passes'
+ci_task 'make checkstyle' 'style' 'A check to see if style passes'
 
 # This script needs to be run prior with the --pending flag if you want to see pending flags
 if [ "$PENDING" = "true" ]; then
