8d4a20cd73db6c384415fe1997f5402ebc9e75ae
Jon <jjones646@gmail.com>
2015-08-29 17:21:08 -0400

    confirmed that the firmware2015 and firmware2015-prog targets are workingclear

--- util/mbed/mbed-load.sh
+++ util/mbed/mbed-load.sh
@@ -11,11 +11,20 @@ if [ $UID -ne 0 ]; then
     exec sudo $0 $@
 fi
 
+# checks for passed file existence
+if [ ! -e "$1" ]; then
+    echo "${RED}${WHITEBG}${INVT}File $1 not found...${R}"
+    exit 1
+fi
+
 MBED_DEV=$(sudo blkid -L MBED)
 MBED_DEVICES_PATH="$(ls /dev/disk/by-id/ | grep -i mbed | sed 's\.*\/dev/disk/by-id/&\g')"
 
 if [ -z "$MBED_DEV" ]; then
-    echo "--  No mbed device(s) found"
+    msg="No mbed device detected!"
+    printf "$WHITE $REDBG $PUTLN \n"
+    printf "%*s\n" $(((${#msg}+$(tput cols))/2)) "$msg"     # center align the message
+    printf "$PUTLN$R\n"
     exit 0
 else
     echo "--  device found at $MBED_DEV"
@@ -25,12 +34,35 @@ else
 
     if [ -z "$MBED_DEV" ];then
         if [ -z "${MNT_DIR#/}" ]; then
-            echo "--  mbed is not mounted"
+            msg="mbed found but has no mount point!"
+            printf "$WHITE $REDBG $PUTLN \n"
+            printf "%*s\n" $(((${#msg}+$(tput cols))/2)) "$msg"     # center align the message
+            printf "$PUTLN$R\n"
             exit 0
         fi
     else
         PRGM_SCRIPT="$SCRIPT_DIR/mbed-prgm.py"
-        sudo python $PRGM_SCRIPT --file=$1 --destination=$MNT_DIR
+        python $PRGM_SCRIPT --file=$1 --destination=$MNT_DIR
+
+        if [ $? -eq 0 ]; then
+        # Yay, we made it!
+        msg1="${BOLD}File transfer SUCCESS!${AOFF}${WHITE}${GREENBG}"
+        msg2="Sent ${BLUEBG}$1${GREENBG} to the mbed${AOFF}${WHITE}${GREENBG}"
+
+        printf "$WHITE$GREENBG$PUTLN\n"
+        printf "%*s\n" $(((${#msg1}+$(tput cols))/2)) "$msg1"   # center align the message
+        printf "%*s\n" $(((${#msg2}+$(tput cols))/2)) "$msg2"     
+        printf "$PUTLN$R\n"
+     else
+        # something went wrong
+        msg="${BOLD}File transfer FAILED!${AOFF}${WHITE}${GREENBG}"
+        msg2="Could not send ${BLUEBG}$1${GREENBG} to the mbed${AOFF}${WHITE}${GREENBG}"
+
+        printf "$REDBG$PUTLN\n"
+        printf "%*s\n" $(((${#msg1}+$(tput cols))/2)) "$msg1"   # center align the message
+        printf "%*s\n" $(((${#msg2}+$(tput cols))/2)) "$msg2"     
+        printf "$PUTLN$R\n"
+    fi
     fi
 
 fi
