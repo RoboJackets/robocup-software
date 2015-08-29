d76ef8490bd0ac79f3dec5998c80ba0ccafb9dd5
Jon <jjones646@gmail.com>
2015-08-29 17:28:37 -0400

    changing the hw-test test script so it doesn't upload each target to the mbed

--- util/mbed/mbed-run-tests.sh
+++ util/mbed/mbed-run-tests.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $SCRIPT_DIR/colors
 
 cd $SCRIPT_DIR
-cd ..
+cd ../..
 set -e
 
-make robot2015-i2c-prog
-make robot2015-io-expander-prog
-make robot2015-piezo-prog
-# synthesize the fpga's bitfile and move it to the mbed for the fpga test
-make fpga2015-prog
-make robot2015-fpga-prog
+make robot2015-i2c
+make robot2015-io-expander
+make robot2015-piezo
+make robot2015-fpga
+
+msg="${BOLD}All 2015 firmware hw-test targets successfully built!${AOFF}${WHITE}${GREENBG} "
+printf "$WHITE $GREENBG $PUTLN \n $PUTLN \n"
+printf "%*s\n" $(((${#msg}+$(tput cols))/2)) "$msg"
+printf "$PUTLN \n $PUTLN$R\n"
