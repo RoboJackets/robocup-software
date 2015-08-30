3e327030112278246785a4bb4c4f2585fddb7589
Justin Buchanan <justbuchanan@gmail.com>
2015-08-29 22:11:49 -0400

    ran reformatter

--- soccer/TestMain.cpp
+++ soccer/TestMain.cpp
@@ -6,7 +6,8 @@ int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
 
     // setup config system because some tests rely on it
-    std::shared_ptr<Configuration> config = Configuration::FromRegisteredConfigurables();
+    std::shared_ptr<Configuration> config =
+        Configuration::FromRegisteredConfigurables();
 
     return RUN_ALL_TESTS();
 }
