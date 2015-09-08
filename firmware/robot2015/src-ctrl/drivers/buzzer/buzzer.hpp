f174e639dedb5310aa924f7c6221a35ae4686c05
Jon <jjones646@gmail.com>
2015-09-08 17:16:51 -0400

    adding cmake i2c file & starting buzzer class for the control board's piezo speaker

---
+++ firmware/robot2015/src-ctrl/drivers/buzzer/buzzer.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <mbed.h>
+#include <rtos.h>
+
+#include "pins-ctrl-2015.hpp"
+
+void analogUpdate(void const* args);
+
+class Buzzer : public AnalogOut {
+public:
+    Buzzer() : AnalogOut(RJ_SPEAKER) {};
+    ~Buzzer() {};
+
+    void play(float freq, int dur, float vol = 1.0);
+
+    int getIndex(void);
+    void setIndex(int newIndex);
+
+private:
+    friend void analogUpdate(void const* args);
+
+    int j;
+};
