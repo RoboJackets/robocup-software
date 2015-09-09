61b09a47cbbaa8981022d09e3be948e2bb2dcf87
Justin Buchanan <justbuchanan@gmail.com>
2015-09-09 18:26:32 -0400

    a little cleanup and refactoring

--- soccer/planning/InterpolatedPath.hpp
+++ soccer/planning/InterpolatedPath.hpp
@@ -56,8 +56,8 @@ public:
 
     // Overridden Path Methods
     virtual bool valid() const override { return !waypoints.empty(); }
-    virtual MotionInstant end() const override;
     virtual MotionInstant start() const override;
+    virtual MotionInstant end() const override;
     virtual bool hit(const Geometry2d::ShapeSet& obstacles, float& hitTime,
                      float startTime) const override;
     virtual std::unique_ptr<Path> subPath(
