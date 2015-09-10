8bbf0cc58e11d41cd5cb064396582f19d471e298
Justin Buchanan <justbuchanan@gmail.com>
2015-09-09 22:45:47 -0400

    docs

--- soccer/planning/Path.hpp
+++ soccer/planning/Path.hpp
@@ -64,20 +64,20 @@ public:
     /**
      * Returns a subPath
      *
-     * @param[in]	startTime The startTime for from which the subPath should be
-     taken.
-     * @param[in] 	endTime The endTime from which the subPath should be taken.
-     If it is greater than the duration fo the path,
-                         it should go to the end of the path.
-     * @return 	A unique_ptr to the new subPath
+     * @param startTime The startTime for from which the subPath should be
+     *     taken.
+     * @param endTime The endTime from which the subPath should be taken. If it
+     *     is greater than the duration fo the path, it should go to the end of
+     *     the path.
+     * @return A unique_ptr to the new subPath
      */
     virtual std::unique_ptr<Path> subPath(
         float startTime = 0,
         float endTime = std::numeric_limits<float>::infinity()) const = 0;
 
-    /// Endpoints of the path.  Will raise an InvalidPathException if the path
-    /// is not valid.
+    /// Start instant of the path
     virtual MotionInstant start() const = 0;
+    /// Destination instant of the path
     virtual MotionInstant end() const = 0;
 
     /**
