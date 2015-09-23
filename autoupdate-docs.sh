29d2cd556a9fb8801fc02ffd77c026b568ef0ee4
Justin Buchanan <justbuchanan@gmail.com>
2015-09-23 16:37:23 -0400

    moved doxygen files into doc directory

--- autoupdate-docs.sh
+++ autoupdate-docs.sh
@@ -8,8 +8,8 @@ git config --global user.name "$GIT_USERNAME"
 git config --global user.email $GIT_EMAIL
 mkdir -p api_docs
 git clone -b gh-pages git://github.com/robojackets/robocup-software api_docs/html
-doxygen
-cp doxygen.css api_docs/html
+doxygen doc/Doxyfile
+cp doc/doxygen.css api_docs/html
 cd api_docs/html
 git add --all
 git commit -m 'auto-updated api docs'
