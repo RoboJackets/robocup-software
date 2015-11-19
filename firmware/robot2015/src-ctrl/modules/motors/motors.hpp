dfee5f93d240133b304d7ce2e9895049090c2510
Justin Buchanan <justbuchanan@gmail.com>
2015-11-18 23:25:19 -0500

    removed GPL from motors.hpp

--- firmware/robot2015/src-ctrl/modules/motors/motors.hpp
+++ firmware/robot2015/src-ctrl/modules/motors/motors.hpp
@@ -1,23 +1,3 @@
-/**
- * RoboJackets: RoboCup SSL Firmware
- *
- * Copyright (C) 2015 RoboJackets JJ
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, in version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * motors.hpp - BLDC data structures.
- */
-
 #pragma once
 
 // Includes
