ba2f44f86604d9838fbcd88bd411a902b39be122
Jon <jjones646@gmail.com>
2015-11-23 01:38:00 -0500

    changing NULL to nullptr and adding assertion check

--- firmware/common2015/utils/rtos-mgmt/thread-helper.cpp
+++ firmware/common2015/utils/rtos-mgmt/thread-helper.cpp
@@ -1,6 +1,10 @@
 #include "thread-helper.hpp"
 
-// Helper function for defining threads
+#include "assert.hpp"
+
+// Helper function for creating a class that uses a
+// member function for the object's thread operations
+
 void define_thread(osThreadDef_t& t, void (*task)(void const* arg),
                    osPriority priority, uint32_t stack_size,
                    unsigned char* stack_pointer) {
@@ -9,14 +13,11 @@ void define_thread(osThreadDef_t& t, void (*task)(void const* arg),
     t.tpriority = priority;
     t.stacksize = stack_size;
 
-    if (stack_pointer != NULL) {
+    if (stack_pointer != nullptr) {
         t.stack_pointer = (uint32_t*)stack_pointer;
-        //_dynamic_stack = false;
     } else {
         t.stack_pointer = (uint32_t*)new unsigned char[t.stacksize];
-        if (t.stack_pointer == NULL)
-            error("Error allocating the stack memory\n");
-        //_dynamic_stack = true;
+        ASSERT(t.stack_pointer == nullptr);
     }
 #endif
 }
--- firmware/common2015/utils/rtos-mgmt/thread-helper.hpp
+++ firmware/common2015/utils/rtos-mgmt/thread-helper.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
 #include "mbed.h"
-#include "cmsis_os.h"
-//#include "rtos.h"
+#include "rtos.h"
 
 void define_thread(osThreadDef_t&, void (*task)(void const* arg),
                    osPriority = osPriorityNormal, uint32_t = DEFAULT_STACK_SIZE,
-                   unsigned char* = NULL);
+                   unsigned char* = nullptr);
