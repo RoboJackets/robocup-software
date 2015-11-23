c0d49739d1c20032afae765e1213df4319ef8066
Jon <jjones646@gmail.com>
2015-11-23 05:02:38 -0500

    condensing the thread helper function

--- firmware/common2015/utils/rtos-mgmt/thread-helper.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "mbed.h"
-#include "rtos.h"
-
-void define_thread(osThreadDef_t&, void (*task)(void const* arg),
-                   osPriority = osPriorityNormal, uint32_t = DEFAULT_STACK_SIZE,
-                   unsigned char* = nullptr);
