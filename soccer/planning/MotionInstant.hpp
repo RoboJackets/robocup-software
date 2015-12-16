4c76acd1ca630f9be9a7cd5b3ec65724170364d5
Albert Shaw <ashaw596@gmail.com>
2015-12-15 21:12:57 -0500

    Add RobotInstant and AngleInstant

--- soccer/planning/MotionInstant.hpp
+++ soccer/planning/MotionInstant.hpp
@@ -4,27 +4,53 @@
 
 namespace Planning {
 
-/**
- * This class represents a robot's motion "state" at a given time, including
- * position and velocity.
- */
-struct MotionInstant {
-    explicit MotionInstant(Geometry2d::Point pos = {0, 0},
-                           Geometry2d::Point vel = {0, 0})
-        : pos(pos), vel(vel) {}
+    /**
+     * This class represents a robot's motion "state" at a given time, including
+     * position and velocity.
+     */
+    struct MotionInstant {
+        explicit MotionInstant(Geometry2d::Point pos = {0, 0},
+                               Geometry2d::Point vel = {0, 0})
+            : pos(pos), vel(vel) {}
 
-    Geometry2d::Point pos;
-    Geometry2d::Point vel;
+        Geometry2d::Point pos;
+        Geometry2d::Point vel;
 
-    friend bool operator==(const MotionInstant& a, const MotionInstant& b) {
-        return a.pos == b.pos && a.vel == b.vel;
-    }
+    //    friend bool operator==(const MotionInstant& a, const MotionInstant& b) {
+    //        return a.pos == b.pos && a.vel == b.vel;
+    //    }
 
-    friend std::ostream& operator<<(std::ostream& stream,
-                                    const MotionInstant& instant) {
-        return stream << "MotionInstant(pos=" << instant.pos
-                      << ", vel=" << instant.vel << ")";
-    }
-};
+        friend std::ostream& operator<<(std::ostream& stream,
+                                        const MotionInstant& instant) {
+            return stream << "MotionInstant(pos=" << instant.pos
+                          << ", vel=" << instant.vel << ")";
+        }
+    };
 
+    /**
+     * This class represents a robot's angle "state" at a given time, including
+     * angle and rotation velocity.
+     */
+    struct AngleInstant {
+        explicit AngleInstant(float angle = 0, float angleVel = 0) : angle(angle), angleVel(angleVel) {};
+        float angle;
+        float angleVel;
+
+        //TODO ashaw596 implement stream operator
+    };
+
+
+    /**
+     * This class represents a robot's motion and rotation "state" at a given time, including
+     * position, velocity, angle, and rotationVelocity.
+     */
+    struct RobotInstant {
+        explicit RobotInstant(MotionInstant motion = MotionInstant(),
+                              AngleInstant angle = AngleInstant())
+                : motion(motion), angle(angle) {}
+        MotionInstant motion;
+        AngleInstant angle;
+
+        //TODO ashaw596  implement stream operator
+    };
 }  // namespace Planning
