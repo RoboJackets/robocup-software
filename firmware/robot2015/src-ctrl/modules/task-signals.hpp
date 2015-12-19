6cb5ecfc4222c87a9d9f216c4f6fd9776fbf7cf6
Jon <jjones646@gmail.com>
2015-12-19 02:54:45 -0500

    cleaning up task-signals

--- firmware/robot2015/src-ctrl/modules/task-signals.hpp
+++ firmware/robot2015/src-ctrl/modules/task-signals.hpp
@@ -5,6 +5,3 @@
 
 static const uint32_t MAIN_TASK_CONTINUE = 1 << 0;
 static const uint32_t SUB_TASK_CONTINUE = 1 << 1;
-// static const uint32_t COMM_TASK_CONTINUE = 1 << 2;
-// static const uint32_t CTRL_TASK_CONTINUE = 1 << 3;
-// static const uint32_t CONS_TASK_CONTINUE = 1 << 4;
