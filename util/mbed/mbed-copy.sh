149eb7782d6e05c2458919baadb2a92c20175b8e
Jon <jjones646@gmail.com>
2016-01-18 23:48:10 -0500

    reverting mbed-copy changes from failed udev rule attempts

--- util/mbed/mbed-copy.sh
+++ util/mbed/mbed-copy.sh
@@ -17,9 +17,12 @@ if [ ! -e "/dev/disk/by-id" ]; then
     exit 2
 fi
 
-SHA2="$(sha256sum $1 | awk '{print $1}')"
+MBED_DEVICES="$(ls /dev/disk/by-id/ | grep -i mbed)"
 
-MBED_DEVICES="$(ls /mnt/robot2015/ | grep -i mbed)"
+# This prepends the directory structure to all found mbed devices
+MBED_DEVICES_PATH="$(ls /dev/disk/by-id/ | grep -i mbed | sed 's\.*\/dev/disk/by-id/&\g')"
+
+SHA2="$(sha256sum $1 | awk '{print $1}')"
 
 # errors out if no mbed devices were found
 if [ -z $MBED_DEVICES ]; then
@@ -27,40 +30,42 @@ if [ -z $MBED_DEVICES ]; then
     exit 3
 fi
 
+echo -e Devices path is $MBED_DEVICES_PATH
+
+# this causes the script to fail if any command below fails
+set -e
+
+# use newlines as delimiters in the forloop, rather than all whitespace
+IFS=$'\n'
+
 # loop through all mbed devices, mount them, copy over the bin file, and unmount them
-pushd /mnt/robot2015 &> /dev/null
-for path in ./mbed*; do
-    [ -d "${path}" ] || continue # if not a directory, skip
-    i="$(basename "${path}")"
+for i in $MBED_DEVICES_PATH; do
     echo Installing on $i
-    cp -f $1 $i
-done; popd &> /dev/null
+    sudo mkdir -p /mnt/script/MBED
+    sudo mount $i /mnt/script/MBED
+    sudo cp -f -b $1 /mnt/script/MBED/
+
+    if [ "$SHA2" != "$(sha256sum /mnt/script/MBED/$(echo "$1" | awk 'BEGIN {FS = "/"}; {print $NF}') | awk '{print $1}')" ]; then
+        echo ERROR: Sha Hashes do not match, copy failed. Exiting...
+        exit 1
+    else
+        echo Sha256sums of binary files match, copy successful!
+    fi
+done
 
 MEM_SPACE=$(($(stat --printf="%s" $1)/512))
 echo $(($MEM_SPACE/10))"."$(($MEM_SPACE%10))"% of available memory filled."
 
 # here's hoping the copy takes less than 5 seconds
+# TODO find a better solution...
 sleep 4
-
-# use newlines as delimiters in the forloop, rather than all whitespace
-IFS=$'\n'
 
 # restart mbed code
 MBED_SERIAL_PATH="$(ls /dev/ | grep ttyACM | sed 's\.*\/dev/&\g')"
 
-# send break signal to all mbeds
 for i in $MBED_SERIAL_PATH; do
-python3 - <<PY_END
-
-import serial
-try:
-    print("Attempting reboot on $i")
-    s = serial.Serial("$i", baudrate=57600, timeout=5)
-    s.sendBreak()
-    s.close()
-    print("Reboot started")
-except:
-    print("Failed to reboot $i")
-
-PY_END
+    echo Attempting reboot on $i ...
+    # send break signal to all mbeds
+    sudo python3 -c "import serial; serial.Serial(\"$i\").sendBreak()"
 done
+
