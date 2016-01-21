b5423683d900e445df2a0f716f8fbd3849995978
Jon <jjones646@gmail.com>
2016-01-21 01:22:18 -0500

    make pretty

--- firmware/robot2015/src-ctrl/modules/task-globals.hpp
+++ firmware/robot2015/src-ctrl/modules/task-globals.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 // Global variables shared between task threads for error flagging.
-// The LSB of each halfword indicates where the error code is valid or not. 
+// The LSB of each halfword indicates where the error code is valid or not.
 
 extern uint16_t comm_err;
 extern uint16_t fpga_err;
