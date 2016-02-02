e9e857bddc082a6030bc88cab0763e5b237ee1c4
JNeiger <fcdneiger@gmail.com>
2016-02-02 18:56:17 -0500

    make pretty

--- firmware/robot2015/cpu/ball_sense.cpp
+++ firmware/robot2015/cpu/ball_sense.cpp
@@ -2,41 +2,36 @@
 #include "robot.hpp"
 
 ball_sense::ball_sense() {
-	emitter_pin = new DigitalOut(RJ_BALL_EMITTER);
-	detector_pin = new AnalogIn(RJ_BALL_DETECTOR);	
-	
-	emitter_pin->write(0);
+    emitter_pin = new DigitalOut(RJ_BALL_EMITTER);
+    detector_pin = new AnalogIn(RJ_BALL_DETECTOR);
+
+    emitter_pin->write(0);
 }
 
 void ball_sense::update_ball_sensor() {
-	if (emitter_on)
-	{
-		// Update value
-		sense_light = detector_pin->read_u16();
-		
-		// Shutoff light	
-		emitter_on = false;
-		emitter_pin->write(0);
+    if (emitter_on) {
+        // Update value
+        sense_light = detector_pin->read_u16();
 
-		// Possible break in beam 
-		if (sense_light - sense_dark > sense_threshold)
-		{
-			consec_ctr++;
-		} else
-		{
-			consec_ctr = 0;
-		}
-	} else // Emitter off
-	{
-		// Update value
-		sense_dark = detector_pin->read_u16();
+        // Shutoff light
+        emitter_on = false;
+        emitter_pin->write(0);
 
-		// Turn on light
-		emitter_on = true;
-		emitter_pin->write(1);
-	}
+        // Possible break in beam
+        if (sense_light - sense_dark > sense_threshold) {
+            consec_ctr++;
+        } else {
+            consec_ctr = 0;
+        }
+    } else  // Emitter off
+    {
+        // Update value
+        sense_dark = detector_pin->read_u16();
+
+        // Turn on light
+        emitter_on = true;
+        emitter_pin->write(1);
+    }
 }
 
-bool ball_sense::have_ball() {
-	return consec_ctr >= consec_num;
-} 
+bool ball_sense::have_ball() { return consec_ctr >= consec_num; }
--- firmware/robot2015/cpu/ball_sense.hpp
+++ firmware/robot2015/cpu/ball_sense.hpp
@@ -4,34 +4,35 @@
 
 class ball_sense {
 public:
-	ball_sense();
+    ball_sense();
 
-	// Call once per cycle
-	void update_ball_sensor();
-	
-	// Returns true if beam is broken (Ball is contained)
-	bool have_ball();
+    // Call once per cycle
+    void update_ball_sensor();
+
+    // Returns true if beam is broken (Ball is contained)
+    bool have_ball();
+
 private:
-	DigitalOut *emitter_pin;
-	AnalogIn   *detector_pin;
+    DigitalOut* emitter_pin;
+    AnalogIn* detector_pin;
 
-	// Holds the light sensed when
-	//  emitter is lit (sense_light) and
-	//  dark (sense_dark)
-	int sense_light = 0, sense_dark = 0;
-	
-	// Emitter state (Light or dark)
-	bool emitter_on = false;
+    // Holds the light sensed when
+    //  emitter is lit (sense_light) and
+    //  dark (sense_dark)
+    int sense_light = 0, sense_dark = 0;
 
-	// Difference between the light and dark
-	//  values before the sensor is considered
-	//  broken
-	const int sense_threshold = 8000;
+    // Emitter state (Light or dark)
+    bool emitter_on = false;
 
-	// Number of consecative "broken" senses
-	//  before we are confident the beam is broken
-	const unsigned int consec_num = 2;
-	
-	// Consecative "broken" senses counter
-	unsigned int consec_ctr;
+    // Difference between the light and dark
+    //  values before the sensor is considered
+    //  broken
+    const int sense_threshold = 8000;
+
+    // Number of consecative "broken" senses
+    //  before we are confident the beam is broken
+    const unsigned int consec_num = 2;
+
+    // Consecative "broken" senses counter
+    unsigned int consec_ctr;
 };
