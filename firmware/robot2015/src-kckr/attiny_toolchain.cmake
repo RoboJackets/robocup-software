417a8e00d3ce60ce5f4388b75d89a215a073fd01
Jon <jjones646@gmail.com>
2016-02-06 20:36:18 -0500

    fixing firmware build for kicker board

--- firmware/robot2015/src-kckr/attiny_toolchain.cmake
+++ firmware/robot2015/src-kckr/attiny_toolchain.cmake
@@ -1,6 +1,10 @@
 set(CMAKE_SYSTEM_NAME       Generic)
 set(CMAKE_SYSTEM_PROCESSOR  avr)
 set(CMAKE_SYSTEM_VERSION    1)
+set(AVR_TARGET_ARCH         attiny84a)
+
+set(CMAKE_C_COMPILER_TARGET     ${AVR_TARGET_ARCH})
+set(CMAKE_CXX_COMPILER_TARGET   ${AVR_TARGET_ARCH})
 
 # narrow down the search scope of where cmake looks for programs/libraries
 # for cross compilation
