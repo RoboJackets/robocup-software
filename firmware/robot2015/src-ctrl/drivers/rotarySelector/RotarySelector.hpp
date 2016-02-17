0abdf08f230d2c6e185566c4fcbb3d095026dd8b
Osama Sakhi <msakhi21@gmail.com>
2016-02-16 20:23:16 -0500

    Header file now compiled

--- firmware/robot2015/src-ctrl/drivers/rotarySelector/RotarySelector.hpp
+++ firmware/robot2015/src-ctrl/drivers/rotarySelector/RotarySelector.hpp
@@ -8,9 +8,19 @@ template<typename IN_OUT>
 class RotarySelector {
 
 public:
-    RotarySelector(IN_OUT mb3, IN_OUT mb2, IN_OUT mb1, IN_OUT mb0);
+    RotarySelector::RotarySelector(IN_OUT mb3, IN_OUT mb2, IN_OUT mb1, IN_OUT mb0)
+	: b3(mb3), b2(mb2), b1(mb1), b0(mb0)
+	{}
 
-    int read();
+    /**
+	 * Gives the reading (0x0 - 0xF) of the 4 wires
+	*/
+	int RotarySelector::read()
+	{
+	    // Assumes b3 has Most Significant Bit, b0 has Least Significant Bit
+	    int reading = b3.read() << 3 || b2.read() << 2 || b1.read() << 1 || b0.read();
+	    return reading;
+	}
 
 private:
     IN_OUT b0;
