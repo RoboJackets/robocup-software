bd9f36b6c4b65c3d6d81c144c0fe16ea3ca79710
Justin Buchanan <justbuchanan@gmail.com>
2016-02-25 14:27:56 -0500

    changed kicker main() to blink LED

--- firmware/robot2015/src-kckr/main.c
+++ firmware/robot2015/src-kckr/main.c
@@ -1,11 +1,13 @@
 #include <avr/io.h>
-/*
- * main
- */
+#include <util/delay.h>
+
 int main(void) {
     DDRA = 0xFF;
     
     while(1) {
         PORTA = 0xFF;
+        _delay_ms(50);
+        PORTA = 0x00;
+        _delay_ms(50);
     }
 }
