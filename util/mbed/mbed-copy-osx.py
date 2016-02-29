211683f001b341da52dfed2d0c39505aee40b3c0
Jon <jjones646@gmail.com>
2016-02-29 09:03:36 -0500

    adding python based copy script
    fixing copy script name
    
    adding reset support
    
    adding pyserial to the list for pip2

--- util/mbed/mbed-copy-osx.py
+++ util/mbed/mbed-copy-osx.py
@@ -1,29 +1,57 @@
 #!/usr/bin/env python
 
-# import os
-# import subprocess
-# import xml.etree.ElementTree as et
-import psutil
+import os
+import time
+import shutil
+import serial
+import argparse
+import mbed_lstools as mbedls
+from os.path import abspath, isfile
 
-# def runCmd(proc):
-#     p = subprocess.Popen(proc.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-#     while (True):
-#         retcode = p.poll()
-#         line = p.stdout.readline()
-#         yield line
-#         if (retcode is not None):
-#             break
+# parse in the given file(s)
+parser = argparse.ArgumentParser(description='copies one or more given files to all connected mbed devices.')
+parser.add_argument('files', nargs='+', help='files to move to all connected mbeds')
+args = parser.parse_args()
 
-# Query the USB devices in XML format
-# xmlData = ''
-# for l in runCmd('system_profiler -xml SPUSBDataType'):
-#     xmlData += str(l.replace('\n', '').replace('\t', '').replace(' ', ''))
 
-# xmlTree = et.fromstring(xmlData)
-# for c in xmlTree:
-#     print("{} => {}".format(c.tag, c.attrib))
+def sync_os():
+    try:
+        if hasattr(os, 'sync'):
+            os.sync()
+        else:
+            import ctypes
+            libc = ctypes.CDLL("libc.so.6")
+            libc.sync()
+    except:
+        import subprocess
+        subprocess.check_call(['sync'], shell=True)
 
-# Query the system block device partitions
-diskData = psutil.disk_partitions()
-print(diskData)
+# only select the files that exist from what was given
+files = []
+for file in args.files:
+    f = abspath(file)
+    if isfile(f):
+        files.append(f)
+    else:
+        print('{} does not exist'.format(file))
 
+# get the array of mbeds that are connected
+mbeds = mbedls.create().list_mbeds()
+
+# iterate copying all files to all mbeds
+for mbed in mbeds:
+    # move the files over
+    dst = mbed['mount_point']
+    for f in files:
+        shutil.copy2(f, dst)
+        print('copying {} to {}'.format(f, dst))
+        sync_os()
+    # reset the mbed
+    for x in range(2):
+        try:
+            ss = serial.Serial(mbed['serial_port'], baudrate=57600)
+            ss.sendBreak()
+            ss.flush()
+        except:
+            pass
+        time.sleep(2)
