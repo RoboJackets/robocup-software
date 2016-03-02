1cab36a0612712a5c98897e51b5f588083ce5462
Jpfeltracco <jpfeltracco@gatech.edu>
2016-03-01 19:03:17 -0500

    Added Slave Select functionality and bug-fixed.

--- firmware/robot2015/src-ctrl/main.cpp
+++ firmware/robot2015/src-ctrl/main.cpp
@@ -1,18 +1,50 @@
-/**
- * Program an AVR with an mbed.
- */
+#include "mbed.h"
 #include "KickerBoard.hpp"
 
+
+
+// DigitalOut cs(p8);
+Serial pc(USBTX, USBRX); // tx and rx
 LocalFileSystem local("local");
+int t = 0;
+bool btnState = 0;
+bool btnRead;
+
+int transferAndWait(const char what, SPI& spi);
+
 
 int main() {
-    Serial pc(USBTX, USBRX);
-    pc.baud(57600);
-
+    pc.baud(57600); //set up the serial
     printf("About to flash kicker\r\n");
     KickerBoard kicker(p5,p6,p7,p8, "/local/rj-kickr.nib");
     bool success = kicker.flash(false, true);
     printf("Flashed kicker, success = %s\r\n", success ? "TRUE" : "FALSE");
 
-    return 0;
+    DigitalOut n_kick_select(p9);
+    n_kick_select = 0;
+    // Setup the spi for 8 bit data, high steady state clock,
+    SPI spi(p5, p6, p7); // mosi, miso, sclk
+    // mode 0 doesn't seem to work right, TODO investigate if necessary
+    // we may have to use chip select to get this working properly
+    spi.format(8,0); // allow system to work with arduino
+    spi.frequency(8000);
+
+    while(1) {
+        if (pc.readable()) {
+            pc.getc();
+            n_kick_select = !n_kick_select;
+        }
+        int a;
+        a = transferAndWait(255, spi);
+        pc.printf("Received:");
+        pc.printf("%d\r\n", a);
+        wait(.1); // tested down to .025 seconds between
+    }
 }
+
+int transferAndWait (const char what, SPI& spi)
+{
+    int a = spi.write(what);
+    wait(.020);
+    return a;
+} // end transferAndWait
--- firmware/robot2015/src-kckr/main.c
+++ firmware/robot2015/src-kckr/main.c
@@ -8,54 +8,119 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
 
 /* Bit manip. defines for clarity */
 // BV = bit value
-#define BV(x)           (1 << x)
-#define setBit(P,B)     (P |= BV(B))
-#define clearBit(P,B)   (P &= ~BV(B))
-#define toggleBit(P,B)  (P ^= BV(B))
+#define BV(x)            (1 << x)
+#define SET_BIT(P,B)     (P |= BV(B))
+#define CLEAR_BIT(P,B)   (P &= ~BV(B))
+#define TOGGLE_BIT(P,B)  (P ^= BV(B))
 
-#define DO  PA5 
-#define LED PA0
+/* Inputs */
+#define VOLTAGE     PA1
+#define N_KICK_CS   PA7
 
-volatile char hadInterrupt = 0;
-volatile char data = 0;
+/* Outputs */
+#define KICK        PA2
+#define CHIP        PA3
+#define DO          PA5
+#define LED         PB0
+#define CHARGE      PB1
+
+volatile char had_interrupt_ = 0;
+volatile uint8_t data_ = 0;
+uint8_t spi_enabled = 0;
+
+uint8_t get_voltage();
+void init();
 
 void main()
-{
-    /* Port direction settings */
-    setBit(DDRA, DO); // Set DO/MISO OUT
-    setBit(DDRA, LED); // Set LED OUT
+{    
+    init();
 
-    /* SPI init - Pg. 120 */
-    // 3 Wire Mode DO, DI, USCK - Pg. 124
-    setBit(USICR, USIWM0);
-    // External, positive edge clock - Pg. 125 
-    setBit(USICR, USICS1);
-    // Enable Global Interrupts - Required for below
-    sei();
-    // Enable interrupt
-    setBit(USICR, USIOIE);
-    
     while (1) {
-        if (hadInterrupt) {
-            // Reset interrupt flag
-            hadInterrupt = 0;
-            
-            // Simulate kick by toggling LED
-            if (data == 'k') {
-                toggleBit(PORTA, LED);
+        if (!(PINA & BV(N_KICK_CS))) { // Check if N_KICK_CS == 0
+            if (!spi_enabled) {
+                SET_BIT(USICR, USIOIE); // Enable ISR
+                SET_BIT(DDRA, DO); // Drive DO
+                spi_enabled = 1;
             }
+
+            if (had_interrupt_) {
+                // Simulate kick by toggling LED
+                if (data_ == (uint8_t) 255) {
+                    TOGGLE_BIT(PORTB, LED);
+                }
+                // Reset interrupt flag
+                had_interrupt_ = 0;
+
+                USIDR = get_voltage();
+                //USIDR = data_; 
+            }
+
+        } else if (spi_enabled) {
+            CLEAR_BIT(USICR, USIOIE); // Disable ISR
+            CLEAR_BIT(DDRA, DO); // DO to Z
+            spi_enabled = 0;
         }
+
     }
 }
 
+/* SPI Interrupt */
 ISR(USI_OVF_vect)
 {
-    data = USIBR;
-    hadInterrupt = 1;
-    // Set this to voltage reading later
-    USIDR = 'z';
-} 
+    // Get data from USIDR
+    // Using USIBR caused a bit to be shifted
+    data_ = USIDR;
+    // Clear the interrupt flag (would have been done by USIBR)
+    SET_BIT(USISR, USIOIF);
+    had_interrupt_ = 1;
+}
+
+void init() 
+{
+    /* Port direction settings */
+    SET_BIT(DDRA, KICK);
+    SET_BIT(DDRA, CHIP);
+    SET_BIT(DDRA, DO);
+    SET_BIT(DDRB, LED);
+    SET_BIT(DDRB, CHARGE);
+
+    SET_BIT(PORTA, N_KICK_CS);
+
+    /* SPI init - Pg. 120 */
+    // 3 Wire Mode DO, DI, USCK - Pg. 124
+    SET_BIT(USICR, USIWM0);
+    // External, negative edge clock - Pg. 125 
+    SET_BIT(USICR, USICS1);
+    // SET_BIT(USICR, USICS0);
+    // Enable Global Interrupts - Required for below
+    sei();
+    // Enable SPI interrupt
+    SET_BIT(USICR, USIOIE);
+
+    /* ADC Initialization */
+    CLEAR_BIT(PRR, PRADC); // disable power reduction Pg. 133
+    SET_BIT(ADCSRA, ADEN); // enable the ADC - Pg. 133
+    SET_BIT(ADCSRB, ADLAR); // present left adjusted
+
+    // because we left adjusted and only need
+    // 8 bit precision, we can now read ADCH directly
+}
+
+/* Voltage Function */
+uint8_t get_voltage()
+{
+    // Hard-coded for PA1
+    //SET_BIT(ADMUX, MUX0);
+    //Set lower three bits to value of pin we read from
+    ADMUX |= VOLTAGE;
+    // Start conversation by writing to start bit
+    SET_BIT(ADCSRA, ADSC);
+    // Wait for ADSC bit to clear
+    while (ADCSRA & BV(ADSC));
+    // ADHC will go from 0 to 255 corresponding to
+    // 0 through VCC
+    return ADCH;
+}
