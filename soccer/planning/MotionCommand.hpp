08a540415bf4ff6252f0d8980b8b6536ad8c0fa1
Albert Shaw <ashaw596@gmail.com>
2016-03-06 14:41:34 -0500

    Added debug tag to robot commands

--- soccer/planning/MotionCommand.hpp
+++ soccer/planning/MotionCommand.hpp
@@ -17,6 +17,7 @@ public:
     virtual ~MotionCommand() = default;
     CommandType getCommandType() const { return commandType; }
     virtual std::unique_ptr<Planning::MotionCommand> clone() const = 0;
+    int debug;
 
 protected:
     MotionCommand(const MotionCommand& that) = default;
