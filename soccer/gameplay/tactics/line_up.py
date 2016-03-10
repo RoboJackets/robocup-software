750f9532ec1a43138f00f9101ea3d6b0a7607c4a
Albert Shaw <ashaw596@gmail.com>
2016-03-10 12:48:44 -0500

    Revert "Fix line_up to work with less robots in simulator"
    This reverts commit 7be83fb7a8bd85b0fc7badd7b7ca944cfd180d33.

--- soccer/gameplay/tactics/line_up.py
+++ soccer/gameplay/tactics/line_up.py
@@ -3,7 +3,7 @@ import behavior
 import skills.move
 import robocup
 import constants
-import main
+
 
 
 class LineUp(composite_behavior.CompositeBehavior):
@@ -33,9 +33,7 @@ class LineUp(composite_behavior.CompositeBehavior):
                             'robots arent lined up')
 
     def execute_running(self):
-        robots = [robot for robot in main.our_robots() if robot is not None]
-        number = len(robots)
-        for i in range(number):
+        for i in range(6):
             pt = self._line.get_pt(0) + (self.diff * float(i))
             self.subbehavior_with_name("robot" + str(i)).pos = pt
 
@@ -52,9 +50,7 @@ class LineUp(composite_behavior.CompositeBehavior):
 
         # add subbehaviors for all robots, instructing them to line up
         self.remove_all_subbehaviors()
-        robots = [robot for robot in main.our_robots() if robot is not None]
-        number = len(robots)
-        for i in range(number):
+        for i in range(6):
             pt = self._line.get_pt(0) + (self.diff * float(i))
             self.add_subbehavior(
                 skills.move.Move(pt),
