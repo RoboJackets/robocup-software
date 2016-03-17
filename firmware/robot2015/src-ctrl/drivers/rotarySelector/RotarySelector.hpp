e3ba55789a80293c66c9f31fad024979d6099359
Justin Buchanan <justbuchanan@gmail.com>
2016-03-17 18:55:29 -0400

    refactored RotarySelector some

--- firmware/robot2015/src-ctrl/drivers/rotarySelector/RotarySelector.hpp
+++ firmware/robot2015/src-ctrl/drivers/rotarySelector/RotarySelector.hpp
@@ -1,30 +1,31 @@
 #pragma once
 
-#include "pins-ctrl-2015.hpp"
-
-// Template generalizes I/O functionality to not only IO expander pins but
-// also standard mBed I/O pins
-template<typename IN_OUT>
+/**
+ * This class facilitates reading the value from a rotary selector.
+ *
+ * This class is templated so that it generalizes I/O functionality to not only
+ * IO expander pins but also standard mBed I/O pins.
+ */
+template <typename DIGITAL_IN>
 class RotarySelector {
-
 public:
-    RotarySelector::RotarySelector(IN_OUT mb3, IN_OUT mb2, IN_OUT mb1, IN_OUT mb0)
-	: b3(mb3), b2(mb2), b1(mb1), b0(mb0)
-	{}
+    /**
+     * Initialize a RotarySelector with the given digital input pins
+     *
+     * @param pins An array of pins ordered from least significant to most
+     */
+    RotarySelector::RotarySelector(std::array<DIGITAL_IN, 4> pins)
+        : _pins(pins) {}
 
     /**
-	 * Gives the reading (0x0 - 0xF) of the 4 wires
-	*/
-	int RotarySelector::read()
-	{
-	    // Assumes b3 has Most Significant Bit, b0 has Least Significant Bit
-	    int reading = b3.read() << 3 || b2.read() << 2 || b1.read() << 1 || b0.read();
-	    return reading;
-	}
+     * Gives the reading (0x0 - 0xF) of the 4 wires
+    */
+    uint8_t RotarySelector::read() {
+        uint8_t reading = 0;
+        for (size_t i = 0; i < 4; i++) reading |= _pins[i].read() << i;
+        return reading;
+    }
 
 private:
-    IN_OUT b0;
-    IN_OUT b1;
-    IN_OUT b2;
-    IN_OUT b3;
+    std::array<DIGITAL_IN, 4> _pins;
 };
