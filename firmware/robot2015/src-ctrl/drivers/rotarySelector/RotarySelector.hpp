f1eb4c0a13d68f75d912acb2e3f39db7e7a9c21f
Justin Buchanan <justbuchanan@gmail.com>
2016-03-19 18:04:15 -0400

    added NUM_PINS parameter

--- firmware/robot2015/src-ctrl/drivers/rotarySelector/RotarySelector.hpp
+++ firmware/robot2015/src-ctrl/drivers/rotarySelector/RotarySelector.hpp
@@ -15,22 +15,24 @@
 template <typename DIGITAL_IN>
 class RotarySelector {
 public:
+    static constexpr size_t NUM_PINS = 4;
+
     /**
      * Initialize a RotarySelector with the given digital input pins
      *
      * @param pins An array of pins ordered from least significant to most
      */
-    RotarySelector(std::array<DIGITAL_IN, 4> pins) : _pins(pins) {}
+    RotarySelector(std::array<DIGITAL_IN, NUM_PINS> pins) : _pins(pins) {}
 
     /**
-     * Gives the reading (0x0 - 0xF) of the 4 wires
+     * Gives the reading (0x0 - 0xF) of the NUM_PINS wires
     */
     uint8_t read() {
         uint8_t reading = 0;
-        for (size_t i = 0; i < 4; i++) reading |= _pins[i].read() << i;
+        for (size_t i = 0; i < NUM_PINS; i++) reading |= _pins[i].read() << i;
         return reading;
     }
 
 private:
-    std::array<DIGITAL_IN, 4> _pins;
+    std::array<DIGITAL_IN, NUM_PINS> _pins;
 };
