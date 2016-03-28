c7e004959b22b0ad676f0f0abd99e05098571689
Justin Buchanan <justbuchanan@gmail.com>
2016-03-28 00:55:04 -0400

    added SharedSPIDevice class

--- firmware/common2015/drivers/shared-spi/SharedSPI.hpp
+++ firmware/common2015/drivers/shared-spi/SharedSPI.hpp
@@ -2,6 +2,9 @@
 
 #include <mbed.h>
 #include <rtos.h>
+
+#include <memory>
+
 #include "assert.hpp"
 
 /**
@@ -10,18 +13,45 @@
  * This makes it easier to correctly use a shared SPI bus (with multiple
  * devices) in a multi-threaded environment.
  */
-class SharedSPI : public mbed::SPI {
+class SharedSPI : public mbed::SPI, public Mutex {
 public:
     SharedSPI(PinName mosi, PinName miso, PinName sck) : SPI(mosi, miso, sck) {}
+};
 
-    osStatus lock(uint32_t millisec = osWaitForever) {
-        return _mutex.lock(millisec);
+
+/**
+ * Classes that provide an interface to a device on the shared spi bus should
+ * inherit from this class.
+ */
+template<class DIGITAL_OUT = mbed::DigitalOut>
+class SharedSPIDevice {
+public:
+    SharedSPIDevice(std::shared_ptr<SharedSPI> spi, DIGITAL_OUT cs,
+                    bool csInverted = true)
+        : _spi(spi), _cs(cs) {
+
+        /// The value we set the chip select pin to in order to assert it (it's
+        /// often inverted).
+        _csAssertValue = csInverted ? 0 : 1;
+
+        /// Initialize to a de-asserted state
+        _cs = !_csAssertValue;
     }
 
-    bool trylock() { return _mutex.trylock(); }
+    void chip_select() {
+        _spi->lock();
+        _cs = _csAssertValue;
+    }
 
-    osStatus unlock() { return _mutex.unlock(); }
+    void chip_deselect() {
+        _cs = _csAssertValue;
+        _spi->unlock();
+    }
+
+protected:
+    std::shared_ptr<SharedSPI> _spi;
+    DIGITAL_OUT _cs;
 
 private:
-    Mutex _mutex;
+    int _csAssertValue;
 };
