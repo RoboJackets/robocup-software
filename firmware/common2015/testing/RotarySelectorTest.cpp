d79348b60d297ac32746247f731dc88781b64f0c
Justin Buchanan <justbuchanan@gmail.com>
2016-03-29 20:03:06 -0400

    added FakeMbed.hpp

--- firmware/common2015/testing/RotarySelectorTest.cpp
+++ firmware/common2015/testing/RotarySelectorTest.cpp
@@ -1,20 +1,11 @@
 #include <gtest/gtest.h>
 
 #include "../../robot2015/src-ctrl/drivers/rotarySelector/RotarySelector.hpp"
-
-class FakeDigitalIn {
-public:
-    FakeDigitalIn(int value) : _value(value) {}
-
-    int read() const { return _value; }
-
-private:
-    int _value;
-};
+#include "FakeMbed.hpp"
 
 // A very simple test that checks that the RotarySelector calculates the value
 // appropriately given a set of binary inputs.
 TEST(RotarySelector, read) {
-    RotarySelector<FakeDigitalIn> selector({1, 1, 0, 1});
+    RotarySelector<fake_mbed::DigitalIn> selector({1, 1, 0, 1});
     EXPECT_EQ(11, selector.read());
 }
