3f9707c43045ac5bd722b03732d8323268b43701
jcarn <jcarnahan3@gatech.edu>
2016-04-05 16:26:50 -0400

    Added rudimentary changes so I could get this branch on another computer, don't push

--- soccer/gameplay/plays/offense/basic_122.py
+++ soccer/gameplay/plays/offense/basic_122.py
@@ -1,15 +1,16 @@
-import play
+import standard_play
 import behavior
 import skills
 import tactics
 import main
+from main import *
 import robocup
 import evaluation
 import constants
 import math
 
 
-class Basic122(play.Play):
+class Basic122(standard_play.StandardPlay):
 
     # how far the 2 support robots should stay away from the striker
     SupportAvoidTeammateRadius = 0.5
@@ -49,10 +50,6 @@ class Basic122(play.Play):
         support2.mark_line_thresh = 1.0
         self.add_subbehavior(support2, 'support2', required=False, priority=1)
 
-        self.add_subbehavior(tactics.defense.Defense(),
-                             'defense',
-                             required=False)
-
     @classmethod
     def score(cls):
         return 10 if main.game_state().is_playing() else float("inf")
@@ -62,6 +59,7 @@ class Basic122(play.Play):
         return True
 
     def execute_running(self):
+        super().execute_running()
         striker = self.subbehavior_with_name('striker')
         support1 = self.subbehavior_with_name('support1')
         support2 = self.subbehavior_with_name('support2')
