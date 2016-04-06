cba47e849fcada756799699ad3bc0336f1c4cfb1
Will Stuckey <guyfleeman@gmail.com>
2016-04-05 20:08:27 -0400

    removed base firmware

--- firmware/base2015/main.cpp
+++ firmware/base2015/main.cpp
@@ -1,30 +1,9 @@
-//
-// Created by matt on 6/22/15.
-//
 #include <mbed.h>
-#include <cmsis_os.h>
-#include "PCLink.hpp"
-
-Serial pc(USBTX, USBRX);
-
-// void hidRx_t(void const *args);
 
 int main() {
     DigitalOut ledOne(LED1);
-    DigitalOut ledTwo(LED2);
-    PCLink defLink;
-    // osThreadDef(hidRx_t, osPriorityNormal, DEFAULT_STACK_SIZE);
-    // osThreadCreate(osThread(hidRx_t), NULL);
-
-    defLink.setSerialDebugging(&pc);
-    defLink.setLed(&ledTwo);
     while (true) {
-        wait(0.1);
-        defLink.read();
+        wait(0.5);
         ledOne = !ledOne;
     }
 }
-
-// void hidRx_t(void const *args) {
-//
-//}
