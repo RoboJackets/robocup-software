726e7953a044e68ef8f4eb2fb6b1965a0b03736c
Albert Shaw <ashaw596@gmail.com>
2016-04-12 19:01:22 -0400

    Update Sim vel and accel multiplier

--- soccer/planning/Path.hpp
+++ soccer/planning/Path.hpp
@@ -220,4 +220,84 @@ public:
     virtual void setStartTime(RJ::Time t) override { path->setStartTime(t); }
 };
 
+class EmptyPath : public Path {
+public:
+    EmptyPath(const RobotInstant &instant): instant(instant) {}
+
+    const RobotInstant instant;
+
+    virtual boost::optional<RobotInstant> evaluate(float t) const override {
+        return boost::none;
+    }
+
+    /**
+     * Returns true if the path hits an obstacle
+     *
+     * @param[in]	shape The obstacles on the field
+     * @param[out]  hitTime the approximate time when the path hits an obstacle.
+     * If no obstacles are hit, behavior is undefined for the final value.
+     * @param[in] 	startTime The time on the path to start checking from
+     * @return 		true if it hits an obstacle, otherwise false
+     */
+    virtual bool hit(const Geometry2d::ShapeSet& obstacles, float& hitTime,
+                     float startTime) const override {
+        return false;
+    }
+
+    /**
+     * Draws the path.  The default implementation adds a DebugRobotPath to the
+     * SystemState that interpolates points along the path.
+     *
+     * @param state The SystemState to draw the path on
+     * @param color The color the path should be drawn
+     * @param layer The layer to draw the path on
+     */
+    virtual void draw(SystemState* const state, const QColor& color = Qt::black,
+                      const QString& layer = "Motion") const override {}
+
+    /**
+     * Returns how long it would take for the entire path to be traversed
+     *
+     * @return The time from start to path completion or infinity if it never
+     * stops
+     */
+    virtual float getDuration() const override { return 0;}
+
+    /**
+     * Returns a subPath
+     *
+     * @param startTime The startTime for from which the subPath should be
+     *     taken.
+     * @param endTime The endTime from which the subPath should be taken. If it
+     *     is greater than the duration fo the path, it should go to the end of
+     *     the path.
+     * @return A unique_ptr to the new subPath
+     */
+    virtual std::unique_ptr<Path> subPath(
+            float startTime = 0,
+            float endTime = std::numeric_limits<float>::infinity()) const override {
+        debugThrow("not implemented");
+        return clone();
+    }
+
+    /// Start instant of the path
+    virtual RobotInstant start() const override {
+        return instant;
+    }
+    /// Destination instant of the path
+    virtual RobotInstant end() const override {
+        return instant;
+    }
+
+    /**
+     * Returns a deep copy of the Path
+     */
+    virtual std::unique_ptr<Path> clone() const override {
+        return std::make_unique<EmptyPath>(*this);
+    }
+
+    virtual RJ::Time startTime() const override { return 0; }
+    virtual void setStartTime(RJ::Time t) override {}
+};
+
 }  // namespace Planning
