57f8be777835fde4ec5e7503a08350ca931fbfaf
John Carnahan <JohnGMCarnahan@gmail.com>
2016-04-14 19:47:13 -0400

    Didn't mean to commit
    This reverts commit e3b6f171a8c998f35c63c6efa24c76156d89eeae.

--- soccer/gameplay/plays/restarts/our_corner_kick.py
+++ soccer/gameplay/plays/restarts/our_corner_kick.py
@@ -67,7 +67,6 @@ class OurCornerKick(standard_play.StandardPlay):
         return True
 
     def execute_running(self):
-    super().execute_running()
         # setup the kicker target
         goal_x = constants.Field.GoalWidth * (1 if main.ball().pos.x < 0 else
                                               -1)
