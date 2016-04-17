66e388ed16d8cb408495851e7b682f67989a46f9
Chachmu <mwoodward@gatech.edu>
2016-04-17 19:41:24 -0400

    Added support for the new protobuff message, began basic play to place the ball

---
+++ soccer/gameplay/plays/restarts/our_placement.py
@@ -0,0 +1,35 @@
+import play
+import behavior
+import robocup
+import main
+import tactics.line_up
+import tactics.penalty
+
+
+# one robot places the ball, the others just line up and wait
+class OurPlacement(play.Play):
+    def __init__(self):
+        super().__init__(continuous=True)
+
+        self.add_transition(behavior.Behavior.State.start,
+                            behavior.Behavior.State.running, lambda: True,
+                            'immediately')
+
+        self.add_transition(
+            behavior.Behavior.State.running, behavior.Behavior.State.completed,
+            lambda: self.kicker.is_done_running(), 'when kicker finishes.')
+
+        self.kicker = tactics.penalty.Penalty()
+        self.add_subbehavior(self.kicker, 'kicker', required=True, priority=10)
+
+        line = robocup.Segment(robocup.Point(1.5, 1), robocup.Point(1.5, 2.5))
+        line_up = tactics.line_up.LineUp(line)
+
+    @classmethod
+    def score(cls):
+        gs = main.game_state()
+        return 0 if gs.is_our_placement() else float("inf")
+
+    @classmethod
+    def is_restart(cls):
+        return True
---
+++ soccer/gameplay/tactics/placement.py
@@ -0,0 +1,71 @@
+import single_robot_composite_behavior
+import behavior
+import main
+import role_assignment
+import enum
+import robocup
+import skills
+import constants
+import skills.pivot_kick
+import skills.capture
+import skills.move
+
+class Placement(single_robot_composite_behavior.SingleRobotCompositeBehavior):
+    class State(enum.Enum):
+        capturing = 1 #waiting
+        placing = 2 #setup
+        standby = 3 #ready
+
+    def __init__(self):
+        super().__init__(continuous=False)
+
+        for substate in Placement.State:
+            self.add_state(substate, behavior.Behavior.State.running)
+
+        self.add_transition(behavior.Behavior.State.start,
+            Placement.State.capturing, lambda: True, 'immediately')
+
+        self.add_transition(Placement.State.capturing, Placement.State.placing,
+            lambda: self.subbehavior_with_name('capture').state==behavior.Behavior.State.completed,
+            'done capturing')
+
+        self.add_transition(Placement.State.placing, Placement.State.standby,
+            lambda: self.subbehavior_with_name('placing').state==behavior.Behavior.State.completed,
+            'done placing')
+
+        self.add_transition(
+            Placement.State.standby, behavior.Behavior.State.completed,
+            lambda: self.subbehavior_with_name('standby').state == behavior.Behavior.State.completed,
+            'finished standby')
+
+    def on_enter_capturing(self):
+        capture=skills.capture.Capture()
+        self.add_subbehavior(capture,'capture', required=True, priority=100)
+
+    def on_exit_capturing(self):
+        self.remove_subbehavior('capture')
+
+    def on_enter_placing(self):
+        place=skills.move.Move(0,0)
+        self.add_subbehavior(place,'place', required=True, priority=100)
+
+    def on_exit_placing(self):
+        self.remove_subbehavior('place')
+
+        direction=constants.Field.CenterPoint-main.ball().pos
+        self.robot.move_to(main.ball().pos+direction.normalized())
+
+    def on_enter_standby(self):
+        standby=skills.move.Move(0,0)
+        self.add_subbehavior(standby,'standby', required=True, priority=100)
+
+    def on_exit_standby(self):
+        self.remove_subbehavior('standby')
+
+    ## prefer to get assigned robot closest to ball
+    def role_requirements(self):
+        reqs = super().role_requirements()
+        if isinstance(reqs, role_assignment.RoleRequirements):
+            reqs.destination_shape = main.ball().pos
+
+        return reqs
