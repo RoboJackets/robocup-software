ca89dfc675adac711740a969d5905cd3c589329a
Jeremy Feltracco <jpfeltracco@gatech.edu>
2016-04-18 18:22:23 -0400

    Fixed attiny hw-test to use shared spi.

--- firmware/robot2015/hw-test/attiny-spi-test.cpp
+++ firmware/robot2015/hw-test/attiny-spi-test.cpp
@@ -1,17 +1,21 @@
 #include "mbed.h"
 #include "KickerBoard.hpp"
+#include "pins-ctrl-2015.hpp"
+#include "SharedSPI.hpp"
 
 // DigitalOut cs(p8);
 Serial pc(USBTX, USBRX);  // tx and rx
-LocalFileSystem local("local");
 
 
 int main() {
     pc.baud(57600);  // set up the serial
-    printf("About to flash kicker\r\n");
-    KickerBoard kicker(p5, p6, p7, p8, p9, "/local/rj-kickr.nib");
-    bool success = kicker.flash(false, true);
-    printf("Flashed kicker, success = %s\r\n", success ? "TRUE" : "FALSE");
+    shared_ptr<SharedSPI> sharedSPI =
+        make_shared<SharedSPI>(RJ_SPI_MOSI, RJ_SPI_MISO, RJ_SPI_SCK);
+    sharedSPI->format(8, 0);  // 8 bits per transferrintf("About to flash kicker\r\n");
+    KickerBoard kickerBoard(sharedSPI, p9, p8,
+                            "/local/rj-kickr.nib");
+    bool kickerReady = kickerBoard.flash(true, true);
+    printf("Flashed kicker, success = %s\r\n", kickerReady ? "TRUE" : "FALSE");
 
     char getCmd;
 
@@ -20,13 +24,13 @@ int main() {
             getCmd = pc.getc();
             switch (getCmd) {
                 case 'k':
-                    kicker.kick(20);
+                    kickerBoard.kick(20);
                     break;
                 case 'c':
-                    kicker.chip(8);
+                    kickerBoard.chip(8);
                     break;
                 case 'r':
-                    int a = kicker.read_voltage();
+                    int a = kickerBoard.read_voltage();
                     pc.printf("Received %d\r\n", a);
                     break;
             }
