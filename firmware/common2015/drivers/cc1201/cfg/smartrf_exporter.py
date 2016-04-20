1d3142942ac9bf20f0b3fa5437e3190e04a9465f
Justin Buchanan <justbuchanan@gmail.com>
2016-04-20 19:24:52 -0400

    make pretty

--- firmware/common2015/drivers/cc1201/cfg/smartrf_exporter.py
+++ firmware/common2015/drivers/cc1201/cfg/smartrf_exporter.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 This python script reads a TI SmartRF xml file and outputs the registers as a C
 header file mapping registers to values.  This functionality is built into
@@ -10,7 +9,8 @@ import sys
 import xml.etree.ElementTree as etree
 
 if len(sys.argv) != 3:
-    print("Usage: %s path/to/smartrf.xml outputfile.h" % sys.argv[0], file=sys.stderr)
+    print("Usage: %s path/to/smartrf.xml outputfile.h" % sys.argv[0],
+          file=sys.stderr)
     exit(1)
 
 infile = sys.argv[1]
@@ -21,11 +21,12 @@ register_settings = root.find('registersettings')
 
 with open(outfile, 'w') as out:
     out.write("// DO NOT MODIFY THIS FILE\n")
-    out.write("// It was automatically generated from a smartrf xml file by the smartrf exporter script\n")
+    out.write(
+        "// It was automatically generated from a smartrf xml file by the smartrf exporter script\n")
     out.write('static const registerSetting_t preferredSettings[] = {\n')
     for reg in register_settings:
         name = reg.find('Name').text
-        if name.startswith('AES'): continue # skip AES registers
+        if name.startswith('AES'): continue  # skip AES registers
         val = reg.find('Value').text
 
         out.write('    {CC1201_%s, %s},\n' % (name, val))
