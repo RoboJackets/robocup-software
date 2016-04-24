5a863ca7c967899f29de110c9738f226420c805c
Jeremy Feltracco <jpfeltracco@gatech.edu>
2016-04-24 15:28:59 -0400

    Changed kickerboard to user timer interrupts for timing kicks/chips, also bug fixed and cleaned up.

--- firmware/robot2015/src-kckr/main.c
+++ firmware/robot2015/src-kckr/main.c
@@ -22,95 +22,18 @@
 #define LED PB0
 #define CHARGE PB1
 
-volatile char had_interrupt_ = 0;
-volatile uint8_t data = 0;
-volatile unsigned chip_on = 0;
-uint8_t spi_enabled = 0;
+typedef enum {OFF, ON, ACTING} state_type;
+volatile state_type state = OFF;
+volatile millis_left = 0;
 
 uint8_t get_voltage();
-void init();
-void trigger(uint8_t time, uint8_t useKicker);
-void delay_us(uint16_t time);
-
-uint8_t time;
 
 void main() {
-    init();
-    while (1) {
-        // if (!(PINA & _BV(N_KICK_CS))) {  // Check if N_KICK_CS == 0
-            // if (!spi_enabled) {
-            //     SET_BIT(USICR, USIOIE);  // Enable ISR
-            //     SET_BIT(DDRA, DO);       // Drive DO
-            //     spi_enabled = 1;
-            // }
-        if (chip_on) {
-            if (had_interrupt_) {
-                char cmd = PARSE_CMD(data);
-
-                switch (cmd) {
-                    case 0x2:  // chip
-                        time = PARSE_TIME(data);
-                        trigger(time, 0);
-                        // trigger(8, 0);
-                        break;
-                    case 0x3:  // kick
-                        time = PARSE_TIME(data);
-                        trigger(time, 1);
-                        break;
-                    default:  // just read voltage otherwise
-                        break;
-                }
-
-                // Reset interrupt flag
-                had_interrupt_ = 0;
-            }
-        } else {
-            had_interrupt_ = 0;
-        }
-        //
-        // } else if (spi_enabled) {
-        //     CLEAR_BIT(USICR, USIOIE);  // Disable ISR
-        //     CLEAR_BIT(DDRA, DO);       // DO to Z
-        //     spi_enabled = 0;
-        // }
-    }
-}
-
-/* SPI Interrupt */
-ISR(USI_OVF_vect) {
-    // Get data from USIDR
-    // Using USIBR caused a bit to be shifted
-    data = USIDR;
-    // Clear the interrupt flag (would have been done by USIBR)
-    SET_BIT(USISR, USIOIF);
-    USIDR = get_voltage();
-    had_interrupt_ = 1;
-}
-
-/* Chip Select Interrupt */
-ISR(PCINT0_vect) {
-    if (!(PINA & _BV(N_KICK_CS))) {
-        // SPI should be enabled
-        SET_BIT(DDRA, DO);
-        //SET_BIT(USICR, USIOIE);  // Enable ISR
-        chip_on = 1;
-    } else {
-        // SPI should not be enabled
-        CLEAR_BIT(DDRA, DO);
-        //CLEAR_BIT(USICR, USIOIE);  // Enable ISR
-        chip_on = 0;
-    }
-}
-
-void init() {
     /* Port direction settings */
-    SET_BIT(DDRA, KICK);
-    SET_BIT(DDRA, CHIP);
-    SET_BIT(DDRA, DO);
-    SET_BIT(DDRB, LED);
-    SET_BIT(DDRB, CHARGE);
+    DDRA |= _BV(KICK) | _BV(CHIP) | _BV(DO);
+    DDRB |= _BV(LED) | _BV(CHARGE);
 
-    SET_BIT(PORTA, N_KICK_CS);
+    DDRA &= ~_BV(N_KICK_CS); // ensure N_KICK_CS is input
 
     // CS Interrupt
     SET_BIT(GIMSK, PCIE0);
@@ -126,6 +49,13 @@ void init() {
     sei();
     // Enable SPI interrupt
     SET_BIT(USICR, USIOIE);
+    // OCR0A is max val of timer
+    // we need 1000 clocks at 1 Mhz to get 1 millisecond
+    // if we prescale by 1024, then we need 125 on timer to get 1 ms exactly
+    // direct access to timer (TCNT0)
+    SET_BIT(TIMSK0, OCIE0A);
+    SET_BIT(TCCR0A, WGM01);
+    OCR0A = 125;
 
     /* ADC Initialization */
     CLEAR_BIT(PRR, PRADC);   // disable power reduction Pg. 133
@@ -133,20 +63,67 @@ void init() {
     SET_BIT(ADCSRB, ADLAR);  // present left adjusted
     // because we left adjusted and only need
     // 8 bit precision, we can now read ADCH directly
+    while (1) { }
 }
 
-void trigger(uint8_t timeKick, uint8_t useKicker) {
-    uint8_t action = useKicker ? KICK : CHIP;
-    TOGGLE_BIT(PORTA, action);
-    // delay_us(timeKick*125);
-    delay_ms(timeKick * 100);
-    TOGGLE_BIT(PORTA, action);
+/* SPI Interrupt */
+ISR(USI_OVF_vect) {
+    // Get data from USIDR
+    uint8_t data = USIDR;
+    // Clear the interrupt flag
+    SET_BIT(USISR, USIOIF);
+
+    USIDR = get_voltage();
+
+    if (state == ON) {
+        uint8_t cmd = PARSE_CMD(data);
+        switch (cmd) {
+            case 0x2: // chip
+                state = ACTING;
+                SET_BIT(PORTA, CHIP);
+                break;
+            case 0x3: // kick
+                state = ACTING;
+                SET_BIT(PORTA, KICK);
+                break;
+            default:
+                break;
+        }
+
+        millis_left = PARSE_TIME(data);
+        SET_BIT(TCCR0B, CS01); // start timer /8 prescale
+    }
+}
+
+/* Chip Select Interrupt */
+ISR(PCINT0_vect) {
+    int is_chip_selected = !(PINA & _BV(N_KICK_CS)); // check if selected
+    if (is_chip_selected) {
+        SET_BIT(DDRA, DO);
+        state = ON;
+    } else {
+        CLEAR_BIT(DDRA, DO);
+        state = OFF;
+    }
+}
+
+/* Timer interrupt for chipping / kicking */
+/* Gets called once per millisecond */
+ISR(TIM0_COMPA_vect) {
+    int is_done = --millis_left <= 0; // no more milliseconds left
+
+    if (is_done) {
+        // could be kicking or chipping, clear both
+        CLEAR_BIT(PORTA, KICK);
+        CLEAR_BIT(PORTA, CHIP);
+        CLEAR_BIT(TCCR0B, CS01); // stop /8 prescaled timer
+        state = ON;
+    }
 }
 
 /* Voltage Function */
 uint8_t get_voltage() {
-    // Hard-coded for PA1
-    // SET_BIT(ADMUX, MUX0);
+    // Hard-coded for PA1, check datasheet before changing
     // Set lower three bits to value of pin we read from
     ADMUX |= VOLTAGE;
     // Start conversation by writing to start bit
@@ -156,16 +133,4 @@ uint8_t get_voltage() {
     // ADHC will go from 0 to 255 corresponding to
     // 0 through VCC
     return ADCH;
-}
-
-void delay_us(uint16_t count) {
-    while (count--) {
-        _delay_us(1);
-    }
-}
-
-void delay_ms(uint16_t count) {
-    while (count--) {
-        _delay_ms(1);
-    }
 }
