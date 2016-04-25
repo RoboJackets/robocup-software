d5218fbe9bc657629946b53e324c091ba220a5d4
Albert Shaw <ashaw596@gmail.com>
2016-04-25 18:30:13 -0400

    Fix tests

--- soccer/planning/EscapeObstaclesPathPlanner.hpp
+++ soccer/planning/EscapeObstaclesPathPlanner.hpp
@@ -16,7 +16,7 @@ public:
         MotionInstant startInstant, const MotionCommand* cmd,
         const MotionConstraints& motionConstraints,
         Geometry2d::ShapeSet& obstacles,
-        const std::vector<DynamicObstacle> &dynamicObstacles,
+        const std::vector<DynamicObstacle> &dynamicObstacles = std::vector<DynamicObstacle>(),
         std::unique_ptr<Path> prevPath = nullptr) override;
 
     /// The MotionCommand type that this planner handles
