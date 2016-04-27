7d1e98ffe7560a752dc17b2e36267b2d06724031
Jeremy Feltracco <jpfeltracco@gatech.edu>
2016-04-26 20:13:30 -0400

    Fixed interrupt collisions with chip select pin and cleaned up.

--- firmware/robot2015/src-kckr/main.c
+++ firmware/robot2015/src-kckr/main.c
@@ -1,108 +1,109 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-
-/* Bit manip. defines for clarity */
-// _BV = bit value
-#define SET_BIT(P, B) (P |= _BV(B))
-#define CLEAR_BIT(P, B) (P &= ~_BV(B))
-#define TOGGLE_BIT(P, B) (P ^= _BV(B))
+#include "pins.h"
 
 #define PARSE_CMD(P) (P >> 6)
 #define PARSE_TIME(P) (P & 0x3F)
 
-/* Inputs */
-#define VOLTAGE PA2
-#define N_KICK_CS PA7
-
-/* Outputs */
-#define KICK PA0
-#define CHIP PA1
-#define DO PA5
-#define LED PB0
-#define CHARGE PB1
-
-typedef enum {OFF, ON, ACTING} state_type;
+typedef enum { OFF, ON, ACTING } state_type;
 volatile state_type state = OFF;
-volatile millis_left = 0;
+volatile unsigned millis_left = 0;
+
+// always up-to-date voltage so we don't have to get_voltage() inside interupts
+uint8_t last_voltage;
 
 uint8_t get_voltage();
 
 void main() {
-    /* Port direction settings */
+    /* Port direction - setting outputs */
     DDRA |= _BV(KICK) | _BV(CHIP) | _BV(DO);
     DDRB |= _BV(LED) | _BV(CHARGE);
 
-    DDRA &= ~_BV(N_KICK_CS); // ensure N_KICK_CS is input
+    DDRA &= ~_BV(N_KICK_CS);  // ensure N_KICK_CS is input
 
-    // CS Interrupt
-    SET_BIT(GIMSK, PCIE0);
-    SET_BIT(PCMSK0, PCINT7);
+    /* Chip Select Interrupts */
+    GIMSK |= _BV(PCIE0);  // Enable pin interrupts
+    // Following is equivalent to SET_BIT(PCMSK0, PCINT7)
+    PCMSK0 |= _BV(N_KICK_CS);  // Enable on N_KICK_CS
 
     /* SPI init - Pg. 120 */
-    // 3 Wire Mode DO, DI, USCK - Pg. 124
-    SET_BIT(USICR, USIWM0);
-    // External, negative edge clock - Pg. 125
-    SET_BIT(USICR, USICS1);
-    // SET_BIT(USICR, USICS0);
-    // Enable Global Interrupts - Required for below
-    sei();
-    // Enable SPI interrupt
-    SET_BIT(USICR, USIOIE);
-    // OCR0A is max val of timer
+    USICR |= _BV(USIWM0)     // 3 Wire Mode DO, DI, USCK - Pg. 124
+             | _BV(USICS1)   // External, negative edge clock - Pg. 125
+             | _BV(USISIE);  // Enable SPI start interrupt
+
+    // Interrupt on TIMER 0
+    TIMSK0 |= _BV(OCIE0A);
+    // CTC - Clear Timer on Compare Match
+    TCCR0A |= _BV(WGM01);
+    // OCR0A is max val of timer before reset
     // we need 1000 clocks at 1 Mhz to get 1 millisecond
-    // if we prescale by 1024, then we need 125 on timer to get 1 ms exactly
-    // direct access to timer (TCNT0)
-    SET_BIT(TIMSK0, OCIE0A);
-    SET_BIT(TCCR0A, WGM01);
+    // if we prescale by 8, then we need 125 on timer to get 1 ms exactly
     OCR0A = 125;
 
     /* ADC Initialization */
-    CLEAR_BIT(PRR, PRADC);   // disable power reduction Pg. 133
-    SET_BIT(ADCSRA, ADEN);   // enable the ADC - Pg. 133
-    SET_BIT(ADCSRB, ADLAR);  // present left adjusted
+    PRR &= ~_BV(PRADC); // disable power reduction Pg. 133
+    ADCSRA |= _BV(ADEN);   // enable the ADC - Pg. 133
+    ADCSRB |= _BV(ADLAR);  // present left adjusted
     // because we left adjusted and only need
     // 8 bit precision, we can now read ADCH directly
-    while (1) { }
+    DDRA |= _BV(DO);
+    state = ON;
+
+    // Enable Global Interrupts
+    sei();
+
+    // We handle voltage readings here
+    while (1) {
+        last_voltage = get_voltage();
+        _delay_ms(100);
+    }
 }
 
-/* SPI Interrupt */
-ISR(USI_OVF_vect) {
-    // Get data from USIDR
+/* SPI Starting Interrupt */
+/* NOTE: This is intentionally not done with an overflow interrupt,
+ * as the software chip select interrupt will be triggered almost at
+ * the same time as an SPI overflow, and pin change interrupts have
+ * higher priority.
+ */
+ISR(USI_STR_vect) {
+    // Wait for overflow flag to become 1
+    while (!(USISR & _BV(USIOIF)))
+        ;
+    // // Get data from USIDR
     uint8_t data = USIDR;
-    // Clear the interrupt flag
-    SET_BIT(USISR, USIOIF);
-
-    USIDR = get_voltage();
-
+    // // Clear the overflow flag
+    USISR |= _BV(USIOIF);  // setting the bit actually clears it
+    // Clear the SPI start flag
+    USISR |= _BV(USISIF);
+    USIDR = last_voltage;
     if (state == ON) {
         uint8_t cmd = PARSE_CMD(data);
         switch (cmd) {
-            case 0x2: // chip
+            case 0x2:  // chip
                 state = ACTING;
-                SET_BIT(PORTA, CHIP);
+                PORTA |= _BV(CHIP);
                 break;
-            case 0x3: // kick
+            case 0x3:  // kick
                 state = ACTING;
-                SET_BIT(PORTA, KICK);
+                PORTA |= _BV(KICK);
                 break;
             default:
                 break;
         }
-
         millis_left = PARSE_TIME(data);
-        SET_BIT(TCCR0B, CS01); // start timer /8 prescale
+        TCCR0B |= _BV(CS01); // start timer /8 prescale
     }
 }
 
 /* Chip Select Interrupt */
 ISR(PCINT0_vect) {
-    int is_chip_selected = !(PINA & _BV(N_KICK_CS)); // check if selected
+    int is_chip_selected = !(PINA & _BV(N_KICK_CS));  // check if selected
     if (is_chip_selected) {
-        SET_BIT(DDRA, DO);
+        DDRA |= _BV(DO);
         state = ON;
     } else {
-        CLEAR_BIT(DDRA, DO);
+        DDRA &= ~_BV(DO);
         state = OFF;
     }
 }
@@ -110,13 +111,13 @@ ISR(PCINT0_vect) {
 /* Timer interrupt for chipping / kicking */
 /* Gets called once per millisecond */
 ISR(TIM0_COMPA_vect) {
-    int is_done = --millis_left <= 0; // no more milliseconds left
+    int is_done = --millis_left <= 0;  // no more milliseconds left
 
     if (is_done) {
         // could be kicking or chipping, clear both
-        CLEAR_BIT(PORTA, KICK);
-        CLEAR_BIT(PORTA, CHIP);
-        CLEAR_BIT(TCCR0B, CS01); // stop /8 prescaled timer
+        PORTA &= ~_BV(KICK);
+        PORTA &= ~_BV(CHIP);
+        TCCR0B &= ~_BV(CS01); /// stop prescaled timer
         state = ON;
     }
 }
@@ -127,9 +128,10 @@ uint8_t get_voltage() {
     // Set lower three bits to value of pin we read from
     ADMUX |= VOLTAGE;
     // Start conversation by writing to start bit
-    SET_BIT(ADCSRA, ADSC);
+    ADCSRA |= _BV(ADSC);
     // Wait for ADSC bit to clear
-    while (ADCSRA & _BV(ADSC));
+    while (ADCSRA & _BV(ADSC))
+        ;
     // ADHC will go from 0 to 255 corresponding to
     // 0 through VCC
     return ADCH;
---
+++ firmware/robot2015/src-kckr/pins.h
@@ -0,0 +1,10 @@
+/* Inputs */
+#define VOLTAGE PA2
+#define N_KICK_CS PA7
+
+/* Outputs */
+#define KICK PA0
+#define CHIP PA1
+#define DO PA5
+#define LED PB0
+#define CHARGE PB1
