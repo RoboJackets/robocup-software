1d925903fb8d688cc2f6fb65bc68a251cabfe8f0
Chachmu <mwoodward@gatech.edu>
2016-04-30 15:00:55 -0400

    made pretty although it still requires breaking a function call in half

--- soccer/gameplay/plays/restarts/placement.py
+++ soccer/gameplay/plays/restarts/placement.py
@@ -4,18 +4,16 @@ import main
 import constants
 import tactics.line_up
 import tactics.our_placement
-import play
+import standard_play
 
 
 # one robot places the ball, the others just line up and wait
-class Placement(play.Play):
+class Placement(standard_play.StandardPlay):
     def __init__(self):
         super().__init__(continuous=True)
 
         if self.has_subbehavior_with_name('defense'):
             self.remove_subbehavior('defense')
-
-        print(self.all_subbehaviors())
 
         self.add_transition(behavior.Behavior.State.start,
                             behavior.Behavior.State.running, lambda: True,
@@ -35,7 +33,7 @@ class Placement(play.Play):
     def check_update(self):
         #for some reason using != on two robocup points always returns false here
         return (self._pos - main.game_state().get_ball_placement_point()).mag(
-            )!= 0 or self._our_restart != main.game_state().is_our_restart()
+        ) != 0 or self._our_restart != main.game_state().is_our_restart()
 
     def execute_running(self):
         #if the ball placement command changes, reset the behaviors accordingly
@@ -71,8 +69,4 @@ class Placement(play.Play):
 
     @classmethod
     def is_restart(cls):
-        return True
-
-    @classmethod
-    def handlesgoalie(cls):
         return True
