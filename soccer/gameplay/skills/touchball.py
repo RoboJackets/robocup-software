5231eaa6207db738f0e189813ee35c92a590a3b5
Jay Kamat <jaygkamat@gmail.com>
2016-05-21 14:13:43 -0700

    Make pretty

--- soccer/gameplay/skills/touchball.py
+++ soccer/gameplay/skills/touchball.py
@@ -9,6 +9,7 @@ import robocup
 import skills.capture
 import math
 
+
 class TouchBall(skills.capture.Capture):
     def __init__(self):
         super().__init__(onlyApproach=True)
@@ -18,10 +19,11 @@ class TouchBall(skills.capture.Capture):
 
     ## Override so this so we only transition when the ball is in front
     def bot_in_front_of_ball(self):
-        adjFactor = robocup.Point(math.cos(self.robot.angle) * -TouchBall.adjDist, math.sin(self.robot.angle) * -TouchBall.adjDist)
+        adjFactor = robocup.Point(
+            math.cos(self.robot.angle) * -TouchBall.adjDist,
+            math.sin(self.robot.angle) * -TouchBall.adjDist)
         return (self.robot.pos - adjFactor).dist_to(main.ball().pos) \
             < TouchBall.adjDist + constants.Robot.Radius
-
 
     ## A touch is different from a capture in that we should try to keep our
     # distance from the ball if possible, and move forward to hit the ball at
@@ -33,11 +35,14 @@ class TouchBall(skills.capture.Capture):
     def find_intercept_point(self):
         approach_vec = self.approach_vector()
 
-        adjFactor = robocup.Point(math.cos(self.robot.angle) * -TouchBall.adjDist, math.sin(self.robot.angle) * -TouchBall.adjDist)
+        adjFactor = robocup.Point(
+            math.cos(self.robot.angle) * -TouchBall.adjDist,
+            math.sin(self.robot.angle) * -TouchBall.adjDist)
         robotPos = self.robot.pos - adjFactor
 
         # multiply by a large enough value to cover the field.
-        approach_line = robocup.Line(main.ball().pos, main.ball().pos + approach_vec * constants.Field.Length)
+        approach_line = robocup.Line(main.ball().pos, main.ball().pos +
+                                     approach_vec * constants.Field.Length)
         pos = approach_line.nearest_point(robotPos)
 
         pos += adjFactor
