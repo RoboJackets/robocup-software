c13dcd31632d41de113ecec6aba7aa9e0ccfd0e9
Jay Kamat <jaygkamat@gmail.com>
2016-05-21 11:48:29 -0700

    Get a rough version of touchpass working again

--- soccer/gameplay/skills/touchball.py
+++ soccer/gameplay/skills/touchball.py
@@ -13,15 +13,46 @@ class TouchBall(skills.capture.Capture):
     def __init__(self):
         super().__init__(onlyApproach=True)
 
-    AdjustDistance = 0.25
+    adjDist = constants.Robot.Radius * 2 # Move back so the
+
+    ## Override so this has no impact on state transitions
+    def bot_in_front_of_ball(self):
+        return False
+
 
     ## A touch is different from a capture in that we should try to keep our
     # distance from the ball if possible, and move forward to hit the ball at
     # the last minute.
     # To do this, let's move the intercept point capture found back a bit.
+    #
+    # In addition, lets try to keep this point stable by choosing the closest
+    # point, instead of the point that we can reach in time closest to the ball
     def find_intercept_point(self):
-        # Capture's interception point.
-        pos = super().find_intercept_point()
+        approach_vec = self.approach_vector()
 
-        pos += robocup.Point(math.cos(self.robot.angle) * -TouchBall.AdjustDistance, math.sin(self.robot.angle) * -TouchBall.AdjustDistance)
-        return pos
+        adjFactor = robocup.Point(math.cos(self.robot.angle) * -TouchBall.adjDist, math.sin(self.robot.angle) * -TouchBall.adjDist)
+
+        # sample every 5 cm in the -approach_vector direction from the ball
+        pos = None
+        # Adjust where this algorithm thinks we are by the adjust factor.
+        robotPos = self.robot.pos - adjFactor
+        finalPos = None
+        minBotTime = float('inf')
+        for i in range(50):
+            dist = i * 0.05
+            pos = main.ball().pos + approach_vec * dist
+            # how long will it take the ball to get there
+            ball_time = evaluation.ball.rev_predict(main.ball().vel, dist)
+            robotDist = (pos - (robotPos)).mag() * 0.6
+            bot_time = robocup.get_trapezoidal_time(robotDist, robotDist, 2.2,
+                                                    1, self.robot.vel.mag(), 0)
+
+            if bot_time < minBotTime or finalPos == None:
+                minBotTime = bot_time
+                finalPos = pos
+
+        # Push found point back a bit to have the ball hit our mouth,
+        # instead of our side.
+        finalPos += adjFactor
+
+        return finalPos
