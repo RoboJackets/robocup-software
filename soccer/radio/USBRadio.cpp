a6c25b6575b6457a438a947cc4d0101d99d5e77c
Justin Buchanan <justbuchanan@gmail.com>
2016-05-22 22:09:35 -0400

    cleanup

--- soccer/radio/USBRadio.cpp
+++ soccer/radio/USBRadio.cpp
@@ -230,7 +230,7 @@ void USBRadio::send(Packet::RadioTx& packet) {
             msg->song = robot.song();
         } else {
             // empty slot
-            msg->uid = 2; // TODO(justin): set this back to zero
+            msg->uid = 2;  // TODO(justin): set this back to zero
         }
     }
 
