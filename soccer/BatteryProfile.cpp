fbd00e841a4126183b199a73b6e40c94c0f22569
Justin Buchanan <justbuchanan@gmail.com>
2016-06-05 19:41:55 -0400

    remove #warning for 2015 robot battery profile. see #289

--- soccer/BatteryProfile.cpp
+++ soccer/BatteryProfile.cpp
@@ -14,7 +14,6 @@ const BatteryProfile RJ2008BatteryProfile({{14.20, 0.20},
                                            {15.10, 0.50},
                                            {16.00, 1.00}});
 
-#warning Battery profile for 2015 robot is untested, actual battery charge may be different than what is seen in soccer
 const BatteryProfile RJ2015BatteryProfile({{17.75, 0.20},
                                            {18.87, 0.50},
                                            {20.00, 1.00}});
