2f6caf43d2ce43c417647d322ed9a0fab94c21de
Justin Buchanan <justbuchanan@gmail.com>
2016-06-09 01:40:47 -0400

    implemented VelocityEstimator for firmware

--- firmware/robot2015/src-ctrl/modules/control/VelocityEstimator.hpp
+++ firmware/robot2015/src-ctrl/modules/control/VelocityEstimator.hpp
@@ -1,4 +1,11 @@
+#pragma once
 
+// TODO: can this be included from somewhere?
+const float PI = 3.14159265358979f;
+
+/**
+ * Estimate robot velocity (x, y, w) from encoder readings
+ */
 class VelocityEstimator {
 public:
     /**
@@ -11,7 +18,28 @@ public:
      */
     virtual std::array<float, 3> update(
         const std::array<uint16_t, 4>& encoderDeltas, uint16_t dt) {
-        // TODO: implement
-        return {0, 0, 0};
+
+        // convert encoder ticks to rad/s
+        std::array<float, 4> wheelRadPerSec;
+        for (int i = 0; i < 4; ++i) {
+            uint16_t encDelta = encoderDeltas[i];
+            wheelRadPerSec[i] = encDelta / ENC_TICKS_PER_TURN * 2 * PI / dt;
+        }
+
+        // TODO: get legit values for these
+        // TODO: move to its own class to be shared with MotionController?
+        const std::array<float, 4> wheel_angles = {PI / 4, 2*PI/4, 3*PI/4, 4*PI/4};
+
+        // calculate robot velocity from wheel velocities and wheel angles
+        std::array<float, 3> vel = {0, 0, 0};
+        for (int i = 0; i < 4; i++) {
+            vel[0] += wheelRadPerSec[i] * cosf(wheel_angles[i]); // x vel
+            vel[1] += wheelRadPerSec[i] * sinf(wheel_angles[i]); // y vel
+            vel[2] += wheelRadPerSec[i]; // w vel
+        }
+
+        return vel;
     }
+
+    static const uint16_t ENC_TICKS_PER_TURN = 2048;
 };
