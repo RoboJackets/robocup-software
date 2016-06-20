ddd28201c044418e4a5ad3d5f5abfd850ebb91b1
Ryan Strat <ryanstrat@users.noreply.github.com>
2016-06-20 15:47:30 -0400

    reset LogViewer to b36a25b, which is  a state before my modifications

--- soccer/LogViewer.cpp
+++ soccer/LogViewer.cpp
@@ -1,82 +1,37 @@
 #include <LogViewer.hpp>
-#include <NewRefereeModule.hpp>
 
 #include <google/protobuf/io/zero_copy_stream_impl.h>
-#include <protobuf/LogFrame.pb.h>
-#include <protobuf/referee.pb.h>
 
 #include <QApplication>
 #include <QFile>
-#include <QTextStream>
 
 #include <algorithm>
 #include <fcntl.h>
-#include <time.h>
-
-#include <iostream>
+#include <stdio.h>
 
 using namespace std;
 using namespace boost;
 using namespace Packet;
 using namespace google::protobuf::io;
-using namespace google::protobuf;
 
-/**
- * Defines usage information for launching the Log-Viewer application
- * @param prog The name of the program
- */
 void usage(const char* prog) {
-    fprintf(stderr, "Usage: %s [options] <filename.log>\n", prog);
-    fprintf(
-        stderr,
-        "\t-bsv: \t<Filename Base>: Exports the log to two files named "
-        "<Filename Base>-robot.bsv and\n\t\t\t<Filename Base>-frame.bsv and "
-        "Prevents launch of GUI\n");
-    fprintf(stderr, "\t-help: \tDisplays this help text\n");
+    fprintf(stderr, "Usage: %s <filename.log>\n", prog);
     exit(1);
 }
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
-    bool bsv = false;
-    char* bsvFilename;
-    char* logFilename;
-
-    // initial check for number of args
-    if (argc < 2 || argc > 4) {
+    if (argc != 2) {
         usage(argv[0]);
-    }
-
-    // Process args
-    for (int i = 1; i < argc; ++i) {
-        const char* var = argv[i];
-
-        if (strcmp(var, "-bsv") == 0 && (i + 1 < argc)) {
-            bsv = true;
-            ++i;
-            bsvFilename = argv[i];
-        } else if (strcmp(var, "-help") == 0) {
-            usage(argv[0]);
-        } else {
-            logFilename = argv[i];
-        }
     }
 
     LogViewer win;
 
-    if (bsv) {
-        // Export BSV. Runs log_viewer in headless mode
-        if (win.exportBSV(logFilename, bsvFilename) == false) {
-            return 0;
-        }
-    } else {
-        // Views the log in the log-viewer GUI
-        win.prepGUI(logFilename);
-        return app.exec();
-    }
+    win.readFrames(argv[1]);
+    win.showMaximized();
 
-    return 1;
+    return app.exec();
 }
 
 LogViewer::LogViewer(QWidget* parent) : QMainWindow(parent) {
@@ -106,158 +61,11 @@ LogViewer::LogViewer(QWidget* parent) : QMainWindow(parent) {
     _updateTimer.start(30);
 }
 
-/**
- * Parses log File & exports a BSV file containing the data of the log file
- * @param logFilename The filename of the RoboJackets protobuf log file
- * @param bsvFilename The base filename that the output filenames will be
- * generated from
- * @return Whether reading the file was sucessful. Specifically, it is the
- * output from readFrames.
- */
-bool LogViewer::exportBSV(char* logFilename, char* bsvFilename) {
-    fprintf(stderr, "Exporting BSV file to %s\n", bsvFilename);
-
-    string bsvBase(bsvFilename);
-
-    string robotFilename = bsvBase + "-robot.bsv";
-    QFile fileRobot(robotFilename.c_str());
-    if (!fileRobot.open(QIODevice::WriteOnly | QIODevice::Text)) return false;
-    QTextStream outRobot(&fileRobot);
-
-    string frameFilename = bsvBase + "-frame.bsv";
-    QFile fileFrame(frameFilename.c_str());
-    if (!fileFrame.open(QIODevice::WriteOnly | QIODevice::Text)) return false;
-    QTextStream outFrame(&fileFrame);
-
-    // Read in data from log file
-    if (this->readFrames(logFilename) == false) {
-        return false;
-    }
-
-    char matchID[MATCH_ID_LENGTH + 1];
-
-    generateMatchID(matchID, MATCH_ID_LENGTH);
-
-    fprintf(stderr, "matchID: %s\n", matchID);
-
-    // Print Headers
-    outRobot << "match_id" << BAR << "timestamp" << BAR << "team" << BAR
-             << "shell_id" << BAR << "x_position" << BAR << "y_position" << BAR
-             << "angle" << endl;
-    outFrame << "match_id" << BAR << "timestamp" << BAR << "ball_x_position"
-             << BAR << "ball_y_position" << BAR << "stage" << BAR << "command"
-             << BAR << "yellow_score" << BAR << "blue_score" << BAR
-             << "yellow_goalie" << BAR << "blue_goalie" << endl;
-
-    for (int i = 0; i < frames.size(); i++) {
-        LogFrame* currentFrame = frames[i].get();
-
-        const uint64 timestamp = currentFrame->timestamp();
-
-        const bool blue_team = currentFrame->blue_team();
-        const char* selfTeam = blue_team ? "blue" : "yellow";
-        const char* oppTeam = !blue_team ? "blue" : "yellow";
-
-        // loop over repeated self value
-        for (int i = 0; i < currentFrame->self_size(); i++) {
-            LogFrame_Robot currentRobot = currentFrame->self(i);
-
-            const float angle = currentRobot.angle();
-            const int32 shell = currentRobot.shell();
-
-            Point position = currentRobot.pos();
-            const float xPos = position.x();
-            const float yPos = position.y();
-
-            // Construct a row of data and print to file
-            outRobot << matchID << BAR << timestamp << BAR << selfTeam << BAR
-                     << shell << BAR << xPos << BAR << yPos << BAR << angle
-                     << endl;
-        }
-
-        for (int i = 0; i < currentFrame->opp_size(); i++) {
-            LogFrame_Robot currentRobot = currentFrame->opp(i);
-
-            const float angle = currentRobot.angle();
-            const int32 shell = currentRobot.shell();
-
-            Point position = currentRobot.pos();
-            const float xPos = position.x();
-            const float yPos = position.y();
-
-            // Construct a row of data and print to file
-            outRobot << matchID << BAR << timestamp << BAR << oppTeam << BAR
-                     << shell << BAR << xPos << BAR << yPos << BAR << angle
-                     << endl;
-        }
-
-        // Get information on the ball
-        LogFrame_Ball ball = currentFrame->ball();
-        Point ballPosition = ball.pos();
-
-        const float xBallPos = ballPosition.x();
-        const float yBallPos = ballPosition.y();
-
-        string stage;
-        string command;
-
-        int blueScore;
-        int yellowScore;
-
-        int blueGoalie;
-        int yellowGoalie;
-
-        if (currentFrame->raw_refbox_size() > 0) {
-            SSL_Referee referee = currentFrame->raw_refbox(0);
-
-            using namespace NewRefereeModuleEnums;
-            stage = stringFromStage((Stage)referee.stage());
-            command = stringFromCommand((Command)referee.command());
-
-            blueScore = referee.blue().score();
-            yellowScore = referee.yellow().score();
-
-            blueGoalie = referee.blue().goalie();
-            yellowGoalie = referee.yellow().goalie();
-        }
-
-        const char* c_stage = stage.c_str();
-        const char* c_command = command.c_str();
-
-        outFrame << matchID << BAR << timestamp << BAR << xBallPos << BAR
-                 << yBallPos << BAR << c_stage << BAR << c_command << BAR
-                 << yellowScore << BAR << blueScore << BAR << yellowGoalie
-                 << BAR << blueGoalie << endl;
-    }
-
-    return true;
-}
-/**
- * Performs the steps required before starting the QT GUI
- * @param logFilename Filename of a RoboJackets protobuf log file
- * @return Whether reading the file was successful
- */
-bool LogViewer::prepGUI(const char* logFilename) {
+bool LogViewer::readFrames(const char* filename) {
     frames.clear();
     ui.timeSlider->setMaximum(0);
 
-    bool read = this->readFrames(logFilename);
-
-    ui.timeSlider->setMaximum(frames.size());
-
-    this->showMaximized();
-    return read;
-}
-
-/**
- * Read frames and parse protobuf from specified file
- * @param filename Filename of a RoboJackets protobuf log file
- * @return true If reading and parsing was flawless
- * @return false If there was any errors or warnings while parsing
- */
-bool LogViewer::readFrames(const char* filename) {
     QFile file(filename);
-
     if (!file.open(QFile::ReadOnly)) {
         fprintf(stderr, "Can't open %s: %s\n", filename,
                 (const char*)file.errorString().toLatin1());
@@ -290,6 +98,7 @@ bool LogViewer::readFrames(const char* filename) {
         ++n;
     }
 
+    ui.timeSlider->setMaximum(frames.size());
     return true;
 }
 
@@ -336,27 +145,6 @@ void LogViewer::updateViews() {
     }
 
     ui.fieldView->update();
-}
-
-/**
- * Generates random alphanumeric strings using PID and time as the random seed.
- * A Null terminator is added after the ID
- * @param id A pointer to a character array that can hold at least length + 1
- * chars
- * @param length Length of the ID to be generated.
- */
-void LogViewer::generateMatchID(char* id, int length) {
-    string chars(
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
-
-    // Avoid using time based srand init
-    srand(getpid() * time(NULL));
-
-    for (int i = 0; i < length; ++i) {
-        id[i] = chars[rand() % chars.length()];
-    }
-
-    id[length] = '\0';
 }
 
 void LogViewer::on_action0_triggered() { ui.fieldView->rotate(0); }
