ce9e71db186495ffbe0c81a2d0a067d29056f573
Jon <jjones646@gmail.com>
2016-06-21 06:31:50 -0400

    adding compile time options for other manual controllers

--- soccer/joystick/GamepadJoystick.cpp
+++ soccer/joystick/GamepadJoystick.cpp
@@ -1,37 +1,33 @@
 #include "GamepadJoystick.hpp"
 
-using namespace Packet;
 using namespace std;
 
-static constexpr RJ::Time Dribble_Step_Time = 125 * 1000;
-static constexpr RJ::Time Kicker_Step_Time = 125 * 1000;
-
-static constexpr float AXIS_MAX = 32768.0f;
-
-static const char* devices[] = {
-    "/dev/input/by-id/usb-Logitech_Logitech_Cordless_RumblePad_2-joystick",
-    "/dev/input/by-id/usb-Logitech_Logitech_Dual_Action-joystick",
-
-    // End of list
-    nullptr};
+namespace {
+constexpr RJ::Time Dribble_Step_Time = 125 * 1000;
+constexpr RJ::Time Kicker_Step_Time = 125 * 1000;
+const float AXIS_MAX = 32768.0f;
+}
 
 GamepadJoystick::GamepadJoystick()
     : _joystick(nullptr), _lastDribblerTime(0), _lastKickerTime(0) {
-    if (SDL_Init(SDL_INIT_JOYSTICK) < 0) {
-        cerr << "SDL could not initialize! SDL Error: " << SDL_GetError()
+
+    if (SDL_Init(SDL_INIT_JOYSTICK) != 0) {
+        cerr << "ERROR: SDL could not initialize! SDL Error: " << SDL_GetError()
              << endl;
         return;
     }
-    if (SDL_NumJoysticks() < 1) {
-        cout << "Warning: No joysticks connected!" << endl;
-    } else {
+
+    if (SDL_NumJoysticks()) {
         _joystick = SDL_JoystickOpen(0);
-        if (_joystick == nullptr) {
-            cerr << "SDL could not open joystick! SDL Error: " << SDL_GetError()
-                 << endl;
-        } else {
+
+        if (_joystick != nullptr) {
             cout << "Joystick connected to " << SDL_JoystickName(0) << endl;
+        } else {
+            cerr << "ERROR: Could not open joystick! SDL Error: " << SDL_GetError()
+                 << endl;
         }
+    } else {
+        cout << "WARNING: No joysticks connected!" << endl;
     }
 }
 
@@ -48,19 +44,32 @@ void GamepadJoystick::update() {
     QMutexLocker(&mutex());
     SDL_JoystickUpdate();
 
+    RJ::Time now = RJ::timestamp();
+
+    /*
+     *  DRIBBLER ON/OFF
+     */
     if (SDL_JoystickGetButton(_joystick, 6)) {
-        _controls.dribble = false;
-    } else if (SDL_JoystickGetButton(_joystick, 4)) {
-        _controls.dribble = true;
+        _controls.dribble = !_controls.dribble;
     }
 
-    RJ::Time now = RJ::timestamp();
-    if (SDL_JoystickGetButton(_joystick, 1)) {
+    /*
+     *  DRIBBLER POWER
+     */
+    // Logitech_Dual_Action Controller
+    // if (SDL_JoystickGetButton(_joystick, 1)) {
+    //
+    // Logitech F310 Controller & Xbox 360 Controller
+    if (SDL_JoystickGetButton(_joystick, 0)) {
+
         if ((now - _lastDribblerTime) >= Dribble_Step_Time) {
             _controls.dribblerPower = max(_controls.dribblerPower - 0.1, 0.0);
             _lastDribblerTime = now;
         }
+
+    // Logitech F310 Controller & Logitech_Dual_Action Controller & Xbox 360 Controller
     } else if (SDL_JoystickGetButton(_joystick, 3)) {
+
         if ((now - _lastDribblerTime) >= Dribble_Step_Time) {
             _controls.dribblerPower = min(_controls.dribblerPower + 0.1, 1.0);
             _lastDribblerTime = now;
@@ -70,12 +79,25 @@ void GamepadJoystick::update() {
         _lastDribblerTime = now - Dribble_Step_Time;
     }
 
-    if (SDL_JoystickGetButton(_joystick, 0)) {
+    /*
+     *  KICKER POWER
+     */
+    // Logitech_Dual_Action Controller
+    // if (SDL_JoystickGetButton(_joystick, 0)) {
+    //
+    // Logitech F310 Controller & Xbox 360 Controller
+    if (SDL_JoystickGetButton(_joystick, 2)) {
+
         if ((now - _lastKickerTime) >= Kicker_Step_Time) {
             _controls.kickPower = max(_controls.kickPower - 0.1, 0.0);
             _lastKickerTime = now;
         }
-    } else if (SDL_JoystickGetButton(_joystick, 2)) {
+    // Logitech_Dual_Action Controller
+    // } else if (SDL_JoystickGetButton(_joystick, 2)) {
+    //
+    // Logitech F310 Controller & Xbox 360 Controller
+    } else if (SDL_JoystickGetButton(_joystick, 1)) {
+
         if ((now - _lastKickerTime) >= Kicker_Step_Time) {
             _controls.kickPower = min(_controls.kickPower + 0.1, 1.0);
             _lastKickerTime = now;
@@ -84,18 +106,36 @@ void GamepadJoystick::update() {
         _lastKickerTime = now - Kicker_Step_Time;
     }
 
-    _controls.kick = SDL_JoystickGetButton(_joystick, 7) |
-                     SDL_JoystickGetButton(_joystick, 5);
+    /*
+     *  KICK TRUE/FALSE
+     */
+    _controls.kick = SDL_JoystickGetButton(_joystick, 7);
 
+    /*
+     *  CHIP TRUE/FALSE
+     */
     _controls.chip = SDL_JoystickGetButton(_joystick, 5);
 
-    // for(int i = 0; i < SDL_JoystickNumButtons(_joystick); i++)
-    //    cout << static_cast<int>(SDL_JoystickGetButton(_joystick, i));
-    // cout << endl;
+    /*
+     *  VELOCITY ROTATION
+     */
+    // Logitech_Dual_Action Controller & Xbox 360 Controller
+    // auto leftX = SDL_JoystickGetAxis(_joystick, 0) / AXIS_MAX;
+    //
+    // Logitech F310 Controller
+    auto leftX = SDL_JoystickGetAxis(_joystick, 6) / AXIS_MAX;
+    _controls.rotation = -leftX;
 
-    auto leftX = SDL_JoystickGetAxis(_joystick, 0) / AXIS_MAX;
+    /*
+     *  VELOCITY TRANSLATION
+     */
+    // Logitech_Dual_Action Controller
+    // auto rightX = SDL_JoystickGetAxis(_joystick, 2) / AXIS_MAX;
+    // auto rightY = -SDL_JoystickGetAxis(_joystick, 3) / AXIS_MAX;
+    //
+    // Logitech F310 Controller & Xbox 360 Controller
     auto rightX = SDL_JoystickGetAxis(_joystick, 2) / AXIS_MAX;
-    auto rightY = -SDL_JoystickGetAxis(_joystick, 3) / AXIS_MAX;
+    auto rightY = -SDL_JoystickGetAxis(_joystick, 4) / AXIS_MAX;
 
     Geometry2d::Point input(rightX, rightY);
 
@@ -116,8 +156,6 @@ void GamepadJoystick::update() {
     }
 
     _controls.translation = Geometry2d::Point(input.x(), input.y());
-
-    _controls.rotation = -leftX;
 }
 
 JoystickControlValues GamepadJoystick::getJoystickControlValues() {
