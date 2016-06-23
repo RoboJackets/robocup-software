141092b6e3b21698a0fb1efd521ed3c793c3ec65
Albert Shaw <ashaw596@gmail.com>
2016-06-22 22:52:44 -0700

    Revert "Make Pretty"
    This reverts commit 305a82222d4f4bc414b38dd78369bc1909dcbf43.

--- soccer/Processor.cpp
+++ soccer/Processor.cpp
@@ -674,7 +674,8 @@ void Processor::updateGeometryPacket(const SSL_GeometryFieldSize& fieldSize) {
         }
     } else {
         cerr << "Error: failed to decode SSL geometry packet. Not resizing "
-                "field." << endl;
+                "field."
+             << endl;
     }
 }
 
