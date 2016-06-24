9df5eb6534f2b521baa2f40d74946243b56be808
Justin Buchanan <justbuchanan@gmail.com>
2016-06-23 22:46:49 -0400

    charge kicker and read voltage in main.cpp

--- firmware/robot2015/src-ctrl/main.cpp
+++ firmware/robot2015/src-ctrl/main.cpp
@@ -197,6 +197,11 @@ int main() {
         return replyBuf;
     };
 
+
+    kickerBoard.charge();
+    LOG(INIT, "Starged charging kicker board");
+    uint8_t kickerVoltage = 0;
+
     // Set the watdog timer's initial config
     Watchdog::Set(RJ_WATCHDOG_TIMER_VALUE);
 
@@ -256,6 +261,10 @@ int main() {
 
         // get the battery voltage
         battVoltage = (batt.read_u16() >> 8);
+
+        // get kicker voltage
+        kickerVoltage = kickerBoard.read_voltage();
+        LOG(INIT, "Kicker voltage: %u", kickerVoltage);
 
         // Set error-indicating leds on the control board
         ioExpander.writeMask(~errorBitmask, IOExpanderErrorLEDMask);
