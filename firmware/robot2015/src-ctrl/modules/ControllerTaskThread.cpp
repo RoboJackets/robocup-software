eb7a74af1faf96ccf3d0728cde919f4510b51814
Justin Buchanan <justbuchanan@gmail.com>
2016-06-24 22:51:35 -0400

    junk

--- firmware/robot2015/src-ctrl/modules/ControllerTaskThread.cpp
+++ firmware/robot2015/src-ctrl/modules/ControllerTaskThread.cpp
@@ -87,7 +87,8 @@ void Task_Controller(void const* args) {
 
     array<int16_t, 5> duty_cycles{};
 
-    pidController.setPidValues(1.5, 0.05, 0);  // TODO: tune pid values
+    // pidController.setPidValues(1.5, 0.05, 0);  // TODO: tune pid values
+    pidController.setPidValues(0.8, 0.05, 0);
 
     // initialize timeout timer
     commandTimeoutTimer = make_unique<RtosTimerHelper>(
