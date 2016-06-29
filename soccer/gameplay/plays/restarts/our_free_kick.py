2a8db8b5ad42b8d2fac4ea8ab38a0f5abcd4e464
Jay Kamat <jaygkamat@gmail.com>
2016-06-29 16:20:12 +0200

    Add timeouts to coordinated passes if requested
    Make indirect kicks kick to goal if we can't figure things out in 15 seconds

--- soccer/gameplay/plays/restarts/our_free_kick.py
+++ soccer/gameplay/plays/restarts/our_free_kick.py
@@ -13,12 +13,16 @@ class OurFreeKick(standard_play.StandardPlay):
 
     running = False
 
-    def __init__(self):
+    def __init__(self, indirect=None):
         super().__init__(continuous=True)
 
         # If we are indirect we don't want to shoot directly into the goal
         gs = main.game_state()
-        self.indirect = gs.is_indirect()
+
+        if indirect != None:
+            self.indirect = indirect
+        else:
+            self.indirect = gs.is_indirect()
 
         self.add_transition(behavior.Behavior.State.start,
                             behavior.Behavior.State.running, lambda: True,
@@ -31,7 +35,6 @@ class OurFreeKick(standard_play.StandardPlay):
         kicker.max_chip_range = 3.0
         # This will be reset to something else if indirect on the first iteration
         kicker.target = constants.Field.TheirGoalSegment
-        self.add_subbehavior(kicker, 'kicker', required=False, priority=5)
 
         # add two 'centers' that just move to fixed points
         center1 = skills.move.Move(robocup.Point(0, 1.5))
@@ -39,25 +42,24 @@ class OurFreeKick(standard_play.StandardPlay):
         center2 = skills.move.Move(robocup.Point(0, 1.5))
         self.add_subbehavior(center2, 'center2', required=False, priority=3)
 
-        lastKicker = kicker
         if self.indirect:
             receive_pt, target_point, probability = evaluation.touchpass_positioning.eval_best_receive_point(
                 main.ball().pos)
             pass_behavior = tactics.coordinated_pass.CoordinatedPass(
-                receive_pt, None, (kicker, lambda x: True))
+                receive_pt, None, (kicker, lambda x: True), preparing_timeout=15)
             # We don't need to manage this anymore
-            self.remove_subbehavior('kicker')
+            self.add_subbehavior(pass_behavior, 'kicker')
 
-            self.add_subbehavior(pass_behavior,
-                                 'receiver',
-                                 required=False,
-                                 priority=5)
             kicker.target = receive_pt
-            lastKicker = pass_behavior
+        else:
+            kicker = skills.line_kick.LineKick()
+            kicker.target = constants.Field.TheirGoalSegment
+            self.add_subbehavior(kicker, 'kicker', required=False, priority=5)
 
         self.add_transition(
             behavior.Behavior.State.running, behavior.Behavior.State.completed,
-            lambda: lastKicker.is_done_running(), 'kicker completes')
+            lambda: self.subbehavior_with_name('kicker').is_done_running()
+            and self.subbehavior_with_name('kicker').state != tactics.coordinated_pass.CoordinatedPass.State.timeout, 'kicker completes')
 
     @classmethod
     def score(cls):
@@ -67,6 +69,15 @@ class OurFreeKick(standard_play.StandardPlay):
 
     def on_enter_running(self):
         OurFreeKick.running = True
+
+    def execute_running(self):
+        if self.indirect \
+           and self.subbehavior_with_name('kicker').state == tactics.coordinated_pass.CoordinatedPass.State.timeout:
+            self.indirect = False
+            self.remove_subbehavior('kicker')
+            kicker = skills.line_kick.LineKick()
+            kicker.target = constants.Field.TheirGoalSegment
+            self.add_subbehavior(kicker, 'kicker', required=False, priority=5)
 
     def on_exit_running(self):
         OurFreeKick.running = False
