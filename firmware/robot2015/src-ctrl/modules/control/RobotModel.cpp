6cf45ba69768133d268b60e6e157424eea38fede
Albert Shaw <ashaw596@gmail.com>
2016-07-05 00:14:43 +0200

    Revert Angles

--- firmware/robot2015/src-ctrl/modules/control/RobotModel.cpp
+++ firmware/robot2015/src-ctrl/modules/control/RobotModel.cpp
@@ -4,9 +4,10 @@ const RobotModel RobotModel2015 = []() {
     RobotModel model;
     model.WheelRadius = 0.02856;
     // note: wheels are numbered clockwise, starting with the top-right
+    // TODO(ashaw596): Check angles.
     model.WheelAngles = {
-        DegreesToRadians(45), DegreesToRadians(315), DegreesToRadians(225),
-        DegreesToRadians(135),
+        DegreesToRadians(38), DegreesToRadians(315), DegreesToRadians(225),
+        DegreesToRadians(42),
     };
     model.WheelDist = 0.0798576;
 
