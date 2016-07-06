4316138d7e0ccd57aa910e54fac532c76ad4f784
Jay Kamat <jaygkamat@gmail.com>
2016-07-06 23:54:40 +0200

    Remove as many firmware files as I can w/o failing compile

--- firmware/robot2011/cpu/main.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// If nonzero, this is called after every update if the USB console is available
-extern void (*debug_update)(void);
--- firmware/robot2015/src-ctrl/config/robot-devices.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "rj-macros.hpp"
-#include "git_version.hpp"
-#include "robot-config.hpp"
-#include "pins-ctrl-2015.hpp"
