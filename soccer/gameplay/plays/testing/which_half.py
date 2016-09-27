4acc6b4902261507f5b2ffbef797cef593a7a7d1
Jeremy Feltracco <jpfeltracco@gatech.edu>
2016-09-26 21:52:14 -0400

    Skeleton implementation of which_half.

---
+++ soccer/gameplay/plays/testing/which_half.py
@@ -0,0 +1,41 @@
+import robocup
+import constants
+import play
+import enum
+import behavior
+import main
+
+
+# Maintains the state of the ball's position by keeping track of which
+# half the ball is on and prints on both entering a given state and
+# continously during the execution of a given state.
+class WhichHalf(play.Play):
+    class State(enum.Enum):
+        # Define your states here.
+        # eg: some_state = 0
+        # -----------------------
+        pass  # remove this once you have put in your states
+
+    def __init__(self):
+        super().__init__(continuous=True)
+
+        # Register the states you defined using 'add_state'.
+        # eg: self.add_state(WhichHalf.State.<???>,
+        #                    behavior.Behavior.State.running)
+        # ----------------------------------------------------
+
+        # Add your state transitions using 'add_transition'.
+        # eg: self.add_transition(behavior.Behavior.State.start,
+        #                         self.State.<???>, lambda: True,
+        #                         'immediately')
+        # eg: self.add_transition(self.State.<???>, self.State.<???>,
+        #                         lambda: <???>,
+        #                         'state change message')
+        # ------------------------------------------------------------
+
+    # Define your own 'on_enter' and 'execute' functions here.
+    # eg: def on_enter_<???>(self):
+    #         print('Something?')
+    # eg: def execute_<???>(self):
+    #         print('Something?')
+    # ---------------------------------------------------------
