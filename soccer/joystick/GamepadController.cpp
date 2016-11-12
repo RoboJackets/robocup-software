0178f0b9ba8a424de989220a85abcca2bb491a08
Jay Kamat <jaygkamat@gmail.com>
2016-11-11 22:56:27 -0500

    Fix a few joystick mappings

--- soccer/joystick/GamepadController.cpp
+++ soccer/joystick/GamepadController.cpp
@@ -6,6 +6,8 @@ namespace {
     constexpr RJ::Time Dribble_Step_Time = 125 * 1000;
     constexpr RJ::Time Kicker_Step_Time = 125 * 1000;
     const float AXIS_MAX = 32768.0f;
+    // cutoff for counting triggers as 'on'
+    const float TRIGGER_CUTOFF = 0.9;
 }
 
 GamepadController::GamepadController()
@@ -25,6 +27,7 @@ GamepadController::GamepadController()
 dpdown:h0.4,dpright:h0.0,dpright:h0.2,dpup:h0.0,dpup:h0.1,leftshoulder:h0.0,\
 dpup:h0.1,leftshoulder:h0.0,leftshoulder:b4,lefttrigger:b6,rightshoulder:b5,\
 righttrigger:b7,leftstick:b10,rightstick:b11,leftx:a0,lefty:a1,rightx:a2,righty:a3,");
+    SDL_GameControllerAddMapping("0300000057696920552047616d654300,Wii U GameCube Adapter Port 1,platform:Linux,x:b3,a:b0,b:b1,y:b2,start:b7,dpleft:b10,dpdown:b9,dpright:b11,dpup:b8,lefttrigger:a2,rightshoulder:b6,righttrigger:a5,leftx:a0,lefty:a1,rightx:a3,righty:a4,");
 
     // Controllers will be detected later if needed.
     connected = false;
@@ -96,9 +99,9 @@ void GamepadController::update() {
     /*
      *  DRIBBLER ON/OFF
      */
-    if (SDL_GameControllerGetButton(_controller, SDL_CONTROLLER_BUTTON_RIGHTSHOULDER)) {
+    if (SDL_GameControllerGetButton(_controller, SDL_CONTROLLER_BUTTON_LEFTSHOULDER)) {
         _controls.dribble = false;
-    } else if (SDL_GameControllerGetAxis(_controller, SDL_CONTROLLER_AXIS_TRIGGERLEFT)) {
+    } else if (SDL_GameControllerGetAxis(_controller, SDL_CONTROLLER_AXIS_TRIGGERLEFT) / AXIS_MAX > TRIGGER_CUTOFF) {
         _controls.dribble = true;
     }
 
@@ -144,18 +147,17 @@ void GamepadController::update() {
     /*
      *  KICK TRUE/FALSE
      */
-    _controls.kick =
-        SDL_GameControllerGetButton(_controller, SDL_CONTROLLER_BUTTON_RIGHTSHOULDER);
+    _controls.kick = SDL_GameControllerGetButton(_controller, SDL_CONTROLLER_BUTTON_RIGHTSHOULDER);
 
     /*
      *  CHIP TRUE/FALSE
      */
-    _controls.chip =
-        SDL_GameControllerGetAxis(_controller, SDL_CONTROLLER_AXIS_TRIGGERRIGHT);
+    _controls.chip = SDL_GameControllerGetAxis(_controller, SDL_CONTROLLER_AXIS_TRIGGERRIGHT) / AXIS_MAX > TRIGGER_CUTOFF;
 
     /*
      *  VELOCITY ROTATION
      */
+    // Logitech F310 Controller
     _controls.rotation =
         -1 * SDL_GameControllerGetAxis(_controller, SDL_CONTROLLER_AXIS_LEFTX) /
         AXIS_MAX;
