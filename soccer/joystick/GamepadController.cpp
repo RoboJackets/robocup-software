285cc21c201c3d812f0fbdbe904075540d8ba6c2
Jay Kamat <jaygkamat@gmail.com>
2016-11-13 14:59:01 -0500

    Swap triggers for dribbler control and add community mappings

--- soccer/joystick/GamepadController.cpp
+++ soccer/joystick/GamepadController.cpp
@@ -25,6 +25,11 @@ GamepadController::GamepadController()
 dpdown:h0.4,dpright:h0.0,dpright:h0.2,dpup:h0.0,dpup:h0.1,leftshoulder:h0.0,\
 dpup:h0.1,leftshoulder:h0.0,leftshoulder:b4,lefttrigger:b6,rightshoulder:b5,\
 righttrigger:b7,leftstick:b10,rightstick:b11,leftx:a0,lefty:a1,rightx:a2,righty:a3,");
+    // Attempt to add additional mappings (relative to run)
+    if (SDL_GameControllerAddMappingsFromFile("../external/sdlcontrollerdb/gamecontrollerdb.txt") == -1) {
+        cout << "Failed adding additional SDL Gamecontroller Mappings: " << SDL_GetError() << endl;
+    }
+
     // SDL_GameControllerAddMapping("0300000057696920552047616d654300,Wii U GameCube Adapter Port 1,platform:Linux,x:b3,a:b0,b:b1,y:b2,start:b7,dpleft:b10,dpdown:b9,dpright:b11,dpup:b8,lefttrigger:a2,rightshoulder:b6,righttrigger:a5,leftx:a0,lefty:a1,rightx:a3,righty:a4,");
 
     // Controllers will be detected later if needed.
@@ -98,9 +103,9 @@ void GamepadController::update() {
      *  DRIBBLER ON/OFF
      */
     if (SDL_GameControllerGetButton(_controller, SDL_CONTROLLER_BUTTON_LEFTSHOULDER)) {
-        _controls.dribble = false;
-    } else if (SDL_GameControllerGetAxis(_controller, SDL_CONTROLLER_AXIS_TRIGGERLEFT) / AXIS_MAX > TRIGGER_CUTOFF) {
         _controls.dribble = true;
+    } else if (SDL_GameControllerGetAxis(_controller, SDL_CONTROLLER_AXIS_TRIGGERLEFT) / AXIS_MAX > TRIGGER_CUTOFF) {
+        _controls.dribble = false;
     }
 
     /*
