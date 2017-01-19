5526e3967dac3bc04377cd2c1f0704ed9db826cd
Chachmu <mwoodward@gatech.edu>
2017-01-18 23:16:40 -0500

    Added default values to runcomp.sh

--- runcomp.sh
+++ runcomp.sh
@@ -2,11 +2,11 @@
 
 read -p "Defend Plus X (0 or 1): " defendPlusX
 read -p "Vision Channel (1,2 or Full): " visionChannel
-read -p "Specify Playbook file: " playbook
+read -p "Specify Playbook file (default comp.pbk): " playbook
 
 make
 
-until ./run/soccer -pbk $playbook -defend $defendPlusX -vision $visionChannel; do
+until ./run/soccer -pbk ${playbook:-"comp.pbk"} -defend ${defendPlusX:-"0"} -vision ${visionChannel:-"full"}; do
 	echo "Soccer crashed with exit code $?. Restarting" >&2
 	sleep 1
 done
