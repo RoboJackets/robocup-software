387ad6714bc91dd047169bf7fed025a716418df2
Jay Kamat <jaygkamat@gmail.com>
2017-01-25 23:31:00 -0500

    Customize field green and tweak colors a bunch

--- soccer/FieldView.cpp
+++ soccer/FieldView.cpp
@@ -63,6 +63,8 @@ FieldView::FieldView(QWidget* parent) : QWidget(parent) {
 
     // enable mouse tracking so we can update position label
     setMouseTracking(true);
+
+    show();
 }
 
 void FieldView::leaveEvent(QEvent* event) { _posLabel->setVisible(false); }
@@ -98,6 +100,9 @@ void FieldView::rotate(int value) {
 
 void FieldView::paintEvent(QPaintEvent* e) {
     QPainter p(this);
+    QStyleOption opt;
+    opt.init(this);
+    style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
 
     // antialiasing drastically improves rendering quality
     p.setRenderHint(QPainter::Antialiasing);
