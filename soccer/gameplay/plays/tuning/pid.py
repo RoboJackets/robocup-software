6c30551049c8772cbd281e892591bc283ec5741d
Chachmu <mwoodward@gatech.edu>
2017-01-29 16:54:40 -0500

    Created tuning play that moves back and forth along a line

---
+++ soccer/gameplay/plays/tuning/pid.py
@@ -0,0 +1,76 @@
+import behavior
+import robocup
+import main
+import enum
+import constants
+import tactics.line_up
+import tactics.tune_pid
+import skills.move
+import play
+
+
+# plan is for this to eventually loop through all robots and tune one at a time
+class pid(play.Play):
+    class State(enum.Enum):
+        prep = 1 #go to the start point, line up other bots
+        testing = 2  # Drive a test line
+
+    def __init__(self):
+        super().__init__(continuous=True)
+
+        for state in pid.State:
+            self.add_state(state, behavior.Behavior.State.running)
+
+        self.add_transition(behavior.Behavior.State.start,
+                            pid.State.prep, lambda: True,
+                            'immediately')
+
+        self.add_transition(pid.State.prep, pid.State.testing, lambda: self.subbehavior_with_name('move').state == behavior.Behavior.State.completed,'finished moving')
+
+
+        self.add_transition(pid.State.testing, behavior.Behavior.State.completed,
+                            lambda: self.check_continue(), 'command changed')
+
+        self.positions=[]
+
+    def create_lineup(self):
+        xsize = constants.Field.Width / 2 - .5
+
+        return robocup.Segment(
+            robocup.Point(xsize, .25), robocup.Point(xsize, 1.5))
+
+
+    def check_continue(self):
+        return False
+
+    def on_enter_prep(self):
+        xsize = constants.Field.Width/2
+        move = skills.move.Move(robocup.Point(-xsize+.1,2))
+        self.add_subbehavior(move, 'move', required=True, priority=100)
+
+        line_up = tactics.line_up.LineUp(self.create_lineup())
+        self.add_subbehavior(line_up, 'line_up', required=True, priority=80)
+
+    def on_exit_prep(self):
+        print("exit prep")
+        self.remove_subbehavior('move')
+
+    def on_enter_testing(self):
+        print("enter")
+        tune = tactics.tune_pid.Tune_pid()
+        self.add_subbehavior(tune, 'tune', required=True, priority=100)
+
+    def on_execute_testing(self):
+        main.system_state().draw_line(self.line, constants.Colors.Green,"tuning line")
+
+    def on_exit_testing(self):
+        print("exit")
+        self.remove_subbehavior('tune')
+
+    @classmethod
+    def score(cls):
+        return 10;
+
+    @classmethod
+    def handles_goalie(cls):
+        return True
---
+++ soccer/gameplay/tactics/tune_pid.py
@@ -0,0 +1,78 @@
+import single_robot_composite_behavior
+import behavior
+import main
+import constants
+import role_assignment
+import enum
+import robocup
+import skills
+import skills.move
+import time
+
+
+class Tune_pid(single_robot_composite_behavior.SingleRobotCompositeBehavior):
+    class State(enum.Enum):
+        tune = 1
+        process=2
+
+    def __init__(self):
+        super().__init__(continuous=False)
+
+        self.pause_time = 0
+
+        for substate in Tune_pid.State:
+            self.add_state(substate, behavior.Behavior.State.running)
+
+        self.add_transition(behavior.Behavior.State.start, Tune_pid.State.tune, lambda: True,'immediately')
+
+
+        self.add_transition(Tune_pid.State.tune, Tune_pid.State.process,lambda: self.subbehavior_with_name('move').state == behavior.Behavior.State.completed,'finished moving')
+
+
+        self.add_transition(Tune_pid.State.process, Tune_pid.State.tune,lambda: not self.check_complete(),'continue tuning')
+
+        self.add_transition(Tune_pid.State.process, behavior.Behavior.State.completed, lambda: self.check_complete(), 'done tuning')
+
+        self.positions=[]
+
+        xsize = constants.Field.Width/2
+
+        self.left_point=robocup.Point(-xsize,2)
+        self.right_point=robocup.Point(xsize,2)
+        self.line = robocup.Segment(self.left_point,self.right_point)
+
+    def check_complete(self):
+        return False;
+
+    def on_enter_tune(self):
+        if((self.robot.pos-self.left_point).mag() < .1):
+            move = skills.move.Move(self.right_point)
+            print("go right")
+        else:
+            move= skills.move.Move(self.left_point)
+            print("go left")
+
+        self.add_subbehavior(move, 'move', required=True, priority=100)
+
+    def execute_tune(self):
+        pos=self.robot.pos
+        self.positions.append(pos)
+
+    def on_exit_tune(self):
+        print("EXIT")
+        print(self.positions)
+        self.remove_subbehavior('move')
+
+    def on_enter_process(self):
+        print("process stuff")
+
+    def on_exit_process(self):
+        print("uh")
+
+
+    def role_requirements(self):
+        reqs = super().role_requirements()
+        if isinstance(reqs, role_assignment.RoleRequirements):
+            reqs.required_shell_id = 0
+
+        return reqs
