f3c37154bbe323fec98a177776741267b3ac5549
mmurley3 <mmurley3@gatech.edu>
2017-02-01 03:56:53 -0500

    Defenders don't move out of the way of our own kicker trying to kick at our goal during testing

--- soccer/gameplay/plays/testing/test_defense_and_kicker.py
+++ soccer/gameplay/plays/testing/test_defense_and_kicker.py
@@ -18,14 +18,24 @@ class TestDefenseAndKicker(play.Play):
     def on_enter_running(self):
         b = tactics.defense.Defense()
         self.add_subbehavior(b, name='defense', required=True)
-
+        
         kick = skills.pivot_kick.PivotKick()
+
         kick.target = constants.Field.OurGoalSegment
         kick.aim_params['desperate_timeout'] = 3
         self.add_subbehavior(kick, 'kick', required=False)
 
     def execute_running(self):
         kick = self.subbehavior_with_name('kick')
+
+        sublist = self.subbehavior_with_name('defense').all_subbehaviors()
+        roblist = []
+        for behavior in sublist:
+            print(behavior)
+            roblist.append(behavior.robot)
+
+        kick.shot_obstacle_ignoring_robots = roblist
+        
         if kick.is_done_running():
             kick.restart()
 
