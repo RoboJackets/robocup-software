839b49f7cda91c3f2a5603eddf0f383393de492d
Kevin Chen <kevinjulianchen@hotmail.com>
2017-02-14 23:45:55 +0000

    Added straight line path planning files

---
+++ soccer/planning/StraightLinePathPlanner.cpp
@@ -0,0 +1,10 @@
+#include "EscapeObstaclesPathPlanner.hpp"
+#include <Configuration.hpp>
+#include "RRTUtil.hpp"
+#include "RoboCupStateSpace.hpp"
+#include "TrapezoidalPath.hpp"
+
+
+std::unique_ptr<Path> StraightLinePathPlanner::run(PlanRequest& planRequest) {
+	
+}
