31545d8163ba0aa0152af84b64b9d065a2a916e4
Jay Kamat <jaygkamat@gmail.com>
2017-04-05 01:46:46 -0400

    Add motivational speeches to coach

--- soccer/gameplay/plays/training/coach.py
+++ soccer/gameplay/plays/training/coach.py
@@ -5,7 +5,7 @@ import enum
 import behavior
 import main
 import skills.move
-import math
+import subprocess
 
 # TODO needs to be refactored into a tactic.
 
@@ -16,10 +16,12 @@ class Coach(play.Play):
     SpinPerTick = 1
 
     OurScore = 0
+    TheirScore = 0
 
     class State(enum.Enum):
         watching = 0
         celebrating = 1
+        motivating = 2
 
     def __init__(self):
         super().__init__(continuous=True)
@@ -27,24 +29,35 @@ class Coach(play.Play):
 
         self.add_state(Coach.State.watching, behavior.Behavior.State.running)
         self.add_state(Coach.State.celebrating, behavior.Behavior.State.running)
+        self.add_state(Coach.State.motivating, behavior.Behavior.State.running)
 
         self.add_transition(behavior.Behavior.State.start, self.State.watching,
                             lambda: True, 'immediately')
 
         # Transitions into and out of celebration
         self.add_transition(self.State.watching, self.State.celebrating,
-                            lambda: self.score_increased(), 'goal scored')
+                            lambda: self.our_score_increased(), 'goal scored')
         self.add_transition(self.State.celebrating, self.State.watching,
                             lambda: self.spin_angle > Coach.MaxSpinAngle, 'done with celebration')
 
-    def score_increased(self):
+        self.add_transition(self.State.watching, self.State.motivating,
+                            lambda: self.their_score_increased(), 'Let a goal through')
+        self.add_transition(self.State.motivating, self.State.watching,
+                            lambda: True, 'done talking')
+
+    def our_score_increased(self):
         if (Coach.OurScore < main.game_state().our_score):
-            print(Coach.OurScore)
-            print(main.game_state().our_score)
             return True
         else:
             Coach.OurScore = main.game_state().our_score
             return False
+    def their_score_increased(self):
+        if (Coach.TheirScore < main.game_state().their_score):
+            return True
+        else:
+            Coach.TheirScore = main.game_state().their_score
+            return False
+
     def on_enter_running(self):
         move_point = robocup.Point(-constants.Field.Width / 2 - constants.Robot.Radius * 2,
                                    constants.Field.Length / 3)
@@ -81,3 +94,26 @@ class Coach(play.Play):
             facing_point.rotate(move.robot.pos, Coach.SpinPerTick)
             self.spin_angle += Coach.SpinPerTick
             move.robot.face(facing_point)
+
+    def on_enter_motivating(self):
+        Coach.TheirScore = main.game_state().their_score
+
+        print("\n\nListen up:")
+        print(Coach.fortune_wrapper())
+
+    @staticmethod
+    ## Returns a fortune.
+    def fortune_wrapper():
+        try:
+            # Prefer -o if available
+            fortune = subprocess.check_output(["fortune", "-o"])
+            fortune = fortune.decode("utf-8")
+        except subprocess.CalledProcessError:
+            try:
+                # Fall back to any fortune
+                fortune = subprocess.check_output(["fortune"])
+                fortune = fortune.decode("utf-8")
+            except subprocess.CalledProcessError:
+                # We must be on Windows, give a sna... enlightening remark. (install fortune!)
+                fortune = '''"There is no such thing as a typical user, and if there were, it wouldn't be you" - #usability'''
+        return fortune
