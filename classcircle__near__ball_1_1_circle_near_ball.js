d42d88efaf64d667543e2f4dee215f680e8a1b2b
georgeburdell <georgeburdell@users.noreply.github.com>
2017-04-05 00:53:18 +0000

    auto-updated api docs

--- classcircle__near__ball_1_1_circle_near_ball.js
+++ classcircle__near__ball_1_1_circle_near_ball.js
@@ -1,8 +1,8 @@
 var classcircle__near__ball_1_1_circle_near_ball =
 [
     [ "__init__", "classcircle__near__ball_1_1_circle_near_ball.html#a64ce2a9ff7fdec93f63e998428ee184d", null ],
-    [ "execute_completed", "classcircle__near__ball_1_1_circle_near_ball.html#a968e6d56e124cb3c0001d8c847e5d510", null ],
     [ "execute_running", "classcircle__near__ball_1_1_circle_near_ball.html#a1fea90f905cfbbb1fa366d8bc7ba07b9", null ],
     [ "get_circle_points", "classcircle__near__ball_1_1_circle_near_ball.html#a676092569e24197061e1bd7efb129021", null ],
-    [ "normalize_angle", "classcircle__near__ball_1_1_circle_near_ball.html#a174ad9b0eaf5b974ee6eeb2fc8bd1beb", null ]
+    [ "normalize_angle", "classcircle__near__ball_1_1_circle_near_ball.html#a174ad9b0eaf5b974ee6eeb2fc8bd1beb", null ],
+    [ "num_robots", "classcircle__near__ball_1_1_circle_near_ball.html#a34adbf2bebfe619949b4bce9d2fe4a82", null ]
 ];
