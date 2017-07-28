9b6c3c07f85126af670563e012870d99f53b2a2f
Chachmu <mwoodward@gatech.edu>
2017-07-28 10:00:13 +0900

    fix rename of lineup

--- soccer/gameplay/plays/restarts/defend_penalty.py
+++ soccer/gameplay/plays/restarts/defend_penalty.py
@@ -19,7 +19,7 @@ class DefendPenalty(play.Play):
         line = robocup.Segment(
             robocup.Point(1.5, 1.3), robocup.Point(1.5, 2.5))
         lineup = tactics.line_up.LineUp(line)
-        self.add_subbehavior(lineup, 'line')
+        self.add_subbehavior(lineup, 'lineup')
 
     @classmethod
     def score(cls):
