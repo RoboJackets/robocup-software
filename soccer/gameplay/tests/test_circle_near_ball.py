53e283b163030c2c9e3b5ebd9166ca06c369bed9
Jason Chan <jason27chan@gmail.com>
2017-09-05 23:13:20 +0000

    add comments test_circle_near_ball

--- soccer/gameplay/tests/test_circle_near_ball.py
+++ soccer/gameplay/tests/test_circle_near_ball.py
@@ -20,39 +20,58 @@ class TestCircleNearBall(unittest.TestCase):
 		width = constants.Field.Width
 		length = constants.Field.Length
 		
+		# Checks that the points around the ball are within the bounds of the field
 		def assert_circle_points(self):
 			array = self.circle_near_ball.get_circle_points(6)
+			array = [robocup.Point(0, -123)]
 			for point in array:		
-				self.assertTrue(point.x >= width * -1 / 2 and point.x <= width / 2, msg="failure of get_circle_points")
-				self.assertTrue(point.y >= 0 and point.y <= length, msg="failure of get_circle_points")
+				self.assertTrue(point.x >= width * -1 / 2 and point.x <= width / 2, msg="failure of get_circle_points. X position of points are outside the field. Point's x position is %d" %point.x)
+				self.assertTrue(point.y >= 0 and point.y <= length, msg="failure of get_circle_points. Y positions of points are outside the field. Point's y position is %d" %point.y )
 		
+		# Checks a point on the field to see if the circle around it is valid
 		def test_point(self, x, y):
 			main.set_ball(Moc_Ball(x, y))
 			assert_circle_points(self)
 
+		# Checks center of field
 		test_point(self, constants.Field.CenterPoint.x, constants.Field.CenterPoint.y)
-		test_point(self, width * -1 - constants.Robot.Radius * 100, 
-				length * -1 - constants.Robot.Radius * 100)
-		test_point(self, width * -1, 0)
+		
+		# Checks a point that should be clearly outside the field
+		test_point(self, width * -1, length * -1)
+		
+		# Checks bottom left corner of field
+		test_point(self, width * -1 / 2, 0)
+
+		# Checks (0, 0). Should be right in front of our goal
 		test_point(self, 0, 0)
-		test_point(self, width, 0)
-		test_point(self, width, length / 2)
-		test_point(self, width - constants.Robot.Radius, constants.Robot.Radius)
-		test_point(self, width + constants.Robot.Radius, constants.Robot.Radius * -1)
+
+		# Checks bottom right corner of field
+		test_point(self, width / 2, 0)
+
+		# Checks middle right part of field
+		test_point(self, width / 2, length / 2)
+
+		# Checks a point just inside the field
+		test_point(self, width / 2 - constants.Robot.Radius, constants.Robot.Radius)
+		
+		# Checks a point just outside of the field
+		test_point(self, width / 2 + constants.Robot.Radius, constants.Robot.Radius * -1)
 
 	def test_normalize_angle(self):
 		angle = -math.pi
 		while (angle < 0):
 			self.assertEqual(self.circle_near_ball.normalize_angle(angle),
-				angle + 2 * math.pi)
+				angle + 2 * math.pi, msg="Unexpected value for normalize angle.")
 			angle += math.pi / 6
 		angle = 0
 		while (angle <= 2 * math.pi):
 			self.assertEqual(angle, 
-				self.circle_near_ball.normalize_angle(angle))
+				self.circle_near_ball.normalize_angle(angle), 
+				msg="Unexpected value for normalize angle.")
 			angle += math.pi / 6
 		angle = 3 * math.pi
 		while (angle < 4 * math.pi):
 			self.assertEqual(angle - 2 * math.pi,
-				self.circle_near_ball.normalize_angle(angle))
+				self.circle_near_ball.normalize_angle(angle), 
+				msg="Unexpected value for normalize angle.")
 			angle += math.pi / 6
--- soccer/gameplay/tests/test_field.py
+++ soccer/gameplay/tests/test_field.py
@@ -9,6 +9,9 @@ class Moc_Robot:
 		self.pos = robocup.Point(x, y)
 		self.visible = True;
 
+	def set_pos(self, x, y):
+		self.pos = robocup.Point(x, y)
+
 class TestField(unittest.TestCase):
 	def __init__(self, *args, **kwargs):
 		super(TestField, self).__init__(*args, **kwargs)
@@ -16,7 +19,16 @@ class TestField(unittest.TestCase):
 	def setUp(self):
 		self.our_robots = [Moc_Robot(0, 0), Moc_Robot(0, 0), Moc_Robot(0, 0), 
 				Moc_Robot(0, 0), Moc_Robot(0, 0), Moc_Robot(0, 0)]
-		# main.set_our_robots(self.our_robots);
+		main.set_their_robots(self.our_robots)
+
+	def set_robot_pos(self, x, y):
+		for robot in self.our_robots:
+			robot.set_pos(x, y)
 
 	def test_space_coeff_at_pos(self):
-		self.assertTrue(evaluation.field.space_coeff_at_pos(robocup.Point(0, 0), excluded_robots=[], robots=self.our_robots) == 1)
+		def run_function(x, y):
+			return evaluation.field.space_coeff_at_pos(robocup.Point(x, y))
+		
+		self.assertTrue(run_function(0, 0) == 1)
+		self.set_robot_pos(0, constants.Field.Length)
+		self.assertTrue(run_function(0, 0) == 0) 
