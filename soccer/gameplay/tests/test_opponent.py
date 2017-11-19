2f0bddd0466aad18e92d2ae1ba1df74779723538
Jason Chan <jason27chan@gmail.com>
2017-11-19 20:40:25 +0000

    this doesn't work so why am i even writing these functions

--- soccer/gameplay/tests/test_opponent.py
+++ soccer/gameplay/tests/test_opponent.py
@@ -55,7 +55,7 @@ class TestOpponent(unittest.TestCase):
 
 		# 3 robots are located at our goal, ball is at our goal
 		self.set_robot_pos(3, 0, length)
-		self.assertEqual(run_function, 3, "some enemy robots located at our goal are not considered on offense")
+		self.assertEqual(run_function(), 3, "some enemy robots located at our goal are not considered on offense")
 
 		# Enemy robots are on their side of the field, ball is located close to them
 		self.set_robot_pos(6, 0, length * 3 / 4)
@@ -66,4 +66,49 @@ class TestOpponent(unittest.TestCase):
 		self.set_robot_pos(6, 0, length / 4)
 		self.assertEqual(run_function(), 6, "Enemy robots located on our side of the field are not considered on offense")
 
+	def test_get_closest_opponent(self):
+		length = constants.Field.Length
+		width = constants.Field.Width
+
+		# Tests a point for get_closest_opponent
+		# 
+		# @param x: x coordinate of test point
+		# @param y: y coordinate of test point
+		#
+		def test_point(x, y, directional_weight=1, excluded_robots=[]):
+			return evaluation.opponent.get_closest_opponent(robocup.Point(x, y), directional_weight, excluded_robots)
+
+		def printPoints():
+			robots = main.their_robots()
+			print()
+			for i in range(len(robots)):
+				print("Robot {} pos:({}, {})".format(i, robots[i].pos.x, robots[i].pos.y))
+
+
+		# All robots are at (0, 0), check that we get the first robot in the array
+		self.assertEqual(test_point(0, 0), main.their_robots()[0])
+
+		# All robots are at (0, 0), check that function works when the point is far
+		self.assertEqual(test_point(0, length / 2), main.their_robots()[0])
+
+		main.their_robots()[1].set_pos(0, length / 2)
+		
+		# This would work if the function worked
+		self.assertEqual(test_point(width / 2, length / 2), main.their_robots()[1])
+		
+
+		self.assertEqual(test_point(0, length / 4, 0), main.their_robots()[1])
+
+		main.their_robots()[1].set_pos(0, length / 2)
+		self.assertEqual(test_point(0, length / 4, .5), main.their_robots()[0])
+
+		# 5 robots are at (0, 0), one robot is closer to the center. 
+		# Check this robot is returned when we test the center of the field. 
+		main.their_robots()[1].set_pos(0, length / 4)
+		
+		# This does not return what it should because function is broken
+		self.assertEqual(test_point(0, length / 2), main.their_robots()[0])
+
+		# This does not return what it should because this function is broken
+		self.assertEqual(test_point(0, length / 2, excluded_robots=[main.their_robots()[0]]), main.their_robots()[0])
 
