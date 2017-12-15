24c5dbc3df29447e45a18464a5a2dcbbbecce46d
wfu <wfu@college.harvard.edu>
2017-12-15 15:54:49 -0500

    Added xbee libraries to include and cmake

--- soccer/radio/XBEERadio.hpp
+++ soccer/radio/XBEERadio.hpp
@@ -1,25 +1,40 @@
 #pragma once
 
 #include "Radio.hpp"
+#include <xbee.h>
 
 #include <mutex>
 
+/**
+ * @brief Radio with XBEE Series 1
+ * 
+ * @details This class allows us to communicate with XBEE Series 1 for 
+ * robot control. Currently a work in progress. 
+ */
 class XBEERadio : public Radio {
-public:
-    XBEERadio();
-    ~XBEERadio();
+    public:
+        XBEERadio();
+        XBEERadio(int id);
+        ~XBEERadio();
 
-    virtual bool isOpen() const override;
-    virtual void send(Packet::RadioTx& packet) override;
-    virtual void receive() override;
 
-    virtual void channel(int n) override;
-    void switchTeam(bool) override {}
+        virtual bool isOpen() const override;
+        virtual void send(Packet::RadioTx& packet) override;
+        virtual void receive() override;
 
-protected:
-    bool open();
+        virtual void channel(int n) override;
+        void switchTeam(bool) override {}
 
-    void command(uint8_t cmd);
-    void write(uint8_t reg, uint8_t value);
-    uint8_t read(uint8_t reg);
+    protected:
+        struct xbee *xbee;
+        struct xbee_con *con;
+        struct xbee_conAddress address;
+        struct xbee_conSettings settings;
+        xbee_err ret;
+
+        bool open();
+
+        void command(uint8_t cmd);
+        void write(uint8_t reg, uint8_t value);
+        uint8_t read(uint8_t reg);
 };
