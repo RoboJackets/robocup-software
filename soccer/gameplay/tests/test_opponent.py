e010b02beeff97c9cd2c191dddae6f7d72085814
Jason Chan <jason27chan@gmail.com>
2018-01-31 01:10:09 +0000

    get_closest_opponent worksgit add soccer/gameplay/tests/test_opponent.py

--- soccer/gameplay/tests/test_opponent.py
+++ soccer/gameplay/tests/test_opponent.py
@@ -94,21 +94,21 @@ class TestOpponent(unittest.TestCase):
 		main.their_robots()[1].set_pos(0, length / 2)
 		
 		# This would work if the function worked
-		self.assertEqual(test_point(width / 2, length / 2), main.their_robots()[0])
+		self.assertEqual(test_point(width / 2, length / 2), main.their_robots()[1])
 		
 
-		self.assertEqual(test_point(0, length / 4, 0), main.their_robots()[1])
+		self.assertEqual(test_point(0, length / 4, 0), main.their_robots()[0])
 
 		main.their_robots()[1].set_pos(0, length / 2)
-		self.assertEqual(test_point(0, length / 4, .5), main.their_robots()[0])
+		self.assertEqual(test_point(0, length / 4, .5), main.their_robots()[1])
 
 		# 5 robots are at (0, 0), one robot is closer to the center. 
 		# Check this robot is returned when we test the center of the field. 
 		main.their_robots()[1].set_pos(0, length / 4)
 		
 		# This does not return what it should because function is broken
-		self.assertEqual(test_point(0, length / 2), main.their_robots()[0])
+		self.assertEqual(test_point(0, length / 2), main.their_robots()[1])
 
 		# This does not return what it should because this function is broken
-		self.assertEqual(test_point(0, length / 2, excluded_robots=[main.their_robots()[0]]), main.their_robots()[0])
+		self.assertEqual(test_point(0, length / 2, excluded_robots=[main.their_robots()[0]]), main.their_robots()[1])
 
