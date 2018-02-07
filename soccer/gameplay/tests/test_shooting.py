3bd50cc8d28f71cfa996794f3fdff97b09258132
Jason Chan <jason27chan@gmail.com>
2018-02-07 02:04:41 +0000

    categorize tests using functions

--- soccer/gameplay/tests/test_shooting.py
+++ soccer/gameplay/tests/test_shooting.py
@@ -15,25 +15,89 @@ class Moc_Robot:
 class TestShooting(unittest.TestCase):
 	def __init__(self, *args, **kwargs):
 		super(TestShooting, self).__init__(*args, **kwargs)
+		self.system_state = robocup.SystemState()	
 
-	# def setUp(self):
-	# 	main.init()
-	# 	self.robots = [Moc_Robot(0, 0)]
-	# 	main.set_their_robots(self.robots)
+	def setUp(self):
+		main.set_system_state(self.system_state)
 
-	def test_eval_shot(self):
-		length = constants.Field.Length
-		width = constants.Field.Width
+		for robot in main.system_state().their_robots:
+			robot.set_vis_for_testing(True)
 
-		def run_function(x, y):
-			return evaluation.shooting.eval_shot(robocup.Point(x, y))
+		self.length = constants.Field.Length
+		self.width = constants.Field.Width
+		self.botRadius = constants.Robot.Radius
 
-		self.assertGreater(run_function(0, length), 0.99)
-		self.assertGreater(run_function(0, 3 * length / 4), 0.99)
+		self.their_robots = main.system_state().their_robots[0:5]
+		self.our_robots = main.system_state().our_robots[0:5]
 
-		self.assertGreater(run_function(width / 4, 3 * length / 4), 0.99)
-		self.assertGreater(run_function(-width / 4, 3 * length / 4), 0.99)
+		self.success = 1
+		self.failure = 0
 
-		# print("width: " ,width)
-		# print("length: ", length)
-		# print(run_function(width / 2, 3 * length / 4))
+
+
+	def eval_shot(self, x, y, excluded_robots=[]):
+		return evaluation.shooting.eval_shot(robocup.Point(x, y), excluded_robots)
+
+
+	# Set the location of a robot 
+	# We must use this function so that the C++ can act on the robot location
+	# 
+	# @param a_bot: bot to change position
+	# @param x: new x position of bot
+	# @param y: new y position of bot
+	#
+	def set_bot_pos(self, a_bot, x, y):
+		a_bot.set_pos_for_testing(robocup.Point(x, y))
+
+	def test_eval_shot_clear_field(self):
+
+		self.assertEqual(self.eval_shot(0, self.length), self.success)
+		
+		self.assertEqual(self.eval_shot(0, 3 * self.length / 4), self.success)
+
+		self.assertEqual(self.eval_shot(0, self.length / 2 + self.botRadius), self.success)
+
+		self.assertGreater(self.eval_shot(self.width / 4, 3 * self.length / 4), 0.99)
+		self.assertGreater(self.eval_shot(self.width / -4, 3 * self.length / 4), 0.99)
+
+		self.assertGreater(self.eval_shot(self.width / 4, self.length / 2 + self.botRadius), 0.95)
+		self.assertGreater(self.eval_shot(self.width / -4, self.length / 2 + self.botRadius), 0.95)
+
+	@unittest.skip("demonstrating skipping")
+	def test_eval_shot_problem_cases(self):
+		# This case return almost 1 when it should fail
+		self.assertEqual(self.eval_shot(0, 2 * self.length), self.failure)
+
+		# This case returns 0 when it should return 1
+		self.assertEqual(self.eval_shot(0, self.length / 2), self.success)
+
+		# Corner shots do not have 100% chance to hit the goal
+		self.assertGreater(self.eval_shot(self.width / 2, self.length), 0.99)
+
+	def test_eval_shot_with_bots_and_exclusion(self):
+		their_bot1, their_bot2, their_bot3, their_bot4, their_bot5 = self.their_robots
+		our_bot1, our_bot2, our_bot3, our_bot4, our_bot5 = self.our_robots
+
+		shooting_pos = 3 * self.length / 4
+
+		self.set_bot_pos(their_bot1, 0, shooting_pos + self.botRadius)
+		self.assertLess(self.eval_shot(0, shooting_pos), 0.05)
+
+		self.assertEqual(self.eval_shot(0, shooting_pos, [their_bot1]), self.success)
+
+		self.set_bot_pos(our_bot1, 0, shooting_pos + self.botRadius)
+		self.assertEqual(self.eval_shot(0, shooting_pos, [their_bot1]), 1)
+		
+		self.set_bot_pos(their_bot1, 0, shooting_pos + self.length / 8)
+		self.assertLess(self.eval_shot(0, shooting_pos), self.success)
+		self.assertEqual(self.eval_shot(0, shooting_pos, [their_bot1]), self.success)
+
+
+	def test_eval_shot_excluded_bots(self):
+		their_bot1, their_bot2, their_bot3, their_bot4, their_bot5 = self.their_robots
+		our_bot1, our_bot2, our_bot3, our_bot4, our_bot5 = self.our_robots
+
+		print("hello 1")
+		# self.assertEqual(self.eval_shot(0, 3 * length / 4, [their_bot1]), self.success)
+
+
