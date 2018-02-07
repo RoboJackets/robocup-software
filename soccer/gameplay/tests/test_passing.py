7ea7c9e186a1099abcd01582ed9285dcbeeff134
Jason Chan <jason27chan@gmail.com>
2018-02-07 02:05:03 +0000

    clean up some stuff. may want to format

--- soccer/gameplay/tests/test_passing.py
+++ soccer/gameplay/tests/test_passing.py
@@ -10,16 +10,15 @@ class Moc_Ball:
 
 class Moc_Robot:
 	def __init__(self, x, y):
-		self.pos = robocup.Point(float(x), float(y))
+		self.pos = robocup.Point(x, y)
 		self.visible = True;
 
 	def set_pos(self, x, y):
-		self.pos = robocup.Point(float(x), float(y))
+		self.pos = robocup.Point(x, y)
 
 class TestPassing(unittest.TestCase):
 	def __init__(self, *args, **kwargs):
 		super(TestPassing, self).__init__(*args, **kwargs)
-		self.config = robocup.Configuration.FromRegisteredConfigurables()
 		self.system_state = robocup.SystemState()	
 	
 	def setUp(self):
@@ -29,6 +28,13 @@ class TestPassing(unittest.TestCase):
 		for robot in main.system_state().their_robots:
 			robot.set_vis_for_testing(True)
 
+	# Set the location of a robot 
+	# We must use this function so that the C++ can act on the robot location
+	# 
+	# @param a_bot: bot to change position
+	# @param x: new x position of bot
+	# @param y: new y position of bot
+	#
 	def set_bot_pos(self, a_bot, x, y):
 		a_bot.set_pos_for_testing(robocup.Point(x, y))
 
@@ -38,7 +44,6 @@ class TestPassing(unittest.TestCase):
 		botRadius = constants.Robot.Radius
 
 		bot1, bot2, bot3, bot4, bot5 = main.system_state().their_robots[0:5]
-		# bot1, bot2, bot3, bot4, bot5 = main.system_state().our_robots[0:5]
 
 		def run_function(x1, y1, x2, y2, excluded_robots=[]):
 			return evaluation.passing.eval_pass(robocup.Point(x1, y1), robocup.Point(x2, y2), excluded_robots)
@@ -51,7 +56,7 @@ class TestPassing(unittest.TestCase):
 		# This is an edge case that we discussed to be okay. 
 		# In the future, if we want a pass that starts and ends at the same point 
 		# to be considered successful, we need to modify the eval_pass function. 
-		self.assertEqual(run_function(0, 0, 0, 0), fail, "point pass to itself succeeded")
+		self.assertEqual(run_function(0, 0, 0, 0), fail, "point pass to itself fail")
 
 		# Test a point passing to a close point. Should be successful
 		self.assertEqual(run_function(0, 0, 0, 0.1), success, "point pass to close point fail")
@@ -67,12 +72,12 @@ class TestPassing(unittest.TestCase):
 		self.assertEqual(run_function(0, 0, 0, length / 2), fail, "pass with robot in front of the shooter success")
 
 		# Test excluded_robots
-		self.assertEqual(run_function(0, 0, 0, length / 2, [bot1]), success, "test excluded_robots")
+		self.assertEqual(run_function(0, 0, 0, length / 2, [bot1]), success, "fail excluded_robots")
 
 		# Robot halfway between shooter and goal point. 
 		self.set_bot_pos(bot1, 0, length / 4)
-		self.assertGreater(run_function(0, 0, 0, length / 2), fail, "robot halfway between shooter and goal point fail")
-		self.assertLess(run_function(0, 0, 0, length / 2), success, "robot halfway between shooter and goal point success")
+		self.assertGreater(run_function(0, 0, 0, length / 2), fail, "robot halfway between shooter and goal point considered complete failure")
+		self.assertLess(run_function(0, 0, 0, length / 2), success, "robot halfway between shooter and goal point considered complete success")
 
 		
 
