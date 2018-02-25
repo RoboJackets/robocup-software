fd7a9b124cbd764d32c68a9772da10d259d51c5f
Jason Chan <Jason27chan@users.noreply.github.com>
2018-02-25 19:45:54 +0000

    remove spaces

--- soccer/gameplay/main.py
+++ soccer/gameplay/main.py
@@ -19,7 +19,7 @@ PLAYBOOKS_DIR = GAMEPLAY_DIR + '/playbooks'
 _has_initialized = False
 
 
-def init(logging = True):
+def init(logging=True):
     # by default, the logger only shows messages at the WARNING level or greater
     logging.getLogger().setLevel(logging.INFO)
 
