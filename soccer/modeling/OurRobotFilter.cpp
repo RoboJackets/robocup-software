8f7d8dbec97185640cfd9bf29988c58fc549f7b3
JNeiger <fcdneiger@gmail.com>
2018-05-30 01:54:06 +0000

    Started filling in the math

--- soccer/modeling/OurRobotFilter.cpp
+++ soccer/modeling/OurRobotFilter.cpp
@@ -1,35 +1,137 @@
 #include "OurRobotFilter.hpp"
 
-OutRobotFilter::OutRobotFilter() {}
-
-void OutRobotFilter::createConfiguration(Configuration* cfg) {
+void OurRobotFilter::createConfiguration(Configuration* cfg) {
     // TODO: Add all the config variables
     // Thinking just camera_frame_delay for right now
 }
 
-void OutRobotFilter::update(const std::array<RobotObservation, Num_Cameras>& obs, RobotPose* robot, RJ::Time currentTime, u_int32_t frameNumber) {
+OurRobotFilter::OurRobotFilter() : filt(),
+    F_k(StateSize, StateSize). B_k(StateSize, StateSize),
+    H_k_both(7, StateSize), H_k_enco(4, StateSize), Q_k(StateSize, StateSize),
+    R_k_both(7, StateSize), R_k_enco(4, StateSize),
+    x_hat_k_k(StateSize), p_k_k(StateSize, StateSize),
+    x_hat_k_k_camera(StateSize), p_k_k_camera(StateSize, StateSize) {
+    
+    // TODO: This feels terrible, maybe find some better way to do this
+    //       Load from a .mat? Auto-gen C++ function from matlab?
+    F_k <<      1.0000,        0,        0,   0.0001,   0.0001,  -0.0001,  -0.0001,        0,        0,        0,        0,
+                     0,   1.0000,        0,  -0.0002,   0.0002,   0.0002,  -0.0002,        0,        0,        0,        0,
+                     0,        0,   1.0000,  -0.0013,  -0.0009,  -0.0009,  -0.0013,        0,        0,        0,        0,
+                     0,        0,        0,   0.5979,   0.0897,  -0.2399,   0.1173,        0,        0,        0,        0,
+                     0,        0,        0,   0.0765,   0.5591,   0.1579,  -0.2442,        0,        0,        0,        0,
+                     0,        0,        0,  -0.2408,   0.1453,   0.5553,   0.0898,        0,        0,        0,        0,
+                     0,        0,        0,   0.1309,  -0.2452,   0.0766,   0.6028,        0,        0,        0,        0,
+                     0,        0,        0,   0.0126,   0.0013,  -0.0027,   0.0015,   1.0000,        0,        0,        0,
+                     0,        0,        0,   0.0011,   0.0122,   0.0019,  -0.0027,        0,   1.0000,        0,        0,
+                     0,        0,        0,  -0.0027,   0.0018,   0.0122,   0.0013,        0,        0,   1.0000,        0,
+                     0,        0,        0,   0.0016,  -0.0027,   0.0011,   0.0127,        0,        0,        0,   1.0000;
+     
+    B_k <<           0,        0,        0,        0,        0,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        0,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        0,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        0,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        0,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        0,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        0,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,  -0.0167,        0,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,  -0.0167,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        0,  -0.0167,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        0,        0,  -0.0167,        0,        0,        0,        0;
+     
+    H_k_both <<      1,        0,        0,        0,        0,        0,        0,        0,        0,        0,        0,
+                     0,        1,        0,        0,        0,        0,        0,        0,        0,        0,        0,
+                     0,        0,        1,        0,        0,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,        1,        0,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        1,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        0,        1,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        0,        0,        1,        0,        0,        0,        0;
+     
+    H_k_enco <<      0,        0,        0,        1,        0,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        1,        0,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        0,        1,        0,        0,        0,        0,        0,
+                     0,        0,        0,        0,        0,        0,        1,        0,        0,        0,        0;
+
+    R_k_both << 0.0100,        0,        0,        0,        0,        0,        0,
+                     0,   0.0100,        0,        0,        0,        0,        0,
+                     0,        0,   0.0100,        0,        0,        0,        0,
+                     0,        0,        0,   0.0001,        0,        0,        0,
+                     0,        0,        0,        0,   0.0001,        0,        0,
+                     0,        0,        0,        0,        0,   0.0001,        0,
+                     0,        0,        0,        0,        0,        0,   0.0001;
+
+    R_k_enco << 0.0001,        0,        0,        0,
+                     0,   0.0001,        0,        0,
+                     0,        0,   0.0001,        0,
+                     0,        0,        0,   0.0001;
+
+    x_hat_k_k = Eigen::VectorXd::Zero(StateSize);
+    p_k_k     = Eigen::MatrixXd::Zero(StateSize, StateSize);
+    x_hat_k_k_camera = Eigen::VectorXd::Zero(StateSize);
+    p_k_k_camera     = Eigen::VectorXd::Zero(StateSize, StateSize);
+}
+
+void OurRobotFilter::update(const std::array<RobotObservation, Num_Cameras>& obs, RobotPose* robot, RJ::Time currentTime, u_int32_t frameNumber) {
     filt.update(obs, robot, currentTime, frameNumber);
+
+    // Get sensor output
+    // Get robot commanded velocities
 
     // Calculate 1 frame for the new camera
     // Calculate N frames for the encoders
 }
 
-void OutRobotFilter::calculateOneFrame(Eigen::Vector& x_hat_k1_k1, Eigen::Vector& p_k1_k1, 
-                                       Eigen::Vector* x_hat_k_k, Eigen::Vector* p_k_k, 
+void OurRobotFilter::calculateOneFrame(Eigen::VectorXd& x_hat_k1_k1, Eigen::VectorXd& p_k1_k1, 
+                                       Eigen::VectorXd& z_k, Eigen::VectorXd& u_k_partial,
+                                       Eigen::VectorXd* x_hat_k_k, Eigen::VectorXd* p_k_k, 
                                        FilterMode mode) {
+    unsigned int OutputSize;
+    Eigen::MatrixXd* H_k;
+    Eigen::MatrixXd* R_k;
+
+    if (mode == FilterMode::BOTH_UPDATE) {
+        OutputSize = 7; // Encoder(4) + Camera(3)
+        H_k = &H_k_both;
+        R_k = &R_k_both;
+    } else { // mode == FilterMode::ENCODER_UPDATE
+        OutputSize = 4; // Encoder(4)
+        H_k = &H_k_enco;
+        R_k = &R_k_enco;
+    }
+
+    Eigen::MatrixXd BodyToGlobal(3, 3);
+    Eigen::MatrixXd F_k_rot(StateSize, StateSize);
+    Eigen::VectorXd u_k(StateSize);
+    Eigen::VectorXd x_hat_k_k1(StateSize);
+    Eigen::MatrixXd p_k_k1(StateSize, StateSize);
+    Eigen::VectorXd y_tilda_k(OutputSize);
+    Eigen::VectorXd y_tilda_k_k(OutputSize);
+    Eigen::MatrixXd s_k(OutputSize, OutputSize);
+    Eigen::MatrixXd k_k(StateSize, OutputSize);
+    Eigen::MatrixXd I(StateSize, StateSize);
+
+    u_k = Eigen::VectorXd::Zero(StateSize);
+    u_k.block(3, 6) = u_k_partial; // TODO: Make sure this works with vectors
+    I = Eigen::MatrixXd::Identity(StateSize, StateSize);
+
     // Rotate F_k
-    // x_hat_k_k1 = F_k * x_hat_k1_k1 + B_K * u
-    // P_k_k1 = F_k * p_k1_k1 * F_k' + Q
-    // y_tilda_k = z_k - H_k * x_hat_k_k1
-    // S_k = R_k + H_k * P_k_k1 * H_k'
-    // K_k = P_k_k1 * H_k * S_k^-1
-    // x_hat_k_k = x_hat_k_k1 + K_k * y_tilda_k
-    // P_k_k = (I - K_k * H_k) * P_k_k1 * (I - K_k * H_k)' + K_k * R_k * K_k'
-    // y_tilda_k_k = z_k - H_k * x_hat_k_k
+    calculateRotationMat(x_hat_k_k1(2), &BodyToGlobal);
+    // F_k_rot(1:3, 4:end) = BodyToGlobal * F_k(1:3, 4:end)
+
+    x_hat_k_k1 = F_k_rot * x_hat_k1_k1 + B_k * u_k;
+    p_k_k1 = F_k_rot * p_k1_k1 * F_k_rot.transpose() + Q_l;
+
+    y_tilda_k = z_k - *H_k * x_hat_k_k1;
+    s_k = *R_k + *H_k * p_k_k1 * (*H_k).transpose();
+
+    k_k = p_k_k1 * *H_k * s_k.inverse();
+    
+    x_hat_k_k = x_hat_k_k1 + k_k * y_tilda_k;
+    p_k_k = (I - k_k * *H_k) * p_k_k1 * (I - k_k * *H_k).transpose() + k_k * *R_k * k_k.transpose();
+    y_tilda_k_k = z_k - *H_k * x_hat_k_k;
 }
 
-void OutRobotFilter::calculateRotationMat(double heading, Eigen::Matrix* rot) {
-    // cos -sin 0
-    // sin  cos 0
-    //   0    0 1
+void OurRobotFilter::calculateRotationMat(double heading, Eigen::Matrix* rot) {d
+    rot << cos(heading), -sin(heading), 0,
+           sin(heading),  cos(heading), 0,
+                      0,             0, 1;
 }
--- soccer/modeling/OurRobotFilter.hpp
+++ soccer/modeling/OurRobotFilter.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <array>
-//TODO: Include Eigen
+#include <Eigen/Dense>
 
+#include <common/Geometry2d/Util.hpp>
+#include <rc-fshare/robot_model.hpp>
 #include "Configuration.hpp"
 #include "Robot.hpp"
 #include "RobotFilter.hpp"
@@ -35,8 +37,10 @@ private:
      * 
      * @param x_hat_k1_k1 Previous state estimate (in)
      * @param p_k1_k1 Previous covariance estimation (in)
-     * @param x_hat_k_k Output state estimate after filter
-     * @param p_k_k Output covaraince estimation
+     * @param z_k Measured output from sensors for this frame (in)
+     * @param u_k_partial Target robot velocities (body coordinate system) for this frame? (in) 
+     * @param x_hat_k_k Output state estimate after filter (out)
+     * @param p_k_k Output covaraince estimation (out)
      * @param Mode Whether we are using both encoder and camera data or just the encoder
      *        This changes the "C" matrix as well as the noise matrix R
      * 
@@ -47,9 +51,10 @@ private:
      *  4 Encoder values
      *  4 Encoder sigma (I term on the robot sum)
      */
-    void calculateOneFrame(Eigen::Vector& x_hat_k1_k1, Eigen::Vector& p_k1_k1, 
-                           Eigen::Vector* x_hat_k_k, Eigen::Vector* p_k_k, 
-                           FilterMode mode);
+void calculateOneFrame(Eigen::VectorXd& x_hat_k1_k1, Eigen::VectorXd& p_k1_k1, 
+                       Eigen::VectorXd& z_k, Eigen::VectorXd& u_k_partial,
+                       Eigen::VectorXd* x_hat_k_k, Eigen::VectorXd* p_k_k, 
+                       FilterMode mode)
 
 
     /**
@@ -60,13 +65,15 @@ private:
     RobotFilter flit; // Base filter to clean up the output camera data
 
 // Kalman filter matrices
-    Eigen::Matrix F_k; // A
-    Eigen::Matrix B_k; // B
-    Eigen::Matrix H_k_both; // C with both camera and encoder output
-    Eigen::Matrix H_k_enco; // C with only encoder output
-    Eigen::Matrix Q_k; // Covariance of process noise
-    Eigen::Matrix R_k_both; // Variance of observation noise for both camera and encoder
-    Eigen::Matrix R_k_enco; // Variance of observation noise for only the encoder
+    static constexpr StateSize = 11;
+
+    Eigen::MatrixXd F_k; // A
+    Eigen::MatrixXd B_k; // B
+    Eigen::MatrixXd H_k_both; // C with both camera and encoder output
+    Eigen::MatrixXd H_k_enco; // C with only encoder output
+    Eigen::MatrixXd Q_k; // Covariance of process noise
+    Eigen::MatrixXd R_k_both; // Variance of observation noise for both camera and encoder
+    Eigen::MatrixXd R_k_enco; // Variance of observation noise for only the encoder
 
     // Estimated State Vector for step k, given k's step information
     //   X Position
@@ -74,17 +81,19 @@ private:
     //   Heading Angle (Reference to X+)
     //   4 Encoder values
     //   4 Encoder sigma (I term sum on the robot)
-    Eigen::Vector x_hat_k_k;
-    Eigen::Vector p_k_k; // Covariance of the current step
+    Eigen::VectorXd x_hat_k_k;
+    Eigen::MatrixXd p_k_k; // Covariance of the current step
 
     // States at the last camera frame
     // We use this as the initial state to step forward using encoder inputs
-    Eigen::Vector x_hat_k_k_camera;
-    Eigen::vector p_k_k_camera;
+    Eigen::VectorXd x_hat_k_k_camera;
+    Eigen::VectorXd p_k_k_camera;
 
 // Constants / Transformations
-    // Conversion factor between encoder ticks to wheel velocity (rads / sec)
-    static constexpr double EncoderToWheelVel = 0; // 2*pi / t_soccer_period / (2048 * 3)
+    // Conversion factor between encoder ticks (per sample period) to wheel velocity (rads / sec)
+    //   2048 ticks/motor revolution, 3 motor revolution/wheel revolution,
+    //   1/60 secs/sample period, 2pi to convert revolutions/second to rad/sec
+    static constexpr double EncoderToWheelVel = 2.0*M_PI / (1.0 / 60.0) / (2048.0 * 3.0)
 
     // Conversion factor between camera output (mm) to position estimate (m)
     static constexpr double CameraToMeter = 1.0 / 1000.0; 
@@ -93,17 +102,13 @@ private:
     static constexpr int Camera_Frame_Delay = 5;
 
     // TODO: Need BotToWheel and WheelToBot transformation
-
+    auto BotToWheel = RobotModel::get().BotToWheel;
+    auto WheelToBot = RobotModel::get().WheelToBot;
 
 // Input History
     // List of all the past encoder readings between present and the last camera reading
-    std::vector<Eigen::Vector> pastEncoders; // TODO: Look into better list structures
+    std::vector<Eigen::VectorXd> pastEncoders; // TODO: Look into better list structures
 
     // Past inputs
-    std::vector<Eigen::Vector> pastU;
-
-    // Past headings
-    std::vector<double> pastHeading;
-
-
+    std::vector<Eigen::VectorXd> pastU;
 }
