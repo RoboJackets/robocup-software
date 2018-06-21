a89d70db275da8d90feb755a97c5b5731c0d537c
Jason Chan <jason27chan@gmail.com>
2018-06-21 03:08:17 +0000

    playing around with constants

--- soccer/gameplay/plays/restarts/our_free_kick.py
+++ soccer/gameplay/plays/restarts/our_free_kick.py
@@ -64,7 +64,14 @@ class OurFreeKick(standard_play.StandardPlay):
             kicker.kick_power = self.bump_power 
 
         if self.indirect:
-            receive_pt, receive_value = evaluation.passing_positioning.eval_best_receive_point(main.ball().pos)
+            receive_pt, receive_value = evaluation.passing_positioning.eval_best_receive_point(
+                    main.ball().pos, 
+                    field_weights=(0, 1, 5),
+                    nelder_mead_args=(robocup.Point(0.1, 0.25), robocup.Point(0.01, 0.01),1,2,.75,.5,50,1,0.01),
+                    weights=(1, 4, 5, 6)) #playing around with constants to get good receive point. Failed
+            print(receive_pt)
+            print(constants.Field.Length)
+            print(receive_pt.y + constants.Robot.Radius * 6 >= constants.Field.Length)
             print(receive_value)
             pass_behavior = tactics.coordinated_pass.CoordinatedPass(
                 receive_pt,
