23d34df8ee1723ec134a0324a485ac90b46f658e
Alan Penkar <adplax18@gmail.com>
2018-11-27 19:59:58 -0500

    Added test plays for 2 through 5 defenders (not including the goalie)

--- soccer/gameplay/plays/testing/test_defense_5_d.py
+++ soccer/gameplay/plays/testing/test_defense_5_d.py
@@ -6,7 +6,7 @@ import main
 
 
 ## Runs our Defense tactic
-class TestDefense(play.Play):
+class TestDefense5D(play.Play):
     def __init__(self):
         super().__init__(continuous=True)
         self.add_transition(behavior.Behavior.State.start,
@@ -14,7 +14,7 @@ class TestDefense(play.Play):
                             "immediately")
 
     def on_enter_running(self):
-        b = tactics.defense.Defense()
+        b = tactics.defense.Defense(defender_priorities=[20,19,18, 17, 16])
         self.add_subbehavior(b, name='defense', required=True)
 
     def on_exit_running(self):
