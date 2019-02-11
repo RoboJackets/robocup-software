99643fb8956e174be4c57b75f5a995707f0a816d
bopas2 <langthomas.tl@gmail.com>
2019-02-11 21:12:36 +0000

    set and get methods for defense and mark point as well as completed state

--- soccer/gameplay/tactics/wall.py
+++ soccer/gameplay/tactics/wall.py
@@ -23,8 +23,8 @@ class Wall(composite_behavior.CompositeBehavior):
 
         self.number_of_defenders = num_defenders
         self.curvature = curvature
-        self.mark_point = main.ball().pos if mark_point == None else mark_point
-        self.defense_point = defender_point
+        self._mark_point = main.ball().pos if mark_point == None else mark_point
+        self._defense_point = defender_point
         self.dist_from_mark = dist_from_mark
         self.defender_spacing = defender_spacing
 
@@ -34,6 +34,10 @@ class Wall(composite_behavior.CompositeBehavior):
         self.add_transition(behavior.Behavior.State.start,
                             Wall.State.defense_wall, lambda: True,
                             "immediately")
+
+        self.add_transition(
+            Wall.State.defense_wall,
+            behavior.Behavior.State.completed, lambda: self.completed(), "robot kicked")
 
     def on_enter_defense_wall(self):
         self.remove_all_subbehaviors()
@@ -51,6 +55,13 @@ class Wall(composite_behavior.CompositeBehavior):
                 required=False,
                 priority=self.number_of_defenders - i)
 
+    def completed(self):
+        for bhvr in self.all_subbehaviors():
+             if not bhvr.is_done_running():
+                return False
+        self.remove_all_subbehaviors()
+        return True
+
     # Finds the point on the wall (arc) at which the defender robot should move to
     def point_on_arc(self, midpoint, angle, defender_number):
         x_pt = midpoint.x - defender_number * (constants.Robot.Radius * self.defender_spacing) * math.cos(angle + (defender_number * self.curvature))
@@ -63,3 +74,23 @@ class Wall(composite_behavior.CompositeBehavior):
         x_pt = self.mark_point.x + self.dist_from_mark / dist_from_defense_and_mark * (self.defense_point.x - self.mark_point.x)
         y_pt = self.mark_point.y + self.dist_from_mark / dist_from_defense_and_mark * (self.defense_point.y - self.mark_point.y)
         return robocup.Point(x_pt, y_pt)
+
+    @property
+    def defense_point(self):
+        return self._defense_point
+
+    @property
+    def mark_point(self):
+        return self._mark_point
+
+    @defense_point.setter
+    def defense_point(self, point):
+        self.remove_all_subbehaviors()
+        self._defense_point = point
+        self.on_enter_defense_wall()
+
+    @mark_point.setter
+    def mark_point(self, point):
+        self.remove_all_subbehaviors()
+        self._mark_point = point
+        self.on_enter_defense_wall()
