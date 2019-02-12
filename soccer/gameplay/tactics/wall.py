ff0996a1b5b5aa1e12b9989f4a44cbbb631fe38f
bopas2 <langthomas.tl@gmail.com>
2019-02-12 07:52:47 +0000

    Fixed bug where wall would form incorrectly if defense point wasn't home goal

--- soccer/gameplay/tactics/wall.py
+++ soccer/gameplay/tactics/wall.py
@@ -14,10 +14,10 @@ class Wall(composite_behavior.CompositeBehavior):
 
     def __init__(self,
                  num_defenders = 3,                         # number of defenders we're making the wall with (default 3)
-                 curvature =  0,                             # curvature (in radians) of the wall 0 - pi/2
+                 curvature =  0,                            # 'curvature' (in radians) of the wall 
                  mark_point = None,                         # what point we are defending against (default is ball)
                  defender_point = robocup.Point(0, 0),      # what point we are defending (default is goal)
-                 defender_spacing = 3.5,                      # number of robot radii between the centers of the defenders in the wall
+                 defender_spacing = 3.5,                    # number of robot radii between the centers of the defenders in the wall
                  dist_from_mark = 1,                        # distance from the mark point we want to build the wall
                  defender_priorities = [20, 19, 18, 17, 16]): # default defense priorities                       
         super().__init__(continuous=True)
@@ -30,6 +30,9 @@ class Wall(composite_behavior.CompositeBehavior):
         self.defender_spacing = defender_spacing
         self.defender_priorities = defender_priorities
 
+        # Information for movement calculations to reduce redundancy 
+        self.midpoint = None
+
         self.add_state(Wall.State.defense_wall, 
                        behavior.Behavior.State.running)
 
@@ -39,10 +42,9 @@ class Wall(composite_behavior.CompositeBehavior):
 
     def on_enter_defense_wall(self):
         self.remove_all_subbehaviors()
-        midpoint = self.mark_point + (self.defense_point - self.mark_point).normalized() * self.dist_from_mark
-        angle = (midpoint - self.defense_point).angle()
+        self.update_midpoint()
         for i, priority in enumerate(self.defender_priorities[:self.number_of_defenders]):
-            pt = self.calculate_destination(midpoint, angle, i)
+            pt = self.calculate_destination(i)
             self.add_subbehavior(
                 skills.move.Move(pt),
                 name="robot" + str(i),
@@ -50,11 +52,15 @@ class Wall(composite_behavior.CompositeBehavior):
                 priority=priority)
 
     # Finds the point on the arc the defender should move to
-    def calculate_destination(self, midpoint, angle, robot_number):
+    def calculate_destination(self, robot_number):
         defender_number = robot_number - self.number_of_defenders / 2 + .5
-        x_perp = robocup.Point(1, self.defense_point.y) #TODO FIX
-        x_perp.rotate(self.defense_point, angle + defender_number * self.curvature + math.pi/2)
-        return midpoint + x_perp * constants.Robot.Radius * self.defender_spacing * defender_number        
+        direct = (self.mark_point - self.defense_point).normalized()
+        arc_angle = defender_number * self.curvature + math.pi/2
+        direct.rotate(robocup.Point(0,0), arc_angle)
+        return self.midpoint + direct * constants.Robot.Radius * self.defender_spacing * defender_number
+
+    def update_midpoint(self):
+        self.midpoint = self.mark_point + (self.defense_point - self.mark_point).normalized() * self.dist_from_mark
 
     @property
     def defense_point(self):
@@ -68,20 +74,18 @@ class Wall(composite_behavior.CompositeBehavior):
     @defense_point.setter
     def defense_point(self, point):
         self._defense_point = point
-        midpoint = self.mark_point + (self.defense_point - self.mark_point).normalized() * self.dist_from_mark
-        angle = (midpoint - self.defense_point).angle()
+        self.update_midpoint()
         for i in range(self.number_of_defenders):
             if self.has_subbehavior_with_name("robot" + str(i)):
                 behavior = self.subbehavior_with_name("robot" + str(i))
-                behavior.pos = self.calculate_destination(midpoint, angle, i)
+                behavior.pos = self.calculate_destination(i)
 
-    # Changes the point we are building a wall against, updates move behaviors
+    # Changes the point we are defending against, updates move behaviors
     @mark_point.setter
     def mark_point(self, point):
         self._mark_point = point
-        midpoint = self.mark_point + (self.defense_point - self.mark_point).normalized() * self.dist_from_mark
-        angle = (midpoint - self.defense_point).angle()
+        self.update_midpoint()
         for i in range(self.number_of_defenders):
             if self.has_subbehavior_with_name("robot" + str(i)):
                 behavior = self.subbehavior_with_name("robot" + str(i))
-                behavior.pos = self.calculate_destination(midpoint, angle, i)
+                behavior.pos = self.calculate_destination(i)
