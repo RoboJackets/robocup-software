d94b5a6f119e5fb0c275a2838f4e7ecbe26b1a5c
Jason Chan <jason27chan@gmail.com>
2019-02-12 20:19:53 -0500

    fix imports, checks for indirect kick

--- soccer/gameplay/plays/restarts/basic_indirect.py
+++ soccer/gameplay/plays/restarts/basic_indirect.py
@@ -1,15 +1,11 @@
-import standard_play
-import behavior
-import skills.move
-import skills.pivot_kick
-import constants
-import robocup
-import math
 import main
-import tactics.coordinated_pass
-import evaluation.passing_positioning
-import enum
+import robocup
+import behavior
+import constants
 
+import standard_play
+import skills.line_kick
+import tactics.coordinated_pass
 
 class BasicIndirect(standard_play.StandardPlay):
 
@@ -20,7 +16,6 @@ class BasicIndirect(standard_play.StandardPlay):
                             behavior.Behavior.State.running, lambda: True,
                             'immediately')
 
-        # FIXME: this could also be a PivotKick
         kicker = skills.line_kick.LineKick()
         kicker.use_chipper = True
 
@@ -35,18 +30,18 @@ class BasicIndirect(standard_play.StandardPlay):
             receiver_required=False,
             kicker_required=False,
             prekick_timeout=9)
-        self.add_subbehavior(pass_behavior, 'kicker')
+        self.add_subbehavior(pass_behavior, 'pass')
 
         self.add_transition(
             behavior.Behavior.State.running, behavior.Behavior.State.completed,
-            lambda: self.subbehavior_with_name('kicker').is_done_running() and self.subbehavior_with_name('kicker').state != tactics.coordinated_pass.CoordinatedPass.State.timeout,
-            'kicker completes')
+            lambda: self.subbehavior_with_name('pass').is_done_running() and self.subbehavior_with_name('pass').state != tactics.coordinated_pass.CoordinatedPass.State.timeout,
+            'pass completes')
 
     @classmethod
     def score(cls):
         gs = main.game_state()
         return 0 if behavior.Behavior.State.running or (
-            gs.is_ready_state() and gs.is_our_free_kick()) else float("inf")
+            gs.is_ready_state() and gs.is_our_indirect_kick()) else float("inf")
 
     @classmethod
     def is_restart(cls):
