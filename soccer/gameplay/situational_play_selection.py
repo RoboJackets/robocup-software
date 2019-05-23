18fe834055d3465f4d8a9ed9b23e4a13b31e9ca0
Christopher Lindbeck <clindbeck3@gatech.edu>
2019-05-22 21:00:48 -0400

    advanced successive if statement ai algorithem added along with better possession analysis

--- soccer/gameplay/situational_play_selection.py
+++ soccer/gameplay/situational_play_selection.py
@@ -3,6 +3,9 @@ import main
 import time
 import robocup
 import math
+from enum import Enum
+import constants
+
 
 class SituationalPlaySelector:
 
@@ -13,27 +16,32 @@ class SituationalPlaySelector:
     #I am thinking about wheither this should be based on the last frame, or totally fresh each frame.
     #I'm thinking the latter makes more sense, some timers can handle that type of behavior.
 
-    situations = {
-            'kickoff' : 0, #Plays that can perform our kickoff
-            'indirect_kick' : 0, #Plays that can perform our indirect kicks
-            'direct_kick' : 0, #Plays that can perform our direct kicks
-            'defend_restart_offensive' : 0, #Plays for defending our opponents restart on their side of the field
-            'defend_restart_defensive' : 0, #Plays for defending our opponents restart on our side of the field
-            'clear' : 0, #play for clearing the ball from our side of the field (should include defensive caution)
-            'defend_clear' : 0, #Plays for defending the opponents clear, when the ball is on their side.
-            'defend_goal' : 0, #Plays for defending our goal from opponents near it with the ball
-            'attack_goal' : 0, #Plays for attacking the opponents goal, when we have the ball near it
-            'offensive_scramble' : 0, #Plays for getting a loose ball when the ball is on the opponents half
-            'defensive_scramble' : 0, #Plays for getting a loose ball when the ball is on our half
-            'save_ball' : 0, #Plays that will trigger when the ball is headed out of the field with no obstuctions
-            'save_shot' : 0, #Plays that will trigger when the ball is headed directly at our goal
-            'offensive_pile_up' : 0, #Plays to handle a pile up on their side of the field
-            'defensive_pile_up': 0} #Plays to handle a pile up on our side of the field
+    
+
+    situations = [
+            'kickoff', #Plays that can perform our kickoff
+            'indirect_kick', #Plays that can perform our indirect kicks
+            'direct_kick', #Plays that can perform our direct kicks
+            'defend_restart_offensive', #Plays for defending our opponents restart on their side of the field
+            'defend_restart_defensive', #Plays for defending our opponents restart on our side of the field
+            'clear', #play for clearing the ball from our side of the field (should include defensive caution)
+            'defend_clear', #Plays for defending the opponents clear, when the ball is on their side.
+            'defend_goal', #Plays for defending our goal from opponents near it with the ball
+            'attack_goal', #Plays for attacking the opponents goal, when we have the ball near it
+            'offensive_scramble', #Plays for getting a loose ball when the ball is on the opponents half
+            'defensive_scramble', #Plays for getting a loose ball when the ball is on our half
+            'save_ball', #Plays that will trigger when the ball is headed out of the field with no obstuctions
+            'save_shot', #Plays that will trigger when the ball is headed directly at our goal
+            'offensive_pile_up', #Plays to handle a pile up on their side of the field
+            'defensive_pile_up'] #Plays to handle a pile up on our side of the field
        
 
     def __init__(self):
         print("Don't make an instance of this class you bafoon!") 
         exit() #This is a joke I'll need to remove at some point
+
+    currentSituation = dict()
+
 
     isSetup = False
     gameState = None
@@ -41,15 +49,7 @@ class SituationalPlaySelector:
     robotList = list()
     activeRobots = list()
 
-
-    lastFullPossession = dict()
-    possessionLength = dict()
-
-    possessionLastCalc = 0.0
-
-    robotPossessionTimer = 0.0
-
-    count = 0
+    ballLocation = None
 
     @classmethod
     def setupStates(cls):
@@ -59,6 +59,8 @@ class SituationalPlaySelector:
             cls.robotList.append(g)
         for g in cls.systemState.their_robots:
             cls.robotList.append(g)
+
+        cls.zeroCurrentSituation()
 
         cls.updateRobotList()
 
@@ -70,6 +72,12 @@ class SituationalPlaySelector:
                cls.activeRobots.append(g)
 
     @classmethod
+    def zeroCurrentSituation(cls):
+        for g in cls.situations:
+            cls.currentSituation[g] = False
+
+
+    @classmethod
     def updateAnalysis(cls):
 
         if(not cls.isSetup):
@@ -78,17 +86,15 @@ class SituationalPlaySelector:
         else:
             cls.updateRobotList()
 
+
+        cls.ballLocation = cls.locationUpdate()
         cls.ballPossessionUpdate()
-        #cls.scoreUpdate()
 
     #It would be interesting to evaluate characteristics about our enemy
     #Like some kind of manuverability/speed characteristic
     #Would have to have one for each team
    
-    
-    #there should also probably be a factor for when the ball is moving too
-    #fast to reasonably be captured, if that is a concern.
-
+    '''
     def ballVelFactor(ballVel):
         return 1.0 
 
@@ -118,7 +124,6 @@ class SituationalPlaySelector:
         factor = obsDrop * cls.clip((math.sqrt(x**2 + y**2) - obstDist) * 0.5, minValue=0.0) * (3 / math.sqrt((x-y)**2 + (y-x)**2))
         return factor
 
-
     #A triweight kernal approximation of a gaussian
     @staticmethod
     def triweight(x):
@@ -131,12 +136,6 @@ class SituationalPlaySelector:
             return 0.0
         return cls.clip(cls.triweight(cls.trajectory(x,y,v)))
      
-
-    #penalty for not facing the ball as a function of ball velocity and distance to the ball 
-    #@staticmethod
-    #def rotationFactor(ballPos, ballVel, robotPos):
-    #    return 1.0
-
     #Transforms position vector from global to the ball (where the ball is always traveling in the pi / 4 direction)
     #(Really just a general transform but its made to do this in particular)
     @staticmethod
@@ -159,8 +158,7 @@ class SituationalPlaySelector:
     @classmethod
     def get_obstruct_dist(cls):
         return 999999
-
-
+    '''
 
     #So this was going to be super fancy but for now its very simple, basically just based on the angle
     @classmethod
@@ -177,17 +175,6 @@ class SituationalPlaySelector:
             return 0.0
         return (1 - abs(angle / 90))**4
 
-
-
-        '''
-        if(math.sqrt(ballVel.x**2 + ballVel.y**2) < 0.2):
-            return 0.0
-        robotPos = (robot.pos.x, robot.pos.y)
-        robotPos_ball = cls.transformToBall(robotPos, ballPos, ballVel)
-        return cls.ballRecieveFunction(robotPos_ball[0], robotPos_ball[1], math.sqrt(ballVel.x**2 + ballVel.y**2), cls.get_obstruct_dist())
-        '''
-
-
     #A function that determines if the ball is in the mouth of a given robot
     @staticmethod
     def possesses_the_ball(ballPos, robot, distThresh=0.14, angleThresh=35):
@@ -198,71 +185,194 @@ class SituationalPlaySelector:
         return False
 
 
+
+    hasBall = dict()
+    posChangeTime = dict()
+    posDuration = dict()
+    recvProb = dict()
+    ballDist = dict()
+
+
     @classmethod
     def ballPossessionUpdate(cls):
 
-
-        #Ok, so we want to look at a couple of factors here
-
-        #For one, if a bot has the ball in its mouth, than they probably are in possession of the ball
-        #But we want to make sure that the ball is actually in the mouth, not just bouncing off.
-
-        #So we want to look at the duration, direction, and distance away from the ball
-
-        #We will want to think about how we want to register possession when the ball is within a close radius
-
-        #1. Direct ball possession
-            #Distance away from robot mouth
-            #Probably sum of distance and rotation error times some factors
-
-        #2. Recent ball possession
-            #Should include factor for if ball was impelled (kicked)
-            #A quick decaying timer from when the robot genuially had the ball in its mouth past the settle timer
-
-        #3. Ball ingress towards robot
-            #Probably a cone that is narrower based on velocity I would think
-            #Might need to be a continuous function
-
-        #4. 
-
-        
-        '''
-        for i in cls.systemState.our_robots:
-            #print(i)
-            pass
-
-        for i in cls.systemState.their_robots:
-            #print(i)
-            pass
-        '''
         for g in cls.activeRobots:
-            printPoint = robocup.Point(g.pos.x + 0.1, g.pos.y)
-            #cls.systemState.draw_text(str(round(cls.ball_recieve_prob(cls.systemState.ball.pos,cls.systemState.ball.vel,g), 3)), g.pos, (0.3,0,0),"hat")
-            cls.systemState.draw_text(str(cls.possesses_the_ball(cls.systemState.ball.pos,g)), printPoint, (0.3,0,0),"hat")
-        #print(cls.systemState.ball.pos)
+            printPoint1 = robocup.Point(g.pos.x + 0.1, g.pos.y)
+            printPoint2 = robocup.Point(g.pos.x + 0.1, g.pos.y - 0.12)
+            printPoint3 = robocup.Point(g.pos.x + 0.1, g.pos.y - 0.24)
+            printPoint4 = robocup.Point(g.pos.x + 0.1, g.pos.y - 0.36)
 
-        pass
+
+            hasBall = cls.possesses_the_ball(cls.systemState.ball.pos,g)
+            try:
+                hadBall = cls.hasBall[g]
+            except:
+                hadBall = False
+                cls.hasBall[g] = False
+
+            if(hasBall and not hadBall):
+                cls.posChangeTime[g] = time.time()
+
+            if(hadBall and not hasBall):
+                cls.posDuration[g] = abs(time.time() - cls.posChangeTime[g])
+                cls.posChangeTime[g] = time.time()
+
+            cls.hasBall[g] = hasBall
+
+            #cls.systemState.draw_text(str(round(cls.ball_recieve_prob(cls.systemState.ball.pos,cls.systemState.ball.vel,g), 3)), g.pos, (0.3,0,0),"hat")
+            #cls.systemState.draw_text(str(cls.possesses_the_ball(cls.systemState.ball.pos,g)), printPoint, (0.3,0,0),"hat")
+            if(cls.hasBall[g]):
+                cls.systemState.draw_text(str(abs(time.time() - cls.posChangeTime[g])), printPoint1, (0,0,0),"hat")
+            else:
+                cls.systemState.draw_text("N/A", printPoint1, (0,0,0),"hat")
+
+            try:
+                cls.systemState.draw_text(str(round(cls.posDuration[g],3)), printPoint2, (0,0,0),"hat")
+            except:
+                cls.systemState.draw_text("N/A", printPoint2, (0,0,0),"hat")
+
+            cls.recvProb[g] = cls.ball_recieve_prob(cls.systemState.ball.pos, cls.systemState.ball.vel, g)
+
+            cls.systemState.draw_text(str(round(cls.recvProb[g],3)), printPoint3, (0,0,0), "hat")
+
+            try:
+                cls.systemState.draw_text(str(round(abs(cls.posChangeTime[g] - time.time()),3)), printPoint4, (0,0,0), "hat")
+            except:
+                pass
+
+        ourScore = 0.0
+        theirScore = 0.0
+
+        for g in cls.activeRobots:
+            if(g.is_ours()):
+                if(cls.hasBall[g]):
+                    ourScore += abs(time.time() - cls.posChangeTime.get(g,time.time()))
+                elif(cls.posDuration.get(g,0) > 0 and abs(time.time() - cls.posChangeTime[g]) < 4):
+                    ourScore += cls.posDuration[g] / abs(time.time() - cls.posChangeTime[g])
+
+                ourScore += cls.recvProb.get(g,0) * 15
+            else:
+                if(cls.hasBall[g]):
+                    theirScore += abs(time.time() - cls.posChangeTime.get(g,time.time()))
+                elif(cls.posDuration.get(g,0) > 0 and abs(time.time() - cls.posChangeTime[g]) < 4):
+                    theirScore += cls.posDuration[g] / abs(time.time() - cls.posChangeTime[g])
+ 
+                theirScore += cls.recvProb.get(g,0) * 15
+
+        cls.ballPossessionScore = ourScore - theirScore
+
+    
+    class fieldLoc(Enum):
+        defendSide = 1
+        midfield = 2
+        attackSide = 3
 
     @classmethod
     def locationUpdate(cls):
         #This will basically just figure out what part of the field the ball is in.
-        pass
+        #This should probably be appended at some point to account where the ball will be in the near future
+        
+        midfieldFactor = 0.33  
+        
+        ballPos = cls.systemState.ball.pos
+
+        fieldLen = constants.Field.Length
+        midfield = fieldLen / 2
+
+
+        if(ballPos.y < midfield - (midfieldFactor / 2) * fieldLen):
+            return cls.fieldLoc.defendSide
+        elif(ballPos.y > midfield + (midfieldFactor / 2) * fieldLen):
+            return cls.fieldLoc.attackSide
+        else:
+            return cls.fieldLoc.midfield
+
+
+
 
 
     #This function will detect if the ball is about to go out of bounds, or is headed towards the goal
     @classmethod
-    def ballTrajectoryUpdate(cls):
+    def ballTrajectoryUpdate(cls, ballPos, ballVel, factor=0.5):
+        #Find function that determines if a point is in bounds
         pass
 
     @classmethod
     def scoreUpdate(cls):
-        #This will actually update the dict
-        pass
+        cls.zeroCurrentSituation()
 
-    #This method is stupid and does nothing for us (I need to figure out what it was supposed to do)
+        if(cls.gameState.is_our_kickoff()):
+            pass
+        if(cls.gameState.is_our_penalty()):
+            pass
+        if(cls.gameState.is_our_direct()):
+            pass
+        if(cls.gameState.is_our_indirect()):
+            pass
+        if(cls.gameState.is_our_free_kick()):
+            pass
+        if(cls.gameState.is_their_kickoff()):
+            pass
+        if(cls.gameState.is_their_penalty()):
+            pass
+        if(cls.gameState.is_their_direct()):
+            pass
+        if(cls.gameState.is_their_indirect()):
+            pass
+        if(cls.gameState.is_their_free_kick()):
+            pass
+
+        if(cls.ballLocation == cls.fieldLoc.defendSide):
+            if(cls.currentPileup):
+                pass
+            elif(cls.freeBall):
+                pass
+            elif(cls.ourBall):
+                pass
+            elif(cls.theirBall):
+                pass
+            else:
+                print("Situation analysis has done broke")
+        
+        elif(cls.ballLocation == cls.fieldLoc.attackSide):
+            if(cls.currentPileup):
+                pass
+            elif(cls.freeBall):
+                pass
+            elif(cls.ourBall):
+                pass
+            elif(cls.theirBall):
+                pass
+            else:
+                print("Situation analysis has done broke")
+        
+        elif(cls.ballLocation == cls.fieldLoc.midfield):
+            if(cls.currentPileup):
+                pass
+            elif(cls.freeBall):
+                pass
+            elif(cls.ourBall):
+                pass
+            elif(cls.theirBall):
+                pass
+            else:
+                print("Situation analysis has done broke")
+
+
+    def updatePileup(cls):
+        cls.currentPileup = cls.isPileup()
+            
+    @classmethod
+    def isPileup(cls):
+        possessingRobots
+        for g in cls.activeRobots:
+            pass
+        #Will attempt to detect if there is a pileup on the field 
+
+
     @classmethod
     def getBonus(cls):
-        return max([situations.get(t) for t in cls.situations])
+        return max([cls.situations.get(t) for t in cls.situations])
 
 
 
