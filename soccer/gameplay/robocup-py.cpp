a37e796706a8b2143a474d5820a4b6c583b1f1d3
Christopher Lindbeck <clindbeck9@gmail.com>
2019-06-20 19:08:45 -0400

    A line got moved on accident before causing compile error, fixed

--- soccer/gameplay/robocup-py.cpp
+++ soccer/gameplay/robocup-py.cpp
@@ -780,7 +780,6 @@ BOOST_PYTHON_MODULE(robocup) {
         .def("is_placement", &GameState::placement)
         .def("is_direct", &GameState::direct)
         .def("is_indirect", &GameState::indirect)
-        .def("is_penalty_shootout", &GameState::isPenaltyShootout);
         .def("is_our_kickoff", &GameState::ourKickoff)
         .def("is_our_penalty", &GameState::ourPenalty)
         .def("is_our_direct", &GameState::ourDirect)
@@ -807,6 +806,7 @@ BOOST_PYTHON_MODULE(robocup) {
         .def("is_halftime", &GameState::isHalftime)
         .def("is_overtime1", &GameState::isOvertime1)
         .def("is_overtime2", &GameState::isOvertime2)
+        .def("is_penalty_shootout", &GameState::isPenaltyShootout);
 
     class_<Robot>("Robot", init<int, bool>())
         .def("shell_id", &Robot::shell)
