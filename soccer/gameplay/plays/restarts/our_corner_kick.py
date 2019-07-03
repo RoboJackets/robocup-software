3add00d935b534c9cbe9c994c863baf3d7faebed
tjones320 <tjones320@gatech.edu>
2019-07-03 21:56:56 +1000

    made corner kick two phases to rotate around ball

--- soccer/gameplay/plays/restarts/our_corner_kick.py
+++ soccer/gameplay/plays/restarts/our_corner_kick.py
@@ -5,6 +5,7 @@ import tactics
 import robocup
 import constants
 import main
+import enum
 
 
 class OurCornerKick(standard_play.StandardPlay):
@@ -16,13 +17,62 @@ class OurCornerKick(standard_play.StandardPlay):
     MaxKickSpeed = 0.5
     MaxKickAccel = 0.5
 
+    class State(enum.Enum):
+        move = 1
+
+        kick = 2
+
     def __init__(self):
         super().__init__(continuous=True)
 
+        for s in OurCornerKick.State :
+            self.add_state(s, behavior.Behavior.State.running)
+
         self.add_transition(behavior.Behavior.State.start,
-                            behavior.Behavior.State.running, lambda: True,
+                            OurCornerKick.State.move, lambda: True,
                             'immediately')
 
+        self.add_transition(OurCornerKick.State.move,
+                            OurCornerKick.State.kick,
+                            lambda: self.subbehavior_with_name('move behind').state == behavior.Behavior.State.completed,
+                            'kick')
+
+        self.kicker = skills.line_kick.LineKick()
+        self.add_transition(OurCornerKick.State.kick,
+                            behavior.Behavior.State.completed,
+                            self.kicker.is_done_running, 'kicker is done')
+
+    @classmethod
+    def score(cls):
+        gs = main.game_state()
+        if gs.is_ready_state() and gs.is_our_direct() and main.ball().pos.y > (
+                constants.Field.Length - 1.0):
+            return 0
+        else:
+            return float("inf")
+
+    @classmethod
+    def is_restart(cls):
+        return True
+
+    def on_enter_move(self):
+        goal_x = constants.Field.GoalWidth * (1 if main.ball().pos.x < 0 else -1)
+        self.move_behind_pos = (main.ball().pos - robocup.Segment(
+            robocup.Point(goal_x, constants.Field.Length), robocup.Point(
+                goal_x,
+                constants.Field.Length - OurCornerKick.TargetSegmentWidth)).center()).normalized() * 0.2 + main.ball().pos
+        self.add_subbehavior(skills.move.Move(self.move_behind_pos),'move behind', required = True, priority = 5)
+
+    def execute_move(self):
+        goal_x = constants.Field.GoalWidth * (1 if main.ball().pos.x < 0 else -1)
+        self.move_behind_pos = (main.ball().pos - robocup.Segment(
+        robocup.Point(goal_x, constants.Field.Length), robocup.Point(goal_x,
+        constants.Field.Length - OurCornerKick.TargetSegmentWidth)).center()).normalized() * 0.2 + main.ball().pos
+
+    def on_exit_move(self):
+        self.remove_all_subbehaviors()
+
+    def on_enter_kick(self):
         self.kicker = skills.line_kick.LineKick()
         self.kicker.use_chipper = True
         self.kicker.chip_power = OurCornerKick.ChipperPower  # TODO: base this on the target dist from the bot
@@ -48,26 +98,8 @@ class OurCornerKick(standard_play.StandardPlay):
                              required=False,
                              priority=3)
 
-        self.add_transition(behavior.Behavior.State.running,
-                            behavior.Behavior.State.completed,
-                            self.kicker.is_done_running, 'kicker is done')
 
-    @classmethod
-    def score(cls):
-        gs = main.game_state()
-
-        if gs.is_ready_state() and gs.is_our_direct() and main.ball().pos.y > (
-                constants.Field.Length - 1.0):
-            return 0
-        else:
-            return float("inf")
-
-    @classmethod
-    def is_restart(cls):
-        return True
-
-    def execute_running(self):
-        super().execute_running()
+    def execute_kick(self):
         # setup the kicker target
         goal_x = constants.Field.GoalWidth * (1 if main.ball().pos.x < 0 else
                                               -1)
