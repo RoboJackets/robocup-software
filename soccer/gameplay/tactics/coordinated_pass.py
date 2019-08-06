a4de96af874b02d547ceb2f022f6cbe6f68a3cef
Joseph Neiger <fcdneiger@gmail.com>
2019-08-06 14:40:27 -0400

    Added comments on pass kick power

--- soccer/gameplay/tactics/coordinated_pass.py
+++ soccer/gameplay/tactics/coordinated_pass.py
@@ -145,6 +145,9 @@ class CoordinatedPass(composite_behavior.CompositeBehavior):
 
         kickpower = max(0.05, min(kickpower, 1.0))
 
+        # Very simple tuning right now
+        # It is setup to use kickerpower once the distance scale has been
+        # tuned correctly
         kicker.kick_power = 0.6 #kickpower
         kicker.enable_kick = False  # we'll re-enable kick once both bots are ready
 
