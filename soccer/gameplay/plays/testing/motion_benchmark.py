21b68d324301725b3d2900dbc7fa14b30e4e9bb2
Christopher Lindbeck <clindbeck3@gatech.edu>
2019-10-02 09:36:30 -0400

    forgot to commit this file

--- soccer/gameplay/plays/testing/motion_benchmark.py
+++ soccer/gameplay/plays/testing/motion_benchmark.py
@@ -10,8 +10,13 @@ import enum
 import tools
 import skills.latency_test
 import skills.benchmark.motion_benchmark
+import skills.benchmark.basic_motion_test.BasicMotionTest
+import robocup
 
 class RunMotionBenchmark(play.Play):
+
+    benchmark = skills.benchmark.motion_benchmark.MotionBenchmark()
+
     class State(enum.Enum):
         
         #Move to a central location 
@@ -34,7 +39,86 @@ class RunMotionBenchmark(play.Play):
                             behavior.Behavior.State.completed,
                             lambda: self.all_subbehaviors_completed(),
                             'all subbehaviors completed')
+
+        
+
+        superBasicTest = BasicMotionTest(numberOfRuns, self)
+        superBasicTest.points.append(robocup.Point(1.2,1.2))
+        superBasicTest.points.append(robocup.Point(1.2,2.2))
+        superBasicTest.points.append(robocup.Point(2.2,1.2))
+        superBasicTest.title = "Test Triangle"
+        self.benchmark.addTest(superBasicTest)
+
+        basicMid = BasicMotionTest(numberOfRuns, self)
+        basicMid.points.append(robocup.Point(1.2,1.2))
+        basicMid.points.append(robocup.Point(-1.2,1.2))
+        basicMid.points.append(robocup.Point(0,3.5))
+        basicMid.title = "Mid Size Motion Triangle"
+        self.benchmark.addTest(basicMid)
+
+        basicSmall = BasicMotionTest(numberOfRuns, self)
+        basicSmall.points.append(robocup.Point(-0.75, 1.2))
+        basicSmall.points.append(robocup.Point(0.75, 1.2))
+        basicSmall.points.append(robocup.Point(0,2.4))
+        basicSmall.title = "Small Motion Triangle"
+        self.benchmark.addTest(basicSmall)
+
+        basicLarge = BasicMotionTest(numberOfRuns, self)
+        basicLarge.points.append(robocup.Point(-1.7,1.5))
+        basicLarge.points.append(robocup.Point(0, 4.8))
+        basicLarge.points.append(robocup.Point(1.7, 1.5))
+        basicLarge.title = "Large Motion Triangle"
+        self.benchmark.addTest(basicLarge)
+
+        basicSmaller = BasicMotionTest(numberOfRuns, self)
+        basicSmaller.points.append(robocup.Point(0.25, 1.2))
+        basicSmaller.points.append(robocup.Point(-0.25, 1.2))
+        basicSmaller.points.append(robocup.Point(0, 1.7))
+        basicSmaller.title = "Smaller Motion Triangle"
+        self.benchmark.addTest(basicSmaller)
+        
+        basicTiny = BasicMotionTest(numberOfRuns, self)
+        basicTiny.points.append(robocup.Point(0.085, 1.2))
+        basicTiny.points.append(robocup.Point(-0.085, 1.2))
+        basicTiny.points.append(robocup.Point(0, 1.285))
+        basicTiny.title = "Tiny Motion Triangle"
+        self.benchmark.addTest(basicTiny)
+
+        basicMicro = BasicMotionTest(numberOfRuns, self)
+        basicMicro.points.append(robocup.Point(0.034, 1.2))
+        basicMicro.points.append(robocup.Point(-0.034, 1.2))
+        basicMicro.points.append(robocup.Point(0,1.242))
+        basicMicro.title = "Micro Motion Triangle"
+        self.benchmark.addTest(basicMicro)
+        
+        midFace = BasicMotionTest(numberOfRuns, self)
+        midFace.points.append(robocup.Point(1.2,1.2))
+        midFace.points.append(robocup.Point(-1.2,1.2))
+        midFace.points.append(robocup.Point(0,3.5))
+        midFace.facePoints.append(robocup.Point(0,2.8))
+        midFace.facePoints.append(robocup.Point(0,0))
+        midFace.facePoints.append(robocup.Point(2,0))
+        midFace.title = "Mid size triangle while facing points #1"
+        self.benchmark.addTest(midFace)
+        
+        midFace2 = self.BasicMotionTest(numberOfRuns, self)
+        midFace2.points.append(robocup.Point(1.2,1.2))
+        midFace2.points.append(robocup.Point(-1.2,1.2))
+        midFace2.points.append(robocup.Point(0,3.5))
+        midFace2.facePoints.append(robocup.Point(0,2.8))
+        midFace2.facePoints.append(robocup.Point(0,0))
+        midFace2.facePoints.append(robocup.Point(2,0))
+        midFace2.title = "Mid size triangle while facing points #2"
+        self.benchmark.addTest(midFace2)
+
+        pureRot = self.BasicMotionTest(numberOfRuns, self)
+        pureRot.facePoints.append(robocup.Point(0,2.8))
+        pureRot.facePoints.append(robocup.Point(0,0))
+        pureRot.facePoints.append(robocup.Point(2,0))
+        pureRot.title = "Pure Rotational Test"
+        self.benchmark.addTest(pureRot)
+
   
     def on_enter_run(self):
-        self.add_subbehavior(skills.benchmark.motion_benchmark.MotionBenchmark(), 'move2')
+        self.add_subbehavior(self.benchmark, 'Runs the benchmark')
 
