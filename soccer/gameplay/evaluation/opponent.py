184a188e37aa73a4eac347586d4c4892331a87c3
Jason Chan <jason27chan@gmail.com>
2019-10-20 14:39:21 -0400

    add a bunch of functions

--- soccer/gameplay/evaluation/opponent.py
+++ soccer/gameplay/evaluation/opponent.py
@@ -1,6 +1,7 @@
 import main
 import robocup
 import constants
+import math
 
 
 ## Estimates the number of robots on offense on the opposing team
@@ -65,3 +66,137 @@ def get_closest_opponent(pos, direction_weight=0, excluded_robots=[]):
                 closest_dist = dist
 
     return closest_bot
+
+## Gets list of threats
+#
+# @return sorted tuple of threat positions and score 
+def get_threat_list(unused_threat_handlers):
+    import evaluation.ball as ball_eval
+    kick_eval = robocup.KickEvaluator(main.system_state())
+
+    # List of (position, score, Robot/None)
+    threats = []
+    potential_threats = main.their_robots()
+
+    # find the primary threat
+    # if the ball is not moving OR it's moving towards our goal, it's the primary threat
+    # if it's moving, but not towards our goal, the primary threat is the robot on their team most likely to catch it
+    if (main.ball().vel.mag() > 0.4):
+        if ball_eval.is_moving_towards_our_goal():
+            # Add tuple of pos and score
+            threats.append((main.ball().pos, 1, None))
+        else:
+            # Get all potential receivers
+            potential_receivers = []
+            for opp in potential_threats:
+                if estimate_potential_recievers_score(opp) == 1:
+                    potential_receivers.append((opp.pos, 1, opp))
+
+            if len(potential_receivers) > 0:
+                # Add best receiver to threats
+                # TODO Calc shot chance
+                best_tuple = min(potential_receivers,
+                                    key=lambda rcrv_tuple: rcrv_tuple[1])
+                threats.append((best_tuple[0], .81, best_tuple[2]))
+            else:
+                # Just deal with ball if no recievers
+                threats.append((main.ball().pos, .9, None))
+    else:
+        # Assume opp is dribbling ball
+        if not constants.Field.OurGoalZoneShape.contains_point(
+                main.ball().pos):
+            # TODO: Calc shot chance
+            threats.append((main.ball().pos, 1, None))
+
+    # if there are threats, check pass and shot chances
+    # If the first item is not a ball, it is most likely a pass
+    if len(threats) > 0 and threats[0][0] != main.ball().pos:
+        for opp in potential_threats:
+
+            # Exclude robots that have been assigned already
+            excluded_bots = []
+            for r in map(lambda bhvr: bhvr.robot, unused_threat_handlers):
+                excluded_bots.append(r)
+
+            threats.append((opp.pos, estimate_risk_score(
+                opp, excluded_bots), opp))
+    else:
+        for opp in potential_threats:
+
+            # Exclude all robots
+            kick_eval.excluded_robots.clear()
+            kick_eval.add_excluded_robot(opp)
+            for r in main.our_robots():
+                kick_eval.add_excluded_robot(r)
+
+            point, shotChance = kick_eval.eval_pt_to_our_goal(opp.pos)
+
+            # Note: 0.5 is a bullshit value
+            threats.append((opp.pos, 0.5 * shotChance, opp))
+
+    # Prevent threats from being below our goal line (causes incorrect pos)
+    def _adjust_pt(threat):
+        pt = threat[0]
+        pt.y = max(pt.y, 0.1)
+        return (pt,) + threat[1:]
+
+    threats = list(map(_adjust_pt, threats))
+    threats.sort(key=lambda threat: threat[1], reverse=True)
+
+    return threats
+
+## Estimate potential reciever score (likelihood of opponent passing to this robot)
+#  
+#  @param bot Robot to estimate score at
+#  @return The potential receiver score at that point
+def estimate_potential_recievers_score(bot):
+    ball_travel_line = robocup.Line(main.ball().pos,
+                                    main.ball().pos + main.ball().vel)
+
+    dot_product = (bot.pos - main.ball().pos).dot(ball_travel_line.delta())
+    nearest_pt = ball_travel_line.nearest_point(bot.pos)
+    dx = (nearest_pt - main.ball().pos).mag()
+    dy = (bot.pos - nearest_pt).mag()
+    angle = abs(math.atan2(dy, dx))
+
+    # Only returns 1 if the opp is moving in the opposite direction as the ball
+    # and the angle between the ball ray starting at its current position and the opp position
+    # is less than pi/4
+    if (angle < math.pi / 4 and dot_product > 0):
+        return 1
+    else:
+        return 0
+
+## Estimate risk score based on old defense.py play
+#  
+#  @param bot Robot to estimate score at
+#  @param exluded_Bots Robots to exclude from the defense when calculating shot
+#  @return The risk score at that point (Shot chance * pass chance)
+def estimate_risk_score(bot, excluded_bots=[]):
+    import evaluation.passing as pass_eval
+    kick_eval = robocup.KickEvaluator(main.system_state())
+
+    excluded_bots.append(bot)
+
+    passChance = pass_eval.eval_pass(
+        main.ball().pos, bot.pos, excluded_robots=excluded_bots)
+
+    kick_eval.excluded_robots.clear()
+    for r in excluded_bots:
+        kick_eval.add_excluded_robot(r)
+
+    point, shotChance = kick_eval.eval_pt_to_our_goal(bot.pos)
+
+    return passChance * shotChance
+
+## Returns whether a position is marked by one of our robots
+#
+# @param pos Position to check
+# @return True or False if a robot is on the mark line
+def is_marked(pos):
+    line = robocup.Line(pos, robocup.Point(0, 0))
+
+    for bot in main.our_robots():
+        if line.dist_to(bot.pos) < constants.Robot.Radius:
+            return True
+    return False
