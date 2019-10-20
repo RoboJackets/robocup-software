9aaaa82be17292008e576ddf9ed9996690ea89f5
Jason Chan <jason27chan@gmail.com>
2019-10-20 14:39:31 -0400

    struggling

--- soccer/gameplay/plays/defense/defend_goal.py
+++ soccer/gameplay/plays/defense/defend_goal.py
@@ -5,7 +5,11 @@ import constants
 import enum
 
 import standard_play
+import tactics.positions.submissive_goalie as submissive_goalie
 import tactics.positions.submissive_defender as submissive_defender
+import evaluation.opponent as eval_opp
+import tactics.positions.wing_defender as wing_defender
+import tactics.defense
 
 ## Play that uses submissive defenders and wingers to defend
 #  an attack close to our goal
@@ -21,10 +25,33 @@ class DefendGoal(standard_play.StandardPlay):
             behavior.Behavior.State.running, lambda: True,
             'Immediately')
 
-        for i in range(num_defenders):
-            defender = submissive_defender.SubmissiveDefender()
-            self.add_subbehavior(defender, 'defender' + str(i), required=True)
+        goalie = submissive_goalie.SubmissiveGoalie()
+        goalie.shell_id = main.root_play().goalie_id
+        # self.add_subbehavior(goalie, "goalie", required=True)
 
-        # for i in range(num_wingers):
-        #     winger = winger.WingerDefense()
-        #     self.add_subbehavior('winger' + str(i), winger)
+        # for i in range(num_defenders):
+        #     defender = submissive_defender.SubmissiveDefender()
+        #     self.add_subbehavior(defender, 'defender' + str(i))
+
+        # self.threat_list = list(map(lambda threat: threat[0], eval_opp.get_threat_list(self.all_subbehaviors())))
+        # self.threat_list = eval_opp.get_threat_list(self.all_subbehaviors())
+
+        self.add_subbehavior(tactics.defense.Defense(), 'defense', required=False)
+
+        self.wingers = {}
+        for i in range(self.num_wingers):
+            self.wingers['winger' + str(i)] = wing_defender.WingDefender()
+        
+
+    def execute_running(self):
+        # print(self.threat_list[0])
+
+        # for point, threat, roboot in self.threat_list:
+            # print(point)
+        for bhvr in self.all_subbehaviors():
+            print(bhvr)
+        # for name, bhvr in self.wingers:
+        #     for bot in main.their_robots():
+        #         if not eval_opp.is_marked(bot.pos):
+        #             bhvr.mark_robot(bot)
+        #             self.add_subbehavior(bhvr, name)
