4221db6363a3d0c4150b7c1dfae35800859da836
Hussain <hgynai@gatech.edu>
2019-12-19 17:10:02 -0500

    Made pretty-lines

--- soccer/gameplay/plays/offense/clear.py
+++ soccer/gameplay/plays/offense/clear.py
@@ -16,94 +16,81 @@ import random
 #   Want to Kick ball up field and get other robots in position
 #
 
-class Clear(standard_play.StandardPlay):
 
+class Clear(standard_play.StandardPlay):
     class State(enum.Enum):
         get_ball = 1, 'Get the ball and movee other robots up'
         clear_ball = 2, 'Kick the ball upfield'
 
     def __init__(self):
-        super().__init__(continuous = False)
+        super().__init__(continuous=False)
 
         for s in Clear.State:
             self.add_state(s, behavior.Behavior.State.running)
 
         #Points to move up offense bots to
-        self.offense_point_1 = robocup.Point(0.40*constants.Field.Width,
-                                             0.95*constants.Field.Length)
+        self.offense_point_1 = robocup.Point(0.40 * constants.Field.Width,
+                                             0.95 * constants.Field.Length)
 
-        self.offense_point_2 = robocup.Point(-0.40*constants.Field.Width,
-                                             0.95*constants.Field.Length)
+        self.offense_point_2 = robocup.Point(-0.40 * constants.Field.Width,
+                                             0.95 * constants.Field.Length)
 
         #Points to move midfield bots to
-        self.midfield_point_1 = robocup.Point(0.20*constants.Field.Width,
-                                              0.70*constants.Field.Length)
+        self.midfield_point_1 = robocup.Point(0.20 * constants.Field.Width,
+                                              0.70 * constants.Field.Length)
 
-        self.midfield_point_2 = robocup.Point(-0.40*constants.Field.Width,
-                                             0.60*constants.Field.Length)
+        self.midfield_point_2 = robocup.Point(-0.40 * constants.Field.Width,
+                                              0.60 * constants.Field.Length)
 
-        self.points = [self.offense_point_1, self.offense_point_2, self.midfield_point_1, self.midfield_point_2]
+        self.points = [
+            self.offense_point_1, self.offense_point_2, self.midfield_point_1,
+            self.midfield_point_2
+        ]
 
         self.offense_points = [self.offense_point_1, self.offense_point_2]
 
         self.midfield_points = [self.midfield_point_1, self.midfield_point_2]
 
         self.add_transition(behavior.Behavior.State.start,
-                            Clear.State.get_ball,
-                            lambda: True,
-                            'Immidiatley')
+                            Clear.State.get_ball, lambda: True, 'Immidiatley')
 
-        self.add_transition(Clear.State.get_ball,
-                            Clear.State.clear_ball,
-                            lambda: self.subbehavior_with_name('Capture ball').is_done_running(),
-                            'After ball is captured')
+        self.add_transition(
+            Clear.State.get_ball,
+            Clear.State.clear_ball, lambda: self.subbehavior_with_name(
+                'Capture ball').is_done_running(), 'After ball is captured')
 
     def on_enter_get_ball(self):
         self.remove_all_subbehaviors()
         #Capture the ball and move robot up
-        self.add_subbehavior(skills.capture.Capture(),
-                            'Capture ball',
-                            required = True)
-        
+        self.add_subbehavior(
+            skills.capture.Capture(), 'Capture ball', required=True)
+
         count = 0
         for i in self.offense_points:
-            count+=1
-            self.add_subbehavior(skills.move.Move(i),
-                                'move to point ' + str(count),
-                                required = False)
+            count += 1
+            self.add_subbehavior(
+                skills.move.Move(i),
+                'move to point ' + str(count),
+                required=False)
 
     def on_enter_clear_ball(self):
         #Chip ball to either offense points, while still moving robots up
         self.remove_all_subbehaviors()
 
-        num = random.randint(0,1)
-        self.add_subbehavior(tactics.coordinated_pass.CoordinatedPass(self.points[num],
-                                                    receiver_required = False, 
-                                                    use_chipper=True),
-                                                            'clear',
-                                                             required = True)
+        num = random.randint(0, 1)
+        self.add_subbehavior(
+            tactics.coordinated_pass.CoordinatedPass(
+                self.points[num], receiver_required=False, use_chipper=True),
+            'clear',
+            required=True)
 
-        self.add_subbehavior(skills.move.Move(self.offense_points[1-num]),
-                                              'keep moving',
-                                              required = False)
+        self.add_subbehavior(
+            skills.move.Move(self.offense_points[1 - num]),
+            'keep moving',
+            required=False)
         # count = 0
         # for k in self.offense_points:
         #     count+=1
         #     self.add_subbehavior(skills.move.Move(k),
         #                         'move to point ' + str(count) + ' 2',
         #                         required = False, priority = 1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
