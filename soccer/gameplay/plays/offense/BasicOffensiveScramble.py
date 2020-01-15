017e55817aca5ac0ed2d4cafc060d1cc43f6647f
Hussain <hgynai@gatech.edu>
2020-01-14 20:30:55 -0500

    Switched to just using the running state, added comments, made it so the across point is now a mirror of the ball's point across the y axis

--- soccer/gameplay/plays/offense/BasicOffensiveScramble.py
+++ soccer/gameplay/plays/offense/BasicOffensiveScramble.py
@@ -11,49 +11,62 @@ import tactics.coordinated_pass
 import skills.move
 import skills.capture
 
+# A basic play for the offensive scramble situation
+# One robot will attempt to capture the ball
+# Another robot will be sent behind the capturing one to a "dropback point"
+# Another robot will be sent across the field from the capturing one
+#
+
+
 class BasicOffensiveScramble(standard_play.StandardPlay):
 
     _situationList = [
         situational_play_selection.SituationalPlaySelector.Situation.OFFENSIVE_SCRAMBLE
     ] # yapf: disable
 
-    class State(enum.Enum):
-        get_ball = 1, 'Start the play'
-
     def __init__(self):
-        super().__init__(continuous = False)
-
-        for s in BasicOffensiveScramble.State:
-            self.add_state(s, behavior.Behavior.State.running)
+        super().__init__(continuous=False)
 
         self.add_transition(behavior.Behavior.State.start,
-            BasicOffensiveScramble.State.get_ball, lambda: True, 'Immidiatley')
+                            BasicOffensiveScramble.State.running, lambda: True,
+                            'Immediately')
 
-        self.add_transition(BasicOffensiveScramble.State.get_ball,
-            behavior.Behavior.State.completed,
-            lambda: self.subbehavior_with_name('Capture ball').state == behavior.
-            Behavior.State.completed or self.subbehavior_with_name('Capture ball').state == behavior.
-            Behavior.State.failed, 'Captured')
+        self.add_transition(
+            BasicOffensiveScramble.State.running,
+            behavior.Behavior.State.completed, lambda: self.
+            subbehavior_with_name('Capture ball').is_done_running(),
+            'Captured')
 
     @classmethod
     def score(cls):
         return 10
 
+    #Gets the point that the robot that will behind the capturing one will be
     def get_dropback_point(self):
         ball_pos = main.ball().pos
-        dropback_point = robocup.Point(ball_pos.x, ball_pos.y - constants.Field.Length/6)
+        dropback_point = robocup.Point(ball_pos.x,
+                                       ball_pos.y - constants.Field.Length / 6)
         return dropback_point
 
+    #Get point across from capturing robot another robot should be
     def get_across_point(self):
         ball_pos = main.ball().pos
         sign = 1
-        if (ball_pos.x >= 0):
-            sign = -1
-        across_point = robocup.Point(ball_pos.x + (sign * constants.Field.Width/5), ball_pos.y)
+        mirror_x = 0 - main.ball().pos.x
+        if (ball_pos.x <= constants.Field.Width / 4 and
+                ball_pos.x >= -constants.Field.Width /
+                4):  #Check if the ball is near the center line of the field
+            if (ball_pos.x >= 0):
+                sign = -1
+            #If the ball is near the center then the across point should be a set distance across the ball position
+            across_point = robocup.Point(
+                ball_pos.x + (sign * constants.Field.Width / 2), ball_pos.y)
+        else:
+            #If the ball is not near the center, the across point is the mirror of the ball position across the y axis
+            across_point = robocup.Point(mirror_x, ball_pos.y)
         return across_point
 
-    def on_enter_get_ball(self):
-        self.remove_all_subbehaviors()
+    def on_enter_running(self):
         self.add_subbehavior(
             skills.capture.Capture(), 'Capture ball', required=True)
 
@@ -67,7 +80,3 @@ class BasicOffensiveScramble(standard_play.StandardPlay):
             skills.move.Move(across_point),
             'move to across point',
             required=False)
-
-    def on_exit_get_ball(self):
-        self.remove_all_subbehaviors()
-
