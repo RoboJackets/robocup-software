2eed37d0cb85d83bc96de144b600bcab9038e3f9
tcontis <thomas@contis.org>
2020-01-17 16:32:05 -0500

    Implemented radio functionality into RadioNode

--- soccer/radio/RadioNode.cpp
+++ soccer/radio/RadioNode.cpp
@@ -1,5 +1,48 @@
+#include "Constants.hpp"
+#include "NetworkRadio.hpp"
+#include "Radio.hpp"
 #include "RadioNode.hpp"
+#include "SimRadio.hpp"
 
-RadioNode::RadioNode(Context *context, bool sim): _context(context) {
+RadioNode::RadioNode(Context *context, bool simulation, bool _blueTeam): _context(context) {
+    _simulation = simulation;
+    _radio = _simulation
+             ? static_cast<Radio*>(new SimRadio(&_context, _blueTeam))
+             : static_cast<Radio*>(new NetworkRadio(NetworkRadioServerPort));
+}
+
+bool RadioNode::isOpen() {
+    return _radio.isOpen();
+}
+
+void RadioNode::switchTeam(bool blueTeam) {
+    _radio.switchTeam(blueTeam);
+}
+
+void RadioNode::run(){
+
+    // Read radio reverse packets
+    _radio->receive();
+
+    while (_radio->hasReversePackets()) {
+        Packet::RadioRx rx = _radio->popReversePacket();
+        _context.state.logFrame->add_radio_rx()->CopyFrom(rx);
+
+        // Store this packet in the appropriate robot
+        unsigned int board = rx.robot_id();
+        if (board < Num_Shells) {
+            // We have to copy because the RX packet will survive past this
+            // frame but LogFrame will not (the RadioRx in LogFrame will be
+            // reused).
+            _context.state.self[board]->setRadioRx(rx);
+            _context.state.self[board]->radioRxUpdated();
+        }
+    }
+
+    if (_radio) {
+        construct_tx_proto((*_context.state.logFrame->mutable_radio_tx()),
+                           _context.robot_intents, _context.motion_setpoints);
+        _radio->send(*_context.state.logFrame->mutable_radio_tx());
+    }
 
 }
--- soccer/radio/RadioNode.hpp
+++ soccer/radio/RadioNode.hpp
@@ -1,14 +1,20 @@
 #pragma once
 
 #include "Node.hpp"
+#include "Radio.hpp"
 
 class RadioNode : public Node {
 public:
     RadioNode(Context* context, bool sim);
 
+    bool isOpen();
     void run() override;
+    void switchTeam(bool blueTeam);
 
 private:
+
+    bool _blueTeam;
     Context* _context;
-    bool _sim;
+    Radio _radio;
+    bool _simulation;
 };
