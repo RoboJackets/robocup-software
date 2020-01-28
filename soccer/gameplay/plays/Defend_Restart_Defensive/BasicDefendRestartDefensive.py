b87ebc9dbbc33928345a83ac8ea0ce71806ac024
Jason Chan <jason27chan@gmail.com>
2020-01-28 18:30:35 -0500

    simplify play down to wall and standard defense

--- soccer/gameplay/plays/Defend_Restart_Defensive/BasicDefendRestartDefensive.py
+++ soccer/gameplay/plays/Defend_Restart_Defensive/BasicDefendRestartDefensive.py
@@ -10,16 +10,13 @@ import tactics.positions.submissive_defender as submissive_defender
 import evaluation.opponent as eval_opp
 import tactics.positions.wing_defender as wing_defender
 import skills.mark as mark
-import tactics.defense
+import tactics.wall as wall
 import situational_play_selection
 
 
-## Play that uses submissive defenders to defend
-#  an attack close to our goal.
+## Restart that uses standard defense and uses the remaining
+#  robots to form a wall
 #
-#  By default, we will use standard defense (two submissive
-#  defenders, one goalie) and additional marking robots. 
-#  
 class BasicDefendRestartDefensive(standard_play.StandardPlay):
 
     _situationList = [
@@ -36,30 +33,6 @@ class BasicDefendRestartDefensive(standard_play.StandardPlay):
                             behavior.Behavior.State.running, lambda: True,
                             'Immediately')
 
-        for i in range(num_defenders):
-            self.add_subbehavior(mark.Mark(), 'mark' + str(i), required=False)
-
-        # Keep track of which robots are currently being defended
-        self.defended = {}
-        for i in range(len(main.their_robots())):
-            self.defended[i] = False
-
-    def execute_running(self):
-        for i in range(len(main.their_robots())):
-            if not eval_opp.is_marked(main.their_robots()[i].pos):
-                self.defended[i] = False
+        self.add_subbehavior(wall.Wall(), 'wall', required=False)
 
 
-        # mark highest threat robot
-        for i in range(self.num_defenders):
-            mark_bhvr = self.subbehavior_with_name('mark' + str(i))
-
-            threat_found = False            
-            for threat_pt, _, _ in eval_opp.get_threat_list([mark_bhvr]):
-                print(threat_pt)
-                closest_opp = eval_opp.get_closest_opponent(threat_pt)
-                if not threat_found and (closest_opp.pos - main.ball().pos).mag() > constants.Field.CenterRadius + constants.Robot.Radius * 2:
-                    print((closest_opp.pos - main.ball().pos).mag())
-                    # print(constants.Field.CenterRadius)
-                    mark_bhvr.mark_robot = closest_opp
-                    threat_found = True
