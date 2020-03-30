a9364c5f38b31e3a522eb0b79f7117c83c5b1c13
Christopher Lindbeck <clindbeck9@gmail.com>
2020-03-29 21:22:53 -0400

    made some modifications

--- soccer/gameplay/forces/force_utils.py
+++ soccer/gameplay/forces/force_utils.py
@@ -7,15 +7,60 @@ import math
 #I really want like n-log(2,x) where n is the magnatude at 0
 
 
+#Out of all the bullcrap I've written I think these are probably what I would want
+#I can't think of a reason that you would need more granular control?
+
+#I should possibly change "push" and "pull" arguments to sample, to represent that is the point you are sampling the field at
+
+#It's also possible that I should just do a "push" and then have you negate that to get a pull, but they need to decay and be offset differently so IDK.
+#I think this can be made to work to cover all types of fields that you would want.
+
+
+#Be really careful when changing clipLow, keeping in mind that this will allow your force to change directions i.e. from a push to a pull, in the middle of the field, which can get confusing
+def log_push(anchor, sample, base, decay, clipLow=0.0, clipHigh=float('inf')):
+    return None
+
+def log_pull(anchor, sample, base, decay, clipLow=0.0, clipHigh=float('inf')):
+    return None
 
 
 
-def scale_log_inc(input_vec, base=2, signed=False):
+def poly_push(anchor, sample, x0=0, x1=0, x2=3, x3=4):
+    return None
+
+def poly_pull(anchor, sample):
+
+#This might have 
+def distance_maigntain()
+
+
+#Flips a vector 
+def invert(input_vec):
+    return input_vec
+
+def rotate(input_vec, degrees):
+    return input_vec
+
+
+def norm_split(input_vec):
+    return input_vec.norm(), input_vec.mag()
+
+
+def log_compress(input_vec, base=2):
+    mag = input_vec.mag()
+    norm = 
     return input_vec
     #I should probably not allow negatives here
 
+
+
+def log_decay(input_vec, base=2):
+    return None
+
+
+
 #Roots are not as intense as log functinos in many cases
-def scale_root_inc(input_vec, root=2, signed=False):
+def scale_root_compress(input_vec, root=2, signed=False):
     return input_vec
 
 def scale_clip_inc(input_vec, mag_max=float("inf"), mag_min=0, signed=False):
@@ -30,14 +75,14 @@ def vec_inv_inc(input_vec, add_one=True):
     #This is basically going to be 1/x or 1/(x+1)
     return input_vec
 
-def vec_inv_tan_inc(input_vec):
+def vec_arctan_inc(input_vec):
     #Inverse tangent is anot
     return input_vec
 
 
 
 ##
-# The decreasing functions are for making a force smaller 
+# The decreasing functions are for making a force smaller as it's magnatude increases
 #
 #
 ##
