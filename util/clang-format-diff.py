3e46a8d9f2b7639add08cec237fe467dc2860702
Kyle Stachowicz <kylestach99@gmail.com>
2020-05-11 10:41:26 -0700

    Format again???? Why...

--- util/clang-format-diff.py
+++ util/clang-format-diff.py
@@ -32,30 +32,45 @@ from io import StringIO
 
 
 def main():
-    parser = argparse.ArgumentParser(description=
-                                     'Reformat changed lines in diff. Without -i '
-                                     'option just output the diff that would be '
-                                     'introduced.')
-    parser.add_argument('-i', action='store_true', default=False,
-                        help='apply edits to files instead of displaying a diff')
-    parser.add_argument('-p', metavar='NUM', default=0,
+    parser = argparse.ArgumentParser(
+        description='Reformat changed lines in diff. Without -i '
+        'option just output the diff that would be '
+        'introduced.')
+    parser.add_argument(
+        '-i',
+        action='store_true',
+        default=False,
+        help='apply edits to files instead of displaying a diff')
+    parser.add_argument('-p',
+                        metavar='NUM',
+                        default=0,
                         help='strip the smallest prefix containing P slashes')
-    parser.add_argument('-regex', metavar='PATTERN', default=None,
+    parser.add_argument('-regex',
+                        metavar='PATTERN',
+                        default=None,
                         help='custom pattern selecting file paths to reformat '
                         '(case sensitive, overrides -iregex)')
-    parser.add_argument('-iregex', metavar='PATTERN', default=
-                        r'.*\.(cpp|cc|c\+\+|cxx|c|cl|h|hpp|m|mm|inc|js|ts|proto'
-                        r'|protodevel|java)',
-                        help='custom pattern selecting file paths to reformat '
-                        '(case insensitive, overridden by -regex)')
-    parser.add_argument('-sort-includes', action='store_true', default=False,
+    parser.add_argument(
+        '-iregex',
+        metavar='PATTERN',
+        default=r'.*\.(cpp|cc|c\+\+|cxx|c|cl|h|hpp|m|mm|inc|js|ts|proto'
+        r'|protodevel|java)',
+        help='custom pattern selecting file paths to reformat '
+        '(case insensitive, overridden by -regex)')
+    parser.add_argument('-sort-includes',
+                        action='store_true',
+                        default=False,
                         help='let clang-format sort include blocks')
-    parser.add_argument('-v', '--verbose', action='store_true',
+    parser.add_argument('-v',
+                        '--verbose',
+                        action='store_true',
                         help='be more verbose, ineffective without -i')
-    parser.add_argument('-style',
-                        help='formatting style to apply (LLVM, Google, Chromium, '
-                        'Mozilla, WebKit)')
-    parser.add_argument('-binary', default='clang-format-6.0',
+    parser.add_argument(
+        '-style',
+        help='formatting style to apply (LLVM, Google, Chromium, '
+        'Mozilla, WebKit)')
+    parser.add_argument('-binary',
+                        default='clang-format-6.0',
                         help='location of binary to use for clang-format')
     args = parser.parse_args()
 
@@ -84,7 +99,7 @@ def main():
                 line_count = int(match.group(3))
             if line_count == 0:
                 continue
-            end_line = start_line + line_count - 1;
+            end_line = start_line + line_count - 1
             lines_by_file.setdefault(filename, []).extend(
                 ['-lines', str(start_line) + ':' + str(end_line)])
 
@@ -100,22 +115,25 @@ def main():
         command.extend(lines)
         if args.style:
             command.extend(['-style', args.style])
-        p = subprocess.Popen(command, stdout=subprocess.PIPE,
-                             stderr=None, stdin=subprocess.PIPE)
+        p = subprocess.Popen(command,
+                             stdout=subprocess.PIPE,
+                             stderr=None,
+                             stdin=subprocess.PIPE)
         stdout, stderr = p.communicate()
         if p.returncode != 0:
-            sys.exit(p.returncode);
+            sys.exit(p.returncode)
 
         if not args.i:
             with open(filename) as f:
                 code = f.readlines()
             formatted_code = StringIO(stdout).readlines()
-            diff = difflib.unified_diff(code, formatted_code,
-                                        filename, filename,
-                                        '(before formatting)', '(after formatting)')
+            diff = difflib.unified_diff(code, formatted_code, filename,
+                                        filename, '(before formatting)',
+                                        '(after formatting)')
             diff_string = ''.join(diff)
             if len(diff_string) > 0:
                 sys.stdout.write(diff_string)
+
 
 if __name__ == '__main__':
     main()
