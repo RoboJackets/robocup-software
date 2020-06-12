923d3b4bb1baf203978665e653bc8819d1be7be6
Oswin So <oswinso@gmail.com>
2020-06-12 01:30:32 -0400

    Increases FLOAT_EPSILON to 1e-4 to fix bug with floating point in release.

--- common/Geometry2d/Util.hpp
+++ common/Geometry2d/Util.hpp
@@ -33,7 +33,9 @@ static inline T sign(T f) {
     return 0;
 }
 
-static const float FLOAT_EPSILON = 0.00001;
+// TODO(1485): Make this smaller once we figure out why test are failing in O3.
+//static const float FLOAT_EPSILON = 0.00001;
+static const float FLOAT_EPSILON = 1e-4;
 static bool nearlyEqual(float a, float b) {
     return std::fabs(a - b) < FLOAT_EPSILON;
 }
