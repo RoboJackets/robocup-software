865e5f34ae6938ebb504f4ac0521695a417e2d79
Oswin So <oswinso@gmail.com>
2020-06-14 16:48:07 -0400

    Removed debug prints

--- install/setup.bash
+++ install/setup.bash
@@ -6,25 +6,20 @@
 
 _INSTALL_PATH=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" && pwd)
 
-echo "INSTALL_PATH: $_INSTALL_PATH"
-
 _path_add() {
     if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
-        echo "    Exporting $1 to PATH"
         export PATH="$1${PATH:+":$PATH"}"
     fi
 }
 
 _ld_library_path_add() {
     if [ -d "$1" ] && [[ ":$LD_LIBRARY_PATH:" != *":$1:"* ]]; then
-        echo "    Exporting $1 to LD_LIBRARY_PATH"
         export LD_LIBRARY_PATH="$1${LD_LIBRARY_PATH:+":$LD_LIBRARY_PATH"}"
     fi
 }
 
 _ament_prefix_path_add() {
     if [ -d "$1" ] && [[ ":$AMENT_PREFIX_PATH:" != *":$1:"* ]]; then
-        echo "    Exporting $1 to AMENT_PREFIX_PATH"
         export AMENT_PREFIX_PATH="$1${AMENT_PREFIX_PATH:+":$AMENT_PREFIX_PATH"}"
     fi
 }
