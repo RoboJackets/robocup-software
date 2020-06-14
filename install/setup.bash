d7b575cf4fda4a3b65349cac1bf166a4070fe6cf
Oswin So <oswinso@gmail.com>
2020-06-14 11:21:03 -0400

    wtf why is this not working

--- install/setup.bash
+++ install/setup.bash
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script emulates what the colcon generated setup.bash does
 # kind of in that it sets the PATH, LD_LIBRARY_PATH and AMENT_PREFIX_PATH
@@ -6,20 +6,25 @@
 
 _INSTALL_PATH="$(readlink -f $(dirname -- "$(readlink -f -- "$0")")/install)"
 
+echo "INSTALL_PATH: $_INSTALL_PATH"
+
 _path_add() {
     if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+        echo "    Exporting $1 to PATH"
         export PATH="$1${PATH:+":$PATH"}"
     fi
 }
 
 _ld_library_path_add() {
     if [ -d "$1" ] && [[ ":$LD_LIBRARY_PATH:" != *":$1:"* ]]; then
+        echo "    Exporting $1 to LD_LIBRARY_PATH"
         export LD_LIBRARY_PATH="$1${LD_LIBRARY_PATH:+":$LD_LIBRARY_PATH"}"
     fi
 }
 
 _ament_prefix_path_add() {
     if [ -d "$1" ] && [[ ":$AMENT_PREFIX_PATH:" != *":$1:"* ]]; then
+        echo "    Exporting $1 to AMENT_PREFIX_PATH"
         export AMENT_PREFIX_PATH="$1${AMENT_PREFIX_PATH:+":$AMENT_PREFIX_PATH"}"
     fi
 }
