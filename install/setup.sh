617ddcf3ad780a5da9c58df79c97b7cf92a1e4e0
Oswin So <oswinso@gmail.com>
2020-06-14 03:49:30 -0400

    Fix export and path

--- install/setup.sh
+++ install/setup.sh
@@ -4,25 +4,23 @@
 # kind of in that it sets the PATH, LD_LIBRARY_PATH and AMENT_PREFIX_PATH
 # variables.
 
-_INSTALL_PATH_DIRTY="$(pwd)/$(dirname "$0")"
-_INSTALL_PATH="$(readlink -f "${_INSTALL_PATH_DIRTY}")"
-unset _INSTALL_PATH_DIRTY
+_INSTALL_PATH="$(readlink -f $(dirname -- "$(readlink -f -- "$0")")/install)"
 
 _path_add() {
     if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
-        PATH="$1${PATH:+":$PATH"}"
+        export PATH="$1${PATH:+":$PATH"}"
     fi
 }
 
 _ld_library_path_add() {
     if [ -d "$1" ] && [[ ":$LD_LIBRARY_PATH:" != *":$1:"* ]]; then
-        LD_LIBRARY_PATH="$1${LD_LIBRARY_PATH:+":$LD_LIBRARY_PATH"}"
+        export LD_LIBRARY_PATH="$1${LD_LIBRARY_PATH:+":$LD_LIBRARY_PATH"}"
     fi
 }
 
 _ament_prefix_path_add() {
     if [ -d "$1" ] && [[ ":$AMENT_PREFIX_PATH:" != *":$1:"* ]]; then
-        AMENT_PREFIX_PATH="$1${AMENT_PREFIX_PATH:+":$AMENT_PREFIX_PATH"}"
+        export AMENT_PREFIX_PATH="$1${AMENT_PREFIX_PATH:+":$AMENT_PREFIX_PATH"}"
     fi
 }
 
