9480192d47da3ad0f59d2d686c64927bec860cb7
Oswin So <oswinso@gmail.com>
2020-07-12 18:57:37 -0400

    Connect MotionControlNode directly VisionFilter.

--- soccer/src/soccer/motion/MotionControlNode.hpp
+++ soccer/src/soccer/motion/MotionControlNode.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <rj_constants/constants.hpp>
+#include <ros2_temp/async_message_queue.h>
+#include <rj_msgs/msg/world_state.hpp>
 #include <vector>
 
 #include "MotionControl.hpp"
@@ -23,4 +25,7 @@ private:
                    std::array<MotionSetpoint, Num_Shells>* setpoints);
     Context* _context;
     std::vector<MotionControl> _controllers{};
+    using WorldStateMsg = rj_msgs::msg::WorldState;
+    using AsyncWorldStateMsgQueue = ros2_temp::AsyncMessageQueue<WorldStateMsg, ros2_temp::MessagePolicy::kLatest>;
+    AsyncWorldStateMsgQueue::UniquePtr world_state_queue_;
 };
