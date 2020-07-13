fe13331c4728dcf59a1ef712eae4329ade18a181
Oswin So <oswinso@gmail.com>
2020-07-12 21:18:35 -0400

    Added missing conversions for WorldState.

--- soccer/src/soccer/WorldState.hpp
+++ soccer/src/soccer/WorldState.hpp
@@ -3,6 +3,9 @@
 #include <Geometry2d/Pose.hpp>
 #include <rj_common/time.hpp>
 #include <rj_constants/constants.hpp>
+#include <rj_msgs/msg/detail/ball_state__builder.hpp>
+#include <rj_msgs/msg/detail/robot_state__builder.hpp>
+#include <rj_msgs/msg/detail/world_state__builder.hpp>
 
 #include "planning/Instant.hpp"
 #include "planning/Trajectory.hpp"
@@ -17,16 +20,52 @@ constexpr double kBallDecayConstant = 0.180;
  * what time it was last seen.
  */
 struct RobotState {
+    using Msg = rj_msgs::msg::RobotState;
+
     Geometry2d::Pose pose;
     Geometry2d::Twist velocity;
     RJ::Time timestamp;
     bool visible = false;
+
+    /**
+     * @brief Default constructor: an invalid robot with visible=false.
+     */
+    RobotState() = default;
+
+    RobotState(const Geometry2d::Pose& pose, const Geometry2d::Twist& velocity,
+               const RJ::Time& timestamp, bool visible)
+        : pose{pose},
+          velocity{velocity},
+          timestamp{timestamp},
+          visible{visible} {}
+
+    /**
+     * @brief Implicit conversion from RobotState::Msg.
+     * @param msg
+     */
+    RobotState(const Msg& msg)
+        : RobotState{msg.pose, msg.velocity, RJ::FromROSTime(msg.stamp),
+                     msg.visible} {}
+
+    /**
+     * @brief Implicit conversion to RobotState::Msg.
+     * @return
+     */
+    [[nodiscard]] operator Msg() const {
+        return rj_msgs::build<Msg>()
+            .stamp(RJ::ToROSTime(timestamp))
+            .pose(pose)
+            .velocity(velocity)
+            .visible(visible);
+    }
 };
 
 /**
  * @brief Our belief about the ball's current position and velocity.
  */
 struct BallState {
+    using Msg = rj_msgs::msg::BallState;
+
     Geometry2d::Point position;
     Geometry2d::Point velocity;
     RJ::Time timestamp;
@@ -45,6 +84,13 @@ struct BallState {
         : position(position), velocity(velocity), timestamp(timestamp) {
         visible = true;
     }
+
+    /**
+     * @brief Implicit conversion from BallState::Msg.
+     * @param msg
+     */
+    BallState(const Msg& msg)
+        : BallState{msg.position, msg.velocity, RJ::FromROSTime(msg.stamp)} {}
 
     /**
      * @brief Predict the ball's state at a particular instance in time.
@@ -121,10 +167,20 @@ struct BallState {
 };
 
 struct WorldState {
+    using Msg = rj_msgs::msg::WorldState;
+
     WorldState() {
         their_robots.resize(Num_Shells);
         our_robots.resize(Num_Shells);
     }
+
+    /**
+     * @brief Implicit conversion from WorldState::Msg.
+     */
+    WorldState(const Msg& msg)
+        : their_robots(msg.their_robots.begin(), msg.their_robots.end()),
+          our_robots(msg.our_robots.begin(), msg.our_robots.end()),
+          ball{msg.ball} {}
 
     RobotState& get_robot(bool ours, int shell) {
         if (ours) {
