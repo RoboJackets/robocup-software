db163e1bfe75c2ee8781f7fd034fd7fa22ce3e4f
Christopher Lindbeck <clindbeck9@gmail.com>
2020-11-15 19:46:12 -0500

    added a blank test file

---
+++ rj_gameplay/tests/stp/test_rc.py
@@ -0,0 +1,14 @@
+from stp.rc import Ball, Robot, WorldState, Field
+
+def test_Robot() -> None:
+    pass
+
+def test_Ball() -> None:
+    pass
+
+def test_WorldState() -> None:
+    pass
+
+def test_Field() -> None:
+    pass
+
