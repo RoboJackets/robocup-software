6d9b2d82d6fc17fedf01df71659acb9155f05a84
Christopher Lindbeck <clindbeck9@gmail.com>
2021-01-17 11:56:57 -0500

    fixing syntax errors

--- rj_gameplay/stp/rc.py
+++ rj_gameplay/stp/rc.py
@@ -61,7 +61,7 @@ class Robot:
     @classmethod
     def generate_basic_test_robot(cls, robot_id: RobotId, is_ours: bool = True,
             pose: np.ndarray = np.array([0.0, 0.0, 0.0]), 
-            twist: np.ndarray = np.array([0.0, 0.0, 0.0])) -> Robot:
+            twist: np.ndarray = np.array([0.0, 0.0, 0.0])):
         """
         Returns a robot with default options for use in testing
         """
@@ -198,7 +198,7 @@ class Ball:
         self.__visible = visible
 
     @classmethod
-    def generate_test_ball(cls, pos: np.ndarray = np.array([0.0,0.0]), vel: np.ndarray([0.0,0.0]), visible: bool = True) -> Ball:
+    def generate_test_ball(cls, pos: np.ndarray = np.array([0.0,0.0]), vel: np.ndarray = np.array([0.0,0.0]), visible: bool = True):
         ball = cls(pos, vel, visible)
         return ball
 
@@ -307,27 +307,27 @@ class Field:
         self.__floor_width_m = floor_width_m
 
     @classmethod
-    def generate_divA_field(cls) -> Field:
+    def generate_divA_field(cls):
         """
         Generate a division A field
 
         Penalty distances and "goal_flat" need to be fixed
         """
-        field = cls(length_m = 12.0, width_m = 9.0, border_m = 0.3, line_width_m = 0.01, goal_width_m = 1.8, goal_depth_m = 0.18, goal_height_m = 0.16, penalty_short_dist_m = float('nan'), penalty_long_dist_m = float('nan'), center_radius_m = 0.5, center_diameter_m = 1.0, goal_flat_m = float('nan'), floor_length_m: 13.4, floor_width_m = 10.04):
+        field = cls(length_m = 12.0, width_m = 9.0, border_m = 0.3, line_width_m = 0.01, goal_width_m = 1.8, goal_depth_m = 0.18, goal_height_m = 0.16, penalty_short_dist_m = float('nan'), penalty_long_dist_m = float('nan'), center_radius_m = 0.5, center_diameter_m = 1.0, goal_flat_m = float('nan'), floor_length_m = 13.4, floor_width_m = 10.04)
         return field
 
     @classmethod
-    def generate_divB_field(cls) -> Field:
+    def generate_divB_field(cls):
         """
         Generate a division B field
 
         Penalty distances and "goal_flat" need to be fixed
         """
-        field = cls(length_m = 9.0, width_m = 6.0, border_m = 0.3, line_width_m = 0.01, goal_width_m = 1.0, goal_depth_m = 0.18, goal_height_m = 0.16, penalty_short_dist_m = float('nan'), penalty_long_dist_m = float('nan'), center_radius_m = 0.5, center_diameter_m = 1.0, goal_flat_m = float('nan'), floor_length_m: 10.04, floor_width_m = 7.4):
+        field = cls(length_m = 9.0, width_m = 6.0, border_m = 0.3, line_width_m = 0.01, goal_width_m = 1.0, goal_depth_m = 0.18, goal_height_m = 0.16, penalty_short_dist_m = float('nan'), penalty_long_dist_m = float('nan'), center_radius_m = 0.5, center_diameter_m = 1.0, goal_flat_m = float('nan'), floor_length_m =  10.04, floor_width_m = 7.4)
         return field
 
     @classmethod
-    def generate_our_field(cls) -> Field:
+    def generate_our_field(cls):
         """
         Generates the practice field that we have
         """
@@ -484,7 +484,7 @@ class GameInfo:
         self.__our_restart = our_restart
 
     @classmethod
-    def generate_test_playing_gameinfo(cls) -> GameInfo: 
+    def generate_test_playing_gameinfo(cls): 
         info = cls(GamePeriod.FIRST_HALF, GameState.PLAYING, GameRestart.NONE, False)
         return info
 
@@ -573,7 +573,7 @@ class WorldState:
 
     @classmethod
     def generate_basic_test_worldstate(cls, our_robots = None, their_robots = None, ball = None,
-            game_info = GameInfo.generate_test_playing_gameinfo(), field = Field.generate_divB_field()) -> WorldState:
+            game_info = GameInfo.generate_test_playing_gameinfo(), field = Field.generate_divB_field()):
         """
         generates a test worldstate with 12 robots and a ball in the play state
         """
