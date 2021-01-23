f184aa0bc9591ee6d9bc753b292a1b2d4afc769b
Tohwx <weixiongtoh@gmail.com>
2021-01-23 13:05:39 -0500

    Revert "update skeleton"
    This reverts commit 7c134ace79e8b0ae12bc7707084e195cd85d5e2e.

--- rj_param_utils/include/rj_param_utils/global_param.hpp
+++ rj_param_utils/include/rj_param_utils/global_param.hpp
@@ -6,26 +6,56 @@
 #include <rj_param_utils/param.hpp>
 #include <rj_param_utils/ros2_param_provider.hpp>
 
+/**
+ * @brief Create node to store global parameters.
+ */
+auto global_param_node = rclcpp::Node::make_shared("global_parameter_node");
+
+// TODO: Get values of constants from physics and update
+// global_param_node->declare_parameter("");
+
+
 
 namespace params {
 /**
- * Implementation of a global parameter provider used by every node.
+ * Implementation of a global parameter server.
  */
 
 class ROS2GlobalParamProvider : public ::params::ROS2ParamProvider {
 public:
-    explicit ROS2GlobalParamProvider(rclcpp::Node* node, const rclcpp::Node* global_node); 
+    explicit ROS2GlobalParamProvider(rclcpp::Node* node, const std::string& module) : node_(node) {}
+
+    /**
+     * @brief Updates the global parameter with the passed in full_name and
+     * matching type with new_value.
+     * returing true if the parameter was found.
+     * @tparam ParamType Type of the parameter.
+     * @param full_name Name of the parameter to update.
+     * @return Whether the parameter was updated successfully.
+     */
+    template <typename ParamType>
+    bool UpdateGlobalParameter(const std::string& full_name, const ParamType& new_value);  
+
+    /**
+     * @brief Sets a new global parameter with the passed in full_name and 
+     * with new_value.
+     * @tparam ParamType Type of the paramater.
+     * @param full_name The full_name of the parameter to set.
+     * @param new_value The new value of the parameter.
+     */
+    template <typename ParamType>
+    void SetGlobalParameter(const std::string& full_name, const ParamType& new_value);
+
+    /**
+     * @brief Propagates the values of the global parameters to a specified node.
+     * @param node Node which has parameters to update. 
+     */
+    void PropagateParameters(rclcpp::Node* node);
 
 private:
     rclcpp::Node* node_;
-    rclcpp::Node* global_node_;
-    rclcpp::AsyncParameterClient::SharedPtr params_client_;
-    rclcpp::Subscription<rcl_interfaces::msg::ParameterEvent>::SharedPtr params_client_sub_;
-    
-    /**
-     * @brief Updates the parameters in the global params node.
-     */
-    void UpdateGlobalParams();
+    auto parameters_client = std::make_shared<rclcpp::AsyncParametersClient>(node_);
 }
+
 
 } // namespace params
