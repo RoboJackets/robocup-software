b5ef4752b9a568c0de96bec82449990989096bfd
Marcus Ma <mamarcus64@gmail.com>
2021-02-06 22:03:22 +0000

    second commit

--- rj_gameplay/pass_costing/test_cost.py
+++ rj_gameplay/pass_costing/test_cost.py
@@ -1,18 +1,15 @@
+import numpy as np
 import stp.rc as rc
 import stp.testing as testing
 import rj_gameplay.gameplay_node as gameplay_node
 import rclpy
-import numpy as np
 
 def test_cost(world_state: rc.WorldState, robot: rc.Robot) -> float:
 	print("pain")
 	return 1
 
 def main():
-	np.ndarray(shape=(3,))
-	pose = np.array([3, 3, 3.14])
-	field = testing.generate_divB_field()
-	print(type(pose))
+	np.ndarray(shape=(3,1))
 	world_state = rc.WorldState(["e"], [], None)
 	for i in world_state.our_robots:
 		test_cost(world_state, i)
@@ -20,4 +17,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
