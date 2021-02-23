6e38e0747cd78d29cfa6712f5e24e6c6069f4b12
Tohwx <weixiongtoh@gmail.com>
2021-02-23 09:31:45 -0500

    tidy files'

--- rj_param_utils/testing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wx/WX/robocup-software/rj_param_utils/testing
-# Build directory: /home/wx/WX/robocup-software/rj_param_utils/testing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
