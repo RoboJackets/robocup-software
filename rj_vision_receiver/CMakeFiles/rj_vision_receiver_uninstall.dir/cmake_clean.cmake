5e1a6c7ce95a61b653c26b5a557ff7b9cb1d36c6
Tohwx <weixiongtoh@gmail.com>
2021-02-23 11:27:57 -0500

    remove cmake files in rj_vision_receiver

--- rj_vision_receiver/CMakeFiles/rj_vision_receiver_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rj_vision_receiver_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rj_vision_receiver_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
