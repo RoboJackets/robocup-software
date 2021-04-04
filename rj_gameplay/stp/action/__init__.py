a45893270fab05d71295e7285f7f0eeebce8a235
Christopher Lindbeck <clindbeck9@gmail.com>
2021-04-04 13:35:33 -0400

    trying to shutoff the other gameplay system

--- rj_gameplay/stp/action/__init__.py
+++ rj_gameplay/stp/action/__init__.py
@@ -7,12 +7,20 @@ from rj_msgs.msg import RobotIntent
 #from rclpy.publisher import Publisher
 
 
+
+
+class IIntentAction(ABC):
+
+    def tick(self, RobotIntent):
+        hat
+
+
 class IAction(ABC):
     """Interface for actions."""
 
     #@abstractmethod
-    def tick(self) -> None:
-        """Ticks the action. Publishing RobotIntent on the provided publisher if applicable"""
+    def tick(self, RobotIntent) -> None:
+        """Ticks the action. Modifying the passed RobotIntent object"""
         ...
 
     #@abstractmethod
@@ -49,64 +57,3 @@ class IFiniteAction(IAction, ABC):
         ...
 
 
-ActionT = TypeVar("ActionT", bound=IAction)
-
-
-class Registry:
-    """Registry that holds instances of actions indexed by the type."""
-
-    __slots__ = ["_dict"]
-
-    def __init__(self):
-        self._dict: Dict[Type[IAction], IAction] = {}
-
-    def __getitem__(self, key: Type[ActionT]) -> ActionT:
-        # The below can throw a KeyError.
-        action: IAction = self._dict[key]
-
-        # Check that the item we got was an instance of the expected type.
-        if not isinstance(action, key):
-            raise KeyError("Action {} is not an instance of key {}".format(action, key))
-
-        return action
-
-    def __setitem__(self, key: Type[ActionT], value: ActionT) -> None:
-        # Check that the item we're setting is an instance of the expected type.
-        if not isinstance(value, key):
-            raise KeyError("Action {} is not an instance of {}".format(value, key))
-
-        self._dict.__setitem__(key, value)
-
-    def __delitem__(self, key: Type[ActionT]) -> None:
-        self._dict.__delitem__(key)
-
-    def __len__(self) -> int:
-        return self._dict.__len__()
-
-    def __iter__(self):
-        return self._dict.__iter__()
-
-    def __contains__(self, item) -> bool:
-        return self._dict.__contains__(item)
-
-
-class Factory:
-    """Factory that creates actions according to the ActionRegistry passed in."""
-
-    __slots__ = ["_registry"]
-
-    _registry: Registry
-
-    def __init__(self, registry: Registry):
-        self._registry = registry
-
-    def create(self, action: Type[ActionT]) -> ActionT:
-        """Creates an instance of the action given the type of the interface of the
-        action."""
-        if action not in self._registry:
-            # TODO: Create new class for this error category.
-            raise ValueError(
-                "Trying to create action {}, but not in registry!".format(action)
-            )
-
-        return self._registry[action]
