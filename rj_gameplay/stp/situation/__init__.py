cb3d03ce44c285846913497cf0e088a7afb47096
Christopher Lindbeck <clindbeck9@gmail.com>
2021-04-08 09:41:24 -0400

    more things did not spark joy

--- rj_gameplay/stp/situation/__init__.py
+++ rj_gameplay/stp/situation/__init__.py
@@ -11,9 +11,9 @@ class IPlaySelector(ABC):
     """Interface for play selector."""
 
     @abstractmethod
-    def select(self, world_state: rc.WorldState) -> Tuple[ISituation, stp.play.IPlay]:
-        """Selects the best situation and play given given the current world state.
+    def select(self, world_state: rc.WorldState) -> stp.play.IPlay:
+        """Selects the best play given given the current world state.
         :param world_state: The current state of the world.
-        :return: A tuple of the best situation and best play.
+        :return: The best play.
         """
         ...
