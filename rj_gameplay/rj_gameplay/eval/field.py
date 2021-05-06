16910162563d5ff71bb286d88a8a48f4b0bf93cf
Sumedh Garimella <10521124+hdemusg@users.noreply.github.com>
2021-05-05 22:55:58 -0400

    field updates - 5/5
    finished space coeff at pos function!

--- rj_gameplay/rj_gameplay/eval/field.py
+++ rj_gameplay/rj_gameplay/eval/field.py
@@ -3,11 +3,33 @@ sys.path.insert(1, "../../stp")
 import rc
 
 class Field:
-   def space_coeff_at_pos(point, excluded_robots, robots):
-       if excluded_robots is None:
-        excluded_robots = []
+   ## Determines how much "space" there is at a pos
+   #    "Space" is how empty an area of the field is
+   # @param pos: point to evalute
+   # @returns Number between 0 and 1 representing the closeness of robots
+   # The higher the number, the more robots closer to the position
+   def space_coeff_at_pos(point, excluded, robots, field):
+       if excluded is None:
+        excluded = []
        if robots is None:
         robots = main.their_robots()
+        
+       max_dist = sqrt(field.length_m**2 + (field.width_m**2)/4)
+    
+       total = 0
+       sensitivity = 8
+    
+       for bot in robots:
+            if bot.visible and bot not in excluded:
+               pos = pose[:-1]
+               dist = sqrt((pos[0] - point[0])**2 + (pos[1] - [point[1])**2)
+               u = (sensitivity * dist) / max_dist
+
+               # Use Triweight kernal function (Force to be positive)
+               # Graph looks much like a normal distribution
+               total += max((35 / 32) * pow((1 - pow(u, 2)), 3), 0)
+                                                               
+       return min(total, 1)
 
 '''
 import main
