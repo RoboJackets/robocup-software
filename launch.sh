e5f27c856d5b5f71257270c0af9ea6233e38931d
Arvind Srinivasan <arvindsrinivasan@gatech.edu>
2021-05-10 17:07:16 -0400

    Create launch.sh

---
+++ launch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if ! command -v git >/dev/null; then
+    echo "Git is not installed!!!" >&2
+    exit 1
+fi
+
+if [ "$(git rev-parse --abbrev-ref HEAD)" = "HEAD" ]; then
+    echo "HEAD IS DETACHED AT COMMIT: $(git rev-parse HEAD)"
+else
+    echo "Branch is: $(git rev-parse --abbrev-ref HEAD)"
+fi
+
+source /opt/ros/foxy/setup.bash
+make
+source install/setup.bash
+ros2 launch rj_robocup soccer.launch.py
+
+echo "Soccer exited normally."
