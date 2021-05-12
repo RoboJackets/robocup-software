38d6c3daf2b0fd65043614c1248df7b77e3e9f83
Kevin Fu <kevin.fu842@gmail.com>
2021-05-12 17:04:41 -0400

    first pass

--- rj_gameplay/rj_gameplay/action/kick.py
+++ rj_gameplay/rj_gameplay/action/kick.py
@@ -4,8 +4,13 @@ from abc import ABC, abstractmethod
 
 import stp.role as role
 import stp.action as action
-import numpy as np
 import stp.rc as rc
+import numpy as np
+# from rj_msgs.msg import RobotIntent, MotionCommand, EmptyMotionCommand, PathTargetMotionCommand, LinearMotionInstant
+from rj_geometry_msgs.msg import Point
+from typing import Optional
+import math
+from rj_msgs import msg
 
 
 class IKick(action.IAction, ABC):
@@ -18,13 +23,21 @@ class Kick(IKick):
     Kick action
     TODO: update with actions implementation
     """
-    def __init__(self, point: np.ndarray):
-        self.point = point
-        self.count = -1
-        #for stub
+    def __init__(self, 
+            robot_id : int, 
+            target_point : np.ndarray):
 
-    def tick(self, robot: rc.Robot, ctx: action.Ctx) -> None:
-        print('robot:', robot.id, 'kicking')
+        self.robot_id = robot_id
+        self.target_point = target_point 
+
+        self.count = -1
+
+    def tick(self, intent: msg.RobotIntent) -> msg.RobotIntent:
+        # print('robot:', self.robot_id, 'kicking')
+        intent.shoot_mode = intent.SHOOT_MODE_KICK # not sure if this is how you ref that constant in RobotIntent.msg
+        intent.kick_speed = kMaxKick #255
+        intent.is_active = True
+
         self.count += 1
 
     def done(self) -> bool:
