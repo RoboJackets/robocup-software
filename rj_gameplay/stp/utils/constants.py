d7dd036b3932e6f0399ddcf71e3ad7adb5c36a46
Sumedh Garimella <10521124+hdemusg@users.noreply.github.com>
2021-05-18 18:51:45 -0400

    creating constants.py

---
+++ rj_gameplay/stp/utils/constants.py
@@ -0,0 +1,47 @@
+import robocup
+import math
+
+DegreesToRadians = math.pi / 180.0
+RadiansToDegrees = 180.0 / math.pi
+OurChipRoll = 3  # Average distance at which a ball is slow enough to capture
+TheirChipping = (.1, .8)
+
+
+class OurChipping:
+    MIN_CARRY = .1
+    MAX_CARRY = .8
+    CAPTURE_DISTANCE = 3
+    MIN_CARRY_TIME = .1
+    MAX_CARRY_TIME = .4
+    CAPTURE_DISTANCE_TIME = 1.2
+
+class Colors:
+    White = (255, 255, 255)
+    Black = (0, 0, 0)
+    Green = (0, 255, 0)
+    Red = (255, 0, 0)
+    Blue = (0, 0, 255)
+
+
+class Robot:
+    Radius = 0.09
+    MaxKickSpeed = 8  # m/s
+    ChipClearance = (
+        .1, .6)  # min and max distance a chip will go over another robot
+
+    class Dribbler:
+        MaxPower = 127
+
+        # "Normal" Dribbler speed to be used for generic ball capture/movement
+        StandardPower = 127
+
+
+class Ball:
+    Radius = 0.0215
+    Mass = 0.04593  # mass of golf ball (kg)
+
+class Evaluation:
+    SlowThreshold = 1
+
+# Set to the global FieldDimensions object by the GameplayModule
+Field = robocup.FieldDimensions.CurrentDimensions
