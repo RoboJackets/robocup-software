8976cb368675aba3c4805ffa17b4b7a749c213eb
Kasra Sohrab <kasra.sohrab@gmail.com>
2021-05-18 21:14:15 -0400

    intial dribble action modifications

--- rj_gameplay/rj_gameplay/action/dribble.py
+++ rj_gameplay/rj_gameplay/action/dribble.py
@@ -4,20 +4,28 @@ from abc import ABC, abstractmethod
 
 import stp.role as role
 import stp.action as action
-import numpy as np
+import stp.rc as rc
+from rj_msgs.msg import RobotIntent
+from typing import Optional
+from rj_msgs import msg
 
 
 class IDribble(action.IAction, ABC):
 
-    def done(self) -> bool:
+    def is_done(self) -> bool:
         pass
 
-class Drible(IDribble):
+class Dribble(IDribble):
+    """
+    Dribble action
+    """
+    def __init__(self, robot_id: int):
+        self.robot_id = robot_id
 
-    def __init__(self):
-        pass
+    def tick(self, intent: msg.RobotIntent) -> msg.RobotIntent:
+    	intent.dribbler_speed = 1.0
+        intent.is_active = True
+        return intent
 
-
-    def tick(self, ctx: action.Ctx) -> None:
-    	# TODO: update with actions implementation
-        pass
+    def is_done(self) -> bool:
+        return False;
