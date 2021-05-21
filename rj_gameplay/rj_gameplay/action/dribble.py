71e2250231f7bddb748aa1641ca875bbbfe8db41
Kasra Sohrab <kasra.sohrab@gmail.com>
2021-05-21 10:48:00 -0400

    fixed spacing

--- rj_gameplay/rj_gameplay/action/dribble.py
+++ rj_gameplay/rj_gameplay/action/dribble.py
@@ -19,12 +19,13 @@ class Dribble(IDribble):
     """
     Dribble action
     """
-    def __init__(self, robot_id: int,
+    def __init__(self,
+            robot_id : int,
             dribbler_speed : int = 1.0,
-            priority : int = 0):
+            priority : int = 0) -> None:
+
         self.robot_id = robot_id
         self.dribbler_speed = 1.0
-
 
     def tick(self, intent: msg.RobotIntent) -> msg.RobotIntent:
     	intent.dribbler_speed = self.dribbler_speed
