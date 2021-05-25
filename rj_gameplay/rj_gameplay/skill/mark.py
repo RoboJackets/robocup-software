b2c98e72837522b39118a824dd7ed282bd5c145b
Kevin Fu <kevin.fu842@gmail.com>
2021-05-25 17:34:23 -0400

    mark uses ActionBehavior now

--- rj_gameplay/rj_gameplay/skill/mark.py
+++ rj_gameplay/rj_gameplay/skill/mark.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Callable
+from typing import Callable, Optional
 
 import rj_gameplay.eval as eval
 import argparse
@@ -11,8 +11,47 @@ import numpy as np
 import stp.skill as skill
 import stp.role as role
 import stp.action as action
+from rj_gameplay.action import move
 from stp.skill.action_behavior import ActionBehavior
 import stp.rc as rc
+import stp.utils.constants as constants
+
+from rj_geometry_msgs.msg import Point, Segment
+
+def get_mark_point(world_state: rc.WorldState, mark_robot_id: int):
+    ratio = 0.9 # pct of line robot should be on (1.0 = on mark_pt)
+
+    ball_pos = world_state.ball.pos
+
+    print("-"*81)
+    print(ball_pos)
+    # print(world_state.their_robots)
+    # print(world_state.our_robots)
+    # if len(world_state.their_robots) is 0: return
+
+    # mark_pos = world_state.their_robots[mark_robot_id]
+    mark_pos = world_state.field.center_field_loc
+    print(mark_pos)
+
+    #Finds the line from the ball to the mark position and creates a line between them
+    #removing the overlap with the ball on one side and robot on the other
+    #This assumes even with mark position parameter that there is a robot there to avoid
+    mark_line_dir = np.linalg.norm(ball_pos - mark_pos)
+    start_pos = ball_pos - mark_line_dir * constants.Ball.Radius
+    end_pos = mark_pos + mark_line_dir * 2.0 * constants.Robot.Radius
+
+    ratio_pt = ((end_pos - start_pos) * ratio) + start_pos
+    print(ratio_pt)
+    return ratio_pt
+
+    """
+    # TODO: change name of Segment's param from "pt" to "pts"
+    ball_mark_line = Segment(pt=[
+        Point(x=start_pos[0], y=start_pos[1]),
+        Point(x=end_pos[0], y=end_pos[1])
+    ])
+    """
+
 
 class IMark(skill.ISkill, ABC):
     ...
@@ -49,8 +88,14 @@ class Mark(IMark):
         self.root = self.mark_behavior
         self.root.setup_with_descendants()
 
-    def tick(self, robot: rc.Robot, new_target: np.ndarray, world_state: rc.WorldState) -> None:
+    def tick(self, robot: rc.Robot, world_state: rc.WorldState) -> None:
         self.robot = robot
-        self.target_point = new_target
+        if world_state and world_state.ball.visible:
+            # TODO: update w/ robot id (or remove that param from mark_cost)
+            self.target_point = get_mark_point(world_state, -1)
+            # self.target_point = world_state.ball.pos
         actions = self.root.tick_once(robot, world_state)
         return actions
+
+    def is_done(self, world_state):
+        return self.move.is_done(world_state)
