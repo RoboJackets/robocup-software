df9cea0bc1d9ada66ac2471542375741b6ed6513
Adithya Vasudev <57768064+adithya-gv@users.noreply.github.com>
2021-05-25 16:12:51 -0700

    Add rosconverter detectionrobot (#1612)
    * add rosconverter detectionrobot
    
    * Typo Fix
    
    * Finished Fixes
    
    * Fixed Typ
    
    Co-authored-by: Kyle Stachowicz <kylestach99@gmail.com>

--- rj_utils/include/rj_utils/conversions.hpp
+++ rj_utils/include/rj_utils/conversions.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <rj_protos/messages_robocup_ssl_wrapper.pb.h>
 #include <rj_convert/ros_convert.hpp>
 #include <rj_msgs/msg/detection_ball.hpp>
 #include <rj_msgs/msg/detection_robot.hpp>
@@ -33,6 +34,41 @@ using DetectionRobotMsg = rj_msgs::msg::DetectionRobot;
 [[nodiscard]] DetectionRobotMsg to_ros_msg(const SSL_DetectionRobot& robot);
 
 namespace rj_convert {
+
+template<>
+struct RosConverter<SSL_DetectionRobot, DetectionRobotMsg> {
+	static DetectionRobotMsg to_ros(const SSL_DetectionRobot& from) {
+		DetectionRobotMsg to;
+		
+		convert_to_ros(from.confidence(), &to.confidence);
+		convert_to_ros(from.robot_id(), &to.robot_id);
+		convert_to_ros(from.x(), &to.x);
+		convert_to_ros(from.y(), &to.y);
+		convert_to_ros(from.orientation(), &to.orientation);
+		convert_to_ros(from.pixel_x(), &to.pixel_x);
+		convert_to_ros(from.pixel_y(), &to.pixel_y);
+		convert_to_ros(from.height(), &to.height);
+		
+		return to;
+	}
+	
+	static SSL_DetectionRobot from_ros(const DetectionRobotMsg& from) {
+		SSL_DetectionRobot to;
+		
+		to.set_confidence(convert_from_ros(from.confidence));
+		to.set_robot_id(convert_from_ros(from.robot_id));
+		to.set_x(convert_from_ros(from.x));
+		to.set_y(convert_from_ros(from.y));
+		to.set_orientation(convert_from_ros(from.orientation));
+		to.set_pixel_x(convert_from_ros(from.pixel_x));
+		to.set_pixel_y(convert_from_ros(from.pixel_y));
+		to.set_height(convert_from_ros(from.height));
+		
+		return to;
+	}
+};
+
+ASSOCIATE_CPP_ROS(SSL_DetectionRobot, DetectionRobotMsg)
 
 template <>
 struct RosConverter<SSL_DetectionBall, DetectionBallMsg> {
