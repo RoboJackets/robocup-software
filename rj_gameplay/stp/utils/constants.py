53fe9765027e47df349efe78adffa59c95da3f46
Kevin Fu <kevin.fu842@gmail.com>
2021-05-26 23:59:38 -0400

    renamed classes to be less confusing when used in other gameplay files

--- rj_gameplay/stp/utils/constants.py
+++ rj_gameplay/stp/utils/constants.py
@@ -6,7 +6,7 @@ import math
 OUR_CHIP_ROLL = 3  # Average distance at which a ball is slow enough to capture
 THEIR_CHIPPING = (.1, .8)
 
-class OurChipping:
+class OurChippingConstants:
     MIN_CARRY = .1
     MAX_CARRY = .8
     CAPTURE_DISTANCE = 3
@@ -14,7 +14,7 @@ class OurChipping:
     MAX_CARRY_TIME = .4
     CAPTURE_DISTANCE_TIME = 1.2
 
-class Colors:
+class ColorConstants:
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
     GREEN = (0, 255, 0)
@@ -22,24 +22,24 @@ class Colors:
     BLUE = (0, 0, 255)
 
 
-class Robot:
+class RobotConstants:
     RADIUS = 0.09
     MAX_KICK_SPEED = 8  # m/s
     CHIP_CLEARANCE = (
         .1, .6)  # min and max distance a chip will go over another robot
 
-    class Dribbler:
+    class DribblerConstants:
         MAX_POWER = 127
 
         # "Normal" Dribbler speed to be used for generic ball capture/movement
         STANDARD_POWER = 127
 
 
-class Ball:
+class BallConstants:
     RADIUS = 0.0215
     MASS = 0.04593  # mass of golf ball (kg)
 
-class Evaluation:
+class EvaluationConstants:
     SLOW_THRESHOLD = 1
 
 # for field dimensions, use world_state.field (see /stp/rc.py for details)
