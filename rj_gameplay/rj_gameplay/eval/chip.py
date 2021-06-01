c0c31f3696cd4b49d358b161c7bcb9a51a6413fb
Sumedh Garimella <10521124+hdemusg@users.noreply.github.com>
2021-06-01 18:48:32 -0400

    removed comments from chip

--- rj_gameplay/rj_gameplay/eval/chip.py
+++ rj_gameplay/rj_gameplay/eval/chip.py
@@ -9,23 +9,14 @@ class Chip:
     def distance(loc1, loc2):
         return sqrt(abs(loc1[1] - loc2[1])**2 + abs(loc1[0] - loc2[0])**2)
 
+    # Returns an array of their robots that we can chip over based on the current ball position
+    # this only operates on the distances from the ball to each robot and the Chip distances in constants.py
+
 	def chippable_robots(ball, their_robots):
 	    bpos = ball.pos
         dist = distance(bpos, r.pos[:-1])
         return [r for r in their_robots if (dist > constants.OurChipping.MIN_CARRY and dist < constants.OurChipping.MAX_CARRY)]
 		#return None
 
-	#ball = rc.Ball([0,4.5],[0, -1], True)
-#
-# Returns an array of their robots that we can chip over based on the current ball position
-# this only operates on the distances from the ball to each robot and the Chip distances in constants.py
-#
-'''
-def chippable_robots() -> List[robocup.OpponentRobot]:
-    bp = main.ball().pos
-    return [
-        rob for rob in main.system_state().their_robots
-        if (rob.pos - bp).mag() > constants.OurChipping.MIN_CARRY and (
-            rob.pos - bp).mag() < constants.OurChipping.MAX_CARRY
-    ]
- '''
+    ball = rc.Ball([0,4.5],[0, -1], True)
+
