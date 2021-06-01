1e8690e8918ea841dce2358984ce8cec5f3921f1
Sumedh Garimella <10521124+hdemusg@users.noreply.github.com>
2021-06-01 18:43:45 -0400

    Chip - 6/1

--- rj_gameplay/rj_gameplay/eval/chip.py
+++ rj_gameplay/rj_gameplay/eval/chip.py
@@ -1,15 +1,21 @@
 import sys
 sys.path.insert(1, "../../stp")
 import rc
+sys.path.append("/utils")
+import constants
 
 class Chip:
+    
+    def distance(loc1, loc2):
+        return sqrt(abs(loc1[1] - loc2[1])**2 + abs(loc1[0] - loc2[0])**2)
 
-	def chippable_robots():
+	def chippable_robots(ball, their_robots):
 	    bpos = ball.pos
-        #return []
-		return None
+        dist = distance(bpos, r.pos[:-1])
+        return [r for r in their_robots if (dist > constants.OurChipping.MIN_CARRY and dist < constants.OurChipping.MAX_CARRY)]
+		#return None
 
-	ball = rc.Ball([0,4.5],[0, -1], True)
+	#ball = rc.Ball([0,4.5],[0, -1], True)
 #
 # Returns an array of their robots that we can chip over based on the current ball position
 # this only operates on the distances from the ball to each robot and the Chip distances in constants.py
