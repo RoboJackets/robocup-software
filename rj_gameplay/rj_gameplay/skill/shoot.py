cfda5f59e9292a6f60d2e95aaf4a15f818936c51
Yudai Hatano <noah.bjyu@gmail.com>
2021-06-08 22:28:33 +0000

    ctx added

--- rj_gameplay/rj_gameplay/skill/shoot.py
+++ rj_gameplay/rj_gameplay/skill/shoot.py
@@ -24,20 +24,22 @@ class Shoot(IShoot):
 
     def __init__(self) -> None:
         self.robot: rc.Robot = None
+        self.ctx = None
         self.__name__ = 'Shoot'
         self.root = sequence.RJSequence()
         self.pivot = pivot.Pivot(self.robot.pose[0:2], rc.Field.their_goal_loc)
         self.kick = kick.Kick(rc.Field.their_goal_loc)
-        #Add more logic for aiming and kicking
         self.pivot_behavior = ActionBehavior('Pivot', self.pivot, self.robot) 
         self.kick_behavior = ActionBehavior('Kick', self.kick, self.robot)
         self.root.add_children([self.capture_behavior, self.pivot_behavior, self.kick_behavior])
         self.root.setup_with_descendants()
 
-    def tick(self, world_state: rc.WorldState, robot:rc.Robot) -> None:
-        return self.root.tick_once(robot)
-        
-    def is_done(self, world_state) -> bool:
+    def tick(self robot:rc.Robot, ctx) -> None:
+        self.root.tick_once(robot, ctx)
+     
+
+    #if Kick and Pivot is done, Shoot should be done.
+    def is_done(self, world_state: rc.WorldState) -> bool:
     	if self.pivot.is_done(world_state) and self.kick.done(): 
     		return True
     	else:
