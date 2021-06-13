663a8192aa82d5d9eb97d89e532db50c4d50ee29
Sumedh Garimella <10521124+hdemusg@users.noreply.github.com>
2021-06-13 01:25:05 -0400

    path updates - 6/13 at 1:25am

--- rj_gameplay/rj_gameplay/eval/path.py
+++ rj_gameplay/rj_gameplay/eval/path.py
@@ -10,13 +10,11 @@ from typing import List, Optional, Tuple
 #  @param blocking_robots The list of robots to dodge
 #  @param dodge_dist The amount of distance to dodge left or right to miss a robot
 #  @return the distance from start to end
-def estimate_path_length(start, end,
-                         blocking_robots: List[robocup.Robot],
-                         dodge_dist: float) -> float:
+def estimate_path_length(start, end, blocking_robots, dodge_dist):
     total = 0
     next_pt = start
     prev_pt = start
-    line = robocup.Segment(start, end)
+    line = [start, end]
     iterations = 0
     max_iterations = 10
 
@@ -25,28 +23,34 @@ def estimate_path_length(start, end,
     while (blocking_robot is not None) and (iterations < max_iterations):
         # Find next point
         # Next point is +-dodge_dist * perp_vect
-        robot_vec = (blocking_robot.pos - next_pt)
-        perp_vec = robot_vec.perp_cw().normalized()
+        robot_vec = (blocking_robot.pos[:-1] - next_pt)
+        perp = [-robot_vec[1], robot_vec[0]]
+        pn = sqrt(perp[0]**2 + perp[1]**2)
+        perp_vec = perp / pn
 
-        pt1 = perp_vec * dodge_dist + blocking_robot.pos - next_pt
-        pt2 = perp_vec * -dodge_dist + blocking_robot.pos - next_pt
+        pt1 = perp_vec * dodge_dist + blocking_robot.pos[:-1] - next_pt
+        pt2 = perp_vec * -dodge_dist + blocking_robot.pos[:-1] - next_pt
+        m1 = sqrt(pt1[0]**2 + pt1[1]**2)
+        m2 = sqrt(pt2[0]**2 + pt2[1]**2)
 
         # Find shortest path
-        if (pt1.mag() < pt2.mag()):
+        if (m1 < m2):
             next_pt = pt1
         else:
             next_pt = pt2
 
         # Add dist to total
-        total += (next_pt - prev_pt).mag()
+        t = (next_pt - prev_pt)
+        total += sqrt(t[0]**2 + t[1]**2)
 
         prev_pt = next_pt
-        line = robocup.Segment(next_pt, end)
+        line = [next_pt, end]
         blocking_robot = find_intersecting_robot(line, blocking_robots,
                                                  dodge_dist)
         iterations += 1
 
-    total += (end - next_pt).mag()
+    tx = (end - next_pt) 
+    total += sqrt(tx[0]**2 + tx[1]**2)
 
     return total
 
@@ -61,37 +65,33 @@ def estimate_path_length(start, end,
 #       Whether we can collect the ball before the opponen
 #       The closest robot on our team
 #  @note If any imputs are None, their values are defaulted
-def can_collect_ball_before_opponent(
-        our_robots_to_check: Optional[List[robocup.OurRobot]] = None,
-        their_robots_to_check: Optional[List[robocup.OpponentRobot]] = None,
-        our_robots_to_dodge: Optional[List[robocup.OurRobot]] = None,
-        their_robots_to_dodge: Optional[List[robocup.OpponentRobot]] = None,
-        valid_error_percent: float = 0.05) -> Tuple[bool, robocup.Robot]:
+def can_collect_ball_before_opponent(ball, their_robots, our_robots, our_robots_to_check, their_robots_to_check, our_robots_to_dodge, their_robots_to_dodge, valid_error_percent):
     if our_robots_to_check is None:
-        our_robots_to_check = main.our_robots()
+        our_robots_to_check = our_robots
 
     if their_robots_to_check is None:
-        their_robots_to_check = main.their_robots()
+        their_robots_to_check = their_robots
 
     if our_robots_to_dodge is None:
-        our_robots_to_dodge = main.our_robots()
+        our_robots_to_dodge = our_robots
 
     if their_robots_to_dodge is None:
-        their_robots_to_dodge = main.their_robots()
+        their_robots_to_dodge = their_robots
 
     shortest_opp_time = float("inf")
     shortest_our_time = float("inf")
-    dodge_dist = constants.Robot.Radius
+    dodge_dist = 0.09
     closest_robot = None
 
     # TODO: Do some sort of prediction as the ball moves
-    target_pos = main.ball().pos
+    target_pos = ball.pos
 
     # TODO: Take velocity and acceleration into account
     # Find closest opponent robot
     for bot in their_robots_to_check:
         dist = estimate_path_length(bot.pos, target_pos, our_robots_to_dodge,
                                     dodge_dist)
+        td = (target_pos - bot.pos)
         target_dir = (target_pos - bot.pos).normalized()
         time = robocup.get_trapezoidal_time(dist, dist, 2.2, 1,
                                             target_dir.dot(bot.vel) /
