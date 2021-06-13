e947bc282dcd197373e76844a7a26d8682db8cfe
Sumedh Garimella <10521124+hdemusg@users.noreply.github.com>
2021-06-13 01:03:02 -0400

    pass changes at 1:03 am

--- rj_gameplay/rj_gameplay/eval/pass.py
+++ rj_gameplay/rj_gameplay/eval/pass.py
@@ -22,18 +22,94 @@ class Pass:
         #pass_dist = to_point.dist_to(from_point)
         pass_dist = sqrt(abs(from_point[0] - to_point[0])**2 + abs(from_point[1] - to_point[1])**2)
         pass_dir = to_point - from_point
+        pass_perp = [-pass_dir[1], pass_dir[0]]
+        receive_seg_half_len = math.tan(pass_angle) * pass_dist
+        [(to_point + pass_perp * receive_seg_half_len, to_point + pass_perp * -receive_seg_half_len)]
         
         '''
-        pass_dir = to_point - from_point
-        pass_perp = pass_dir.perp_ccw()
-        receive_seg_half_len = math.tan(pass_angle) * pass_dist
-        receive_seg = robocup.Segment(to_point + pass_perp * receive_seg_half_len,
-                                      to_point + pass_perp * -receive_seg_half_len)
 
         win_eval = robocup.WindowEvaluator(main.context())
         for r in excluded_robots:
             win_eval.add_excluded_robot(r)
         windows, best = win_eval.eval_pt_to_seg(from_point, receive_seg)
+        
+        auto end = target.delta().magsq();
+
+    // if target is a zero-length segment, there are no windows
+    if (end == 0) {
+        return make_pair(std::vector<Window>{}, std::nullopt);
+    }
+
+    if (debug) {
+        context_->debug_drawer.draw_line(target, QColor{"Blue"}, "Debug");
+    }
+
+    std::vector<Window> windows = {Window{0, end}};
+
+    // apply the obstacles
+
+    std::vector<Robot*> bots(context_->state.self.size() + context_->state.opp.size());
+
+    auto filter_predicate = [&](const Robot* bot) -> bool {
+        return bot != nullptr && bot->visible() &&
+               find(excluded_robots.begin(), excluded_robots.end(), bot) == excluded_robots.end();
+    };
+
+    auto end_it = copy_if(context_->state.self.begin(), context_->state.self.end(), bots.begin(),
+                          filter_predicate);
+
+    end_it =
+        copy_if(context_->state.opp.begin(), context_->state.opp.end(), end_it, filter_predicate);
+
+    bots.resize(distance(bots.begin(), end_it));
+
+    std::vector<Point> bot_locations;
+    for_each(bots.begin(), bots.end(),
+             [&bot_locations](Robot* bot) { bot_locations.push_back(bot->pos()); });
+
+    bot_locations.insert(bot_locations.end(), hypothetical_robot_locations.begin(),
+                         hypothetical_robot_locations.end());
+
+    for (auto& pos : bot_locations) {
+        auto d = (pos - origin).mag();
+        // whether or not we can ship over this bot
+        auto chip_overable = chip_enabled && (d < max_chip_range - kRobotRadius) &&
+                             (d > min_chip_range + kRobotRadius);
+        if (!chip_overable) {
+            obstacle_robot(windows, origin, target, pos);
+        }
+    }
+
+    auto p0 = target.pt[0];
+    auto delta = target.delta() / end;
+
+    for (auto& w : windows) {
+        w.segment = Segment{p0 + delta * w.t0, p0 + delta * w.t1};
+        w.a0 = radians_to_degrees(static_cast<float>((w.segment.pt[0] - origin).angle()));
+        w.a1 = radians_to_degrees(static_cast<float>((w.segment.pt[1] - origin).angle()));
+        fill_shot_success(w, origin);
+    }
+
+    std::optional<Window> best;
+    if (!windows.empty()) {
+        best = *max_element(windows.begin(), windows.end(), [](Window& a, Window& b) -> bool {
+            return a.segment.delta().magsq() < b.segment.delta().magsq();
+        });
+    }
+    if (debug) {
+        if (best) {
+            context_->debug_drawer.draw_line(Segment{origin, best->segment.center()},
+                                             QColor{"Green"}, "Debug");
+        }
+        for (Window& window : windows) {
+            context_->debug_drawer.draw_line(window.segment, QColor{"Green"}, "Debug");
+            context_->debug_drawer.draw_text(QString::number(window.shot_success),
+                                             window.segment.center() + Point(0, 0.1),
+                                             QColor{"Green"}, "Debug");
+        }
+    }
+
+    return make_pair(windows, best);
 
         # this is our estimate of the likelihood of the pass succeeding
         # value can range from zero to one
