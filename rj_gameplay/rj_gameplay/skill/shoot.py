ce52fbd7c72388206565ba60aa067b52954120e8
Yudai Hatano <noah.bjyu@gmail.com>
2021-06-18 03:46:36 +0000

    rj_sequence imported, tick in shoot.py updated

--- rj_gameplay/rj_gameplay/skill/shoot.py
+++ rj_gameplay/rj_gameplay/skill/shoot.py
@@ -10,9 +10,9 @@ import stp.skill as skill
 import stp.role as role
 import stp.action as action
 from rj_gameplay.action import pivot, kick
-from stp.skill.action_behavior import ActionBehavior
+from stp.skill.action_behavior import ActionBehavior, RobotActions
+from stp.skill.rj_sequence import RjSequence as Sequence
 import stp.rc as rc
-import stp.skill.sequence as sequence
 import numpy as np
 
 class IShoot(skill.ISkill, ABC):
@@ -21,14 +21,16 @@ class IShoot(skill.ISkill, ABC):
 """
 A shoot skill which aims at the goal and shoots
 """
+random_shoot = random.uniform(-0.5,0.5)
+
 class Shoot(IShoot):
 
-    def __init__(self, chip: bool, kick_speed: float, target_point: np.array = np.array([0., 10.])) -> None:
+    def __init__(self, chip: bool, kick_speed: float, target_point: np.array = np.array([random_shoot, 12.])) -> None:
         self.robot: rc.Robot = None
         self.ctx = None
         self.target_point = target_point
         self.__name__ = 'Shoot'
-        self.root = sequence.RJSequence()
+        self.root = Sequence("Sequence")
         if self.robot is not None:
             self.pivot = pivot.Pivot(self.robot.id, self.robot.pose[0:2], target_point, 1.0)
             self.kick = kick.Kick(self.robot.id, chip, kick_speed)
@@ -40,8 +42,10 @@ class Shoot(IShoot):
         self.root.add_children([self.pivot_behavior, self.kick_behavior])
         self.root.setup_with_descendants()
 
-    def tick(self, robot:rc.Robot, ctx) -> None:
-        return self.root.tick_once(robot, ctx)
+    def tick(self, robot: rc.Robot, world_state: rc.WorldState) -> RobotActions:
+        self.robot = robot
+        self.pivot.pivot_point = world_state.ball.pos
+        return self.root.tick_once(robot, world_state)
      
 
     #if Kick and Pivot is done, Shoot should be done.
