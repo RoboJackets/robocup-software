f10c0987d9ac659d3ae6c82993ed605e0fe8f226
vagrant <hgynai732@gmail.com>
2021-06-20 19:44:59 +0000

    made play hold type of plays instead of instances

--- rj_gameplay/stp/basic_play_selector.py
+++ rj_gameplay/stp/basic_play_selector.py
@@ -10,24 +10,24 @@ from typing import Tuple, Dict
 #TODO: Put new plays into the dict properly
 #TODO: Create different dictionaries for different playbooks
 PLAY_DICT = {}
-PLAY_DICT[situations.Kickoff] = [plays.line_up.LineUp()]
+PLAY_DICT[situations.Kickoff] = [plays.line_up.LineUp]
 PLAY_DICT[situations.DefendRestartOffensive] = []
 PLAY_DICT[situations.DefendRestartMidfield] = []
 PLAY_DICT[situations.DefendRestartDefensive] = []
-PLAY_DICT[situations.Clear] = [plays.passing_tactic_play.PassPlay()]
-PLAY_DICT[situations.DefendClear] = [defensive_clear.DefensiveClear()]
-PLAY_DICT[situations.DefendGoal] = [plays.wall_ball.WallBall()]
+PLAY_DICT[situations.Clear] = [plays.passing_tactic_play.PassPlay]
+PLAY_DICT[situations.DefendClear] = [defensive_clear.DefensiveClear]
+PLAY_DICT[situations.DefendGoal] = [plays.wall_ball.WallBall]
 PLAY_DICT[situations.MidfieldClear] = []
-PLAY_DICT[situations.AttackGoal] = [plays.passing_tactic_play.PassPlay()]
-PLAY_DICT[situations.OffensiveScramble] = [plays.passing_tactic_play.PassPlay()]
-PLAY_DICT[situations.MidfieldScramble] = [plays.passing_tactic_play.PassPlay()]
-PLAY_DICT[situations.DefensiveScramble] = [plays.passing_tactic_play.PassPlay()]
+PLAY_DICT[situations.AttackGoal] = [plays.passing_tactic_play.PassPlay]
+PLAY_DICT[situations.OffensiveScramble] = [plays.passing_tactic_play.PassPlay]
+PLAY_DICT[situations.MidfieldScramble] = [plays.passing_tactic_play.PassPlay]
+PLAY_DICT[situations.DefensiveScramble] = [plays.passing_tactic_play.PassPlay]
 PLAY_DICT[situations.SaveBall] = []
 PLAY_DICT[situations.SaveShot] = []
-PLAY_DICT[situations.OffensivePileup] = [plays.passing_tactic_play.PassPlay()]
-PLAY_DICT[situations.MidfieldPileup] = [plays.passing_tactic_play.PassPlay()]
-PLAY_DICT[situations.DefensivePileup] = [plays.passing_tactic_play.PassPlay()]
-PLAY_DICT[situations.MidfieldDefendClear] = [plays.defensive_clear.DefensiveClear()]
+PLAY_DICT[situations.OffensivePileup] = [plays.passing_tactic_play.PassPlay]
+PLAY_DICT[situations.MidfieldPileup] = [plays.passing_tactic_play.PassPlay]
+PLAY_DICT[situations.DefensivePileup] = [plays.passing_tactic_play.PassPlay]
+PLAY_DICT[situations.MidfieldDefendClear] = [plays.defensive_clear.DefensiveClear]
 PLAY_DICT[situations.Shootout] = []
 PLAY_DICT[situations.DefendShootout] = []
 PLAY_DICT[situations.Penalty] = []
@@ -54,7 +54,7 @@ class BasicPlaySelector(situation.IPlaySelector):
                 if isinstance(self.curr_situation, sit):
                     plays_selection = possible_plays
             if plays_selection:
-                self.curr_play = plays_selection[0]
+                self.curr_play = plays_selection[0]()
                 return (self.curr_situation, self.curr_play)
             else:
                 return (self.curr_situation, self.curr_play)
