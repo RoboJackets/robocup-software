9b91c7753523063cf553dadc40f6382ea099d10c
Yudai Hatano <noah.bjyu@gmail.com>
2021-06-21 03:31:16 +0000

    changed kick speed

--- rj_gameplay/rj_gameplay/tactic/striker_tactic.py
+++ rj_gameplay/rj_gameplay/tactic/striker_tactic.py
@@ -71,7 +71,7 @@ class StrikerTactic(tactic.ITactic):
         self.capture = tactic.SkillEntry(capture.Capture())
         self.capture_cost = CaptureCost()
         self.shoot = tactic.SkillEntry(
-            shoot.Shoot(chip=False, kick_speed=40., target_point=self.target_point))
+            shoot.Shoot(chip=False, kick_speed=8., target_point=self.target_point))
 
     def compute_props(self):
         pass
@@ -92,7 +92,7 @@ class StrikerTactic(tactic.ITactic):
 
         if self.shoot.skill.is_done(world_state):
             self.shoot = tactic.SkillEntry(
-            shoot.Shoot(chip=False, kick_speed=40., target_point=self.target_point))
+            shoot.Shoot(chip=False, kick_speed=8., target_point=self.target_point))
 
 
         striker = [robot for robot in world_state.our_robots if robot.has_ball_sense]
