3cd2f5caf2748125e47c4d145a3d401ce2a31d08
Kevin Fu <kevin.fu842@gmail.com>
2021-06-21 00:27:58 -0400

    add defend restart, fill in more defend situations with basicDef

--- rj_gameplay/stp/basic_play_selector.py
+++ rj_gameplay/stp/basic_play_selector.py
@@ -4,20 +4,20 @@ import stp.rc as rc
 import rj_gameplay.situation.decision_tree.analyzer as analyzer
 import rj_gameplay.situation.decision_tree.plays as situations
 import rj_gameplay.play as plays
-from rj_gameplay.play import basic122, basic_defense, basic_scramble, defensive_clear, restart
+from rj_gameplay.play import basic122, basic_defense, basic_scramble, defensive_clear, defend_restart, restart
 from typing import Tuple, Dict
 
 #TODO: Put new plays into the dict properly
 #TODO: Create different dictionaries for different playbooks
 PLAY_DICT = {}
 PLAY_DICT[situations.Kickoff] = [basic_scramble.Scramble]
-PLAY_DICT[situations.DefendRestartOffensive] = []
-PLAY_DICT[situations.DefendRestartMidfield] = []
-PLAY_DICT[situations.DefendRestartDefensive] = []
+PLAY_DICT[situations.DefendRestartOffensive] = [defend_restart.DefendRestart]
+PLAY_DICT[situations.DefendRestartMidfield] = [defend_restart.DefendRestart]
+PLAY_DICT[situations.DefendRestartDefensive] = [defend_restart.DefendRestart]
 PLAY_DICT[situations.Clear] = [defensive_clear.DefensiveClear]
 PLAY_DICT[situations.DefendClear] = [defensive_clear.DefensiveClear]
 PLAY_DICT[situations.DefendGoal] = [basic_defense.BasicDefense]
-PLAY_DICT[situations.MidfieldClear] = []
+PLAY_DICT[situations.MidfieldClear] = [defensive_clear.DefensiveClear]
 PLAY_DICT[situations.AttackGoal] = [basic122.Basic122]
 PLAY_DICT[situations.OffensiveScramble] = [basic_scramble.Scramble]
 PLAY_DICT[situations.MidfieldScramble] = [basic_scramble.Scramble]
@@ -29,9 +29,9 @@ PLAY_DICT[situations.MidfieldPileup] = [basic_scramble.Scramble]
 PLAY_DICT[situations.DefensivePileup] = [basic_scramble.Scramble]
 PLAY_DICT[situations.MidfieldDefendClear] = [defensive_clear.DefensiveClear]
 PLAY_DICT[situations.Shootout] = []
-PLAY_DICT[situations.DefendShootout] = []
+PLAY_DICT[situations.DefendShootout] = [basic_defense.BasicDefense]
 PLAY_DICT[situations.Penalty] = []
-PLAY_DICT[situations.DefendPenalty] = []
+PLAY_DICT[situations.DefendPenalty] = [basic_defense.BasicDefense]
 PLAY_DICT[situations.OffensiveKick] = []
 PLAY_DICT[situations.DefensiveKick] = []
 PLAY_DICT[situations.MidfieldKick] = []
