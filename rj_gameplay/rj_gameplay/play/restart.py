c3d3473a967cda0c1135d0a1bce208f77f9e4bd6
Kevin Fu <kevin.fu842@gmail.com>
2021-06-20 23:41:18 -0400

    swap to four marks, one clear, one goalie

--- rj_gameplay/rj_gameplay/play/restart.py
+++ rj_gameplay/rj_gameplay/play/restart.py
@@ -1,7 +1,7 @@
 import stp.play as play
 import stp.tactic as tactic
 
-from rj_gameplay.tactic import pass_tactic, pass_seek, nmark_tactic, goalie_tactic
+from rj_gameplay.tactic import pass_tactic, pass_seek, nmark_tactic, goalie_tactic, clear_tactic
 import stp.skill as skill
 import stp.role as role
 from stp.role.assignment.naive import NaiveRoleAssignment
@@ -18,13 +18,17 @@ class RestartPlay(play.IPlay):
 
         # TODO: simplify tactic with list (see basic_defense.py)
         self.goalie_tactic = goalie_tactic.GoalieTactic()
+        self.clear_tactic = clear_tactic.Clear(np.array([0.0, 6.0]))
+        # TODO: make it pass
+        """
         self.pass_tactic = pass_tactic.Pass(
             self.target_point, pass_tactic.PasserCost(self.target_point),
             pass_tactic.PassToClosestReceiver(self.target_point))
         self.seek_tactic = pass_seek.Seek(
             self.target_point, pass_seek.restart_seek,
             pass_seek.SeekCost(self.target_point))
-        self.nmark_tactic = nmark_tactic.NMarkTactic(3)
+        """
+        self.nmark_tactic = nmark_tactic.NMarkTactic(4)
 
         self.role_assigner = NaiveRoleAssignment()
 
@@ -40,11 +44,11 @@ class RestartPlay(play.IPlay):
     ) -> Tuple[Dict[Type[tactic.SkillEntry], List[role.RoleRequest]], List[tactic.SkillEntry]]:
         # Get role requests from all tactics and put them into a dictionary
         role_requests: play.RoleRequests = {}
-        if not self.pass_tactic.is_done(world_state):
-            role_requests[self.pass_tactic] = self.pass_tactic.get_requests(world_state, None)
-            role_requests[self.seek_tactic] = self.seek_tactic.get_requests(world_state, None)
-            role_requests[self.nmark_tactic] = self.nmark_tactic.get_requests(world_state, None)
-            role_requests[self.goalie_tactic] = self.goalie_tactic.get_requests(world_state, None)
+        # role_requests[self.pass_tactic] = self.pass_tactic.get_requests(world_state, None)
+        # role_requests[self.seek_tactic] = self.seek_tactic.get_requests(world_state, None)
+        role_requests[self.clear_tactic] = self.clear_tactic.get_requests(world_state, None)
+        role_requests[self.nmark_tactic] = self.nmark_tactic.get_requests(world_state, None)
+        role_requests[self.goalie_tactic] = self.goalie_tactic.get_requests(world_state, None)
 
         # Flatten requests and use role assigner on them
         flat_requests = play.flatten_requests(role_requests)
@@ -54,17 +58,18 @@ class RestartPlay(play.IPlay):
         # Get list of all skills with assigned roles from tactics
         skill_dict = {}
         skills = []
-        if not self.pass_tactic.is_done(world_state):
-            skills = self.pass_tactic.tick(role_results[self.pass_tactic], world_state)
-            skills += self.seek_tactic.tick(role_results[self.seek_tactic], world_state)
-            skills += self.nmark_tactic.tick(role_results[self.nmark_tactic])
-            skills += self.goalie_tactic.tick(role_results[self.goalie_tactic])
-            skill_dict.update(role_results[self.pass_tactic])
-            skill_dict.update(role_results[self.seek_tactic])
-            skill_dict.update(role_results[self.nmark_tactic])
-            skill_dict.update(role_results[self.goalie_tactic])
+        skills = self.clear_tactic.tick(role_results[self.clear_tactic], world_state)
+        # skills = self.pass_tactic.tick(role_results[self.pass_tactic], world_state)
+        # skills += self.seek_tactic.tick(role_results[self.seek_tactic], world_state)
+        skills += self.nmark_tactic.tick(role_results[self.nmark_tactic])
+        skills += self.goalie_tactic.tick(role_results[self.goalie_tactic])
+        skill_dict.update(role_results[self.clear_tactic])
+        # skill_dict.update(role_results[self.pass_tactic])
+        # skill_dict.update(role_results[self.seek_tactic])
+        skill_dict.update(role_results[self.nmark_tactic])
+        skill_dict.update(role_results[self.goalie_tactic])
 
         return (skill_dict, skills)
 
     def is_done(self, world_state: rc.WorldState):
-        return self.seek_tactic.is_done(world_state)
+        return self.clear_tactic.is_done(world_state)
