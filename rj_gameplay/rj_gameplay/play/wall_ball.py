581dcffe085f0cca17d13c18ac835bdede848b84
HussainGynai <43356296+HussainGynai@users.noreply.github.com>
2021-09-14 20:12:33 -0400

    Update wall_ball.py

--- rj_gameplay/rj_gameplay/play/wall_ball.py
+++ rj_gameplay/rj_gameplay/play/wall_ball.py
@@ -35,7 +35,8 @@ class WallBall(play.IPlay):
         # Flatten requests and use role assigner on them
         flat_requests = play.flatten_requests(role_requests)
         flat_results = self.role_assigner.assign_roles(flat_requests, world_state, prev_results)
-
+        role_results = play.unflatten_results(flat_results)
+        
         # Get list of all skills with assigned roles from tactics
         skill_dict = {}
         skills = self.wall_tactic.tick(world_state,
