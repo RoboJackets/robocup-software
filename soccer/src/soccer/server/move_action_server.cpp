f5d7f5c52df360fe77a186b93d0e7267d37b7992
Alex Sohrab <kasra.sohrab@gmail.com>
2021-10-19 20:18:09 -0400

    broken cmake

--- soccer/src/soccer/server/move_action_server.cpp
+++ soccer/src/soccer/server/move_action_server.cpp
@@ -2,65 +2,56 @@
 #include <memory>
 #include <thread>
 
-#include "rj_robocup/action/move.hpp"
 #include <rj_common/utils.hpp>
 
+#include "rj_robocup/action/move.hpp"
+
 namespace server {
-	class MoveActionServer : public rclcpp::Node {
-		using Move = rj_robocup::action::Move;
-		using GoalHandleMove = rclcpp_action::ServerGoalHandle<Move>;
+class MoveActionServer : public rclcpp::Node {
+    using Move = rj_robocup::action::Move;
+    using GoalHandleMove = rclcpp_action::ServerGoalHandle<Move>;
 
-		explicit MoveActionServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions()) : Node("move_action_server", options) {
-			using namespace std::placeholders;
-			this->action_server_ = rclcpp_action::create_server<Move>(
-					this, "move", 
-					std::bind(&MoveActionServer::handle_goal, this, _1, _2),
-					std::bind(&MoveActionServer::handle_cancel, this, _1),
-					std::bind(&MoveActionServer::handle_accepted, this, _1),
-				);
-		}
+    explicit MoveActionServer(const rclcpp::NodeOptions& options = rclcpp::NodeOptions())
+        : Node("move_action_server", options) {
+        using namespace std::placeholders;
+        this->action_server_ = rclcpp_action::create_server<Move>(
+            this, "move", std::bind(&MoveActionServer::handle_goal, this, _1, _2),
+            std::bind(&MoveActionServer::handle_cancel, this, _1),
+            std::bind(&MoveActionServer::handle_accepted, this, _1), );
+    }
 
-		private:
-			rclcpp_action::Server<Move>::SharedPtr action_server_;
-			rclcpp_action::GoalResponse handle_goal(
-					const rclcpp_action::GoalUUID & uuid,
-					std::shared_ptr<const Move::Goal> goal)
-			{
-				cout << "handle goal reached" << endl;
-				(void)uuid;
-				return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
-			}
+private:
+    rclcpp_action::Server<Move>::SharedPtr action_server_;
+    rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID& uuid,
+                                            std::shared_ptr<const Move::Goal> goal) {
+        cout << "handle goal reached" << endl;
+        (void)uuid;
+        return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+    }
 
-			rclcpp_action::CancelResponse handle_cancel(
-					const std::shared_ptr<GoalHandleMove> goal_handle)
-			{
-				cout << "cancel reached" << endl;
-				(void)goal_handle;
-				return rclcpp_action::CancelResponse::ACCEPT;
-			}
+    rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<GoalHandleMove> goal_handle) {
+        cout << "cancel reached" << endl;
+        (void)goal_handle;
+        return rclcpp_action::CancelResponse::ACCEPT;
+    }
 
-			void handle_accepted(
-					const std::shared_ptr<GoalHandleMove> goal_handle)
-			{
-				using namespace std::placeholders;
-				cout << "accepted reached" << endl;
-				std::thread{std::bind(&MoveActionServer::execute, this, _1), goal_handle}.detach();
-			}
+    void handle_accepted(const std::shared_ptr<GoalHandleMove> goal_handle) {
+        using namespace std::placeholders;
+        cout << "accepted reached" << endl;
+        std::thread{std::bind(&MoveActionServer::execute, this, _1), goal_handle}.detach();
+    }
 
-			void execute(const std::shared_ptr<GoalHandleMove> goal_handle) 
-			{
-				cout << "executing" << endl;
-				const auto goal = goal_handle->get_goal();
+    void execute(const std::shared_ptr<GoalHandleMove> goal_handle) {
+        cout << "executing" << endl;
+        const auto goal = goal_handle->get_goal();
 
-				auto feedback = std::make_shared<Move::Feedback>();
-				auto result = std::make_shared<Move::Result>();
+        auto feedback = std::make_shared<Move::Feedback>();
+        auto result = std::make_shared<Move::Result>();
 
-				result->is_done = true;
-				goal_handle->succeed(result);
-			}
-
-
-	}
+        result->is_done = true;
+        goal_handle->succeed(result);
+    }
 }
+}  // namespace server
 
 RCLCPP_COMPONENTS_REGISTER_NODE(rj_robocup::MoveActionServer)
