d935953aed369d10d53935c300c1eea8f5f7a530
kasohrab <kasra.sohrab@gmail.com>
2021-10-24 20:13:58 -0400

    now it compiles

--- soccer/src/soccer/server/move_action_server.cpp
+++ soccer/src/soccer/server/move_action_server.cpp
@@ -2,18 +2,18 @@
 #include <memory>
 #include <thread>
 
-# ros2 action includes
+// ros2 action includes
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
 
-# rj includes
+// rj includes
 #include <rj_common/utils.hpp>
 #include <rj_msgs/action/move.hpp>
 
 namespace server {
 class MoveActionServer : public rclcpp::Node {
-    using Move = rj_robocup::action::Move;
+    using Move = rj_msgs::action::Move;
     using GoalHandleMove = rclcpp_action::ServerGoalHandle<Move>;
 
     explicit MoveActionServer(const rclcpp::NodeOptions& options = rclcpp::NodeOptions())
@@ -22,32 +22,32 @@ class MoveActionServer : public rclcpp::Node {
         this->action_server_ = rclcpp_action::create_server<Move>(
             this, "move", std::bind(&MoveActionServer::handle_goal, this, _1, _2),
             std::bind(&MoveActionServer::handle_cancel, this, _1),
-            std::bind(&MoveActionServer::handle_accepted, this, _1), );
+            std::bind(&MoveActionServer::handle_accepted, this, _1));
     }
 
 private:
     rclcpp_action::Server<Move>::SharedPtr action_server_;
     rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID& uuid,
                                             std::shared_ptr<const Move::Goal> goal) {
-        cout << "handle goal reached" << endl;
+        std::cout << "handle goal reached" << std::endl;
         (void)uuid;
         return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
     }
 
     rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<GoalHandleMove> goal_handle) {
-        cout << "cancel reached" << endl;
+        std::cout << "cancel reached" << std::endl;
         (void)goal_handle;
         return rclcpp_action::CancelResponse::ACCEPT;
     }
 
     void handle_accepted(const std::shared_ptr<GoalHandleMove> goal_handle) {
         using namespace std::placeholders;
-        cout << "accepted reached" << endl;
+        std::cout << "accepted reached" << std::endl;
         std::thread{std::bind(&MoveActionServer::execute, this, _1), goal_handle}.detach();
     }
 
     void execute(const std::shared_ptr<GoalHandleMove> goal_handle) {
-        cout << "executing" << endl;
+        std::cout << "executing" << std::endl;
         const auto goal = goal_handle->get_goal();
 
         auto feedback = std::make_shared<Move::Feedback>();
@@ -56,7 +56,8 @@ private:
         result->is_done = true;
         goal_handle->succeed(result);
     }
-}
+}; // class 
 }  // namespace server
 
-RCLCPP_COMPONENTS_REGISTER_NODE(rj_robocup::MoveActionServer)
+// TODO : Use this?
+// RCLCPP_COMPONENTS_REGISTER_NODE(rj_robocup::MoveActionServer)
