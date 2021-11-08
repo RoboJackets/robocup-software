84ee9189d2377a49495ebc981d1a02ff3926b1ea
Yudai Hatano <53668296+Yudai-8-om@users.noreply.github.com>
2021-11-07 19:33:09 -0500

    Update rj_gameplay/stp/role/assignment/naive.py
    Co-authored-by: Kevin Fu <30275369+kfu02@users.noreply.github.com>

--- rj_gameplay/stp/role/assignment/naive.py
+++ rj_gameplay/stp/role/assignment/naive.py
@@ -113,7 +113,7 @@ class NaiveRoleAssignment(assignment.IRoleAssignment):
             # Throw an exception if the returned cost is not finite.
             if not isfinite(unassigned_cost):
                 raise ValueError(
-                    "Got a non-finite cost ({}) for request {} and unassinged robot".format(
+                    "Got a non-finite cost ({}) for request {} and unassigned robot {}".format(
                         cost, request, robot
                     )
                 )
