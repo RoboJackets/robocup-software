5820c83389d746d831a140418e3359ef4ddf1cbe
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-16 19:58:30 +0800

    Delete soccer directory

--- soccer/src/soccer/referee/internal_referee_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <rclcpp/rclcpp.hpp>
-
-#include "internal_referee.hpp"
-
-int main(int argc, char** argv) {
-    rclcpp::init(argc, argv);
-    auto ref = std::make_shared<referee::InternalReferee>();
-    rclcpp::spin(ref);
-}
