1de9fd00805a6680175e26c21c61cf43c62cd844
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-16 19:23:40 +0800

    Delete rj_common directory

--- rj_common/src/referee_enums.cpp
+++
@@ -1,9 +0,0 @@
-#include "rj_common/referee_enums.hpp"
-
-namespace referee_module_enums {
-
-std::string string_from_stage(Stage s) { return SSL_Referee_Stage_Name(s); }
-
-std::string string_from_command(Command c) { return SSL_Referee_Command_Name(c); }
-
-}  // namespace referee_module_enums
