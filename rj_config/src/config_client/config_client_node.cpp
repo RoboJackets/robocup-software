acb0160784976509c56a2768f5ca9d194755d9fc
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-16 19:24:23 +0800

    Delete rj_config directory

--- rj_config/src/config_client/config_client_node.cpp
+++
@@ -1,5 +0,0 @@
-#include <config_client/config_client_node.hpp>
-
-namespace config_client {
-ConfigClientNode::ConfigClientNode(const std::string& name) : Node{name}, config_client_(this) {}
-}  // namespace config_client
