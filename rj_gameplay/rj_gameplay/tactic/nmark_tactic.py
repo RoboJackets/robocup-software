e4e1765cd45012d16774c4d3bfbed70f543e5db9
Nathaniel Wert <N8.Wert.B@gmail.com>
2022-01-10 12:42:30 -0500

    fixed nmark_tactic
    changed role.RoleRequests back to role.RoleRequest

--- rj_gameplay/rj_gameplay/tactic/nmark_tactic.py
+++ rj_gameplay/rj_gameplay/tactic/nmark_tactic.py
@@ -106,7 +106,7 @@ class NMarkTactic(tactic.ITactic):
 
     def get_requests(
         self, world_state: rc.WorldState, props
-    ) -> Dict[tactic.SkillEntry[Any],List[role.RoleRequests]]:
+    ) -> Dict[tactic.SkillEntry[Any], List[role.RoleRequest]]:
         """
         :return: role request for n markers
         """
