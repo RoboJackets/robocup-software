7a5ac2b54e823a1c41f1462fae1492fb119abd15
Nathaniel Wert <n8.wert.b@gmail.com>
2022-01-18 21:06:46 -0500

    Changed a type I added by accident

--- rj_gameplay/rj_gameplay/skill/capture.py
+++ rj_gameplay/rj_gameplay/skill/capture.py
@@ -43,7 +43,7 @@ class Capture(skill.Skill):
         ball_pos = world_state.ball.pos
         #robot_pos = world_state.our_robots[self.robot.id].pose[0:2]
 
-        # this doesn'tdetriments of multiple hard drive partitions work:
+        # this doesn't work:
         robot_pos = self.robot.pos
         # because self.robot is passed on init and never updated
 
