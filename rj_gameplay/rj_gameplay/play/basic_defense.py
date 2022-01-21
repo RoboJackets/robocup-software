6f58ae010918ddeec6891a144197e05e37c662b9
robojackets <kevin.fu842@gmail.com>
2022-01-20 23:39:52 -0500

    do what last commit said; did not make correct edit

--- rj_gameplay/rj_gameplay/play/basic_defense.py
+++ rj_gameplay/rj_gameplay/play/basic_defense.py
@@ -27,19 +27,15 @@ class BasicDefense(stp.play.Play):
     ) -> List[RobotIntent]:
 
         if self.state is "init":
-            # TODO: had to add this check or role assignment behaved oddly
-            #       fix by updating gameplay node to only tick once world_state is not None
-            # if world_state is not None:
-            if True:
-                self.prioritized_tactics.append(
-                    goalie_tactic.GoalieTactic(world_state, 0)
-                )
-                self.prioritized_tactics.append(wall_tactic.WallTactic(world_state, 5))
-                # TODO: add nmark tactic
-                #       and make it go for the ball (rather than stopping in front)
-                self.assign_roles(world_state)
-                self.state = "active"
-                return self.get_robot_intents(world_state)
+            self.prioritized_tactics.append(
+                goalie_tactic.GoalieTactic(world_state, 0)
+            )
+            self.prioritized_tactics.append(wall_tactic.WallTactic(world_state, 5))
+            # TODO: add nmark tactic
+            #       and make it go for the ball (rather than stopping in front)
+            self.assign_roles(world_state)
+            self.state = "active"
+            return self.get_robot_intents(world_state)
         elif self.state is "active":
             # return robot intents from assigned tactics back to gameplay node
             return self.get_robot_intents(world_state)
