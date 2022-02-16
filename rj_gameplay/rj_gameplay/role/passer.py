87534c89f71282fe8cb2dec88ac519d826456516
N8BWert <n8.wert.b@gmail.com>
2022-02-15 20:52:08 -0500

    when possessing ball robot finds best pass and shot

--- rj_gameplay/rj_gameplay/role/passer.py
+++ rj_gameplay/rj_gameplay/role/passer.py
@@ -3,11 +3,12 @@ from lzma import is_check_supported
 
 import stp.role
 import stp.rc
+from stp.rc import WorldState
 
 from typing import List, TypedDict, Tuple
 
 from rj_gameplay.skill import receive, pivot_kick  # , line_kick
-import rj_gameplay.rj_gameplay.gameplay_node as gameplay_node
+import rj_gameplay.gameplay_node as gameplay_node
 
 from rj_msgs.msg import RobotIntent
 
@@ -37,14 +38,14 @@ class BallReleaseState(Enum):
 
 
 # Constant for how far a robot can be from the pass line before stopping the pass from occuring
-PASS_CUTOFF = 0.1
-# Constant for how far in front of (but equally distant) a point this robot will investigate when
-# checking for leading passes
-THETA_LEADING_OFFSET = 0.1
+PASS_DISTANCE_CUTOFF = 0.1
 # Constant for how far from the path of the ball the closest enemy robot can be
-SHOOT_COVERAGE_CUTOFF = 0.1
+SHOOT_DISTANCE_CUTOFF = 0.1
 # Number of divisions for finding the best shot on goal
 NUM_DIVISIONS = 15
+# Aggressiveness hyperparameter alters the distance robots are from the robot the farther down the field a robot is (basically aggressiveness weights how
+# much this robot wants to pass down the field)
+AGGRESSIVENESS = 1.0001
 
 class PasserRole(stp.role.Role):
     def __init__(self, robot: stp.rc.Robot) -> None:
@@ -62,8 +63,9 @@ class PasserRole(stp.role.Role):
         # BEGIN dynamic tendency variables #
 
         ### TODO: ADD LEFT AND RIGHT GOAL POSTS INTO FILE ###
-        # left_goal_post = 
-        # right_goal_post = 
+        self.left_goal_post = None
+        
+        self.right_goal_post = None
         
         # floating point value [0,1] representing how open this robot is, i.e. how much coverage
         # is the other team placing on this robot and how "at risk" is the ball towards being stolen.
@@ -73,7 +75,7 @@ class PasserRole(stp.role.Role):
         # floating point value [0,1] representing how much of the percieved goal is being covered by the
         # other teams goalie and players.  Basically, in the line of sight of this robot what percent of the goal is
         # being occupied by the opposing teams goalie (and players) + a buffer of _____%.
-        self.shot_on_goal = tuple(np.zeros(0, 0), 0)
+        self.shot_on_goal = (np.zeros((1, 2)), 0)
 
         self.min_rob_dist_pass = []
 
@@ -99,24 +101,35 @@ class PasserRole(stp.role.Role):
          - on pass signal from Tactic: pivot_kick to point, let receiver get ball, done
         """
 
-        our_vecs, their_vecs = self.calc_displacement_vecs(world_state)
-        self.min_rob_dist_pass = self.calc_min_distance_from_lines(our_vecs, their_vecs)
+        if self.left_goal_post is None:
+            self.left_goal_post = world_state.field.their_goal_loc
+            self.left_goal_post[0] -= world_state.field.goal_width_m / 2
+            self.right_goal_post = world_state.field.their_goal_loc
+            self.right_goal_post[0] += world_state.field.goal_width_m / 2
 
         intent = None
 
-        if self._state == "init":
+        if self._state == State.initializing:
             self.receive_skill = receive.Receive(robot=self.robot)
             intent = self.receive_skill.tick(world_state)
-            self._state = "receiving"
-        elif self._state == "receiving":
+            self._state = State.receiving
+        elif self._state == State.receiving:
             intent = self.receive_skill.tick(world_state)
             if self.receive_skill.is_done(world_state):
-                self._state = "pass_ready"
-        elif self._state == "pass_ready":
+                self._state = State.possessing
+        elif self._state == State.possessing:
+            # Initialize values
+            our_vecs, their_vecs = self.calc_displacement_vecs(world_state)
+            self.min_rob_dist_pass = self.calc_min_distance_from_lines(world_state, our_vecs, their_vecs)
+            self.shot_on_goal = self.best_goal_shot(world_state, NUM_DIVISIONS, self.left_goal_post, self.right_goal_post)
+
             # TODO: dribble until the receiver is ready
-            pass
+            
+            # Hanlde State Transitions #
+            if self.shot_on_goal[1] < SHOOT_DISTANCE_CUTOFF:
+                self._state = State.shooting
         # this state transition is done by the PassTactic, which is not canonical FSM
-        elif self._state == "init_execute_pass":
+        elif self._state == State.passing:
             # TODO: make these params configurable
             self.pivot_kick_skill = pivot_kick.PivotKick(
                 robot=self.robot,
@@ -124,13 +137,15 @@ class PasserRole(stp.role.Role):
                 chip=False,
                 kick_speed=4.0,  # TODO: adjust based on dist from target_point
             )
-            self._state = "execute_pass"
-        elif self._state == "execute_pass":
+            self._ball_state = BallReleaseState.executing
+        elif self._ball_state == BallReleaseState.executing:
             intent = self.pivot_kick_skill.tick(world_state)
 
             if self.pivot_kick_skill.is_done(world_state):
-                self._state = "kick_done"
+                self._ball_state = BallReleaseState.finalized
                 # end FSM
+        elif self._state == State.shooting:
+            pass
 
         return intent
 
@@ -140,7 +155,7 @@ class PasserRole(stp.role.Role):
     def calc_displacement_vecs(self, world_state: stp.rc.WorldState) -> Tuple[np.ndarray]:
         """
         Calculates the vector between the robot assigned the passer role and the other robots on the field.  For the case where the robot is checking against
-        it's own position, the vector given is (0, 0)
+        it's own position, the vector given is (0, 0).  The goalie is also given the vector (0, 0)
 
         :param world_state: the world_state representation we have each tick
         :type world_state: stp.rc.WorldState
@@ -149,16 +164,16 @@ class PasserRole(stp.role.Role):
         and the other teams robots
         :type return: 2 np.ndarray of dimension [(NUM_ROBOTS / 2), 3]
         """
-        our_vecs = np.zeros(gameplay_node.NUM_ROBOTS / 2, 3)
-        for i in range(0, gameplay_node.NUM_ROBOTS / 2):
-            if (world_state.our_robots[i].id != self.robot.id):
+        our_vecs = np.zeros((gameplay_node.NUM_ROBOTS // 2, 2))
+        for i in range(0, gameplay_node.NUM_ROBOTS // 2):
+            if (world_state.our_robots[i].id != self.robot.id or world_state.our_robots[i].id == world_state.goalie_id):
                 our_vecs[i,0:2] = world_state.our_robots[i].pose[0:2] - self.robot.pose[0:2]
-        their_vecs = np.zeros(gameplay_node.NUM_ROBOTS / 2, 2)
-        for j in range(0, gameplay_node.NUM_ROBOTS / 2):
+        their_vecs = np.zeros((gameplay_node.NUM_ROBOTS // 2, 2))
+        for j in range(0, gameplay_node.NUM_ROBOTS // 2):
             their_vecs[i,0:2] = world_state.their_robots[i].pose[0:2] - self.robot.pose[0:2]
         return our_vecs, their_vecs
 
-    def calc_min_distance_from_lines(self, our_vecs: np.ndarray, their_vecs: np.ndarray) -> np.ndarray:
+    def calc_min_distance_from_lines(self, world_state: WorldState, our_vecs: np.ndarray, their_vecs: np.ndarray) -> np.ndarray:
         """
         Creates a list of the distance from the closest robot to the path the ball will have to take to make a direct pass to each teammate.
 
@@ -171,11 +186,11 @@ class PasserRole(stp.role.Role):
         :return list of distance from robot closest to the linear path of the ball to the path the ball will have to take to make a direct path.
         :type return: np.ndarray of size [(our_robots):1] (distance)
         """
-        final_distances = np.zeros(1, gameplay_node.NUM_ROBOTS / 2)
-        for i in range(0, gameplay_node.NUM_ROBOTS / 2):
+        final_distances = np.zeros((1, gameplay_node.NUM_ROBOTS // 2))
+        for i in range(0, gameplay_node.NUM_ROBOTS // 2):
             distances = []
             for our_vec in our_vecs:
-                if our_vec == np.zeros(1, 2) or our_vecs[i] == our_vec:
+                if our_vec.all() == np.zeros((1, 2)).all() or our_vecs[i].all() == our_vec.all():
                     continue
                 dot_over_mag = np.dot(our_vecs[i], our_vec) / (np.linalg.norm(our_vecs[i]) * np.linalg.norm(our_vec))
                 if dot_over_mag < 0:
@@ -186,7 +201,7 @@ class PasserRole(stp.role.Role):
                 if dot_over_mag < 0:
                     continue
                 distances.append(np.linalg.norm(np.cross(their_vec, our_vecs[i])) / np.linalg.norm(our_vecs[i]))
-            final_distances[i] = min(distances)
+            final_distances[0, i] = min(distances) * (world_state.our_robots[i].pose[1] * AGGRESSIVENESS)
         return final_distances
 
     def best_goal_shot(self, world_state: stp.rc.WorldState, divisions: int, left_post: np.ndarray, right_post: np.ndarray) -> Tuple[np.ndarray, float]:
@@ -206,7 +221,7 @@ class PasserRole(stp.role.Role):
         :type return: Tuple[np.ndarray [1, 2] (x, y), float]
         """
         largest_gap = 0
-        best_shot_location = np.zeros(1, 2)
+        best_shot_location = np.zeros((1, 2))
 
         front_robs = []
         for robot in world_state.robots:
@@ -229,7 +244,7 @@ class PasserRole(stp.role.Role):
                     closest_index = index
                 index += 1
             
-            distance = np.linalg.norm(np.cross(self.robot.pose[0:2], (front_robs[closest_index] - self.robot.pose[0:2]))) / np.linalg.norm(self.robot.pose[0:2])
+            distance = np.linalg.norm(np.cross(self.robot.pose[0:2], (front_robs[closest_index].pose[0:2] - self.robot.pose[0:2]))) / np.linalg.norm(self.robot.pose[0:2])
             if distance > largest_gap:
                 largest_gap = distance
                 best_shot_location = shot_position
