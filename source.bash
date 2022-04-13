ec64116b05925144c5d7fe983b3ad7009fcaca9e
kfu02 <kevin.fu842@gmail.com>
2022-04-12 21:13:08 -0400

    fix source.bash to be more general

--- source.bash
+++ source.bash
@@ -1,10 +1,10 @@
-if [ $SHELL = '/usr/bin/bash' ]; then
+if [[ $SHELL == *"bash"* ]]; then
     echo "bash detected, sourcing bash"
     source /opt/ros/foxy/setup.bash
     source install/setup.bash
 fi
 
-if [ $SHELL = '/usr/bin/zsh' ]; then
+if [[ $SHELL == *"zsh"* ]]; then
     echo "zsh detected, sourcing zsh"
     source /opt/ros/foxy/setup.zsh
     source install/setup.zsh
