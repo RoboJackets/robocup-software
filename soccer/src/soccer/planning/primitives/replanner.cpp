16e683678b1c1848cea3cf288b23f5394645440f
kfu02 <kevin.fu842@gmail.com>
2022-06-12 15:00:11 -0400

    reenable dyn obs

--- soccer/src/soccer/planning/primitives/replanner.cpp
+++ soccer/src/soccer/planning/primitives/replanner.cpp
@@ -119,13 +119,11 @@ Trajectory Replanner::create_plan(Replanner::PlanParams params, Trajectory previ
     RJ::Time hit_time = RJ::Time::max();
 
     // Use short-circuiting to only check dynamic trajectories if necessary.
-    // TODO: reenable partial replanning
     bool should_partial_replan =
-        trajectory_hits_static(previous_trajectory, params.static_obstacles, start_time, &hit_time);
-    /* || trajectory_hits_dynamic(previous_trajectory, params.dynamic_obstacles, start_time,
-     * nullptr,
-     */
-    /*                         &hit_time); */
+        trajectory_hits_static(previous_trajectory, params.static_obstacles, start_time,
+                               &hit_time) ||
+        trajectory_hits_dynamic(previous_trajectory, params.dynamic_obstacles, start_time, nullptr,
+                                &hit_time);
 
     if (should_partial_replan) {
         if (hit_time - start_time < partial_replan_lead_time() * 2) {
