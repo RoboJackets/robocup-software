75b9cc648ac0dc63a92919fba11ac2cc1659c225
Prabhanjan Nayak <prabhanjanmnayak@gmail.com>
2022-06-28 20:57:38 -0400

    added runner role and tactic

---
+++ rj_gameplay/rj_gameplay/role/runner_role.py
@@ -0,0 +1,53 @@
+import stp.rc
+import stp.role
+from rj_msgs.msg import RobotIntent
+
+from rj_gameplay.skill import move
+
+
+class Runner(stp.role.Role):
+    """
+    In many ways, this role behaves similar to the DumbMove Role. It will move to the target point while facing the point. This would act as a wrapper to the move skill.
+
+    :param robot: Robot that will be performing this role
+    :type robot: stp.rc.Robot
+    :param target_point: the target point that the robot will attempt to move to
+    :type target_point: (, optional)
+    """
+
+    def __init__(self, robot: stp.rc.Robot, target_point) -> None:
+        super().__init__(robot)
+
+        self.target_point = target_point
+        self.face_point = target_point
+
+        self.move_skill = None
+
+    def tick(
+        self, world_state: stp.rc.WorldState, target_point=None, face_point=None
+    ) -> RobotIntent:
+
+        skill_needs_update = self.move_skill is None
+
+        if target_point is not None:
+            self.target_point = target_point
+            skill_needs_update = True
+
+        if face_point is not None:
+            self.face_point = face_point
+            skill_needs_update = True
+
+        # create skill with correct target & face_point
+        if skill_needs_update:
+            self.move_skill = move.Move(
+                robot=self.robot,
+                target_point=self.target_point,
+                face_point=self.face_point,
+            )
+
+        intent = self.move_skill.tick(world_state)
+        return intent
+
+    def is_done(self, world_state) -> bool:
+        if self.move_skill is not None:
+            return self.move_skill.is_done(world_state)
---
+++ rj_gameplay/rj_gameplay/tactic/runner_tactic.py
@@ -0,0 +1,52 @@
+from typing import List, Tuple
+
+import numpy as np
+import stp
+from rj_msgs.msg import RobotIntent
+
+from rj_gameplay.role import runner_role
+
+
+class RunnerTactic(stp.tactic.Tactic):
+    """
+    In many ways, this tactic behaves similar to the DumbMove Tactic. Given a list of points, it will move to each of them. Since this is a tutorial tactic, we want only Robot 1 assigned to this tactic.
+
+    :param world_state: the current state of gameplay
+    :type world_state: stp.rc.WorldState
+    :param runner_id: the robot id that will complete this task on init
+    :type runner_id: int
+    :param pts: an array of points that the robot will move to
+    :type pts: np.ndarray
+    """
+
+    def __init__(self, world_state: stp.rc.WorldState, runner_id: int, pts: np.ndarray):
+        # special case where we want robot 1 to be the runner, from init
+        super().__init__(world_state)
+        self.move_points = pts
+        for pt in self.move_points:
+            self._role_requests.append(
+                (stp.role.cost.PickRobotById(runner_id), runner_role.Runner)
+            )
+
+    def init_roles(self, world_state: stp.rc.WorldState) -> None:
+        # only has one role, but it's easier to copy-paste the structure
+        for i, robot in enumerate(self.assigned_robots):
+            role = self._role_requests[i][1]
+            pt = self.move_points[i]
+            if role is runner_role.Runner:
+                self.assigned_roles.append(role(robot, pt))
+
+    def tick(
+        self, world_state: stp.rc.WorldState
+    ) -> List[Tuple[int, RobotIntent]]:  # (id, intent)
+
+        # assumes all roles requested are filled, because tactic is one unit
+        if len(self.assigned_roles) != len(self._role_requests):
+            self.init_roles(world_state)
+
+        # only has one role request, but it's easier to copy-paste the structure
+        return [(role.robot.id, role.tick(world_state)) for role in self.assigned_roles]
+
+    def is_done(self, world_state: stp.rc.WorldState) -> bool:
+        # special case: we know the only role is Runner, so we borrow that is_done()
+        return self.assigned_roles[0].is_done(world_state)
