dbb8981edf0fb31b8c89ffb3fc6ee77cb62d3a92
Sumedh Garimella <10521124+hdemusg@users.noreply.github.com>
2022-07-19 19:20:15 -0400

    Updated PenaltyDefense
    I realized that the robots should be a bit more dispersed along the line to prepare to clear the penalty in the event of a save.

--- rj_gameplay/rj_gameplay/play/penalty_defense.py
+++ rj_gameplay/rj_gameplay/play/penalty_defense.py
@@ -20,6 +20,7 @@ class PenaltyDefense(stp.play.Play):
     """This is a play that represents the lineup procedure in the event that an opposing team has been awarded a penalty kick. 
        Per rule 3 in Section 5.3.5 of the RoboCup SSL Playbook (https://robocup-ssl.github.io/ssl-rules/sslrules.html#_penalty_kick),
        "Throughout the penalty kick procedure, all other robots have to be 1m behind the ball such that they do not interfere the penalty kick procedure".
+       As such, this play assigns one goalkeeper robot and lines up the remaining robots at least 1m behind the ball to prepare for the penalty. 
 
     :param state: An enum value that represents the current state of the play.
     :type state: class:`State`
@@ -39,8 +40,8 @@ class PenaltyDefense(stp.play.Play):
             self.prioritized_tactics.append(goalie_tactic.GoalieTactic(world_state, 0))
             num_liners = len(world_state.our_visible_robots) - 1
             # 7.5 meters to our goal (designed so that 5 robots can be at least 1 meter behind the penalty line before the penalty is taken)
-            start_pt = np.array([-1.8, 7.5])
-            end_pt = np.array([1.8, 7.5])
+            start_pt = np.array([-2.1, 7.25])
+            end_pt = np.array([2.1, 7.25])
             self.prioritized_tactics.append(
                 line_tactic.LineTactic(world_state, num_liners, start_pt, end_pt)
             )
