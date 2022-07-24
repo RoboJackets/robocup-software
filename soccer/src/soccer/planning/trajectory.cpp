78ee4561d35a97d7ab52a9194a216d37ff687894
Kevin Fu <kevin.fu842@gmail.com>
2022-07-24 17:45:51 -0400

    rm kyle debug print

--- soccer/src/soccer/planning/trajectory.cpp
+++ soccer/src/soccer/planning/trajectory.cpp
@@ -25,7 +25,6 @@ Trajectory::Trajectory(Trajectory a, const Trajectory& b) {
     if (!a_end.position().near_point(b_begin.position(), 1e-6) ||
         !a_end.linear_velocity().near_point(b_begin.linear_velocity(), 1e-6) ||
         a_end.stamp != b_begin.stamp) {
-        SPDLOG_WARN("FUCKING RAAAAWWWWWW {} {}", a_end.pose.heading(), b_begin.pose.heading());
         a_end = b.first();
         // throw std::invalid_argument(
         // "Cannot splice trajectories a and b, where a.last() != b.first()");
