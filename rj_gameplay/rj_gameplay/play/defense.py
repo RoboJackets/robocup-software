d2d6ddd629c599ae11876543e7192ee2793a21f5
Ryan <rkbard@gmail.com>
2022-09-25 20:00:55 -0400

    tutorial-step-2

--- rj_gameplay/rj_gameplay/play/defense.py
+++ rj_gameplay/rj_gameplay/play/defense.py
@@ -35,6 +35,7 @@ class Defense(stp.play.Play):
         if self._state == State.INIT:
             self.prioritized_tactics.append(goalie_tactic.GoalieTactic(world_state, 0))
             num_wallers = min(4, len(world_state.our_visible_robots) - 1)
+            #bruh
             self.prioritized_tactics.append(
                 wall_tactic.WallTactic(world_state, num_wallers)
             )
