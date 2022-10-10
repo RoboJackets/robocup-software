36a52a0bc7e0cac43d25144ccc342d0cf1e155ed
Kevin Fu <30275369+kfu02@users.noreply.github.com>
2022-10-09 21:17:03 -0400

    restore two team control (#1937)
    * make network radio port respond to ROS param
    
    * rm unused radio_node.cpp
    
    * make soccer.launch.py take in separate yamls for network/other params
    
    - separates sim/real networking param files from other params
    - adds new alt param for multiple broadcast server_ports
    
    * TODOs and make pretty
    
    * started real2play
    
    * replace broken real2play launch file with bash script
    
    * rm excessive extra .yamls, replace with server_port param
     - also sets make real2play = launch bash script
    
    * cleanup
    
    * rm kNetworkRadioServerPort
    
    * replace sim2play.launch with bash, like real2play
    
    * Revert "replace sim2play.launch with bash, like real2play"
    
    This reverts commit 8743c8dbf1c517cbfaeb649a86bffb43be5a888d.
    
    * mv param to sim radio where it is used
    
    * use param_config_yaml everywhere
    
    * do param config less janky, revive bash script sim2play
    
    * add comments to launch
    
    * add some more comments
    
    * comply with GH Actions style flags
    
    * clarify comment
    
    * add team name as launch flag, make run_sim flag control yaml used
    
    * bash script doesn't really work, so add domain id to makefile
    
    * fix launching sim
    
    * make pretty-lines
    
    Co-authored-by: RoboCup Field Comp <fake-field-comp-email@not-real.com>
    Co-authored-by: kasohrab <kasra.sohrab@gmail.com>

---
+++ launch/real2play.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script runs two instances of soccer at once, with slightly different
+# network configs, to allow for simulating two teams at once on the field.
+#
+# It is equivalent to calling the following in separate terminals:
+#
+# ROS_DOMAIN_ID=0 make run-real-ex
+# ROS_DOMAIN_ID=1 make run-alt-real
+#
+# One CTRL-C will terminate both processes.
+# See: https://stackoverflow.com/questions/3004811/how-do-you-run-multiple-programs-in-parallel-from-a-bash-script
+
+(trap 'kill 0' SIGINT; (ROS_DOMAIN_ID=0 make run-real-ex) & (ROS_DOMAIN_ID=1 make run-alt-real))
