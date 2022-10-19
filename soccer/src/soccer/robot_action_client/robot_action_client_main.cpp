59e5148e769c7bba5d018c1d8896606a0ec6145b
Kevin Fu <30275369+kfu02@users.noreply.github.com>
2022-10-18 23:43:23 -0400

    rm debug print

--- soccer/src/soccer/robot_action_client/robot_action_client_main.cpp
+++ soccer/src/soccer/robot_action_client/robot_action_client_main.cpp
@@ -5,7 +5,6 @@ int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
     rj_utils::set_spdlog_default_ros2("processor");
 
-    SPDLOG_ERROR("Robot action client is being spun");
     auto robot_action_client_node = std::make_shared<robot_action_client::RobotActionClient>();
     start_global_param_provider(robot_action_client_node.get(), kGlobalParamServerNode);
     rclcpp::spin(robot_action_client_node);
