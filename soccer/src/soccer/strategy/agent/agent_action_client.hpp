99ceb2c7fd90f9ccaf194aa03a12862fbcc40647
N8BWert <n8.wert.b@gmail.com>
2022-11-08 20:10:52 -0500

    begin writing agent communication

--- soccer/src/soccer/strategy/agent/agent_action_client.hpp
+++ soccer/src/soccer/strategy/agent/agent_action_client.hpp
@@ -18,6 +18,7 @@
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rj_msgs/action/robot_move.hpp"
 #include "robot_intent.hpp"
+#include "rj_msgs/srv/AgentCommunication.srv"
 #include "strategy/agent/position/defense.hpp"
 #include "strategy/agent/position/goalie.hpp"
 #include "strategy/agent/position/offense.hpp"
@@ -46,6 +47,12 @@ private:
     rclcpp::Subscription<rj_msgs::msg::WorldState>::SharedPtr world_state_sub_;
     rclcpp::Subscription<rj_msgs::msg::CoachState>::SharedPtr coach_state_sub_;
 
+    // server for receiving instructions from other agents
+    rclcpp::Service<rj_msgs::srv::AgentCommunication>::SharedPtr robot_communication_server_;
+
+    // clients for receiving instructions from the other agents
+    rclcpp::Client<rj_msgs::srv::AgentCommunication>::SharedPtr robot_communication_clients_[kNumShells];
+
     // callbacks for subs
     void world_state_callback(const rj_msgs::msg::WorldState::SharedPtr& msg);
     void coach_state_callback(const rj_msgs::msg::CoachState::SharedPtr& msg);
@@ -65,9 +72,52 @@ private:
      */
     void send_new_goal();
 
+<<<<<<< HEAD
     /*
      * @brief calls and executes current_position_'s current desired task
      */
+=======
+    /**
+     * @brief sends a robot communication to a specific robot
+     * 
+     * @param robot_id the robot to communicate with
+     */
+    void send_unicast(int&& robot_id);
+
+    /**
+     * @brief sends a robot communication to all robots
+     * 
+     */
+    void send_broadcast();
+
+    /**
+     * @brief sends a robot communication to a specific group of robots
+     * 
+     * @param robot_ids the robot ids to send communication to
+     */
+    void send_multicast(int&& robot_ids[]);
+
+    /**
+     * @brief sends a robot communication and accepts only the first response
+     * 
+     * *NOTE* this is not the exact same as anycast in IP routing, but pretty similar
+     * 
+     */
+    void send_anycast();
+
+    void receive_communication_callback(const std::shared_ptr<rj_msgs::srv::AgentCommunication::Request> request,
+                                        std::shared_ptr<rj_msgs::srv::AgentCommunication::Response> response);
+
+    // TODO(#1957): add back this if needed, or delete
+    // cancel latest goal every time a new goal comes in, to avoid overloading memory with many
+    // threads
+    /* void cancel_last_goal(); */
+    // after goal is asynchronously canceled, reset last_goal_handle_
+    /* void cancel_goal_callback(rclcpp_action::Client<RobotMove>::CancelResponse::SharedPtr); */
+    /* GoalHandleRobotMove::SharedPtr last_goal_handle_; */
+
+    rclcpp::TimerBase::SharedPtr get_task_timer_;
+>>>>>>> 0192610ac9... begin writing agent communication
     void get_task();
     rclcpp::TimerBase::SharedPtr get_task_timer_;
     // note that this is our RobotIntent struct (robot_intent.hpp), not a
