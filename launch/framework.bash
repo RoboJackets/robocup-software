f90f60cc541f12ae1c8c8b50f1f8151157da6db7
Sid Parikh <sidparikh@gatech.edu>
2022-11-30 23:12:16 +0000

    add makefile shortcut

--- launch/framework.bash
+++ launch/framework.bash
@@ -1,2 +1,2 @@
 echo "launching ER-Force framework and our UI"
-(trap 'kill 0' SIGINT; find ~ -name 'simulator-cli' -type f -exec '{}' -g 2020B ';' & make run-sim & wait)
+(trap 'kill 0' SIGINT; find ~ -name 'simulator-cli' -type f -exec '{}' -g 2020B ';' & make run-sim-only & wait)
