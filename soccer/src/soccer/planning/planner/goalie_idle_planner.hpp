c9ffed119f051435ee2123ddcde59ad07529424f
kfu02 <kevin.fu842@gmail.com>
2022-12-05 21:36:58 -0500

    make Goalie idle only, but via planning

--- soccer/src/soccer/planning/planner/goalie_idle_planner.hpp
+++ soccer/src/soccer/planning/planner/goalie_idle_planner.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <spdlog/spdlog.h>
+
 #include "planning/instant.hpp"
+#include "planning/planner/path_target_planner.hpp"
 #include "planning/planner/planner.hpp"
 #include "planning/primitives/replanner.hpp"
 #include "planning/trajectory.hpp"
 #include "rj_geometry/point.hpp"
+/* #include <rj_msgs/msg/path_target_motion_command.hpp> */
+#include "planning/planner/motion_command.hpp"
+#include "planning/primitives/angle_planning.hpp"
 
 namespace planning {
 /**
@@ -17,10 +23,20 @@ public:
 
     Trajectory plan(const PlanRequest& plan_request) override;
 
+    /*
+     * @return Point for Goalie to stand in when no shot is coming. Expects
+     * ball to be slow.
+     */
+    rj_geometry::Point get_idle_pt(const WorldState* world_state) const;
+
     void reset() override;
     [[nodiscard]] bool is_done() const override;
 
+    double draw_radius = kRobotRadius;
+    QColor draw_color = Qt::black;
+
 private:
+    Trajectory previous_{};
 };
 
 }  // namespace planning
