a71fe6427c053197bb3d53ec05bf292602c9bcbe
kfu02 <kevin.fu842@gmail.com>
2022-12-23 14:19:41 -0500

    cleanup + todos for next steps

--- soccer/src/soccer/strategy/agent/position/goalie.cpp
+++ soccer/src/soccer/strategy/agent/position/goalie.cpp
@@ -2,7 +2,7 @@
 
 namespace strategy {
 
-// TODO: lock Goalie id to id given by the ref
+// TODO(Kevin): lock Goalie id to id given by the ref
 Goalie::Goalie(int r_id) : Position(r_id) { position_name_ = "Goalie"; }
 
 std::optional<rj_msgs::msg::RobotIntent> Goalie::get_task() {
@@ -17,25 +17,8 @@ std::optional<rj_msgs::msg::RobotIntent> Goalie::get_task() {
 
     WorldState* world_state = this->world_state();  // thread-safe getter
     if (shot_on_goal_detected(world_state)) {
-        /* if (world_state->ball.velocity.mag() > 1.0) { */
-        // create PathTargetMotionCommand, set goal position to block_pt
-        /*
-        auto ptmc = rj_msgs::msg::PathTargetMotionCommand{};
-        ptmc.target.position = rj_convert::convert_to_ros(block_pt);
-
-        // make goalie face the ball
-        rj_geometry::Point ball_pos = world_state->ball.position;
-        auto face_pt = ball_pos;
-        ptmc.override_face_point = {rj_convert::convert_to_ros(face_pt)};
-
-        ptmc.ignore_ball = true;  // allow goalie to intersect ball's path
-
-        // update intent
-        intent.motion_command.path_target_command = {ptmc};
-        intent.motion_command.name = "path_target";
-        return intent;
-        */
-
+        // TODO(Kevin): fix intercept planner's is_done, then add in logic to
+        // clear/pass ball once done intercepting
         auto intercept_mc = rj_msgs::msg::InterceptMotionCommand{};
         intercept_mc.target.x = 0.0;
         intercept_mc.target.y = 0.1;
@@ -43,19 +26,11 @@ std::optional<rj_msgs::msg::RobotIntent> Goalie::get_task() {
         intent.motion_command.name = "intercept";
         return intent;
     } else {
-        // send idle command a few times in case it doesn't get picked up on init
-        /* if (send_idle_ct_ < 3) { */
         auto goalie_idle = rj_msgs::msg::GoalieIdleMotionCommand{};
         intent.motion_command.goalie_idle_command = {goalie_idle};
         intent.motion_command.name = "goalie_idle";
-
-        /* send_idle_ct_++; */
         return intent;
-        /* } */
     }
-
-    // TODO: capture ball if vel is slow & ball is inside box
-    // (waiting on field pts to be given to world_state)
 
     return std::nullopt;
 }
