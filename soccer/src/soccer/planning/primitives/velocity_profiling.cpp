8c8059fda952229b7ca7e94d93a7999bb8db8ce8
N8BWert <n8.wert.b@gmail.com>
2023-01-10 21:19:13 -0500

    appeasing the style gods

--- soccer/src/soccer/planning/primitives/velocity_profiling.cpp
+++ soccer/src/soccer/planning/primitives/velocity_profiling.cpp
@@ -162,9 +162,10 @@ Trajectory profile_velocity(const BezierPath& path, double initial_speed, double
         Twist twist{derivs1[n].normalized(speed[n]), 0};
 
         // TODO (someone): Make sure destination points sent to planner are within the field radius
-        // A bug exists where a point incredibly far from the field is targetted as the next location for the robot.
-        // In this case it will take the robot an incredibly long time to get there and the trajectory after will have the same
-        // destination time for some reason.
+        // A bug exists where a point incredibly far from the field is targetted
+        // as the next location for the robot.
+        // In this case it will take the robot an incredibly long time to get there
+        // and the trajectory after will have the same destination time for some reason.
         if (interval_time > 1e15) {
             continue;
         }
