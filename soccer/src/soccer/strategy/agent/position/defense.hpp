2302b88a11b920c05fa359a60028b7c3b09348b7
N8BWert <n8.wert.b@gmail.com>
2023-01-15 20:48:54 -0500

    rectify defense.hpp with og branch

--- soccer/src/soccer/strategy/agent/position/defense.hpp
+++ soccer/src/soccer/strategy/agent/position/defense.hpp
@@ -2,18 +2,17 @@
 
 #include <cmath>
 
-#include <rclcpp/rclcpp.hpp>
-#include <rclcpp_action/rclcpp_action.hpp>
 #include <spdlog/spdlog.h>
 
-#include <rj_msgs/action/robot_move.hpp>
+#include <rj_common/time.hpp>
+#include <rj_geometry/geometry_conversions.hpp>
+#include <rj_geometry/point.hpp>
 #include <rj_msgs/msg/empty_motion_command.hpp>
 
-#include "planning/instant.hpp"
 #include "position.hpp"
-#include "rj_common/time.hpp"
-#include "rj_geometry/geometry_conversions.hpp"
-#include "rj_geometry/point.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "rj_msgs/action/robot_move.hpp"
 
 namespace strategy {
 
@@ -26,17 +25,14 @@ public:
     Defense(int r_id);
     ~Defense() override = default;
 
-    rj_msgs::msg::RobotIntent get_task() override;
-
     void receive_communication_response(communication::AgentPosResponseWrapper response) override;
     communication::PosAgentResponseWrapper receive_communication_request(
         communication::AgentPosRequestWrapper request) override;
 
 private:
     void set_test_multicast_request();
+    std::optional<rj_msgs::msg::RobotIntent> get_task() override;
     int move_ct_ = 0;
-
-    std::optional<RobotIntent> derived_get_task(RobotIntent intent) override;
 };
 
 }  // namespace strategy
