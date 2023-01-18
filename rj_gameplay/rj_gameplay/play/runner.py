d0a38f17093c30699f4400dc97b285b20e9c62a3
Kevin Fu <kevin.fu842@gmail.com>
2023-01-17 21:29:13 -0500

    another try

--- rj_gameplay/rj_gameplay/play/runner.py
+++ rj_gameplay/rj_gameplay/play/runner.py
@@ -1,55 +1,88 @@
 from enum import Enum, auto
+from typing import List
 
+import stp
+import stp.rc
+import stp.role
+import stp.role.cost
 from rj_msgs.msg import RobotIntent
 
-from rj_gameplay.skill import receive
+from rj_gameplay.role import runner
 
 
 class State(Enum):
     INIT = auto()
-    PASS_READY = auto()
-    RECEIVE_PASS = auto()
+    GO_TO_EDGE = auto()
+    TOP_RIGHT = auto()
+    BOTTOM_RIGHT = auto()
+    BOTTOM_LEFT = auto()
+    TOP_LEFT = auto()
+    FINAL_LEG = auto()
     DONE = auto()
 
 
-class ReceiverRole(stp.role.Role):
-    def __init__(self, robot: stp.rc.Robot) -> None:
-        super().__init__(robot)
+class Runner(stp.play.Play):
+    """distraction play by running a lap"""
 
-        self.receive_skill = None
-
+    def __init__(self):
+        super().__init__()
+        self._mappings = {}
         self._state = State.INIT
 
-        self._target_point = None
+        self.width = stp.rc.WorldState._field._width_m
+        self.length = stp.rc.WorldState._field._length_m
 
-    @property
-    def pass_ready(self):
-        return self._state == State.PASS_READY
+        self._mappings["start"] = (0.0, self.length)
+        self._mappings["topright"] = (self.width / 2.0, self.length)
+        self._mappings["bottomright"] = (self.width / 2.0, 0.0)
+        self._mappings["bottomleft"] = (-self.width / 2.0, 0.0)
+        self._mappings["topleft"] = (-self.width / 2.0, self.length)
+        self._mappings["end"] = self._mappings["start"]
 
-    def set_receive_pass(self):
-        self._state = State.RECEIVE_PASS
-        self.receive_skill = receive.Receive(robot=self.robot)
-
-    def tick(self, world_state: stp.rc.WorldState) -> RobotIntent:
-        """
-        Assume passer already has ball on init. Then:
-         - on init: continue seeking
-         - interrupt signal from Tactic: go get ball
-         - when got ball: done
-        """
-
-        intent = None
+    def tick(
+        self,
+        world_state: stp.rc.WorldState,
+    ) -> List[RobotIntent]:
 
         if self._state == State.INIT:
-            # TODO: do seek behavior
-            pass
-        elif self._state == State.RECEIVE_PASS:
-            intent = self.receive_skill.tick(world_state)
-            if self.receive_skill.is_done(world_state):
-                self._state = State.DONE
-                # end FSM
-
-        return intent
-
-    def is_done(self, world_state) -> bool:
-        return self._state == State.DONE
+            self._state = State.GO_TO_EDGE
+            return self.get_robot_intents(world_state)
+        elif self._state == State.GO_TO_EDGE:
+            self.prioritized_tactics = [
+                runner.RunnerRole(world_state, self._mappings["start"])
+            ]
+            self._state = State.TOP_RIGHT
+            return self.get_robot_intents(world_state)
+        elif self._state == State.TOP_RIGHT:
+            self.prioritized_tactics = [
+                runner.RunnerRole(world_state, self._mappings["topright"])
+            ]
+            self._state = State.BOTTOM_RIGHT
+            return self.get_robot_intents(world_state)
+        elif self._state == State.BOTTOM_RIGHT:
+            self.prioritized_tactics = [
+                runner.RunnerRole(world_state, self._mappings["bottomright"])
+            ]
+            self._state = State.BOTTOM_LEFT
+            return self.get_robot_intents(world_state)
+        elif self._state == State.BOTTOM_LEFT:
+            self.prioritized_tactics = [
+                runner.RunnerRole(world_state, self._mappings["bottomleft"])
+            ]
+            self._state = State.TOP_LEFT
+            return self.get_robot_intents(world_state)
+        elif self._state == State.TOP_LEFT:
+            self.prioritized_tactics = [
+                runner.RunnerRole(world_state, self._mappings["topleft"])
+            ]
+            self._state = State.FINAL_LEG
+            return self.get_robot_intents(world_state)
+        elif self._state == State.FINAL_LEG:
+            self.prioritized_tactics = [
+                runner.RunnerRole(world_state, self._mappings["end"])
+            ]
+            self._state = State.DONE
+            return self.get_robot_intents(world_state)
+        elif self._state == State.DONE:
+            # return robot intents from assigned tactics back to gameplay node
+            return self.get_robot_intents(world_state)
