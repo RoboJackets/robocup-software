756fb2c4083511c580fb2e9bd9ec827a013b0a57
Nathaniel Wert <N8.Wert.B@gmail.com>
2023-02-14 19:37:10 -0500

    Create Backbone of Agent to Agent Communication System (#1993)
    * begin writing agent communication
    
    * create communication scaffolding v1
    
    * fix compiler errors and create initial messages for testing
    
    * basic communication complete, next is testing
    
    * write tests to ensure broadcast, unicast, and multicast work
    
    * delete unnecessary files
    
    * remove unneccessary debugging statement
    
    * run make pretty-lines
    
    * remove old comment
    
    * create communication structs for agent to position communicaiton
    
    * remove old ros message references
    
    * add test request code back to goalie and add in planning hotfix
    
    * re-add testing code for goalie
    
    * rectify defense.cpp with og branch
    
    * rectify defense.hpp with og branch
    
    * rectify goalie.cpp with og branch
    
    * rectify goalie.hpp with og branch
    
    * rectify offense.cpp with og branch
    
    * rectify offense.hpp with og branch
    
    * rectify position.cpp with og branch
    
    * rectify position.hpp with og branch
    
    * rectify agent_action_client_main.cpp with og branch
    
    * rectify agent_action_client.cpp with og branch
    
    * rectify agent_action_client.hpp with og branch
    
    * fix linker errors
    
    * fix agent_action_client.hpp by coppying from og branch
    
    * fix docstrings
    
    * create preliminary code for ros conversions for agent communication
    
    * rework communication system to use cpp builtin types
    
    * finish writing tests
    
    * fix merge errors
    
    * run 'make pretty-lines'
    
    * use std::array and remove const from robot_id in unicast
    
    * remove references to different communication types to streamline code
    
    * fix world state issues in agent action client and forcefully merge in ros2
    
    * fix build-and-test errors
    
    * make pretty lines and cast ints to prevent build-test errors
    
    * fix clang-tidy warnings
    
    * bruh, I ran make-pretty lines and it seriously broke the style guidelines :(
    
    * remove test code
    
    * run make pretty-lines
    
    * have to run clang-format on position.cpp or else it fails style check

--- soccer/src/soccer/strategy/agent/agent_action_client.cpp
+++ soccer/src/soccer/strategy/agent/agent_action_client.cpp
@@ -27,6 +27,19 @@ AgentActionClient::AgentActionClient(int r_id)
         "strategy/coach_state", 1,
         [this](rj_msgs::msg::CoachState::SharedPtr msg) { coach_state_callback(msg); });
 
+    robot_communication_srv_ = create_service<rj_msgs::srv::AgentCommunication>(
+        fmt::format("agent_{}_incoming", r_id),
+        [this](const std::shared_ptr<rj_msgs::srv::AgentCommunication::Request> request,
+               std::shared_ptr<rj_msgs::srv::AgentCommunication::Response> response) {
+            receive_communication_callback(request, response);
+        });
+
+    // Create clients
+    for (size_t i = 0; i < kNumShells; i++) {
+        robot_communication_cli_[i] =
+            create_client<rj_msgs::srv::AgentCommunication>(fmt::format("agent_{}_incoming", i));
+    }
+
     positions_sub_ = create_subscription<rj_msgs::msg::PositionAssignment>(
         "strategy/positions", 1,
         [this](rj_msgs::msg::PositionAssignment::SharedPtr msg) { update_position(msg); });
@@ -35,6 +48,14 @@ AgentActionClient::AgentActionClient(int r_id)
     int hz = 10;
     get_task_timer_ = create_wall_timer(std::chrono::milliseconds(1000 / hz),
                                         std::bind(&AgentActionClient::get_task, this));
+
+    // TODO(Kevin): make ROS param for this
+    int agent_communication_hz = 60;
+    get_communication_timer_ =
+        create_wall_timer(std::chrono::milliseconds(1000 / agent_communication_hz), [this]() {
+            get_communication();
+            check_communication_timeout();
+        });
 }
 
 void AgentActionClient::world_state_callback(const rj_msgs::msg::WorldState::SharedPtr& msg) {
@@ -43,7 +64,8 @@ void AgentActionClient::world_state_callback(const rj_msgs::msg::WorldState::Sha
     }
 
     WorldState world_state = rj_convert::convert_from_ros(*msg);
-    current_position_->update_world_state(world_state);
+    auto lock = std::lock_guard(world_state_mutex_);
+    last_world_state_ = std::move(world_state);
 }
 
 void AgentActionClient::coach_state_callback(const rj_msgs::msg::CoachState::SharedPtr& msg) {
@@ -73,7 +95,6 @@ void AgentActionClient::get_task() {
         // note that because these are our RobotIntent structs, this comparison
         // uses our custom struct overloads
         if (task != last_task_) {
-            SPDLOG_INFO("robot {} has new task '{}'", robot_id_, task.motion_command_name);
             last_task_ = task;
             send_new_goal();
         }
@@ -117,7 +138,6 @@ void AgentActionClient::send_new_goal() {
     }
 
     auto goal_msg = RobotMove::Goal();
-    // must convert to ROS msg form in order to be sent across ROS nodes
     goal_msg.robot_intent = rj_convert::convert_to_ros(last_task_);
 
     auto send_goal_options = rclcpp_action::Client<RobotMove>::SendGoalOptions();
@@ -156,6 +176,185 @@ void AgentActionClient::result_callback(const GoalHandleRobotMove::WrappedResult
         default:
             return;
     }
+}
+
+void AgentActionClient::get_communication() {
+    if (current_position_ == nullptr) {
+        if (robot_id_ == 0) {
+            current_position_ = std::make_unique<Goalie>(robot_id_);
+        } else if (robot_id_ == 1) {
+            current_position_ = std::make_unique<Defense>(robot_id_);
+        } else {
+            current_position_ = std::make_unique<Offense>(robot_id_);
+        }
+    }
+
+    communication::PosAgentRequestWrapper communication_request =
+        current_position_->send_communication_request();
+
+    bool robots_visible = false;
+    for (u_int8_t i = 0; i < kNumShells; i++) {
+        if (this->world_state()->get_robot(true, i).visible) {
+            robots_visible = true;
+            break;
+        }
+    }
+
+    if (!(communication_request.request == last_communication_) && robots_visible) {
+        // update the last communication
+        last_communication_ = communication_request.request;
+        // create a buffer to hold the responses and the outgoing request
+        communication::AgentPosResponseWrapper buffered_response;
+
+        auto request = std::make_shared<rj_msgs::srv::AgentCommunication::Request>();
+        request->agent_request = rj_convert::convert_to_ros(communication_request.request);
+
+        // send communication requests
+        if (communication_request.broadcast) {
+            for (int i = 0; i < ((int)kNumShells); i++) {
+                if (i != robot_id_ && this->world_state()->get_robot(true, i).visible) {
+                    robot_communication_cli_[i]->async_send_request(
+                        request, [this, i](const std::shared_future<
+                                           rj_msgs::srv::AgentCommunication::Response::SharedPtr>
+                                               response) {
+                            receive_response_callback(response, ((u_int8_t)i));
+                        });
+                }
+            }
+            // set broadcast to true in buffer
+            buffered_response.broadcast = true;
+        } else {
+            for (u_int8_t i : communication_request.target_agents) {
+                robot_communication_cli_[i]->async_send_request(
+                    request, [this, i](const std::shared_future<
+                                       rj_msgs::srv::AgentCommunication::Response::SharedPtr>
+                                           response) { receive_response_callback(response, i); });
+            }
+            // copy the robot ids of the robots sent to into the buffer
+            copy(communication_request.target_agents.begin(),
+                 communication_request.target_agents.end(),
+                 back_inserter(buffered_response.from_robot_ids));
+        }
+
+        buffered_response.associated_request = communication_request.request;
+        buffered_response.urgent = communication_request.urgent;
+        buffered_response.created = RJ::now();
+        buffered_responses_.push_back(buffered_response);
+    }
+}
+
+void AgentActionClient::receive_communication_callback(
+    const std::shared_ptr<rj_msgs::srv::AgentCommunication::Request>& request,
+    const std::shared_ptr<rj_msgs::srv::AgentCommunication::Response>& response) {
+    // TODO (https://app.clickup.com/t/867796fh2): change this default to defense? or NOP?
+    if (current_position_ == nullptr) {
+        communication::AgentResponse agent_response;
+        communication::AgentRequest agent_request =
+            rj_convert::convert_from_ros(request->agent_request);
+        communication::Acknowledge acknowledge{};
+        communication::generate_uid(acknowledge);
+        agent_response.associated_request = agent_request;
+        agent_response.response = acknowledge;
+        response->agent_response = rj_convert::convert_to_ros(agent_response);
+    } else {
+        // Convert agent request into AgentToPosCommRequest
+        communication::AgentPosRequestWrapper agent_request;
+        communication::AgentRequest received_request =
+            rj_convert::convert_from_ros(request->agent_request);
+        agent_request.request = received_request;
+
+        // Give the current position the request and receive the response to send back
+        communication::PosAgentResponseWrapper pos_to_agent_response =
+            current_position_->receive_communication_request(agent_request);
+
+        // Convert PosToAgentCommResponse into AgentResponse
+        communication::AgentResponse agent_response{received_request,
+                                                    pos_to_agent_response.response};
+        response->agent_response = rj_convert::convert_to_ros(agent_response);
+    }
+}
+
+void AgentActionClient::receive_response_callback(
+    const std::shared_future<rj_msgs::srv::AgentCommunication::Response::SharedPtr>& response,
+    u_int8_t robot_id) {
+    // TODO (https://app.clickup.com/t/867796fh2): change this default to defense? or NOP?
+    if (current_position_ == nullptr) {
+        // TODO (https://app.clickup.com/t/867796fh2): change this once coach node merged
+        if (robot_id_ == 0) {
+            current_position_ = std::make_unique<Goalie>(robot_id_);
+        } else if (robot_id_ == 1) {
+            current_position_ = std::make_unique<Defense>(robot_id_);
+        } else {
+            current_position_ = std::make_unique<Offense>(robot_id_);
+        }
+    }
+
+    // Convert response from other agent to c++
+    communication::AgentResponse agent_response =
+        rj_convert::convert_from_ros(response.get()->agent_response);
+
+    for (u_int32_t i = 0; i < buffered_responses_.size(); i++) {
+        if (buffered_responses_[i].associated_request == agent_response.associated_request) {
+            // add the robot id in the corresponding (increasing) location in the received_robot_ids
+            if (buffered_responses_[i].received_robot_ids.empty()) {
+                buffered_responses_[i].received_robot_ids.push_back(robot_id);
+                buffered_responses_[i].responses.push_back(agent_response.response);
+            } else {
+                for (u_int32_t j = 0; j < buffered_responses_[i].received_robot_ids.size(); j++) {
+                    if (j == buffered_responses_[i].received_robot_ids.size() - 1) {
+                        // The response should be added at the end of the buffer
+                        buffered_responses_[i].received_robot_ids.push_back(robot_id);
+                        buffered_responses_[i].responses.push_back(agent_response.response);
+                        break;
+                    } else if (buffered_responses_[i].received_robot_ids[j] > robot_id) {
+                        // The response should be added at i in the buffer
+                        buffered_responses_[i].received_robot_ids.insert(
+                            buffered_responses_[i].received_robot_ids.begin() + j, robot_id);
+                        buffered_responses_[i].responses.insert(
+                            buffered_responses_[i].responses.begin() + j, agent_response.response);
+                        break;
+                    }
+                }
+            }
+
+            // if the message is urgent -> relay the response
+            // if we've received a response from all of the robots we want -> relay the response
+            if (buffered_responses_[i].urgent ||
+                (buffered_responses_[i].broadcast &&
+                 buffered_responses_[i].received_robot_ids.size() >= 5) ||
+                (buffered_responses_[i].received_robot_ids.size() ==
+                 buffered_responses_[i].from_robot_ids.size())) {
+                current_position_->receive_communication_response(buffered_responses_[i]);
+                buffered_responses_.erase(buffered_responses_.begin() + i);
+                return;
+            }
+        }
+    }
+}
+
+void AgentActionClient::check_communication_timeout() {
+    for (u_int32_t i = 0; i < buffered_responses_.size(); i++) {
+        if (RJ::now() - buffered_responses_[i].created > timeout_duration_) {
+            if (current_position_ == nullptr) {
+                if (robot_id_ == 0) {
+                    current_position_ = std::make_unique<Goalie>(robot_id_);
+                } else if (robot_id_ == 1) {
+                    current_position_ = std::make_unique<Defense>(robot_id_);
+                } else {
+                    current_position_ = std::make_unique<Offense>(robot_id_);
+                }
+            }
+
+            current_position_->receive_communication_response(buffered_responses_[i]);
+            buffered_responses_.erase(buffered_responses_.begin() + i);
+        }
+    }
+}
+
+[[nodiscard]] WorldState* AgentActionClient::world_state() {
+    // thread-safe getter for world_state (see update_world_state())
+    auto lock = std::lock_guard(world_state_mutex_);
+    return &last_world_state_;
 }
 
 }  // namespace strategy
--- soccer/src/soccer/strategy/agent/position/goalie.hpp
+++ soccer/src/soccer/strategy/agent/position/goalie.hpp
@@ -2,11 +2,11 @@
 
 #include <cmath>
 
-#include <rclcpp/rclcpp.hpp>
-#include <rclcpp_action/rclcpp_action.hpp>
 #include <spdlog/spdlog.h>
 
-#include <rj_msgs/action/robot_move.hpp>
+#include <rj_common/time.hpp>
+#include <rj_geometry/geometry_conversions.hpp>
+#include <rj_geometry/point.hpp>
 #include <rj_msgs/msg/empty_motion_command.hpp>
 #include <rj_msgs/msg/goalie_idle_motion_command.hpp>
 #include <rj_msgs/msg/intercept_motion_command.hpp>
@@ -14,9 +14,9 @@
 
 #include "planning/planner/intercept_planner.hpp"
 #include "position.hpp"
-#include "rj_common/time.hpp"
-#include "rj_geometry/geometry_conversions.hpp"
-#include "rj_geometry/point.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "rj_msgs/action/robot_move.hpp"
 
 namespace strategy {
 
@@ -28,6 +28,10 @@ class Goalie : public Position {
 public:
     Goalie(int r_id);
     ~Goalie() override = default;
+
+    void receive_communication_response(communication::AgentPosResponseWrapper response) override;
+    communication::PosAgentResponseWrapper receive_communication_request(
+        communication::AgentPosRequestWrapper request) override;
 
 private:
     // temp
