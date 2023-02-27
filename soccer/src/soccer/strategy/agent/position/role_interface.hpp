e5a80a87eb94a181b19b359ded866729e670a5ca
Ninjatest321 <rec.hnk@gmail.com>
2023-02-26 20:09:36 -0500

    Make Pretty Lines

--- soccer/src/soccer/strategy/agent/position/role_interface.hpp
+++ soccer/src/soccer/strategy/agent/position/role_interface.hpp
@@ -19,18 +19,18 @@ namespace strategy {
  * RoleInterface is an abstract superclass. Its subclasses handle strategy logic.
  * The goal is to isolate the strategy logic from ROS interfacing. This allows us
  * swap Role classes at runtime. (Google "Strategy Design Pattern" for more info)
- * 
+ *
  * A good analogy is how the Planner Node uses the various Planner objects
  * (PathTargetPlanner, etc.). The Planner objects take in a plan request and
  * output a Trajectory. Planner objects don't know anything about ROS; that is
  * all handled by the Planner Node.
- * 
- * Similiarly, this abstract superclass serves as an interface for all roles 
- * (ie. Waller, Marker, Seeker, etc.) to follow when being created. These roles 
- * will take in a robot intent from the Defense/Offense classes and output a new 
+ *
+ * Similiarly, this abstract superclass serves as an interface for all roles
+ * (ie. Waller, Marker, Seeker, etc.) to follow when being created. These roles
+ * will take in a robot intent from the Defense/Offense classes and output a new
  * robot intent with the robot's next point depending on their role. The Defense/
- * Offense classes will decide on what role the robot is currently playing based 
- * on the game situation (such as location, etc.) This design pattern allows for 
+ * Offense classes will decide on what role the robot is currently playing based
+ * on the game situation (such as location, etc.) This design pattern allows for
  * more flexibility in our usage.
  */
 class RoleInterface {
@@ -43,7 +43,6 @@ public:
      */
     virtual std::optional<RobotIntent> get_task(RobotIntent intent,
                                                 const WorldState* const world_state) = 0;
-
 
 private:
 };
