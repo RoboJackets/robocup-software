4f7d70c134d18cb173551c52ff18b1a67af136f6
dwu359 <dwu359@gatech.edu>
2023-03-06 00:25:43 +0000

    added role interface

--- soccer/src/soccer/strategy/agent/position/blocker.hpp
+++ soccer/src/soccer/strategy/agent/position/blocker.hpp
@@ -3,16 +3,17 @@
 
 #include <rj_msgs/msg/world_state.hpp>
 
+#include "role_interface.hpp"
 #include "position.hpp"
 #include "rj_geometry/point.hpp"
 
 namespace strategy {
-class Blocker {
+class Blocker : public RoleInterface {
 private:
     const double factor{};
 
 public:
     Blocker(double factor = 0.75);
-    std::optional<RobotIntent> get_task(RobotIntent intent, const WorldState* const world_state);
+    virtual std::optional<RobotIntent> get_task(RobotIntent intent, const WorldState* const world_state) override;
 };
 }  // namespace strategy
