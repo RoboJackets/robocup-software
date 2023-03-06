1c8f6f1ac39a8f95092f8de036096c8d1675bccd
Ninjatest321 <68755425+Ninjatest321@users.noreply.github.com>
2023-03-05 19:30:00 -0500

    Defense to waller refactor (#1996)
    * Change to 4 Wallers
    
    * Single Waller Functionality to Defense Role
    
    * Remove Chages from Defense.py
    
    * Make Waller Role an Independent Class
    
    * Formatted Code
    
    * Fixing Up Some Issues
    
    * More Fixes
    
    * Format
    
    * More Fixes
    
    * Another Fix
    
    * Create Role Interface
    
    * Make Pretty Lines
    
    * Not Static Interface Design
    
    * Some More Fixes
    
    * Make Pretty Lines
    
    * Change Var Names
    
    * Does This Run
    
    * Updata Robocup Fshare
    
    * Revert "Not Static Interface Design"
    
    This reverts commit 645a80afefb9b2a34719999b07754a8aeeb5291b.
    
    * Implement role_interface renewed
    
    * Some More Edits
    
    * Edits Part 2
    
    * Get rid of second const
    
    * Docstring edits

---
+++ soccer/src/soccer/strategy/agent/position/waller.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <cmath>
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <spdlog/spdlog.h>
+
+#include <rj_msgs/action/robot_move.hpp>
+#include <rj_msgs/msg/empty_motion_command.hpp>
+
+#include "planning/instant.hpp"
+#include "position.hpp"
+#include "rj_common/field_dimensions.hpp"
+#include "rj_common/time.hpp"
+#include "rj_constants/constants.hpp"
+#include "rj_geometry/geometry_conversions.hpp"
+#include "rj_geometry/point.hpp"
+#include "role_interface.hpp"
+
+namespace strategy {
+
+/*
+ * The Waller role provides the implementation for a defensive robot that implements
+ * this class to have a waller-like behavior where it aims to serve as a wall between
+ * the ball and the goal.
+ */
+class Waller : public RoleInterface {
+public:
+    Waller(int waller_num);
+    ~Waller() = default;
+
+    /**
+     * @brief  Returns a waller behavior which aims to intercept the path
+     * between the ball and the center of the goal
+     *
+     * @param [RobotIntent intent] [RobotIntent of the Defensive Robot]
+     * @return [RobotIntent with next target point for the robot]
+     */
+    std::optional<RobotIntent> get_task(RobotIntent intent, const WorldState* world_state) override;
+
+private:
+    std::string defense_type_;
+    int waller_pos_;
+};
+
+}  // namespace strategy
