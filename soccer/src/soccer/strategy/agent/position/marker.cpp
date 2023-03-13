b9022c65ee011f4851d0e51c6fa7660d805c4be2
dwu359 <dwu359@gatech.edu>
2023-03-13 15:49:05 +0000

    clang format

--- soccer/src/soccer/strategy/agent/position/marker.cpp
+++ soccer/src/soccer/strategy/agent/position/marker.cpp
@@ -3,7 +3,7 @@
 namespace strategy {
 Marker::Marker(double factor) : factor{factor} {}
 std::optional<RobotIntent> Marker::get_task(RobotIntent intent,
-                                             const WorldState* const world_state) {
+                                            const WorldState* const world_state) {
     // face ball
     planning::PathTargetFaceOption face_option = planning::FaceBall{};
     // avoid ball
@@ -17,14 +17,14 @@ std::optional<RobotIntent> Marker::get_task(RobotIntent intent,
     return intent;
 }
 std::optional<RobotIntent> Marker::get_point_task(RobotIntent intent,
-                                             const rj_geometry::Point& point1, const rj_geometry::Point& point2) {
+                                                  const rj_geometry::Point& point1,
+                                                  const rj_geometry::Point& point2) {
     // face ball
     planning::PathTargetFaceOption face_option = planning::FaceBall{};
     // avoid ball
     constexpr bool IGNORE_BALL = false;
-    planning::LinearMotionInstant goal{
-        (point2 - point1) * factor + point1,
-        rj_geometry::Point{0.0, 0.0}};
+    planning::LinearMotionInstant goal{(point2 - point1) * factor + point1,
+                                       rj_geometry::Point{0.0, 0.0}};
     intent.motion_command = planning::PathTargetMotionCommand{goal, face_option, IGNORE_BALL};
     intent.motion_command_name = "marker_target";
     return intent;
--- soccer/src/soccer/strategy/agent/position/marker.hpp
+++ soccer/src/soccer/strategy/agent/position/marker.hpp
@@ -16,7 +16,7 @@ public:
     Marker(double factor = 0.75);
     virtual std::optional<RobotIntent> get_task(RobotIntent intent,
                                                 const WorldState* const world_state) override;
-    std::optional<RobotIntent> get_point_task(RobotIntent intent,
-                                             const rj_geometry::Point& point1, const rj_geometry::Point& point2);
+    std::optional<RobotIntent> get_point_task(RobotIntent intent, const rj_geometry::Point& point1,
+                                              const rj_geometry::Point& point2);
 };
 }  // namespace strategy
