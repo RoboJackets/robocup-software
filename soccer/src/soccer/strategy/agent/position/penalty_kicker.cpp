78b8801c6b629f265b297fd7e2e392b216781b10
Sid Parikh <sidparikh@gatech.edu>
2023-03-15 00:45:58 +0000

    create penalty kicker

---
+++ soccer/src/soccer/strategy/agent/position/penalty_kicker.cpp
@@ -0,0 +1,8 @@
+
+namespace strategy {
+
+std::optional<RobotIntent> PenaltyKicker::get_task(RobotIntent intent,
+                                                   const WorldState* world_state,
+                                                   FieldDimensions field_dimensions) {}
+
+}  // namespace strategy
