d8979fbafd48815736dba9c3b9a7ebdff0098ca5
Jack Sherling <sherlingjack@gmail.com>
2023-03-26 21:15:53 -0400

    style

--- soccer/src/soccer/referee/external_referee.cpp
+++ soccer/src/soccer/referee/external_referee.cpp
@@ -170,10 +170,10 @@ void ExternalReferee::handle_command(const ExternalReferee::Command& command) {
         case SSL_Referee::PREPARE_PENALTY_BLUE:
             set_play_state(PlayState::setup_penalty(BLUE));
             break;
-        case SSL_Referee::FREE_YELLOW:
+        case SSL_Referee::DIRECT_FREE_YELLOW:
             set_play_state(PlayState::ready_free_kick(YELLOW));
             break;
-        case SSL_Referee::FREE_BLUE:
+        case SSL_Referee::DIRECT_FREE_BLUE:
             set_play_state(PlayState::ready_free_kick(BLUE));
             break;
         case SSL_Referee::TIMEOUT_YELLOW:
