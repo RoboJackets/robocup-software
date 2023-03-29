7d8f95caed337cbad67035bc73881092c7f81f12
Sid Parikh <sidparikh@gatech.edu>
2023-03-29 00:58:26 +0000

    make penalty kicker line up to ball

--- soccer/src/soccer/strategy/agent/position/penalty_kicker.hpp
+++ soccer/src/soccer/strategy/agent/position/penalty_kicker.hpp
@@ -1,10 +1,19 @@
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "planning/instant.hpp"
+#include "planning/planner/motion_command.hpp"
+#include "rj_common/field_dimensions.hpp"
+#include "rj_constants/constants.hpp"
+#include "rj_geometry/point.hpp"
 #include "role_interface.hpp"
 
 namespace strategy {
 
 class PenaltyKicker : public RoleInterface {
 public:
-    PenaltyKicker();
+    PenaltyKicker() = default;
     ~PenaltyKicker() = default;
 
     /**
