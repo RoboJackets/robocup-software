06116eff6d5ac1d917c20ffaffea3921deb9d459
N8BWert <n8.wert.b@gmail.com>
2023-04-11 19:27:05 -0400

    migrate waller.hpp

--- soccer/src/soccer/strategy/agent/position/waller.hpp
+++ soccer/src/soccer/strategy/agent/position/waller.hpp
@@ -26,7 +26,7 @@ namespace strategy {
  */
 class Waller : public RoleInterface {
 public:
-    Waller(int waller_num);
+    Waller(int waller_num, int total_wallers);
     ~Waller() = default;
 
     /**
@@ -42,6 +42,7 @@ public:
 private:
     std::string defense_type_;
     int waller_pos_;
+    int total_wallers_;
 };
 
 }  // namespace strategy
