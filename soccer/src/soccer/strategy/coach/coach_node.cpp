c2b9605e954235bcd0d6881c13670e195133cdc2
p-nayak11 <prabhanjanmnayak@gmail.com>
2023-07-04 08:24:02 -0400

    WIP

--- soccer/src/soccer/strategy/coach/coach_node.cpp
+++ soccer/src/soccer/strategy/coach/coach_node.cpp
@@ -285,6 +285,15 @@ void CoachNode::assign_positions_normal(std::array<uint32_t, kNumShells>& positi
     //  1. 1 Goalie
     //  2. 2 Offense
     //  3. Remaining Defense (always 1 defender)
+
+    /**
+    * Robot 0 - working kicker (0)
+    * Robot 1 - working kicker (2)
+    * Robot 2 - static obstacle (5)
+    * Robot 3 - bad kicker (4)
+    * Robot 4 - working kicker (3)
+    * Robot 5 - working kicker (best) (1)
+    */
     int assign_num = 0;
     for (u_int8_t robot_id = 0; robot_id < kNumShells; robot_id++) {
         if (check_robot_alive(robot_id)) {
