2f800eebda5319df69f26b3cfdaf3b52ce96d483
p-nayak11 <prabhanjanmnayak@gmail.com>
2023-10-22 20:41:36 -0400

    style

--- soccer/src/soccer/strategy/agent/communication/leave_wall_response.hpp
+++ soccer/src/soccer/strategy/agent/communication/leave_wall_response.hpp
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 
 #include <mutex>
 #include <string>
@@ -7,40 +7,42 @@
 
 #include <rj_common/time.hpp>
 #include <rj_convert/ros_convert.hpp>
+
 #include "rj_msgs/msg/leave_wall_response.hpp"
 
 namespace strategy::communication {
 
 struct LeaveWallResponse {
-	uint32_t response_uid;
-	uint8_t robot_id;
+    uint32_t response_uid;
+    uint8_t robot_id;
 };
 
 bool operator==(const LeaveWallResponse& a, const LeaveWallResponse& b);
 void generate_uid(LeaveWallResponse& response);
 
-}
+}  // namespace strategy::communication
 
 namespace rj_convert {
 
 template <>
 struct RosConverter<strategy::communication::LeaveWallResponse, rj_msgs::msg::LeaveWallResponse> {
-	static rj_msgs::msg::LeaveWallResponse to_ros(const strategy::communication::LeaveWallResponse& from) {
-		rj_msgs::msg::LeaveWallResponse result;
-		result.response_uid = from.response_uid;
-		result.robot_id = from.robot_id;
-		return result;
-	}
+    static rj_msgs::msg::LeaveWallResponse to_ros(
+        const strategy::communication::LeaveWallResponse& from) {
+        rj_msgs::msg::LeaveWallResponse result;
+        result.response_uid = from.response_uid;
+        result.robot_id = from.robot_id;
+        return result;
+    }
 
-	static strategy::communication::LeaveWallResponse from_ros(const rj_msgs::msg::LeaveWallResponse& from) {
-		return strategy::communication::LeaveWallResponse{
-			from.response_uid,
-			from.robot_id,
-		};
-	}
-
+    static strategy::communication::LeaveWallResponse from_ros(
+        const rj_msgs::msg::LeaveWallResponse& from) {
+        return strategy::communication::LeaveWallResponse{
+            from.response_uid,
+            from.robot_id,
+        };
+    }
 };
 
 ASSOCIATE_CPP_ROS(strategy::communication::LeaveWallResponse, rj_msgs::msg::LeaveWallResponse);
 
-}
+}  // namespace rj_convert
--- soccer/src/soccer/strategy/agent/communication/pass_request.hpp
+++ soccer/src/soccer/strategy/agent/communication/pass_request.hpp
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 
 #include <mutex>
 #include <string>
@@ -7,43 +7,43 @@
 
 #include <rj_common/time.hpp>
 #include <rj_convert/ros_convert.hpp>
+
 #include "rj_msgs/msg/pass_request.hpp"
 
 namespace strategy::communication {
 
 struct PassRequest {
-	uint32_t request_uid;
-	bool direct;
-	uint8_t from_robot_id;
+    uint32_t request_uid;
+    bool direct;
+    uint8_t from_robot_id;
 };
 
 bool operator==(const PassRequest& a, const PassRequest& b);
 void generate_uid(PassRequest& request);
 
-}
+}  // namespace strategy::communication
 
 namespace rj_convert {
 
 template <>
 struct RosConverter<strategy::communication::PassRequest, rj_msgs::msg::PassRequest> {
-	static rj_msgs::msg::PassRequest to_ros(const strategy::communication::PassRequest& from) {
-		rj_msgs::msg::PassRequest result;
-		result.request_uid = from.request_uid;
-		result.direct = from.direct;
-		result.from_robot_id = from.from_robot_id;
-		return result;
-	}
+    static rj_msgs::msg::PassRequest to_ros(const strategy::communication::PassRequest& from) {
+        rj_msgs::msg::PassRequest result;
+        result.request_uid = from.request_uid;
+        result.direct = from.direct;
+        result.from_robot_id = from.from_robot_id;
+        return result;
+    }
 
-	static strategy::communication::PassRequest from_ros(const rj_msgs::msg::PassRequest& from) {
-		return strategy::communication::PassRequest{
-			from.request_uid,
-			from.direct,
-			from.from_robot_id,
-		};
-	}
-
+    static strategy::communication::PassRequest from_ros(const rj_msgs::msg::PassRequest& from) {
+        return strategy::communication::PassRequest{
+            from.request_uid,
+            from.direct,
+            from.from_robot_id,
+        };
+    }
 };
 
 ASSOCIATE_CPP_ROS(strategy::communication::PassRequest, rj_msgs::msg::PassRequest);
 
-}
+}  // namespace rj_convert
