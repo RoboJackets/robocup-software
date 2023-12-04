9f778ffc197b15e932b659ed9aeea7ee76956ae6
jvogt23 <91633361+jvogt23@users.noreply.github.com>
2023-12-03 19:38:12 -0500

    Undid diagnostic changes

--- soccer/src/soccer/strategy/agent/position/offense.cpp
+++ soccer/src/soccer/strategy/agent/position/offense.cpp
@@ -30,7 +30,7 @@ Offense::State Offense::update_state() {
 
     if (current_state_ == IDLING) {
         send_scorer_request();
-        next_state = PASSING;
+        next_state = SEARCHING;
     } else if (current_state_ == SEARCHING) {
         if (scorer_) {
             next_state = STEALING;
@@ -94,8 +94,6 @@ std::optional<RobotIntent> Offense::state_to_task(RobotIntent intent) {
         intent.motion_command = empty_motion_cmd;
         return intent;
     } else if (current_state_ == PASSING) {
-        //TODO: Remove this pls
-        target_robot_id = 2;
         // attempt to pass the ball to the target robot
         rj_geometry::Point target_robot_pos =
             last_world_state_->get_robot(true, target_robot_id).pose.position();
@@ -107,7 +105,8 @@ std::optional<RobotIntent> Offense::state_to_task(RobotIntent intent) {
         intent.shoot_mode = RobotIntent::ShootMode::KICK;
         // NOTE: Check we can actually use break beams
         intent.trigger_mode = RobotIntent::TriggerMode::ON_BREAK_BEAM;
-        // TODO: Adjust the kick speed based on distance
+        //Adjusts kick speed based on distance. Refer to 
+        //TIGERS Mannheim eTDP from 2019 for details.
         dist = std::sqrt(std::pow((target_robot_pos.x() - this_robot_pos.x()), 2) +
             std::pow((target_robot_pos.y() - this_robot_pos.y()), 2));
         intent.kick_speed = std::sqrt((std::pow(FINAL_BALL_SPEED, 2)) -
