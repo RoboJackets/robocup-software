4bd1e355901ca09a204f46ce267f19e2fe925b01
petergarud <peter.garud04@gmail.com>
2024-01-22 02:18:10 +0000

    fixed compiler error and targets 7/8 part of goal

--- soccer/src/soccer/strategy/agent/position/offense.cpp
+++ soccer/src/soccer/strategy/agent/position/offense.cpp
@@ -126,7 +126,7 @@ std::optional<RobotIntent> Offense::state_to_task(RobotIntent intent) {
         return intent;
     } else if (current_state_ == SHOOTING) {
         rj_geometry::Point their_goal_pos = field_dimensions_.their_goal_loc();
-        rj_geometry::Point scoring_point = their_goal_pos + field_dimensions_.goal_width() / 2.0;
+        rj_geometry::Point scoring_point = their_goal_pos + field_dimensions_.goal_width() * 3.0 / 8.0;
         planning::LinearMotionInstant target{scoring_point};
         auto line_kick_cmd = planning::MotionCommand{"line_kick", target};
         intent.motion_command = line_kick_cmd;
