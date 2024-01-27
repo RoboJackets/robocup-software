6c60694573622ac2c42a0703ff86d4ed019c24d8
sid-parikh <sid-parikh@users.noreply.github.com>
2024-01-27 01:12:15 +0000

    automated style fixes

--- soccer/src/soccer/strategy/agent/position/offense.cpp
+++ soccer/src/soccer/strategy/agent/position/offense.cpp
@@ -350,10 +350,9 @@ void Offense::derived_acknowledge_ball_in_transit() {
 }
 
 rj_geometry::Point Offense::calculate_best_shot() {
-
     // Goal location
     rj_geometry::Point their_goal_pos = field_dimensions_.our_goal_loc();
-    double goal_width = field_dimensions_.goal_width(); // 1.0 meters
+    double goal_width = field_dimensions_.goal_width();  // 1.0 meters
 
     // Ball location
     rj_geometry::Point ball_position = this->last_world_state_->ball.position;
@@ -361,7 +360,8 @@ rj_geometry::Point Offense::calculate_best_shot() {
     rj_geometry::Point best_shot = their_goal_pos;
     double best_distance = -1.0;
     rj_geometry::Point increment(0.05, 0);
-    rj_geometry::Point curr_point = their_goal_pos - rj_geometry::Point(goal_width / 2.0, 0) + increment;
+    rj_geometry::Point curr_point =
+        their_goal_pos - rj_geometry::Point(goal_width / 2.0, 0) + increment;
     for (int i = 0; i < 19; i++) {
         double distance = distance_from_their_robots(ball_position, curr_point);
         if (distance > best_distance) {
@@ -379,11 +379,11 @@ double Offense::distance_from_their_robots(rj_geometry::Point tail, rj_geometry:
     double min_angle = -0.5;
     for (auto enemy : their_robots) {
         rj_geometry::Point enemy_vec = enemy.pose.position() - tail;
-        if (enemy_vec.dot(vec) < 0 ) {
+        if (enemy_vec.dot(vec) < 0) {
             continue;
         }
-        auto projection = (enemy_vec.dot(vec)/vec.dot(vec));
-        enemy_vec = enemy_vec - (projection) * vec;
+        auto projection = (enemy_vec.dot(vec) / vec.dot(vec));
+        enemy_vec = enemy_vec - (projection)*vec;
         double distance = enemy_vec.mag();
         if (distance < (kRobotRadius + kBallRadius)) {
             return -1.0;
@@ -391,7 +391,7 @@ double Offense::distance_from_their_robots(rj_geometry::Point tail, rj_geometry:
         double angle = distance / projection;
         if (min_angle < 0) {
             min_angle = angle;
-        } else if(angle < min_angle) {
+        } else if (angle < min_angle) {
             min_angle = angle;
         }
     }
