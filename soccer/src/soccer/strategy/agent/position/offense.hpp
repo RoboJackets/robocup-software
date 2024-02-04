9395598d976efd3e70956a8d45bdb326b74690d5
Sid Parikh <parikhsid16@gmail.com>
2024-02-04 20:23:44 +0000

    cleaning up code organization

--- soccer/src/soccer/strategy/agent/position/offense.hpp
+++ soccer/src/soccer/strategy/agent/position/offense.hpp
@@ -19,7 +19,7 @@
 
 namespace strategy {
 
-/*
+/**
  * The Offense position handles general offensive tasks, like passing to
  * teammates, moving to get open, or shooting on goal.
  */
@@ -36,10 +36,14 @@ public:
     void derived_acknowledge_ball_in_transit() override;
 
 private:
+    /**
+    * @brief Overriden from Position. Calls next_state and then state_to_task on each tick.
+    */
     std::optional<RobotIntent> derived_get_task(RobotIntent intent) override;
 
     enum State {
         DEFAULT,           // Decide what to do
+        SEEKING_START,     // Calculate seeking point
         SEEKING,           // Get open
         POSSESSION_START,  // Try to shoot and send pass request
         POSSESSION,        // Holding the ball
@@ -49,10 +53,37 @@ private:
         SHOOTING           // Winning the game
     };
 
-    // The longest amount of time we should be
+    /**
+    * @return what the state should be right now. called on each get_task tick
+    */
+    State next_state();
+
+    /**
+    * @return the task to execute. called on each get_task tick AFTER next_state()
+    */
+    std::optional<RobotIntent> state_to_task(RobotIntent intent);
+
+    /**
+    * @brief This FSM has timeouts for certain states. 
+    * Ideally, these would not be necessary; as planners get more sophisticated
+    * they should not get "stuck". However, empirically, the offense FSM in particular
+    * has been observed to deadlock often. 
+    *
+    * One common case is when waiting for a receiver to accept a pass; if no receiver responds,
+    * the timeout is necessary. In the future receivers may be able to respond in the negative
+    * instead of ignoring the request.
+    *
+    * The timeouts are a safety mechanism, and should not be the primary reason for a 
+    * state transition. They are set relatively high for this reason.
+    *
+    * @return the maximum duration to stay in a given state, or -1 if there is no maximum.
+    *
+    */
     static constexpr RJ::Seconds timeout(State s) {
         switch (s) {
             case DEFAULT:
+                return RJ::Seconds{-1};
+            case SEEKING_START:
                 return RJ::Seconds{-1};
             case SEEKING:
                 return RJ::Seconds{-1};
@@ -71,20 +102,26 @@ private:
         }
     }
 
+    /**
+    * @brief Reset the timeout for the current state
+    */
     void reset_timeout();
 
-    bool timed_out();
+    /**
+    * @return if the current state has timed out
+    */
+    bool timed_out() const;
 
+    State current_state_ = State::DEFAULT;
+
+    // The time at which the last state started.
     RJ::Time last_time_;
 
+
+    /* RoleInterface Members */
     Seeker seeker_;
 
-    State update_state();
-
-    std::optional<RobotIntent> state_to_task(RobotIntent intent);
-
-    // current state of the offensive agent (state machine)
-    State current_state_ = State::DEFAULT;
+    /* Constants for State or Task Calculation */
 
     // These variables are for calculating ball speed when passing
     static constexpr float kFinalBallSpeed{0.0f};
@@ -95,23 +132,23 @@ private:
     // Used to tell if the ball is close enough to steal
     static constexpr double kStealBallRadius{0.5};
 
-    // The following functions are calculations to aid state transition or MotionCommand creation.
+    /* Utility functions for State or Task Calculation */
 
     /**
      * @brief Checks if this agent has a good shot, in which case it should shoot instead of
      * passing.
      */
-    bool has_open_shot();
+    bool has_open_shot() const;
 
     /**
      * @brief Calculates the distance of vector from other team's closest robot
      */
-    double distance_from_their_robots(rj_geometry::Point tail, rj_geometry::Point head);
+    double distance_from_their_robots(rj_geometry::Point tail, rj_geometry::Point head) const;
 
     /**
      * @brief Check if this agent could easily steal the ball
      */
-    bool can_steal_ball();
+    bool can_steal_ball() const;
 };
 
 }  // namespace strategy
