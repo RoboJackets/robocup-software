16350caf2484606ea2ab8b255fbc730375a5717b
Sid Parikh <parikhsid16@gmail.com>
2024-02-12 15:31:20 -0500

    settle/collect understand ref state

--- soccer/src/soccer/planning/planner/collect_path_planner.cpp
+++ soccer/src/soccer/planning/planner/collect_path_planner.cpp
@@ -14,6 +14,13 @@ using namespace rj_geometry;
 namespace planning {
 
 Trajectory CollectPathPlanner::plan(const PlanRequest& plan_request) {
+
+    const auto state = plan_request.play_state.state();
+    if (state == PlayState::Stop || state == PlayState::Halt) {
+        // This planner automatically fails if the robot is prohibited from touching the ball.
+        return Trajectory{};
+    }
+
     BallState ball = plan_request.world_state->ball;
 
     const RJ::Time cur_time = plan_request.start.stamp;
