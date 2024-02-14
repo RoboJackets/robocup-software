45c16940792b42e1bc7228bc9f222c44bd8675c0
N8BWert <N8BWert@users.noreply.github.com>
2024-02-14 02:40:35 +0000

    automated style fixes

--- soccer/src/soccer/radio/network_radio.hpp
+++ soccer/src/soccer/radio/network_radio.hpp
@@ -7,6 +7,8 @@
 #include <boost/bimap/multiset_of.hpp>
 #include <boost/config.hpp>
 
+#include <rj_common/network.hpp>
+#include <rj_common/time.hpp>
 #include <rj_msgs/msg/alive_robots.hpp>
 #include <robot_intent.hpp>
 
@@ -14,9 +16,6 @@
 #include "strategy/agent/position/positions.hpp"
 
 #include "rc-fshare/rtp.hpp"
-
-#include <rj_common/time.hpp>
-#include <rj_common/network.hpp>
 
 namespace radio {
 
@@ -34,13 +33,13 @@ protected:
     void receive() override;
     void switch_team(bool blue) override;
 
-    boost::asio::ip::udp::endpoint base_station_endpoint = 
-        boost::asio::ip::udp::endpoint(boost::asio::ip::address::from_string(kBaseStationAddress), kBaseStationPort);
-    
-    boost::asio::ip::udp::endpoint robot_status_endpoint =
-        boost::asio::ip::udp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), kIncomingBaseStationDataPort);
-    boost::asio::ip::udp::endpoint alive_robots_endpoint =
-        boost::asio::ip::udp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), kIncomingBaseStationAliveRobotsPort);
+    boost::asio::ip::udp::endpoint base_station_endpoint = boost::asio::ip::udp::endpoint(
+        boost::asio::ip::address::from_string(kBaseStationAddress), kBaseStationPort);
+
+    boost::asio::ip::udp::endpoint robot_status_endpoint = boost::asio::ip::udp::endpoint(
+        boost::asio::ip::address::from_string("127.0.0.1"), kIncomingBaseStationDataPort);
+    boost::asio::ip::udp::endpoint alive_robots_endpoint = boost::asio::ip::udp::endpoint(
+        boost::asio::ip::address::from_string("127.0.0.1"), kIncomingBaseStationAliveRobotsPort);
 
     // Fuck Around and Find Out
     // https://stackoverflow.com/questions/26243008/error-initializing-a-boost-udp-socket-with-a-boost-io-service
