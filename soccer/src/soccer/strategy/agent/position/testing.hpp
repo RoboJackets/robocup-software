7166b03d9198b4d090767ab49bfa5434799eb730
petergarud <peter.garud04@gmail.com>
2024-02-20 17:29:39 +0000

    added synchronous move to next state

--- soccer/src/soccer/strategy/agent/position/testing.hpp
+++ soccer/src/soccer/strategy/agent/position/testing.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
 #include <spdlog/spdlog.h>
+#include <vector>
+#include <algorithm>
 
 #include "position.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include <rj_common/time.hpp>
 
 #include "rj_geometry/point.hpp"
 
@@ -24,15 +25,17 @@ private:
         BASIC_MOVEMENT_2, // move robot in straight line
     };
 
+    inline static std::vector<bool> is_running_;
+    bool move_on_ = false;
+    int r_id_;
+
+
     State current_state_ = IDLING;
-    RJ::Timestamp time_stamp_;
 
     State update_state();
     std::optional<RobotIntent> derived_get_task(RobotIntent intent) override;
     std::optional<RobotIntent> state_to_task(RobotIntent intent);
-
-    bool move_on_ = false;
-
+    bool proceed();
 };
 
 } // namespace strategy
