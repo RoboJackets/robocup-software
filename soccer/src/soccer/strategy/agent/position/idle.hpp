a94f28f875705a8608ef14ff6c3ad85f86cbcdeb
Sid Parikh <parikhsid16@gmail.com>
2024-02-25 22:02:27 -0500

    free kick works basic. need state machine

---
+++ soccer/src/soccer/strategy/agent/position/idle.hpp
@@ -0,0 +1,29 @@
+#include <rclcpp/rclcpp.hpp>
+
+#include "position.hpp"
+
+namespace strategy {
+class Idle : public Position {
+
+public:
+    Idle(int r_id);
+    ~Idle() = default;
+
+    /**
+     * @brief Does nothing; this position is a special case
+     */
+    void derived_acknowledge_pass() override;
+    /**
+     * @brief Does nothing; this position is a special case
+     */
+    void derived_pass_ball() override;
+    /**
+     * @brief Does nothing; this position is a special case
+     */
+    void derived_acknowledge_ball_in_transit() override;
+
+private:
+    std::optional<RobotIntent> derived_get_task(RobotIntent intent) override;
+};
+}
+
