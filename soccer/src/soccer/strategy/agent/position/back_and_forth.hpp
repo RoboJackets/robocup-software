0afd8664c6e641c85eb9b95312ff5bfe12b418ed
Shourik <sbanerje332@gatech.edu>
2024-03-04 01:57:01 +0000

    code compiles with make again

--- soccer/src/soccer/strategy/agent/position/back_and_forth.hpp
+++ soccer/src/soccer/strategy/agent/position/back_and_forth.hpp
@@ -1,50 +1,50 @@
-// #pragma once
+#pragma once
 
-// #include <algorithm>
-// #include <vector>
+#include <algorithm>
+#include <vector>
 
-// #include <spdlog/spdlog.h>
+#include <spdlog/spdlog.h>
 
-// #include <rj_constants/constants.hpp>
+#include <rj_constants/constants.hpp>
 
-// #include "offense.hpp"
-// #include "position.hpp"
-// #include "rclcpp/rclcpp.hpp"
-// #include "rj_geometry/point.hpp"
+#include "offense.hpp"
+#include "position.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rj_geometry/point.hpp"
 
-// namespace strategy {
+namespace strategy {
 
-// class BackAndForth : public Position {
-// public:
-//     BackAndForth(int r_id);
-//     ~BackAndForth() override = default;
+class BackAndForth : public Position {
+public:
+    BackAndForth(int r_id);
+    ~BackAndForth() override = default;
 
-// private:
-//     enum State {
-//         NULLOPT,
-//         IDLING,
-//         BASIC_MOVEMENT_1,  // move robot to location
-//         BASIC_MOVEMENT_2,  // move robot in straight line
-//         BASIC_MOVEMENT_3, // move robot back in straight line while staring at a fixed point
-//     };
+private:
+    enum State {
+        NULLOPT,
+        IDLING,
+        BASIC_MOVEMENT_1,  // move robot to location
+        BASIC_MOVEMENT_2,  // move robot in straight line
+        BASIC_MOVEMENT_3, // move robot back in straight line while staring at a fixed point
+    };
 
-//     inline static std::vector<bool> is_running_;
-//     inline static int counter = 0;
-//     bool move_on_ = false;
-//     int r_id_;
-//     inline static bool lock_ = false;
-//     inline static bool lock2_ = false;
+    inline static std::vector<bool> is_running_;
+    inline static int counter = 0;
+    bool move_on_ = false;
+    int r_id_;
+    inline static bool lock_ = false;
+    inline static bool lock2_ = false;
 
-//     State current_state_ = IDLING;
-//     State next_ = NULLOPT;
-//     Offense off_;
+    State current_state_ = IDLING;
+    State next_ = NULLOPT;
+    Offense off_;
 
-//     State update_state();
-//     std::optional<RobotIntent> derived_get_task(RobotIntent intent) override;
-//     std::optional<RobotIntent> state_to_task(RobotIntent intent);
-//     bool proceed();
-//     bool derived_check();
-//     bool start();
-// };
+    State update_state();
+    std::optional<RobotIntent> derived_get_task(RobotIntent intent) override;
+    std::optional<RobotIntent> state_to_task(RobotIntent intent);
+    bool proceed();
+    bool derived_check();
+    bool start();
+};
 
-// }  // namespace strategy
+}  // namespace strategy
