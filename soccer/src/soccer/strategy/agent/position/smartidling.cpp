0794d895cda874ccc14a6555107d30bd661cef0b
Shourik <sbanerje332@gatech.edu>
2024-03-13 00:26:13 +0000

    made SmartIdle get away from the ball in penalty position so that it is in a legal position. Next step is to add it to the cmake file so that we can test it, and then be able to switch to the position in robot_factory_position

--- soccer/src/soccer/strategy/agent/position/smartidling.cpp
+++ soccer/src/soccer/strategy/agent/position/smartidling.cpp
@@ -8,8 +8,6 @@ namespace strategy {
 
 SmartIdle::SmartIdle(int r_id) : Position{r_id, "SmartIdle"} {}
 
-SmartIdle::SmartIdle(const Position& other) : Position{other} {}
-
 std::string SmartIdle::get_current_state() { return "SmartIdle"; }
 
 /**
@@ -28,13 +26,59 @@ void SmartIdle::derived_pass_ball(){
 void SmartIdle::derived_acknowledge_ball_in_transit() {}
 
 std::optional<RobotIntent> SmartIdle::derived_get_task(RobotIntent intent) {
-	auto our_position = last_world_state_->last_world_state_->get_robot(true, robot_id_).pose.position();
-	planning::LinearMotionInstant target{ball_position};
- 	planning::MotionCommand prep_command{"escape_obstacles", target, planning::FaceBall{}};
 
- 	intent.motion_command = prep_command;
-
- 	return intent; 
+ 	latest_state_ = update_state();
+    return state_to_task(intent);
 };
+
+SmartIdle::State SmartIdle::update_state() {
+	switch(latest_state_) {
+		case IDLING: {
+			if (distance_to_ball() < 1) {
+				return GET_AWAY;
+			}
+			break;
+		}
+		case GET_AWAY: {
+			if (distance_to_ball() >= 1) {
+				return IDLING;
+			}
+			break;
+		}
+	}
+	return latest_state_;
+}
+
+
+std::optional<RobotIntent> SmartIdle::state_to_task(RobotIntent intent) {
+	switch(latest_state_) {
+		case IDLING: {
+			intent.motion_command = planning::MotionCommand{"escape_obstacles"};
+	 		return intent;
+		} case GET_AWAY: {
+			bool travel_upwards = false;
+			double y_pos = last_world_state_->ball.position.y();
+			if (y_pos - field_dimensions_.their_goal_loc().y() > 0) {
+                travel_upwards = true;
+            }
+
+            double offset = 1.5;
+            if (travel_upwards) {
+            	offset = -offset;
+            }
+
+            rj_geometry::Point target_pt{last_world_state_->get_robot(true, this->robot_id_).pose.position().x(), y_pos + offset};
+            rj_geometry::Point target_vel{0.0, 0.0};
+
+            planning::PathTargetFaceOption face_option{planning::FaceBall{}};
+
+            planning::LinearMotionInstant goal{target_pt, target_vel};
+            intent.motion_command = planning::MotionCommand{"path_target", goal, face_option, false};
+            
+            return intent;
+            break;
+		}
+	}
+}
 
 }  // namespace strategy
--- soccer/src/soccer/strategy/agent/position/smartidling.hpp
+++ soccer/src/soccer/strategy/agent/position/smartidling.hpp
@@ -1,6 +1,13 @@
+#pragma once
+
 #include <rclcpp/rclcpp.hpp>
 
-#include "position.hpp"
+#include "planning/instant.hpp"
+#include "planning/planner/motion_command.hpp"
+#include "rj_common/field_dimensions.hpp"
+#include "rj_constants/constants.hpp"
+#include "rj_geometry/point.hpp"
+#include "role_interface.hpp"
 
 namespace strategy {
 class SmartIdle : public Position {
@@ -26,5 +33,14 @@ public:
 
 private:
     std::optional<RobotIntent> derived_get_task(RobotIntent intent) override;
+
+    enum {
+        GET_AWAY,
+        IDLING
+    }
+
+    State update_state();
+
+    std::optional<RobotIntent> state_to_task(RobotIntent intent);
 };
 }  // namespace strategy
