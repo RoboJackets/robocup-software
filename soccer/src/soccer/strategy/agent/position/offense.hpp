fbc4f145b590ce181d57e6c1590dcb7ef583515b
Aalind Tyagi <tyagiaalind@gmail.com>
2024-03-27 00:44:51 +0000

    separated files

--- soccer/src/soccer/strategy/agent/position/offense.hpp
+++ soccer/src/soccer/strategy/agent/position/offense.hpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <cmath>
 #include <string>
+#include <unordered_map>
 
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
@@ -231,6 +232,10 @@ private:
      * @return the target (within the goal) that would be the most clear shot
      */
     rj_geometry::Point calculate_best_shot() const;
+
+    void broadcast_seeker_request(rj_geometry::Point seeking_point, bool adding);
+
+    std::unordered_map<int, rj_geometry::Point> seeker_points_;
 };
 
 }  // namespace strategy
