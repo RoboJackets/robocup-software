468d7ff3e3ce6eb7a58149569e08649aa8ca10bd
Zach <81196202+ZachB123@users.noreply.github.com>
2024-04-23 20:13:09 -0400

    Create goal boxes (#2224)

--- soccer/src/soccer/planning/global_state.cpp
+++ soccer/src/soccer/planning/global_state.cpp
@@ -82,6 +82,11 @@ rj_geometry::ShapeSet GlobalState::create_defense_area_obstacles() {
     auto our_defense_area{
         std::make_shared<rj_geometry::Rect>(last_field_dimensions_.our_defense_area())};
 
+    auto our_goal_area{std::make_shared<rj_geometry::Rect>(last_field_dimensions_.our_goal_area())};
+
+    auto their_goal_area{
+        std::make_shared<rj_geometry::Rect>(last_field_dimensions_.their_goal_area())};
+
     // Sometimes there is a greater distance we need to keep:
     // https://robocup-ssl.github.io/ssl-rules/sslrules.html#_robot_too_close_to_opponent_defense_area
     bool is_extra_dist_necessary = (last_play_state_.state() == PlayState::State::Stop ||
@@ -99,7 +104,9 @@ rj_geometry::ShapeSet GlobalState::create_defense_area_obstacles() {
     // Combine both defense areas into ShapeSet
     rj_geometry::ShapeSet def_area_obstacles{};
     def_area_obstacles.add(our_defense_area);
+    def_area_obstacles.add(our_goal_area);
     def_area_obstacles.add(their_defense_area);
+    def_area_obstacles.add(their_goal_area);
 
     return def_area_obstacles;
 }
