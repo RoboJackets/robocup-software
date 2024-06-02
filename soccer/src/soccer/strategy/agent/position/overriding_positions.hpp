0340e895b744384f0d258ddb7858687e6e35c383
jvogt23 <jamesvogt70@gmail.com>
2024-06-02 18:53:05 -0400

    partial message addition

---
+++ soccer/src/soccer/strategy/agent/position/overriding_positions.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace Strategy
+{
+    enum OverridingPositions {
+        AUTO = 0,
+        OFFENSE = 1,
+        DEFENSE = 2,
+        GOAL_KICKER = 3,
+        MARKER = 4,
+        PENALTY_PLAYER = 5,
+        SEEKER = 6,
+        WALLER = 7,
+    };
+} // namespace Strategy
