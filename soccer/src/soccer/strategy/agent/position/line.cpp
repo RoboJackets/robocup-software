934e7deaa8096b1d65792250db307150c17ba5ed
Sid Parikh <parikhsid16@gmail.com>
2024-07-14 20:57:23 -0500

    add line

---
+++ soccer/src/soccer/strategy/agent/position/line.cpp
@@ -0,0 +1,46 @@
+#include "line.hpp"
+
+namespace strategy {
+
+Line::Line(const Position& other) : Position{other} { position_name_ = "Line"; }
+
+Line::Line(int r_id) : Position{r_id, "Line"} {}
+
+std::optional<RobotIntent> Line::derived_get_task(RobotIntent intent) {
+    if (check_is_done()) {
+        forward_ = !forward_;
+    }
+
+    if (forward_) {
+        auto motion_command = planning::MotionCommand{
+            "path_target",
+            planning::LinearMotionInstant{
+                rj_geometry::Point{
+                    field_dimensions_.center_field_loc().x() - 1.0,
+                    field_dimensions_.center_field_loc().y() - 2.0 + robot_id_ * 0.75},
+                rj_geometry::Point{0.0, 0.0}},
+            planning::FacePoint{rj_geometry::Point{
+                field_dimensions_.center_field_loc().x() - 1.0,
+                field_dimensions_.center_field_loc().y() - 2.0 + robot_id_ * 0.75}},
+            true};
+
+        intent.motion_command = motion_command;
+    } else {
+        auto motion_command = planning::MotionCommand{
+            "path_target",
+            planning::LinearMotionInstant{
+                rj_geometry::Point{
+                    field_dimensions_.center_field_loc().x() + 1.0,
+                    field_dimensions_.center_field_loc().y() - 2.0 + robot_id_ * 0.75},
+                rj_geometry::Point{0.0, 0.0}},
+            planning::FacePoint{rj_geometry::Point{
+                field_dimensions_.center_field_loc().x() - 1.0,
+                field_dimensions_.center_field_loc().y() - 2.0 + robot_id_ * 0.75}},
+            true};
+
+        intent.motion_command = motion_command;
+    }
+
+    return intent;
+}
+}  // namespace strategy
