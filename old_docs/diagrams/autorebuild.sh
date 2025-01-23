fd609345516bbe2bd9937129db2f7b4e25a316f0
Siddharth Parikh <parikhsid16@gmail.com>
2025-01-23 10:38:27 -0500

    Annual Tradition of Deleting Stuff We Haven't Used In Years (#2311)

--- old_docs/diagrams/autorebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Run this script to rebuild the image files every time a .dot file changes
-
-filewatcher *.dot make
--- save_params.bash
+++
@@ -1,5 +0,0 @@
-nodes=$(ros2 node list)
-for node in ${nodes}
-do
-    ros2 param dump ${node} --print
-done
