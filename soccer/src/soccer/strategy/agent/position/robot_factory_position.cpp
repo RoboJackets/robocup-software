bd5bff0695f34cb355fbe1cf60aedc937a744204
jvogt23 <91633361+jvogt23@users.noreply.github.com>
2025-01-26 19:26:58 -0500

    Update soccer/src/soccer/strategy/agent/position/robot_factory_position.cpp
    Co-authored-by: Siddharth Parikh <parikhsid16@gmail.com>

--- soccer/src/soccer/strategy/agent/position/robot_factory_position.cpp
+++ soccer/src/soccer/strategy/agent/position/robot_factory_position.cpp
@@ -128,7 +128,9 @@ void RobotFactoryPosition::handle_ready() {
 
 void RobotFactoryPosition::update_position() {
     bool manual_position_set = check_for_position_override();
-    if (manual_position_set) return;
+    if (manual_position_set) {
+        return
+    };
 
     switch (current_play_state_.state()) {
         case PlayState::State::Playing: {
