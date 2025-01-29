64cbc9d649237088293b18617024728a6ea8803f
jvogt23 <jamesvogt70@gmail.com>
2025-01-28 20:09:55 -0500

    Last thing

--- soccer/src/soccer/strategy/agent/position/overriding_positions.hpp
+++ soccer/src/soccer/strategy/agent/position/overriding_positions.hpp
@@ -8,7 +8,7 @@ namespace Strategy {
     OverridingPositions refers to the positions that can be manually set in the UI.
     Normally, all robots are set to Auto. If you want to add a new position, add a new value
     to this enum, add a string to the overriding_position_labels vector in
-   main_window.hpp, and add a case to the check_for_position_override method in
+   main_window.hpp, and add a case to the set_position_override_if_requested method in
    RobotFactoryPosition.
 */
 enum OverridingPositions {
