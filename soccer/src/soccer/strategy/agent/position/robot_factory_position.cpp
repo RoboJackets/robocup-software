e553080b82e60c8fc42c6ba8f4823e6b5217f852
rishiso <rishiso@users.noreply.github.com>
2025-02-03 02:59:45 +0000

    automated style fixes

--- soccer/src/soccer/strategy/agent/position/robot_factory_position.cpp
+++ soccer/src/soccer/strategy/agent/position/robot_factory_position.cpp
@@ -246,7 +246,6 @@ void RobotFactoryPosition::set_default_position() {
     // Checking whether we have possesion or if the ball is on their half
     if (our_possession_ || last_world_state_->ball.position.y() >
                                field_dimensions_.center_field_loc().y() - kBallDiameter) {
-        
         // Offensive mode
         // Closest 2 robots on defense, rest on offense
         if (i <= 1) {
