73389da2c10854da8a4032f2bf85d96c1c3c9c45
petergarud <peter.garud04@gmail.com>
2025-02-02 21:43:01 -0500

    First node spun up

--- launch/soccer.launch.py
+++ launch/soccer.launch.py
@@ -217,5 +217,12 @@ def generate_launch_description():
                 parameters=[param_config_filepath],
                 on_exit=Shutdown(),
             ),
+            Node(
+                package="rj_robocup",
+                executable="config_server_param_provider",
+                output="screen",
+                parameters=[param_config_filepath],
+                on_exit=Shutdown(),
+            ),
         ]
     )
---
+++ soccer/src/rj_param_servers/config_server_param_provider.cpp
@@ -0,0 +1,20 @@
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/node.hpp>
+
+class ConfigServerParamProvider : public rclcpp::Node
+{
+public:
+    ConfigServerParamProvider() : Node("config_server_param_provider",
+                                   rclcpp::NodeOptions()
+                                       .allow_undeclared_parameters(true)
+                                       .automatically_declare_parameters_from_overrides(true)) {}
+private:
+};
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<ConfigServerParamProvider>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
