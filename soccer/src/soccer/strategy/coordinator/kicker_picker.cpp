472925a5a8b0d0381199736cd15e3fa9f1eb1f2a
Sid Parikh <parikhsid16@gmail.com>
2025-03-04 17:47:57 -0500

    kicker picker coordinator

--- soccer/src/soccer/strategy/coordinator/kicker_picker.cpp
+++ soccer/src/soccer/strategy/coordinator/kicker_picker.cpp
@@ -1,10 +1,63 @@
-namespace strategy
-{
-    class KickerPicker {
-        KickerPicker() {
-            
+#include "kicker_picker.hpp"
+#include <algorithm>  // for std::any_of
+#include <limits>
+#include <rj_convert/ros_convert.hpp>
+#include <rj_msgs/msg/kicker_picker.hpp>
+#include <rj_constants/topic_names.hpp>
+
+namespace strategy {
+
+KickerPicker::KickerPicker() 
+    : Coordinator("kicker_picker_srv", "kicker_picker_data") {
+    // Subscribe to world state
+    world_state_sub_ = this->create_subscription<rj_msgs::msg::WorldState>(
+        vision_filter::topics::kWorldStateTopic, rclcpp::QoS(1),
+        [this](rj_msgs::msg::WorldState::SharedPtr world_state) {  // NOLINT
+            last_world_state_ = rj_convert::convert_from_ros(*world_state);
+            publish_selected_kicker();
+        });
+}
+
+void KickerPicker::service_callback(RequestPtr request, ResponsePtr response) {
+    bool membership_changed = false;
+    if (request->wants_to_kick) {
+        membership_changed = !kicker_group_members_[request->robot_id];
+        kicker_group_members_[request->robot_id] = true;
+    } else {
+        membership_changed = kicker_group_members_[request->robot_id];
+        kicker_group_members_[request->robot_id] = false;
+    }
+
+    if (membership_changed) {
+        publish_selected_kicker();
+    }
+
+    response->success = true;
+}
+
+void KickerPicker::publish_selected_kicker() {
+    // Find closest robot to ball among group members
+    double min_distance = std::numeric_limits<double>::infinity();
+    uint8_t selected_kicker = kInvalidRobotId; // 
+
+    const auto& ball_pos = last_world_state_.ball.position;
+
+    for (uint8_t i = 0; i < kNumShells; ++i) {
+        if (kicker_group_members_[i]) {
+            const auto& robot = last_world_state_.get_robot(true, i);
+            double distance = ball_pos.dist_to(robot.pose.position());
+            if (distance < min_distance) {
+                min_distance = distance;
+                selected_kicker = i;
+            }
         }
-        ~KickerPicker() = default;
     }
     
+    // Only publish if the selected kicker has changed
+    if (selected_kicker != last_published_kicker_) {
+        publisher_->publish(rj_msgs::msg::KickerPicker().set__robot_id(selected_kicker));
+        last_published_kicker_ = selected_kicker;
+    }
+}
+
 } // namespace strategy
--- soccer/src/soccer/strategy/coordinator/kicker_picker.hpp
+++ soccer/src/soccer/strategy/coordinator/kicker_picker.hpp
@@ -1,17 +1,31 @@
-#include <rclcpp/rclcpp.hpp>
+#pragma once
 
+#include <array>
+#include <rclcpp/rclcpp.hpp>
+#include <rj_msgs/msg/kicker_picker.hpp>
+#include <rj_msgs/msg/world_state.hpp>
+#include <rj_msgs/srv/kicker_picker.hpp>
 #include "coordinator.hpp"
+#include "rj_constants/constants.hpp"
+#include "world_state.hpp"
 
 namespace strategy {
 
-    class KickerPicker : Coordinator {
+class KickerPicker : public Coordinator<KickerPicker, rj_msgs::srv::KickerPicker, rj_msgs::msg::KickerPicker> {
+public:
+    static constexpr uint8_t kInvalidRobotId = kNumShells;
 
-        KickerPicker();
-        ~KickerPicker();
+    KickerPicker();
+    ~KickerPicker() = default;
 
-        
+    void service_callback(RequestPtr request, ResponsePtr response);
 
-    }
+private:
+    void publish_selected_kicker();
 
-
+    std::array<bool, kNumShells> kicker_group_members_ = {false};  // All elements initialized to false
+    WorldState last_world_state_;
+    rclcpp::Subscription<rj_msgs::msg::WorldState>::SharedPtr world_state_sub_;
+    uint8_t last_published_kicker_ = kInvalidRobotId;  // Track last published kicker
+};
 }
