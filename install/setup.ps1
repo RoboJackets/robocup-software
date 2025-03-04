f2fe5e609474e9af9c2b068b004a57d2b7cbc5d3
Sid Parikh <parikhsid16@gmail.com>
2025-03-04 15:12:28 -0500

    wip unsure

--- install/setup.ps1
+++ install/setup.ps1
@@ -21,9 +21,6 @@ function _colcon_prefix_chain_powershell_source_script {
   }
 }
 
-# source chained prefixes
-_colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
-
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
 _colcon_prefix_chain_powershell_source_script "$env:COLCON_CURRENT_PREFIX\local_setup.ps1"
