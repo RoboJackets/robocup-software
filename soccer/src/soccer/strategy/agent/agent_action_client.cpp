f94f2248426b452098d14105cc9e2b6afd08337b
sid-parikh <sid-parikh@users.noreply.github.com>
2025-03-26 00:32:05 +0000

    automated style fixes

--- soccer/src/soccer/strategy/agent/agent_action_client.cpp
+++ soccer/src/soccer/strategy/agent/agent_action_client.cpp
@@ -55,7 +55,9 @@ AgentActionClient::AgentActionClient(int r_id)
 
     override_play_sub_ = create_subscription<rj_msgs::msg::OverridePosition>(
         "override_position/robot_" + std::to_string(r_id), 1,
-        [this](rj_msgs::msg::OverridePosition::SharedPtr msg) { test_play_callback(msg); }); // NOLINT
+        [this](rj_msgs::msg::OverridePosition::SharedPtr msg) {
+            test_play_callback(msg);
+        });  // NOLINT
 
     robot_communication_srv_ = create_service<rj_msgs::srv::AgentCommunication>(
         fmt::format("agent_{}_incoming", r_id),
