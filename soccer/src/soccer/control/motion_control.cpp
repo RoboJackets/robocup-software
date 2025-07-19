8ed4aff9a6263ef150e175f7fd6d761b7339abee
Richard Koulen <96697985+rhkoulen@users.noreply.github.com>
2025-07-19 09:46:07 -0400

    pid6 (#2394)
    * pid6
    
    * oops else
    
    ---------
    
    Co-authored-by: Sid Parikh <parikhsid16@gmail.com>

--- soccer/src/soccer/control/motion_control.cpp
+++ soccer/src/soccer/control/motion_control.cpp
@@ -40,6 +40,79 @@ DEFINE_FLOAT64(params::kMotionControlParamModule, translation_kd, 0.3,
 DEFINE_INT64(params::kMotionControlParamModule, translation_windup, 0,
              "Windup limit for translation (unknown units)");
 
+
+struct {
+    double rotation_kd;
+    double rotation_ki;
+    double rotation_kp;
+    int64_t rotation_windup;
+    double translation_kd;
+    double translation_ki;
+    double translation_kp;
+    int64_t translation_windup;
+} robot_params[6] = {
+{
+     0.1,
+     0.002,
+     7.5,
+     0,
+     0.05,
+     0.015,
+     1.25,
+     0
+},
+{
+     0.1,
+     0.002,
+     7.5,
+     0,
+     0.05,
+     0.015,
+     1.25,
+     0
+},
+{
+     0.1,
+     0.002,
+     7.5,
+     0,
+     0.05,
+     0.015,
+     1.25,
+     0
+},
+{
+     0.1,
+     0.002,
+     7.5,
+     0,
+     0.05,
+     0.015,
+     1.25,
+     0
+},
+{
+     0.1,
+     0.002,
+     7.5,
+     0,
+     0.05,
+     0.015,
+     1.25,
+     0
+},
+{
+     0.1,
+     0.002,
+     7.5,
+     0,
+     0.05,
+     0.015,
+     1.25,
+     0
+},
+};
+
 MotionControl::MotionControl(int shell_id, rclcpp::Node* node)
     : shell_id_(shell_id),
       angle_controller_(0, 0, 0, 50, 0),
@@ -230,6 +303,24 @@ void MotionControl::set_velocity(MotionSetpoint* setpoint, Twist target_vel) {
 }
 
 void MotionControl::update_params() {
+
+    if (shell_id_ < 6) {
+        position_x_controller_.kp = static_cast<float>(robot_params[shell_id_].translation_kp);
+        position_x_controller_.ki = static_cast<float>(robot_params[shell_id_].translation_ki);
+        position_x_controller_.kd = static_cast<float>(robot_params[shell_id_].translation_kd);
+        position_x_controller_.setWindup(robot_params[shell_id_].translation_windup);
+
+        position_y_controller_.kp = static_cast<float>(robot_params[shell_id_].translation_kp);
+        position_y_controller_.ki = static_cast<float>(robot_params[shell_id_].translation_ki);
+        position_y_controller_.kd = static_cast<float>(robot_params[shell_id_].translation_kd);
+        position_y_controller_.setWindup(robot_params[shell_id_].translation_windup);
+
+        angle_controller_.kp = static_cast<float>(robot_params[shell_id_].rotation_kp);
+        angle_controller_.ki = static_cast<float>(robot_params[shell_id_].rotation_ki);
+        angle_controller_.kd = static_cast<float>(robot_params[shell_id_].rotation_kd);
+        angle_controller_.setWindup(robot_params[shell_id_].rotation_windup);
+    } else {
+
     // Update PID parameters
     position_x_controller_.kp = static_cast<float>(PARAM_translation_kp);
     position_x_controller_.ki = static_cast<float>(PARAM_translation_ki);
@@ -245,6 +336,7 @@ void MotionControl::update_params() {
     angle_controller_.ki = static_cast<float>(PARAM_rotation_ki);
     angle_controller_.kd = static_cast<float>(PARAM_rotation_kd);
     angle_controller_.setWindup(PARAM_rotation_windup);
+    }
 }
 
 void MotionControl::reset() {
