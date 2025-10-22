e04d73e3f8ed08a74a66e8230c04f0692dfc936d
Yuvraj Dhadwal <ydhadwal3@gatech.edu>
2025-10-21 21:51:56 -0400

    theoretically finished benchmarking

--- src/rj_utils/include/rj_utils/latency_benchmarking.hpp
+++ src/rj_utils/include/rj_utils/latency_benchmarking.hpp
@@ -1,14 +1,12 @@
-#pragma once
-#include <algorithm>
-#include <chrono>
-#include <ctime>
-#include <cstdint>
-#include <cstdlib>
-#include <fstream>
 #include <string>
 #include <unordered_map>
-#include <utility>
 #include <vector>
+#include <iostream>
+#include <chrono>
+#include <numeric>
+#include <fstream>
+
+#include <cstdint>
 
 namespace latency_benchmarking {
 
@@ -20,28 +18,52 @@ public:
     return R;
   }
 
-  void record(const char* label, uint64_t ns) {
-
+  void record(std::string label, uint64_t time) {
+    registry_[label].push_back(time);
   }
 
   void dump() {
-
+    std::ofstream output_file;
+    output_file.open(path_);
+    for (auto& p : registry_) {
+        output_file << p.first << "     ";
+        for (uint64_t e : p.second) {
+            output_file << e << " ";
+        }
+        output_file << "AVG: " << std::accumulate(p.second.begin(), p.second.end(), 0) / p.second.size();
+        output_file << "\n";
+    }
+    output_file.close();
   }
 
-  void clear() {}
+  void clear() {
+    registry_.clear();
+  }
 
 private:
   // Constructor and creates callback that will dump latency statistics at program end
-  Registry() {
-    std::atexit([] {Registry::instance.dump();});
+  Registry() {}
+  ~Registry() {
+    Registry::instance().dump();
   }
 
-  const time_t now = std::time(nullptr);
-  std::tm* localTime = std::localtime(&now);
-  std::string path_ = "../log/latency_" + localTime->tm_year + 1900 + "_" + localTime->tm_mon + 1 + "_" + localTime->tm_mday
+  std::string path_ = "../../log/latency.txt";
+  std::unordered_map<std::string, std::vector<uint64_t>> registry_;
+};
 
+class Timer{
 
-}
+public:
+  Timer(std::string label) : label_(label), start_(std::chrono::steady_clock::now()) {}
+  ~Timer() {
+    uint64_t time = static_cast<uint64_t>(std::chrono::
+      duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start_).count());
+    Registry::instance().record(label_, time);
+  }
+private:
+const std::chrono::steady_clock::time_point start_;
+std::string label_;
+};
 
 
 } // namespace latency_benchmarking
