553a1ca742ab25a3d6c1015f906afebce2d758ef
Pooja Dayaratna <poojadayaratna@gmail.com>
2025-11-08 20:25:17 +0000

    create runner class

---
+++ src/rj_strategy/include/rj_strategy/agent/position/runner.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "position.hpp"
+
+namespace strategy {
+
+    // creates a new class Runner that inherits from Position
+    class Runner : public Position {
+        public:
+            // basic Runner constructor
+            Runner();
+    };
+}
