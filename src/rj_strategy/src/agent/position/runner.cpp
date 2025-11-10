93b0df0db5e117195f0bd6b94e8f1da359b4234a
Pooja Dayaratna <poojadayaratna@gmail.com>
2025-11-10 01:31:11 +0000

    finished runner.cpp file

--- src/rj_strategy/src/agent/position/runner.cpp
+++ src/rj_strategy/src/agent/position/runner.cpp
@@ -1,73 +1,61 @@
 #include "rj_strategy/agent/position/runner.hpp"
 
-#include "rj_geometry/point.hpp"
-#include "rj_common/planning/instant.hpp"
-#include "rj_common/planning/motion_command.hpp"
-#include <rj_common/planning/instant.hpp>
-
 namespace strategy {
-    Runner::Runner(int r_id) : Position(r_id), current_state_(State::SIDE_1) {}
-    Runner::Runner(const Position& other) : Position(other), current_state_(SIDE_1) {}
+    Runner::Runner(int r_id) : Position{r_id, "Runner"} {}
+    Runner::Runner(const Position& other) : Position{other} {}
 
-    std::string Runner::get_current_state() {
-        switch (current_state_) {
-            case SIDE_1:
-                return "SIDE_1";
-            case SIDE_2:
-                return "SIDE_2";
-            case SIDE_3:
-                return "SIDE_3";
-            case SIDE_4:
-                return "SIDE_4";
-        }
-        return "UNKNOWN";
+    std::string Runner::get_current_state() { 
+        return "Runner"; 
     }
-    
-    Runner::State Runner::next_state(State s) {
-        switch(s) {
-            // In case SIDE_1, return SIDE_2
-            // In case SIDE_2, return SIDE_3
-            // etc
-            case State::SIDE_1:
-                return State::SIDE_2;
-            case State::SIDE_2:
-                return State::SIDE_3;
-            case State::SIDE_3:
-                return State::SIDE_4;
-            case State::SIDE_4:
-                return State::SIDE_1;
+
+    Runner::State Runner::next_state() {
+        if (!check_is_done()) {
+            return current_state_;
         }
 
-        // If none of the above cases are met, return SIDE_1
-        return State::SIDE_1;
+        switch(current_state_) {
+            case TOP_LEFT:
+                return TOP_RIGHT;
+            case TOP_RIGHT:
+                return BOTTOM_RIGHT;
+            case BOTTOM_RIGHT:
+                return BOTTOM_LEFT;
+            case BOTTOM_LEFT:
+                return TOP_LEFT;
+            default:
+                return current_state_;
+        }
     }
-    
+
     std::optional<RobotIntent> Runner::derived_get_task(RobotIntent intent) {
-        // Get current robot position
-        auto my_robot = last_world_state_->get_robot(true, robot_id_);
-        auto current_pos = my_robot.pose.position();
+        // set current state to next state
+        current_state_ = next_state();
+        return state_to_task(intent);
+    }
 
-        // Define a target point (example: move along x-axis)
-        rj_geometry::Point target{1.0, current_pos.y()};
+    // gets called once per 'tick'
+    std::optional<RobotIntent> Runner::state_to_task(RobotIntent intent) {
+        planning::LinearMotionInstant target;
+        switch (current_state_) {
+            case TOP_LEFT:
+                target = planning::LinearMotionInstant{rj_geometry::Point{2.0, 2.5}};
+                break;
+            case TOP_RIGHT:
+                target = planning::LinearMotionInstant{rj_geometry::Point{-2.0, 2.5}};
+                break;
+            case BOTTOM_RIGHT:
+                target = planning::LinearMotionInstant{rj_geometry::Point{-2.0, 6.5}};
+                break;
+            case BOTTOM_LEFT:
+                target = planning::LinearMotionInstant{rj_geometry::Point{2.0, 6.5}};
+                break;
+        }
 
-        // Create LinearMotionInstant from current to target
-        planning::LinearMotionInstant motion{current_pos, target};
-
-        // Assign to motion_command
-        intent.motion_command = planning::MotionCommand{"path_target", motion};
-
+        planning::MotionCommand prep_command{"path_target", target, planning::FaceTarget{}};
+        intent.motion_command = prep_command;
+        // intent defines what the robot is doing in that instant
+        // the returned intent is the direction the robot wants to do towards
         return intent;
     }
 
-    // update method
-    void Runner::update(const WorldState* world_state) {
-        
-        //sets my_robot to rj team robot with a given ID
-        auto my_robot = world_state->get_robot(true, robot_id_);
-        auto current_position = my_robot.pose.position();
-
-        rj_geometry::Point target{3.0, 0.0};
-
-        planning::MotionCommand motion_cmd{"path_target", planning::LinearMotionInstant(current_position, target)};
-    }
 }
