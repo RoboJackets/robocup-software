0925a158eb0637061a43454cf37806ee39bd1d4d
Pooja Dayaratna <poojadayaratna@gmail.com>
2025-11-10 01:30:13 +0000

    finished runner file

--- src/rj_strategy/include/rj_strategy/agent/position/runner.hpp
+++ src/rj_strategy/include/rj_strategy/agent/position/runner.hpp
@@ -1,31 +1,35 @@
-#pragma once
+// ros specific imports
+#include <rclcpp/rclcpp.hpp>
+
+//position class that runner is inheriting
 #include "rj_strategy/agent/position.hpp"
 
 namespace strategy {
-
-    // creates a new class Runner that inherits from Position
+    // class Runner that inherits from Position
     class Runner : public Position {
         public:
-            // basic Runner constructor
-            Runner(int r_id);
+            // "standard" constructor we will be using
+            Runner(int runner_id);
+            /* Class destructor*/
+            // things you can do right before constructor is erased from memory
+            ~Runner() = default;
             Runner(const Position& other);
 
+            // overrides the get_current_state method from position 
             std::string get_current_state() override;
 
-            void update(const WorldState* world_state);
-
         private:
+            // updates current state, based on update return state_to_task (kind of like a wrapper for state_to_task)
             std::optional<RobotIntent> derived_get_task(RobotIntent intent) override;
 
-            // enum for private state variables
-            enum State {
-                SIDE_1,
-                SIDE_2,
-                SIDE_3,
-                SIDE_4
-            };
+            // enum of all the different States we can have
+            enum State {TOP_LEFT, TOP_RIGHT, BOTTOM_RIGHT, BOTTOM_LEFT};
 
-            State current_state_;
-            State next_state(State s);
+            State current_state_ = State::TOP_LEFT;
+
+            State next_state();
+
+            // based the current state, what do we need to do
+            std::optional<RobotIntent> state_to_task(RobotIntent intent);
     };
 }
