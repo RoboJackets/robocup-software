19015c60e91f2c988d468a2605f534e8e57f70fd
shourikb <shourikb@users.noreply.github.com>
2025-11-12 02:58:02 +0000

    automated style fixes

--- src/rj_strategy/src/coordinator/marking_client.cpp
+++ src/rj_strategy/src/coordinator/marking_client.cpp
@@ -49,7 +49,8 @@ void MarkingClient::join_group(StatusCallback callback) {
             // Create subscription to track selected kicker.
             subscription_ = node_->create_subscription<rj_msgs::msg::Marking>(
                 "marking_data", rclcpp::QoS(1).transient_local(),
-                [this](const rj_msgs::msg::Marking::SharedPtr msg) {  // callback=std::move(callback)
+                [this](
+                    const rj_msgs::msg::Marking::SharedPtr msg) {  // callback=std::move(callback)
                     selected_robot_marking_id_ = msg->mark_robot_ids[robot_id_];
                     am_i_marking_ = (selected_robot_marking_id_ != kInvalidRobotId);
                     // callback(Result{true, selected_robot_marking_id_});
