502c3b633769d5042938665ab5d0acf232ee58de
yuvrajdhadwal <yuvrajdhadwal@users.noreply.github.com>
2025-11-17 01:09:40 +0000

    automated style fixes

--- src/rj_benchmarking/include/rj_benchmarking/registry.hpp
+++ src/rj_benchmarking/include/rj_benchmarking/registry.hpp
@@ -1,33 +1,32 @@
 #pragma once
 
-#include <rj_msgs/msg/latency.hpp>
-
-#include <spdlog/spdlog.h>
-#include <rclcpp/rclcpp.hpp>
-
+#include <array>
+#include <cstdint>
+#include <ctime>
+#include <filesystem>
+#include <fstream>
+#include <iterator>
+#include <numeric>
 #include <string>
 #include <unordered_map>
-#include <array>
 #include <vector>
-#include <numeric>
-#include <fstream>
-#include <cstdint>
-#include <filesystem>
-#include <ctime>
-#include <iterator>
 
-class Registry : public rclcpp::Node
-{
+#include <rclcpp/rclcpp.hpp>
+#include <spdlog/spdlog.h>
+
+#include <rj_msgs/msg/latency.hpp>
+
+class Registry : public rclcpp::Node {
 public:
     Registry();
     ~Registry();
 
 private:
-    void topic_callback(const rj_msgs::msg::Latency &msg);
+    void topic_callback(const rj_msgs::msg::Latency& msg);
     void dump();
 
     std::string get_curr_datetime();
-    
+
     // registry[robot_id][label] -> latency sampling
     std::array<std::unordered_map<std::string, std::vector<uint64_t>>, 6> registry_{};
     rclcpp::Subscription<rj_msgs::msg::Latency>::SharedPtr subscription_{};
--- src/rj_benchmarking/src/registry.cpp
+++ src/rj_benchmarking/src/registry.cpp
@@ -1,28 +1,22 @@
 #include "rj_benchmarking/registry.hpp"
 
-Registry::Registry() : rclcpp::Node{"rj_benchmarking"}
-{
+Registry::Registry() : rclcpp::Node{"rj_benchmarking"} {
     // SPDLOG_INFO("TESTING: Registry Built");
     subscription_ = this->create_subscription<rj_msgs::msg::Latency>(
-            "/registry", 100, std::bind(&Registry::topic_callback, this,
-                std::placeholders::_1));
+        "/registry", 100, std::bind(&Registry::topic_callback, this, std::placeholders::_1));
 }
 
-Registry::~Registry()
-{
+Registry::~Registry() {
     dump();
     // SPDLOG_INFO("TESTING: Registry Destroyed");
 }
 
-void Registry::topic_callback(const rj_msgs::msg::Latency &msg)
-{
+void Registry::topic_callback(const rj_msgs::msg::Latency& msg) {
     registry_.at(msg.robot_id)[msg.label].push_back(msg.duration_ns);
     max_rows_ = std::max(max_rows_, static_cast<int>(registry_.at(msg.robot_id)[msg.label].size()));
 }
 
-
-void Registry::dump()
-{
+void Registry::dump() {
     // SPDLOG_INFO("TESTING: Dump Called");
 
     /*
@@ -32,7 +26,7 @@ void Registry::dump()
             3a. first row is labels
         4. Make csvs for all robots
     */
-    std::string base_path{ "./latency" };
+    std::string base_path{"./latency"};
     std::filesystem::create_directories(base_path);
     base_path += "/session_";
     base_path += get_curr_datetime();
@@ -40,33 +34,27 @@ void Registry::dump()
 
     // std::ofstream output_file;
     // output_file.open(path_);
-    for (int i = 0; i < 6; i++)
-    {
+    for (int i = 0; i < 6; i++) {
         std::stringstream ss;
         ss << "/robot_" << i << ".csv";
-        std::ofstream robot_csv{ base_path + ss.str() };
+        std::ofstream robot_csv{base_path + ss.str()};
 
-        for (int row = -1; row < max_rows_; row++)
-        {
-            if (row == -1)
-            {
-                for (std::pair<std::string, std::vector<uint64_t>> labels : registry_.at(i))
-                {
+        for (int row = -1; row < max_rows_; row++) {
+            if (row == -1) {
+                for (std::pair<std::string, std::vector<uint64_t>> labels : registry_.at(i)) {
                     robot_csv << labels.first << ',';
                 }
 
                 robot_csv << '\n';
             }
 
-            for (std::pair<std::string, std::vector<uint64_t>> labels : registry_.at(i))
-            {
+            for (std::pair<std::string, std::vector<uint64_t>> labels : registry_.at(i)) {
                 robot_csv << labels.second[row] << ',';
             }
 
             robot_csv << '\n';
         }
 
-        
         // output_file << "Robot " << i << '\n';
         // for (auto& p : registry_[i])
         // {
@@ -76,19 +64,18 @@ void Registry::dump()
         //         output_file << e << ", ";
         //     }
         //     output_file << "AVG: " << std::accumulate(p.second.begin(), p.second.end(), 0)
-        //                                                                         / p.second.size();
+        //                                                                         /
+        //                                                                         p.second.size();
         //     output_file << '\n';
         // }
     }
     // output_file.close();
 }
 
-std::string Registry::get_curr_datetime()
-{
+std::string Registry::get_curr_datetime() {
     std::time_t time = std::time({});
     char timeString[std::size("yyyy-mm-ddThh:mm:ssZ")];
-    std::strftime(std::data(timeString), std::size(timeString),
-                  "%FT%TZ", std::localtime(&time));
-    std::string out{ timeString };
+    std::strftime(std::data(timeString), std::size(timeString), "%FT%TZ", std::localtime(&time));
+    std::string out{timeString};
     return out;
 }
