c045a293e73d2390ff43bb52186d508bc3cb5943
Yuvraj Dhadwal <ydhadwal3@gatech.edu>
2025-11-19 00:33:48 -0500

    switched to ros2 timer so that it matches sim time rather than real time

--- src/rj_benchmarking/include/rj_benchmarking/timer.hpp
+++ src/rj_benchmarking/include/rj_benchmarking/timer.hpp
@@ -11,13 +11,19 @@
 
 #include "rj_benchmarking/registry_publisher.hpp"
 
+// Lazy static ros_clock
+static rclcpp::Clock& ros_clock() {
+    static rclcpp::Clock clock(RCL_STEADY_TIME);
+    return clock;
+}
+
 class Timer {
 public:
     Timer(const std::string& label, uint8_t robot_id);
     ~Timer();
 
 private:
-    std::string label_;
-    std::uint8_t robot_id_;
-    const std::chrono::steady_clock::time_point start_;
+    const std::string label_;
+    const std::uint8_t robot_id_;
+    const rclcpp::Time start_;
 };
--- src/rj_benchmarking/src/timer.cpp
+++ src/rj_benchmarking/src/timer.cpp
@@ -1,11 +1,10 @@
 #include "rj_benchmarking/timer.hpp"
 
 Timer::Timer(const std::string& label, uint8_t robot_id)
-    : label_(label), robot_id_(robot_id), start_(std::chrono::steady_clock::now()) {}
+    : label_(label), robot_id_(robot_id), start_(ros_clock().now()) {}
 
 Timer::~Timer() {
-    uint64_t time = static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(
-                                              std::chrono::steady_clock::now() - start_)
-                                              .count());
-    RegistryPublisher::getInstance()->publish(label_, robot_id_, time);
+    rclcpp::Duration duration = ros_clock().now() - start_;
+    uint64_t time_ns = static_cast<uint64_t>(duration.nanoseconds());
+    RegistryPublisher::getInstance()->publish(label_, robot_id_, time_ns);
 }
