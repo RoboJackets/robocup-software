854d7b11ce73b93231af23fb9923c1f09fd1e2bc
Yuvraj Dhadwal <146681831+yuvrajdhadwal@users.noreply.github.com>
2025-12-03 23:11:51 -0500

    Feature/latency benchmarking (#2461)
    * started work
    
    * theoretically finished benchmarking
    
    * i'm confused
    
    * created a new node for rj_benchmarking
    
    * started working on registry within the node, there is an issue with multiple Registrys being created? possibly cpp rule of 5
    
    * aaaaaaalind
    
    * end of day work
    
    * starting work to transform to pub/sub model
    
    * its working! we are now measuring latency for pivots!
    
    * added benchmarking as a library import to all packages
    
    * linting
    
    * added new flush mechanism so that latency files are created and easy to use for later data analysis
    
    * removed timer calls in other code
    
    * Fix Code Style On feature/latency_benchmarking (#2462)
    
    automated style fixes
    
    Co-authored-by: yuvrajdhadwal <yuvrajdhadwal@users.noreply.github.com>
    
    * fixed pr comments
    
    * Fix missing newline at end of rotate_path_planner.hpp
    
    * whoops missed some
    
    * more cleanup
    
    * Fix Code Style On feature/latency_benchmarking (#2464)
    
    automated style fixes
    
    Co-authored-by: yuvrajdhadwal <yuvrajdhadwal@users.noreply.github.com>
    
    * added package dependencies
    
    * switched to ros2 timer so that it matches sim time rather than real time
    
    * added sentinel value for timer rows smaller than max_rows
    
    * fixed comment
    
    * Fix Code Style On feature/latency_benchmarking (#2476)
    
    automated style fixes
    
    Co-authored-by: yuvrajdhadwal <yuvrajdhadwal@users.noreply.github.com>
    
    * removed commit
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: yuvrajdhadwal <yuvrajdhadwal@users.noreply.github.com>

---
+++ src/rj_benchmarking/src/timer.cpp
@@ -0,0 +1,10 @@
+#include "rj_benchmarking/timer.hpp"
+
+Timer::Timer(const std::string& label, uint8_t robot_id)
+    : label_(label), robot_id_(robot_id), start_(ros_clock().now()) {}
+
+Timer::~Timer() {
+    rclcpp::Duration duration = ros_clock().now() - start_;
+    uint64_t time_ns = static_cast<uint64_t>(duration.nanoseconds());
+    RegistryPublisher::getRegistryPub()->publish(label_, robot_id_, time_ns);
+}
