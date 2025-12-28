0b9f85222c2160aad34268b833ad7129e7ba8671
IanC456 <63746168+IanC456@users.noreply.github.com>
2025-12-28 17:52:55 -0500

    Added state enums and methods

--- src/rj_strategy/include/rj_strategy/agent/position/runner.hpp
+++ src/rj_strategy/include/rj_strategy/agent/position/runner.hpp
@@ -7,5 +7,31 @@ namespace strategy {
             ~Runner() override = default;
             Runner(const Position& other);
 
+            std::string get_current_state() override;
+
+        private:
+            /**
+             * @brief Overriden from Position. Calls next_state and then state_to_task on each tick.
+             */
+            std::optional<RobotIntent> derived_get_task(RobotIntent intent) override;
+
+            enum State {
+                DEFAULT,           // Decide what to do
+                FORWARD,     // moving forward towards opponent's goal
+                LEFT,           // moving left along opponent's end line
+                BACKWARD // moving back towards its own goal,
+                RIGHT // moving right along its own end line
+            };
+
+            /**
+             * @return what the state should be right now. called on each get_task tick
+             */
+            State next_state();
+
+            /**
+             * @return the task to execute. called on each get_task tick AFTER next_state()
+             */
+            std::optional<RobotIntent> state_to_task(RobotIntent intent);
+        
     }
 }
