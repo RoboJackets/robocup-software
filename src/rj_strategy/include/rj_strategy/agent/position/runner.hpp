d00a15ac6ee451d13f391cb168874710022d6f89
IanC456 <63746168+IanC456@users.noreply.github.com>
2025-12-28 17:36:33 -0500

    Created runner files

---
+++ src/rj_strategy/include/rj_strategy/agent/position/runner.hpp
@@ -0,0 +1,8 @@
+#include "rj_strategy/agent/position.hpp"
+
+namespace strategy {
+    class Runner : public Position {
+        public:
+            
+    }
+}
---
+++ src/rj_strategy/src/agent/position/runner.cpp
@@ -0,0 +1,6 @@
+#include "rj_strategy/agent/position/runner.hpp"
+
+namespace strategy {
+    Runner::Runner(int r_id) : 
+    
+}
