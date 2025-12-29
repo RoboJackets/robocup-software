74354e039b3727fe58f2a9699ed97804ad3a2c79
IanC456 <63746168+IanC456@users.noreply.github.com>
2025-12-28 21:06:10 -0500

    Added derived_get_task() and get_current_state()

--- src/rj_strategy/src/agent/position/runner.cpp
+++ src/rj_strategy/src/agent/position/runner.cpp
@@ -42,4 +42,20 @@ namespace strategy {
             return current_state_;
         }
     }
+
+    std::optional<RobotIntent> Runner::derived_get_task(RobotIntent intent) {
+        // Get next state, and if different, reset clock
+        State new_state = next_state();
+        if (current_state_ != new_state) {
+            SPDLOG_INFO("Robot {}: now {}", robot_id_, state_to_name(current_state_));
+        }
+        current_state_ = new_state;
+        // Calculate task based on state
+        return state_to_task(intent);
+    }
+
+    std::string Runner::get_current_state() {
+        return std::string{"Runner"} + std::to_string(static_cast<int>(current_state_));
+    }
+
 }
