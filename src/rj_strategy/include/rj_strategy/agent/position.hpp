17e205b8a89cbd37b542f87a849930782faf44f1
Sanat Dhanyamraju <53443682+sanatd33@users.noreply.github.com>
2026-02-04 00:43:57 -0500

    Waller Coordinator (#2467)
    * initial waller coordinator
    
    * temp waller
    
    * working waller coord
    
    * Fix Code Style On waller-coordinator (#2468)
    
    automated style fixes
    
    Co-authored-by: sanatd33 <sanatd33@users.noreply.github.com>
    
    * fix comments, break up big function
    
    * Fix Code Style On waller-coordinator (#2471)
    
    automated style fixes
    
    Co-authored-by: sanatd33 <sanatd33@users.noreply.github.com>
    
    * fixed pr comments
    
    * Fix Code Style On waller-coordinator (#2486)
    
    automated style fixes
    
    Co-authored-by: sanatd33 <sanatd33@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: sanatd33 <sanatd33@users.noreply.github.com>

--- src/rj_strategy/include/rj_strategy/agent/position.hpp
+++ src/rj_strategy/include/rj_strategy/agent/position.hpp
@@ -42,6 +42,7 @@
 
 // Coordinators
 #include "rj_strategy/coordinator/kicker_picker_client.hpp"
+#include "rj_strategy/coordinator/waller_client.hpp"
 
 // tell compiler this class exists, but no need to import the whole header
 class AgentActionClient;
@@ -51,6 +52,7 @@ namespace strategy {
 // Client Handles for coordinators
 struct ClientHandles {
     std::unique_ptr<KickerPickerClient> kicker_picker;
+    std::unique_ptr<WallerClient> waller;
 };
 
 /*
