fc37e95fa324aaf2122e0a5f7b534dacbddd3ff0
petergarud <petergarud@users.noreply.github.com>
2026-02-16 00:38:19 +0000

    automated style fixes

--- src/rj_strategy/src/agent/position/defense.cpp
+++ src/rj_strategy/src/agent/position/defense.cpp
@@ -4,9 +4,7 @@ namespace strategy {
 
 Defense::Defense(int r_id) : Position(r_id, "Defense") {}
 
-Defense::Defense(Position&& other) : Position{std::move(other)} {
-    position_name_ = "Defense";
-}
+Defense::Defense(Position&& other) : Position{std::move(other)} { position_name_ = "Defense"; }
 
 std::optional<RobotIntent> Defense::derived_get_task(RobotIntent intent) {
     current_state_ = update_state();
