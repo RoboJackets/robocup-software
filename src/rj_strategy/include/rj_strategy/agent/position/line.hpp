8ae535e3a6e4efff4a7d4e690999783d0df4c5a7
Yuvraj Dhadwal <146681831+yuvrajdhadwal@users.noreply.github.com>
2026-03-08 23:20:55 -0400

    Upgrade line test functionality (#2509)
    * now can take in points for line test
    
    * got communication from callback to line derived get task
    
    * confirmed comms and added comments explaining how to use
    
    * added motion, defaults, and cleared logs
    
    * added comments explaining code
    
    * Fix Code Style On upgrade_line_test_functionality (#2510)
    
    automated style fixes
    
    Co-authored-by: yuvrajdhadwal <yuvrajdhadwal@users.noreply.github.com>
    
    * deleted deprecated code
    
    * Fix Code Style On upgrade_line_test_functionality (#2514)
    
    automated style fixes
    
    Co-authored-by: yuvrajdhadwal <yuvrajdhadwal@users.noreply.github.com>
    
    * added bounding checks for points
    
    * Fix Code Style On upgrade_line_test_functionality (#2518)
    
    automated style fixes
    
    Co-authored-by: yuvrajdhadwal <yuvrajdhadwal@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: yuvrajdhadwal <yuvrajdhadwal@users.noreply.github.com>

--- src/rj_strategy/include/rj_strategy/agent/position/line.hpp
+++ src/rj_strategy/include/rj_strategy/agent/position/line.hpp
@@ -11,6 +11,7 @@ public:
     Line(Position&& other);
     Line(int r_id);
     Line(int r_id, bool forward);
+    Line(int r_id, rj_geometry::Point start, rj_geometry::Point end, uint8_t target_rid);
     ~Line() override = default;
     Line(const Line& other) = default;
     Line(Line&& other) = default;
@@ -22,5 +23,9 @@ private:
     bool forward_ = true;
     bool vertical_ = false;
     bool face_target_ = false;
+
+    rj_geometry::Point start_;
+    rj_geometry::Point end_;
+    uint8_t target_rid_;
 };
 }  // namespace strategy
