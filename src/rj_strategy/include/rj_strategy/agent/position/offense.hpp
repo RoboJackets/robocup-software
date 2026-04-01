1e13e89a2ca0db9c027e69cef6feb8f38abd8371
Aalind Tyagi <atyagi80@gatech.edu>
2026-03-31 21:02:21 -0400

    rebase

--- src/rj_strategy/include/rj_strategy/agent/position/offense.hpp
+++ src/rj_strategy/include/rj_strategy/agent/position/offense.hpp
@@ -260,8 +260,6 @@ private:
 
     void broadcast_seeker_request(rj_geometry::Point seeking_point, bool adding);
 
-    int Offense::get_kick_speed(double distance_to_other_robot);
-
     std::unordered_map<int, rj_geometry::Point> seeker_points_;
 };
 
