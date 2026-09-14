3ca67e039f7608f778cedd50890d42b7338d62af
r.koulen.p@gmail.com <rkoulen3@gatech.edu>
2026-09-13 20:54:36 -0400

    moving these base geometry values over to rj_geometry

---
+++ src/rj_geometry/include/rj_geometry/ball.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+// Geometry derived from RoboCup Rules
+// https://robocup-ssl.github.io/ssl-rules/sslrules.pdf
+
+inline constexpr float kBallDiameter = 0.043f; // m
+inline constexpr float kBallRadius = 0.0215f; // m
+inline constexpr float kBallMass = 0.046f; // kg
